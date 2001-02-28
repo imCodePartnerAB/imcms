596ffe7ed0959493d6b91861d23a9eb38bffc028
src <src@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-02-28 08:08:35 +0000

    Updated the confernce and diagram servlets. Recompiled.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@348 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- servlets/prj.el
+++ servlets/prj.el
@@ -1,6 +1,10 @@
 
 
 
+
+
+
+
 (jde-set-project-name "/home/kreiger/imCMS/servlets/imcms-servlets")
 (jde-set-variables 
  '(jde-gen-session-bean-template (quote ("(jde-import-insert-imports-into-buffer (list \"javax.ejb.*\"
@@ -119,7 +123,7 @@ RemoteException {\"" "'>'n \"}\"'>'n '>'n" "'> \"public void unsetSessionContext
  '(jde-javadoc-describe-field-template "\"* Describe \" (jde-javadoc-field-type modifiers)
  \" \" (jde-javadoc-code name) \" here.\"")
  '(jde-javadoc-gen-link-URL nil)
- '(jde-compile-option-classpath (quote ("/usr/local/java/JSDK2.0/lib/jsdk.jar" "/home/kreiger/imCMS/1.3/server/")))
+ '(jde-compile-option-classpath (quote ("/usr/local/java/JSDK2.0/lib/jsdk.jar" "/home/kreiger/imCMS/1.3/server/classes/" ".")))
  '(jde-bug-jdk-directory "e:/jdk1.3/")
  '(jde-gen-boilerplate-function (quote jde-gen-create-buffer-boilerplate))
  '(jde-gen-entity-bean-template (quote ("(jde-import-insert-imports-into-buffer (list \"javax.ejb.*\"
