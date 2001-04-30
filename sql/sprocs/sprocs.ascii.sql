e0784c028424063eb4603a84d97b5679f6f67f72
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-04-30 15:10:28 +0000

    Changed GetUserPermissionSet, which didn't work well when there were no permissionsets defined for the page.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@490 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/sprocs/sprocs.ascii.sql
+++ sql/sprocs/sprocs.ascii.sql
@@ -1865,6 +1865,7 @@ CREATE PROCEDURE GetUserPermissionSet @meta_id INT, @user_id INT AS
  Column 1: The users most privileged set_id
  Column 2: The users permission-set for this set_id
  Column 3: The permissions for this document. ( At the time of this writing, the only permission there is is wether or not set_id 1 is more privileged than set_id 2, and it's stored in bit 0 )
+
  set_id's:
  0 - most privileged (full rights)
  1 & 2 - misc. They may be equal, and 1 may have permission to modify 2.
@@ -1875,18 +1876,18 @@ SELECT TOP 1 ISNULL((MIN(ISNULL(rr.set_id,4))*CAST(MIN(ISNULL(urc.role_id,1)) AS
   ISNULL(dps.permission_id,0),
   ISNULL(m.permissions,0)
 FROM   roles_rights rr
-RIGHT JOIN  user_roles_crossref urc
+FULL JOIN  user_roles_crossref urc
       ON urc.user_id = @user_id
       AND rr.meta_id = @meta_id
       AND (
         rr.role_id = urc.role_id
        OR urc.role_id < 1
        )      
-JOIN  meta m
+RIGHT JOIN  meta m
       ON m.meta_id = @meta_id
       AND urc.user_id = @user_id
       AND (
-        rr.meta_id = @meta_id
+        rr.meta_id IS NOT NULL
        OR urc.role_id = 0
        )
 LEFT JOIN doc_permission_sets dps
@@ -1894,6 +1895,7 @@ LEFT JOIN doc_permission_sets dps
       AND rr.set_id = dps.set_id
 GROUP BY ISNULL(dps.permission_id,0),m.permissions
 ORDER BY ISNULL((MIN(ISNULL(rr.set_id,4))*CAST(MIN(ISNULL(urc.role_id,1)) AS BIT)),4)
+
 
 GO
 SET QUOTED_IDENTIFIER  OFF    SET ANSI_NULLS  ON 
