8d8d5a80971ff019764786720a53e43f052988ab
rickard <rickard@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-05-03 09:02:08 +0000

    Uppdatering sedan omskrivning av externaldiv/diverse
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@495 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- servlets/Administrator.java
+++ servlets/Administrator.java
@@ -21,7 +21,7 @@ import imcode.util.* ;
  * Html template in use:
  * AdminListDocs.html
  * Error.html
- * 
+ *
  *
  * stored procedures in use:
  * - GetLangPrefixFromId
@@ -41,7 +41,7 @@ public class Administrator extends HttpServlet {
 	    	Object oKeys = (enumKeys.nextElement()) ;
 			Object oValue = (enumValues.nextElement()) ;
 			String theVal = oValue.toString() ;
-			if(theVal.equals("")) 
+			if(theVal.equals(""))
 				return false ;
 			}
 		    return true ;
@@ -79,24 +79,24 @@ public class Administrator extends HttpServlet {
 	    // Does the session indicate this user already logged in?
 	    Object done = session.getValue("logon.isDone");  // marker object
 	    imcode.server.User user = (imcode.server.User) done ;
-	   
+
 	    if (done == null) {
 	      	// No logon.isDone means he hasn't logged in.
-	      
+
 	      	// Lets get the login page
 	      	String host 				= req.getHeader("Host") ;
 	      	// String imcserver 			= Utility.getDomainPref("adminserver", host) ;
 	      	String start_url        	= Utility.getDomainPref( "start_url",host ) ;
-	      
+
 	      	// Save the request URL as the true target and redirect to the login page.
 	      	session.putValue("login.target", HttpUtils.getRequestURL(req).toString());
 	      	String serverName = MetaInfo.getServerName(req) ;
 	      	String startUrl = Utility.getDomainPref( "start_url",host ) ;
-	     
+
 	      	//String startUrl = RmiCon f.getLoginUrl() ;
 	      	res.sendRedirect(serverName + startUrl) ;
 	    	return false;
-		} 	
+		}
 	    return true ;
 	}
 
@@ -105,66 +105,66 @@ public class Administrator extends HttpServlet {
 		is allowed to create new users
 		False if the user isn't an administrator.
 		1 = administrator
-		0 = superadministrator	
+		0 = superadministrator
 	*/
-	 	
+
 	public static boolean checkAdminRights(String server, imcode.server.User user) {
-	 			
-		try {	
-	 			
-			// Lets verify that the user who tries to add a new user is an SUPERADMIN  	
+
+		try {
+
+			// Lets verify that the user who tries to add a new user is an SUPERADMIN
 			RmiLayer imc = new RmiLayer(user) ;
 			int currUser_id = user.getInt("user_id") ;
 			String checkAdminSql = "CheckAdminRights " + currUser_id ;
 			String[] roles = imc.execSqlProcedure(server, checkAdminSql) ;
 
-			for(int i = 0 ; i< roles.length; i++ ){	
+			for(int i = 0 ; i< roles.length; i++ ){
 				String aRole = roles[i] ;
 			  	if(aRole.equalsIgnoreCase("0") )
 			  		return true ;
 		  	}
-			return false ;		 		
-		} catch (ServletException e) {
+			return false ;
+		} catch (IOException e) {
 	 			//log("An error occured in CheckAdminRights") ;
 	 			//log(e.getMessage() ) ;
 	 	}
 	 	return false ;
-	} // checkAdminRights 
+	} // checkAdminRights
 
 
 	/**
 		CheckAdminRights, returns true if the user is an admin.
-		False if the user isn't an administrator	
+		False if the user isn't an administrator
 	*/
 
 	protected static boolean checkAdminRights(HttpServletRequest req, HttpServletResponse res)
 		throws ServletException, IOException {
-	  
+
 		String host 				= req.getHeader("Host") ;
-		String server 			= Utility.getDomainPref("adminserver",host) ;   
+		String server 			= Utility.getDomainPref("adminserver",host) ;
 		imcode.server.User user = getUserObj(req,res) ;
 		if(user == null) {
 			//this.log("CheckadminRights: an error occured, getUserObj") ;
-			return false ;	
+			return false ;
 		} else
 			return checkAdminRights(server, user) ;
 	}
- 
- 
+
+
 	/**
 		GetAdminTemplateFolder. Takes the userobject as argument to detect the language
-	  from the user and and returns the base path to the internal folder, hangs on the 
+	  from the user and and returns the base path to the internal folder, hangs on the
 	  language prefix and an "/admin/" string afterwards...
-	  
+
 	  Example : D:\apache\htdocs\templates\se\admin\
 	*/
 	public String getAdminTemplateFolder (String server, imcode.server.User user) throws ServletException, IOException {
-		
+
 		RmiLayer rmi = new RmiLayer(user) ;
-			
-		// Since our templates are located into the admin folder, we'll have to hang on admin		
+
+		// Since our templates are located into the admin folder, we'll have to hang on admin
 	    String templateLib = rmi.getInternalTemplateFolder(server) ;
-			  
+
 		// Lets get the users language id. Use the langid to get the lang prefix from db.
 	    String langId = user.getString("lang_id") ;
 		String langPrefix = rmi.execSqlProcedureStr(server, "GetLangPrefixFromId " + langId) ;
@@ -183,21 +183,21 @@ public class Administrator extends HttpServlet {
 
 	public String createHtml (HttpServletRequest req, HttpServletResponse res,
 		VariableManager vm, String htmlFile) throws ServletException, IOException {
-	
+
 	 	// Lets get the path to the admin templates folder
 		String host 				= req.getHeader("Host") ;
-		String server 			= Utility.getDomainPref("adminserver",host) ;  
+		String server 			= Utility.getDomainPref("adminserver",host) ;
 		imcode.server.User user = getUserObj(req,res) ;
 		String templateLib = this.getAdminTemplateFolder(server, user) ;
-    
+
     /*
     RmiLayer rmi = new RmiLayer(user) ;
-		
+
 		// Since our templates are located into the admin folder, we'll have to hang on admin
 		// String templateLib = MetaInfo.getInternalTemplateFolder() ;
-		
+
     String templateLib = rmi.getInternalTemplateFolder(server) ;
-		  
+
 		// Lets get the users language id. Use the langid to get the lang prefix from db.
 		String langId = user.getString("lang_id") ;
 		String langPrefix = rmi.execSQlProcedureStr(server, "GetLangPrefixFromId " + langId) ;
@@ -213,7 +213,7 @@ public class Administrator extends HttpServlet {
 		HtmlGenerator htmlObj = new HtmlGenerator(templateLib, htmlFile) ;
 		String html = htmlObj.createHtmlString(vm,req) ;
     	return html ;
-	}		
+	}
 
 	/**
 	  SendHtml. Generates the html page to the browser.
@@ -221,19 +221,19 @@ public class Administrator extends HttpServlet {
 
 	  public void sendHtml (HttpServletRequest req, HttpServletResponse res,
 	    VariableManager vm, String htmlFile) throws ServletException, IOException {
-	  
+
 	    String str = this.createHtml(req, res, vm, htmlFile) ;
 	    HtmlGenerator htmlObj = new HtmlGenerator() ;
 	    htmlObj.sendToBrowser(req,res,str) ;
-	  }    
-	
+	  }
+
 	/**
 		Log function. Logs the message to the log file and console
 	*/
 
 		public void log(String msg) {
 			super.log(msg) ;
-			System.out.println("Administrator: " + msg) ;	
+			System.out.println("Administrator: " + msg) ;
 		}
 
 	/**
@@ -243,13 +243,13 @@ public class Administrator extends HttpServlet {
 
 	public static String getDateToday() {
 		java.util.Calendar cal = java.util.Calendar.getInstance() ;
-			      	
+
 		String year  = Integer.toString(cal.get(Calendar.YEAR)) ;
-		int month = Integer.parseInt(Integer.toString(cal.get(Calendar.MONTH))) + 1; 
+		int month = Integer.parseInt(Integer.toString(cal.get(Calendar.MONTH))) + 1;
 		int day   = Integer.parseInt(Integer.toString(cal.get(Calendar.DAY_OF_MONTH))) ;
 		int hour  = Integer.parseInt(Integer.toString(cal.get(Calendar.HOUR_OF_DAY))) ;
 		int min   = Integer.parseInt(Integer.toString(cal.get(Calendar.MINUTE))) ;
-			
+
 		String dateToDay  = year ;
 		dateToDay += "-" ;
 		dateToDay += month < 10 ? "0" + Integer.toString(month) : Integer.toString(month) ;
@@ -264,11 +264,11 @@ public class Administrator extends HttpServlet {
 	*/
 	public static String getTimeNow() {
 		java.util.Calendar cal = java.util.Calendar.getInstance() ;
-		      	
+
 		int hour  = Integer.parseInt(Integer.toString(cal.get(Calendar.HOUR_OF_DAY))) ;
 	  int min   = Integer.parseInt(Integer.toString(cal.get(Calendar.MINUTE))) ;
 		int sec   = Integer.parseInt(Integer.toString(cal.get(Calendar.SECOND))) ;
-		
+
 	  String timeNow  = "" ;
 		 timeNow += hour < 10 ? "0" + Integer.toString(hour) : Integer.toString(hour) ;
 		 timeNow += ":" ;
@@ -298,51 +298,51 @@ public class Administrator extends HttpServlet {
 		 	String[] theRow = multi[row] ;
 	   		for(int i = 0 ; i < theRow.length ; i++ ) {
 	   			v.add(theRow[i]) ;
-	   		}  
+	   		}
 	 		return v ;
-     	} catch(Exception e) { 
+     	} catch(Exception e) {
      		return v ;
     	}
 	} // getOneRow
-	
+
 	/**
 	  Returns the nbr of rows in the multiarray
 	**/
 	public int getNbrOfRows( String[][] multi ) {
     	try {
     		return multi.length ;
-    	} catch(Exception e) { 
+    	} catch(Exception e) {
      		return 0 ;
-     	}   
+     	}
   	} // getNbrOfRows
-  
+
 	/**
 	 * send error message
-	 * 
+	 *
 	 * @param server
 	 * @param eMailServerMaster
 	 * @param languagePrefix
 	 * @param errorHeader
 	 * @param errorCode is the code to loock upp in ErrMsg.ini file
 	*/
-	protected void sendErrorMessage( String imcserver, String eMailServerMaster, 
-	                               String languagePrefix, String errorHeader, 
+	protected void sendErrorMessage( String imcserver, String eMailServerMaster,
+	                               String languagePrefix, String errorHeader,
 	                               int errorCode, HttpServletResponse response ) throws IOException {
-		
-		ErrorMessageGenerator errroMessage = new ErrorMessageGenerator( imcserver, eMailServerMaster, 
-				languagePrefix,	errorHeader, this.TEMPLATE_ERROR, errorCode );	  
-	
+
+		ErrorMessageGenerator errroMessage = new ErrorMessageGenerator( imcserver, eMailServerMaster,
+				languagePrefix,	errorHeader, this.TEMPLATE_ERROR, errorCode );
+
 		errroMessage.sendHtml( response );
 	}
-	
+
 	/**
 	 * get users language
-	 * 
+	 *
 	 * @param server
 	 * @param langId from userObject
 	*/
 	protected String getLanguagePrefix( String server, int langId ) throws IOException {
-	
+
 		String sqlQ = "GetLangPrefixFromId " + String.valueOf( langId );
 		String languagePrefix = IMCServiceRMI.sqlProcedureStr( server, sqlQ );
 		return languagePrefix;
