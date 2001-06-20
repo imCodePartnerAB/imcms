f9101990ae5188d82fc15b527dd3cb5fe4c58141
rickard <rickard@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-06-20 09:05:29 +0000

    Added sproc SortOrder_GetExistingDoc
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@553 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/sprocs/sprocs.ascii.sql
+++ sql/sprocs/sprocs.ascii.sql
@@ -2,6 +2,10 @@ if exists (select * from sysobjects where id = object_id(N'[dbo].[AddBrowserStat
 drop procedure [dbo].[AddBrowserStatistics]
 GO
 
+if exists (select * from dbo.sysobjects where id = object_id(N'[dbo].[SortOrder_GetExistingDocs ]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
+drop procedure [dbo].[SortOrder_GetExistingDocs ]
+GO
+
 if exists (select * from sysobjects where id = object_id(N'[dbo].[AddNewuser]') and OBJECTPROPERTY(id, N'IsProcedure') = 1)
 drop procedure [dbo].[AddNewuser]
 GO
@@ -3590,3 +3594,31 @@ GO
 SET QUOTED_IDENTIFIER  OFF    SET ANSI_NULLS  ON 
 GO
 
+SET QUOTED_IDENTIFIER OFF 
+GO
+SET ANSI_NULLS OFF 
+GO
+
+CREATE PROCEDURE SortOrder_GetExistingDocs 
+
+	@langPrefixString char(3)
+AS
+/*
+This sproc is used by the GetExistingDoc servlet, it takes the lang id string as argument and returns 
+the sortorder options  display text for that language. 
+Example: SortOrder_GetExistingDocs 'se'.
+
+*/
+
+SELECT sType.sort_by_type , display.display_name
+FROM lang_prefixes lang
+INNER JOIN display_name display
+	ON display.lang_id = lang.lang_id
+	AND lang.lang_prefix = @langPrefixString
+INNER JOIN sort_by sType
+	ON sType.sort_by_id = display.sort_by_id
+GO
+SET QUOTED_IDENTIFIER OFF 
+GO
+SET ANSI_NULLS ON 
+GO
