9a0c049abc60794ec61428fcdc6a3c78ed068a25
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-07-31 08:58:03 +0000

    Added error-pages, and fixed some bugs with permissions, and removing an include.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@657 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- servlets/SaveInclude.java
+++ servlets/SaveInclude.java
@@ -8,6 +8,9 @@ import imcode.util.Check ;
 import imcode.util.Utility ;
 import imcode.util.IMCServiceRMI ;
 
+import java.io.IOException ;
+import java.util.Vector ;
+
 public class SaveInclude extends HttpServlet {
 
     public void init (ServletConfig config) throws ServletException {
@@ -33,32 +36,57 @@ public class SaveInclude extends HttpServlet {
 	    return ;
 	}
 
-	// Check if user has write rights
+	// Check if user has permission to edit includes for this document
 	if ( !IMCServiceRMI.checkDocAdminRights(imcserver,meta_id,user,imcode.server.IMCConstants.PERM_DT_TEXT_EDIT_INCLUDES ) ) {	// Checking to see if user may edit this
-	    byte[] tempbytes ;
-	    tempbytes = AdminDoc.adminDoc(meta_id,meta_id,host,user,req,res) ;
-	    if ( tempbytes != null ) {
-		out.write(tempbytes) ;
-	    }
+	    sendPermissionDenied(imcserver,out,meta_id,user) ;
 	    return ;
 	}
 
-	if ( req.getParameter( "ok" ) != null ) {
-	    String included_meta_id = req.getParameter("include_meta_id") ;
-	    String include_id = req.getParameter("include_id") ;
-	    if (included_meta_id != null && include_id != null) {
-		if ("".equals(included_meta_id.trim())) {
-		    IMCServiceRMI.sqlUpdateProcedure(imcserver,"DeleteInclude "+meta_id_str+","+include_id) ; 
-		} else {
-		    IMCServiceRMI.sqlUpdateProcedure(imcserver,"SetInclude "+meta_id_str+","+include_id+","+included_meta_id) ; 
+	String included_meta_id = req.getParameter("include_meta_id") ;
+	
+	String include_id = req.getParameter("include_id") ;
+	if (included_meta_id != null && include_id != null) {
+	    if ("".equals(included_meta_id.trim())) {
+		IMCServiceRMI.sqlUpdateProcedure(imcserver,"DeleteInclude "+meta_id_str+","+include_id) ; 
+	    } else {
+		try {
+		    int included_meta_id_int = Integer.parseInt(included_meta_id) ;
+		    
+		    // Make sure the user has permission to share the included document
+		    if (IMCServiceRMI.checkUserDocSharePermission(imcserver,user,included_meta_id_int)) {
+			IMCServiceRMI.sqlUpdateProcedure(imcserver,"SetInclude "+meta_id_str+","+include_id+","+included_meta_id) ; 
+		    } else {
+			sendPermissionDenied(imcserver,out,meta_id,user) ;
+			return ;
+		    }
+		} catch (NumberFormatException ignored) {
+		    sendBadId(imcserver,out,meta_id,user) ;
+		    return ;
 		}
 	    }
-	    byte[] tempbytes = AdminDoc.adminDoc(meta_id,meta_id,host,user,req,res) ;
-	    if ( tempbytes != null ) {
-		out.write(tempbytes) ;
-	    }
-	    return ;
 	}
+	
+	byte[] tempbytes = AdminDoc.adminDoc(meta_id,meta_id,host,user,req,res) ;
+	if ( tempbytes != null ) {
+	    out.write(tempbytes) ;
+	}
+	return ;
+    }
+    
+    protected void sendPermissionDenied(String imcserver, ServletOutputStream out, int meta_id, User user) throws IOException {
+	Vector vec = new Vector(2) ;
+	vec.add("#meta_id#") ;
+	vec.add(String.valueOf(meta_id)) ;
+	String htmlStr = IMCServiceRMI.parseDoc(imcserver,vec,"include_permission_denied.html",user.getLangPrefix()) ;
+	out.print(htmlStr) ;
+    }
+
+    protected void sendBadId(String imcserver, ServletOutputStream out, int meta_id, User user) throws IOException {
+	Vector vec = new Vector(2) ;
+	vec.add("#meta_id#") ;
+	vec.add(String.valueOf(meta_id)) ;
+	String htmlStr = IMCServiceRMI.parseDoc(imcserver,vec,"include_bad_id.html",user.getLangPrefix()) ;
+	out.print(htmlStr) ;
     }
 
 }
