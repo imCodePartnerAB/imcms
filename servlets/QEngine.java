a86aa0cfed4b7d6772d18e41f99caed9fb655d7a
monika <monika@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-10-22 13:30:52 +0000

    bug 122 Added servlet that handles the result from the question in QEingine. QEngine is updated with some hidden fields.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@910 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- servlets/QEngine.java
+++ servlets/QEngine.java
@@ -22,18 +22,10 @@ public class QEngine extends HttpServlet
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 	throws ServletException, IOException 
 	{
-		String file = new String("test.txt");
-	
-		BufferedWriter fileW = new BufferedWriter( new FileWriter(file) );
 		
-	
+		String host = req.getHeader("Host") ;
+		String imcServer = Utility.getDomainPref("userserver",host) ;
 		
-		fileW.write("ja: " );
-		fileW.newLine();
-	
-		fileW.flush();
-		fileW.close();
-	/*
 		//get todays date
 		Date theDate = new Date();
 		log("B:theDate: " + theDate);
@@ -51,97 +43,82 @@ public class QEngine extends HttpServlet
 		String type = req.getParameter("type");
 		inFile = req.getParameter("file");
 		
-		BufferedReader file = new BufferedReader(new FileReader(inFile));
+		//gets the filecontent 
+		String resFile = IMCServiceRMI.getInclude(imcServer,inFile);
 		
 		//collect the correct questions/citat/pictures
-		String[] txt = new String[20];
-				
-		char[] date1 = new char[5];
-		char[] date2 = new char[5];
+		HashMap row_texts = new HashMap();
 		
-		file.read();
+		int row = 0;	
 		
-		int nr = 0;	
-		while ( file.read(date1,0,5) != -1 )	
+		PrintWriter out = res.getWriter();
+		
+		//the dates
+		int bIndex = 0;
+		int eIndex = 0;
+		
+		while ( resFile.indexOf((int)';',bIndex) != -1)
 		{
-			file.read();
-			file.read();
-			
-			file.read(date2,0,5);
-			
-			int date1i = Integer.parseInt( new String(date1));
-			int date2i = Integer.parseInt( new String(date2));
-				
-			log("date1i: " + date1i);
-			log("date2i: " + date2i );
-				
-			if ( date >= date1i && date <= date2i )
-			{
-				file.read(); 
-				txt[nr] = file.readLine();
-				nr++;
-			}
-			else
-			{
-				file.readLine();
-			}
-			
-			file.read();
-		}	
+			eIndex = resFile.indexOf((int)';',bIndex);
 		
+			int date1 = Integer.parseInt(resFile.substring(bIndex,eIndex));
+			bIndex = eIndex + 1;
+				
+			eIndex = resFile.indexOf((int)';',bIndex);
+			int date2 = Integer.parseInt(resFile.substring(bIndex,eIndex));
+						
+			bIndex = eIndex + 1;
+		
+			eIndex = resFile.indexOf((int)';',bIndex);
+					
+			if ( date1 <= date && date2 >= date)
+			{
+				row_texts.put(new Integer(row),resFile.substring(bIndex,eIndex));
+			}
+			
+			bIndex = eIndex + 3;
+			row++;
+		}	
+
+		Collection texts = row_texts.values();
+		int nr = texts.size();
+
 		//get one randomised item
-		int selected = 0;
-		if (nr>1)
+		Set rows = row_texts.keySet();
+
+		do
 		{
 			Random random = new Random();
-			selected = random.nextInt(nr);
+			row = random.nextInt(row+1);
 		}
+		while(!rows.contains(new Integer(row)));
 		
-		//return the selected item
-		PrintWriter out = res.getWriter();
-		*/
-		
-	/*	out.println("<html>");
-		out.println("<head>");
-		out.println("<title>testMaBra</title>");
-		out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">");
-		out.println("</head>");
-		out.println("<body bgcolor=\"#FFFFFF\" text=\"#000000\">");
-		out.println("<P>M:test av " + type + " File: " + inFile + "</P>");
-	*/	
-	/*	if( type.equals("pic"))
+		String theText = (String)row_texts.get(new Integer(row));
+
+		if( type.equals("pic"))
 		{
-			out.println("<img src=" + txt[selected] + ">");
-//FIX storlek på bilden?+ " width=\"300\" height=\"400\">");
+			out.println( "<img src=\" " + theText + "\"> ");
 		}
-		else
+		else if(type.equals("quot"))
 		{
-			out.println( txt[selected] );
+			out.println( theText );
+			
+			//raden i filen
+			out.println("<input type=\"hidden\" name=\"quotrow\" value=\"" + row + "\">");
+			out.println("<input type=\"hidden\" name=\"quot\" value=\"" + theText + "\">");
 		}
-		
-		if (type.equals("ques"))
+		else if(type.equals("ques"))
 		{
-			String path = this.getServletPath(req);
-			out.println("<form name=\"answer\" method=\"post\" action=\"" + path + "QEngine\">");
-			out.println("<input type=\"hidden\" name=\"question\" value=\"" + txt[selected] + "\">");
-			out.println("<table width=\"50%\" height=\"100\">");
-			out.println("<tr>");
-			out.println("<td height=\"30\" width=\"25%\"><input type=\"radio\" name=\"answer\" value=\"yes\" ></td>");
-			out.println("<td height=\"30\" width=\"23%\" valign=\"middle\">ja</td>");
-			out.println("<td height=\"30\" width=\"23%\"><input type=\"radio\" name=\"answer\" value=\"no\" ></td>");
-			out.println("<td height=\"30\" width=\"29%\" valign=\"middle\">nej </td>");
-			out.println("</tr>");
-			out.println("</table>");
-			out.println("<div align=\"center\"><input type=\"submit\" name=\"ok\" value=\"ok\"></div>");
-			out.println("</form>");
+			out.println( theText );
+			
+			out.println("<input type=\"hidden\" name=\"quesrow\" value=\"" + row + "\">");
+			out.println("<input type=\"hidden\" name=\"question\" value=\"" + theText + "\">");
 		}
-		
-	/*	out.println("</body>");
-		out.println("</html>");
-		
-	*/	
-	
-	
+		else 
+		{
+			out.println( theText );
+		}
+
 		return ;
 
 	} // End doGet
@@ -151,119 +128,6 @@ public class QEngine extends HttpServlet
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 	throws ServletException, IOException 
 	{
-		log("PostM");
-			
-		//get answer
-		String answer = req.getParameter("answer");
-		log("Answer: " + answer);
-		
-		//get question
-		String question = req.getParameter("question");
-		log("question: " + question );
-		
-		//gå igenom strängen tecken för tecken och byt ut allt utom '_', siffror och bokstäver till '_'.
-		for(int i = 0;i<question.length(); i++)
-		{
-			char c = question.charAt(i);
-			if (!Character.isJavaIdentifierPart(c))
-			{
-				question=question.replace(c,'_');
-			}
-		}
-		
-		
-		//hämta aktuellt värde
-		
-		int yes = 0;
-		int no = 0;
-		
-		//get current library
-		char searched = '\\';
-		int index = inFile.lastIndexOf((int)searched);
-		
-		String fileLib = inFile.substring(0,index+1);
-		
-		//FIX ta bort D:\\
-		File file = new File(fileLib + question.trim() + ".txt");
-		if (file.exists())
-		{
-			BufferedReader fileR = new BufferedReader(new FileReader(file));
-			
-			fileR.skip(3);
-			String y = fileR.readLine();
-			yes = Integer.parseInt(y.trim());
-			
-			fileR.skip(4);
-			String n = fileR.readLine();
-			no = Integer.parseInt(n.trim());
-		
-			fileR.close();
-		}
-		
-		
-		
-		//spara svaret till fil
-		BufferedWriter fileW = new BufferedWriter( new FileWriter(file) );
-		
-		if (answer.equals("yes")) {yes++;} else {no++;}
-		
-		fileW.write("ja: " + yes,0,(Integer.toString(yes)).length()+4);
-		fileW.newLine();
-		fileW.write("nej: " + no,0,Integer.toString(no).length()+5);
-		fileW.newLine();
-		fileW.flush();
-		fileW.close();
-		
-		int total = yes+no;
-		
-		//get the %
-		double yesDiv = (yes==0) ? 0 : ((double)yes/(double)total);
-		double noDiv = (no==0) ? 0 : ((double)no/(double)total);
-		
-		NumberFormat pf = NumberFormat.getPercentInstance();
-		pf.setMaximumFractionDigits(2);
-		
-		String yesProcent = pf.format(yesDiv);
-		String noProcent = pf.format(noDiv);
-		
-		//rita svarssidan
-		PrintWriter out = res.getWriter();
-		
-		out.println("ja " + yesProcent );
-		out.println("nej " + noProcent);
-		
-		
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>testMaBraSvar</title>");
-		out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">");
-		out.println("</head>");
-		out.println("<body bgcolor=\"#FFFFFF\" text=\"#000000\">");
-
-		out.println("<table width=\"400\" border=\"0\" height=\"20\" bordercolor=\"#CCCCCC\">");
-  		out.println("<tr> ");
-		out.println("<td bgcolor=\"#FF0000\" height=\"20\" width=\"" + yesProcent + "\"> </td>");
-		out.println("<td bgcolor=\"#0000FF\" height=\"20\" width=\"" + noProcent + "\"> </td>");
-		out.println("</tr>");
-		out.println("</table>");
-
-		out.println("<P> Filelib: " + fileLib + " Andel jasvar: " + yesProcent + " Andel nejsvar: " + noProcent + "</P>");
-	
-		out.println("</body>");
-		out.println("</html>");
-		
-	/*	
-		VariableManager vm = new VariableManager() ;
-		Html htm = new  Html();
-		vm.addProperty("yesProcent", yesProcent) ;
-		vm.addProperty("noProcent", noProcent);
-	//	vm.addProperty("yes", Integer.toString(yes )) ;
-	//	vm.addProperty("no",Integer.toString(no));
-		
-	//	vm.addProperty("SERVLET_URL",this.getServletPath(req));
-		sendHtml(req,res,vm,"resultatVeckansFraga.htm"); */
-		
-		
 		return ;
 	}	
 
@@ -277,43 +141,6 @@ public class QEngine extends HttpServlet
 		System.out.println("VeckansFraga: " + str ) ;
 	}
 
-	public void sendHtml (HttpServletRequest req, HttpServletResponse res,
-		VariableManager vm, String htmlFile) throws ServletException, IOException
-	{
-
-//FIX template dynamiskt?
-		String templateLib = "C:\\Inetpub\\wwwroot\\maBra\\";
-		
-		log("templateLib: " + templateLib);
-		log("htmlFile: " + htmlFile);
-		
-		HtmlGenerator htmlObj = new HtmlGenerator(templateLib, htmlFile) ;
-		String html = htmlObj.createHtmlString(vm,req) ;
-		
-		htmlObj.sendToBrowser(req,res,html) ;
-	
-	}
-
-
-	public static String getServletPath(HttpServletRequest req)
-    throws ServletException, IOException 
-	{
-        String protocol = req.getScheme();
-        String serverName = req.getServerName();
-        int p = req.getServerPort();
-        String port = (p == 80) ? "" : ":" + p;
-        String servletPath = req.getServletPath() ;
-
-
-        int lastSlash = servletPath.lastIndexOf("/") ;
-        if( lastSlash != -1 ) 
-		{
-            servletPath =  servletPath.substring(0,lastSlash +1) ;
-            String url = protocol + "://" + serverName + port + servletPath ;
-            return url ;
-        }
-        return "" ;
-    }
 
 } // End class
 
