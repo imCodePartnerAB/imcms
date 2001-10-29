c431a390e7c78e3c5f132cac8cf11bd348b6a325
peter <peter@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-10-29 17:30:30 +0000

    bug 232: Changed Search-word delimeter from ";" to ",".
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@937 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/diff/diff.sql
+++ sql/diff/diff.sql
@@ -472,3 +472,53 @@ WHERE simple_name = @aTemplatename
 GO
 
 -- 2001-10-08
+
+
+ALTER PROCEDURE Classification_Fix
+--peter har gjort om denna så att den numera inte använder semicolon som separator utan endast komma
+ @meta_id int ,
+ @string varchar(2000)
+AS
+declare @value varchar(50)
+declare @pos int
+-- Lets delete all current crossreferences, if any
+DELETE 
+FROM meta_classification 
+WHERE meta_id = @meta_id
+--SELECT @string = 'ett;två;tre;fyra;fem'
+-- Lets search for semicolon, if not found then look for a , This is relevant 
+-- when we convert the db. After convertion, only look for semicolons
+--SELECT @pos = PATINDEX('%;%', @string)
+--IF( @pos = 0 ) BEGIN
+ SELECT @pos = PATINDEX('%,%', @string)
+--END
+WHILE @pos > 0
+BEGIN
+ SELECT @value = LEFT(@string,@pos-1)
+ SELECT @pos = LEN(@string) - @pos
+ SELECT @string = RIGHT(@string,@pos)
+ SELECT  @value  = lTrim(rTrim( ( @value ) )) 
+ EXEC ClassificationAdd @meta_id , @value
+ --INSERT INTO data (value) VALUES (@value)
+ SELECT @pos = PATINDEX('%,%', @string)
+ -- PRINT @value
+END
+-- Lets get the last part of the string
+--PRINT @string
+SELECT @value = @string
+SELECT  @value  = lTrim(rTrim( ( @value ) )) 
+EXEC ClassificationAdd @meta_id , @value
+-- INSERT INTO data (value) VALUES (@string)
+GO
+SET QUOTED_IDENTIFIER OFF 
+GO
+SET ANSI_NULLS ON 
+GO
+
+SET QUOTED_IDENTIFIER OFF 
+GO
+SET ANSI_NULLS ON 
+GO
+
+
+-- 2001-10-29
