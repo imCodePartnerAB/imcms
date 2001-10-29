9970e4b8cd342f4c6a3b311b447bd2e6a8c2576b
peter <peter@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-10-29 07:42:18 +0000

    Bug 227: Created a mailservlet to handle mail form- and to the imcms webmaster.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@933 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ servlets/SendMailServlet.java
@@ -0,0 +1,225 @@
+
+/*
+ *
+ * @(#)SendMailServlet.java
+ */
+
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Vector;
+
+import javax.servlet.*;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletOutputStream;
+import javax.servlet.http.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+
+import imcode.util.IMCServiceRMI;
+import imcode.server.*;
+import imcode.util.Utility;
+import java.net.* ;
+import java.util.*;
+/**
+  * PARAMS in use, in doPost()
+  * 	mailTo (the email adress were to send this ex. nisse@swipnet.se)
+  *		mailFrom (the senders email adress, the replyTo adress)
+  *		mailSubject (the subject line) it also decides if its a from or at to mail
+  *		-if there is a subject the mail sends to webbmaster atherwise it sends from the webbmaster 
+  *		mailTemplate (the template used to parse the mailbody)
+  *		mailText0 (textField that parses in the mailbody template)
+  *		mailText1,mailText2,mailText3,mailText4,mailText5,mailText6,mailText7,mailText8,mailText9
+  *		mailDone (the place to sendRedirect when done if null we return)
+  *		mailError (the page to redirect to when an error occures)
+  
+  * Data in webserver config:
+  * smtp_server=smtp.intron.net
+  * smtp_port=25
+  * smtp_timeout=10000
+*/
+
+public class SendMailServlet extends HttpServlet {
+	private final static String CVS_REV = "" ;
+	private final static String CVS_DATE = "" ;
+	
+	private final static String SUBJECT_TEMPLATE = "mail_subject.html";
+	private final static String BODY_TEMPLATE = "mail_body.html";
+	private final static String BODY_TEMPLATE_TO_ADMIN = "mail_body_to_admin.html";
+
+	public void init(ServletConfig config) throws ServletException{
+
+		super.init(config);
+	}
+
+	/**
+	* Showing input document whit out error
+	*/
+
+	public void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException{
+		//loads mailform page UNDER CONSRTRUCTION
+		System.out.println("start doGet");
+		return;
+	}
+
+	/**
+	*   process submit
+	*/
+	public void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException{
+		System.out.println("start doPost");
+		java.util.Enumeration enum = req.getParameterNames();
+		while (enum.hasMoreElements()){
+			log((String)enum.nextElement());
+		}
+		/* server info */
+		String host = req.getHeader("Host") ;
+		String imcserver = Utility.getDomainPref( "adminserver", host );
+		String lang_prefix = IMCServiceRMI.getLanguage(imcserver) ;
+		SystemData sysData 	= imcode.util.IMCServiceRMI.getSystemData (imcserver);
+	
+		// Lets get the parameters we need
+		String webmaster = sysData.getWebMasterAddress();		
+		String mailTo = req.getParameter("mailTo") ;	
+		String mailFrom = req.getParameter("mailFrom") ;
+		String mailSubject = req.getParameter("mailSubject") ;
+		//lets get the meta_id where to send when were done
+		String mailDone = req.getParameter("mailDone") ;
+		//lets get the meta_id where to send if some thing goes wrong
+		String mailError = req.getParameter("mailError") ;	 
+		
+		//ok lets get all mailText stuff
+		String[] mailTextArr = new String[10];
+		for(int i=0; i<10;i++){
+			mailTextArr[i] = req.getParameter("mailText"+i) == null ? "":req.getParameter("mailText"+i);
+		}
+		
+		//lets fix the complete URL
+		StringBuffer url = HttpUtils.getRequestURL(req);
+		StringTokenizer token = new StringTokenizer(url.toString(),"/",true);
+		StringBuffer mailUrl = new StringBuffer();
+		while (token.hasMoreElements()){
+			String temp = token.nextToken();
+			mailUrl.append(temp);
+			if(temp.equals("servlet"))
+				break;			
+		}
+		mailUrl.append("/GetDoc?meta_id="+mailTextArr[0]);
+				
+		Vector mailTextV = new Vector();
+		
+		mailTextV.add("#mailSubject#"); mailTextV.add(mailSubject);
+		mailTextV.add("#mailLink#"); mailTextV.add(mailUrl.toString());
+		for(int i=0; i<10;i++){
+			 //lets setUp the tags and text to parse in the mail body
+			mailTextV.add("#mailText"+i+"#");
+			mailTextV.add( mailTextArr[i] );
+		}
+		Enumeration enum2 = mailTextV.elements();
+		while (enum2.hasMoreElements()){
+			System.out.println(((String)enum2.nextElement()));
+		}
+		
+		 String mailBody;
+		//ok lets see if we got a subject or not
+		if (mailSubject != null){
+			//its a mail to the system
+			mailTo = webmaster;	
+			mailTextV.add("#mailTo#"); mailTextV.add(mailTo);
+			mailTextV.add("#mailFrom#"); mailTextV.add(mailFrom);
+			mailBody = IMCServiceRMI.parseDoc(imcserver, mailTextV, BODY_TEMPLATE_TO_ADMIN, lang_prefix) ;	
+		}else{
+			//its a mail from the system
+			mailFrom = webmaster;
+			mailTextV.add("#mailTo#"); mailTextV.add(mailTo);
+			mailTextV.add("#mailFrom#"); mailTextV.add(mailFrom);
+			//it also means that we must parse the subject line thrue SUBJECT_TEMPLATE so lets rock
+			mailSubject = IMCServiceRMI.parseDoc(imcserver, mailTextV, SUBJECT_TEMPLATE, lang_prefix) ;
+			//ok lets parse the mailbody into a string
+        	mailBody = IMCServiceRMI.parseDoc(imcserver, mailTextV, BODY_TEMPLATE, lang_prefix) ;	
+		}
+		
+		//a simple check that @ symbol exists 		
+		if( !this.validateEmail(mailFrom) || !this.validateEmail(mailTo) ){
+			if (mailError != null){
+				res.sendRedirect("GetDoc?meta_id="+mailError);
+			}else{
+				res.sendRedirect("/imcms/servlet/StartDoc");
+			}
+			return ;
+		}
+				
+
+		/* mailserver info */
+		String mailserver = Utility.getDomainPref( "smtp_server", host );
+		String stringMailPort = Utility.getDomainPref( "smtp_port", host );
+		String stringMailtimeout = Utility.getDomainPref( "smtp_timeout", host );
+		
+		System.out.println(mailSubject)	;
+		// Handling of default-values is another area where java can't hold a candle to perl.
+		int mailport = 25 ;
+		try	{
+			mailport = Integer.parseInt( stringMailPort );
+		}catch (NumberFormatException ignored){
+			// Do nothing, let mailport stay at default.
+		}
+
+		int mailtimeout = 10000 ;
+		try	{
+			mailtimeout = Integer.parseInt( stringMailtimeout );
+		}catch (NumberFormatException ignored){
+			// Do nothing, let mailtimeout stay at default.
+		}
+		// send mail
+		try {
+			SMTP smtp = new SMTP( mailserver, mailport, mailtimeout ) ;
+			smtp.sendMailWait( mailFrom, mailTo, mailSubject , mailBody );
+
+		}catch (ProtocolException ex ){
+			log ("Protocol error while sending mail. " + ex.getMessage()) ;
+			//FIX load some error page
+			if (mailError != null){
+				res.sendRedirect("GetDoc?meta_id="+mailError);
+			}else{
+				res.sendRedirect("/imcms/servlet/StartDoc");
+			}
+			return ;
+		}catch (IOException ex ){
+			log ("The mailservlet probably timed out. " + ex.getMessage()) ;
+			//FIX load some error page
+			if (mailError != null){
+				res.sendRedirect("GetDoc?meta_id="+mailError);
+			}else{
+				res.sendRedirect("/imcms/servlet/StartDoc");
+			}
+			return ;
+		}
+		//här sk vi nog ha en koll om vi ska redirecta eller bara returnera
+		//men det får vi kolla senare
+		if(mailDone != null)
+			res.sendRedirect("GetDoc?meta_id="+mailDone);
+ 		return;
+	}
+
+
+	/**
+	Log function. Logs the message to the log file and console
+	*/
+	public void log(String msg){
+		if(msg == null)msg="";
+		super.log(msg) ;
+		//System.out.println("SendMailServlet: " + msg) ;
+	}
+
+	/* checks if email address is valid (abc@x)*/
+	private boolean validateEmail( String eMail ){
+		int stringLength = eMail.length();
+		int indexAt = eMail.indexOf( "@" );
+
+		if( indexAt > 0 && indexAt < (stringLength - 1) ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
