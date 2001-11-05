53ca604a1cfbbc955a2411670d1185756a05234f
peter <peter@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-11-05 09:24:52 +0000

    Bug 240: Added a new class that handles the GetDoc call when the quoteline should be implemented in the textdocument.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@964 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ servlets/QuoteLineCollector.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+import javax.servlet.http.*;
+
+import imcode.external.*;
+import imcode.server.*;
+import imcode.util.*;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class QuoteLineCollector implements imcode.external.GetDocControllerInterface{
+
+
+	public QuoteLineCollector(){}
+	
+	public String createString(HttpServletRequest req) throws ServletException, IOException
+	{
+		String retValue = null;
+		//lets get the stuff we need to get the quote file
+		String host					= req.getHeader("Host") ;	
+		String imcserver			= Utility.getDomainPref("userserver",host) ;	
+		
+		String fileName				= req.getParameter("qFile");
+		if(fileName == null) 
+			fileName = (String) req.getAttribute("qFile");
+		String qLine				= req.getParameter("qLine");
+		if(qLine == null)
+			qLine = (String) req.getAttribute("qLine");	
+		
+		String resFile = HTMLConv.toHTML(IMCServiceRMI.getFortune(imcserver,fileName));	
+		
+		//System.out.println(resFile);
+		int qInt;
+		try{
+			qInt = Integer.parseInt(qLine);
+		}catch(NumberFormatException nfe){
+			return null;
+		}
+		
+		//System.out.println(resFile);
+		StringTokenizer token = new StringTokenizer(resFile, "#", false);
+		int counter = 1; 
+				
+		while (token.hasMoreTokens()){
+			String tmp = token.nextToken();		
+			if (counter == ((qInt+1)*3)){
+				return tmp;
+			}
+			counter++;		
+		}
+		
+		//we didn't find anything so lets return null
+		return null;
+	}
+}
