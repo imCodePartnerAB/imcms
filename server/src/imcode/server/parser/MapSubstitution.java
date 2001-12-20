35d0b5614372b5fe867b022b69efbe75fbaaa5b1
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2001-12-20 13:12:10 +0000

    Bug 335: Documented MapSubstitution some.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@1061 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/imcode/server/parser/MapSubstitution.java
+++ server/src/imcode/server/parser/MapSubstitution.java
@@ -4,6 +4,40 @@ import java.util.Map ;
 
 import org.apache.oro.text.regex.* ;
 
+/**
+
+This class is a generic Substitiution to be used with
+the jakarta-oro regexp classes.
+It uses a java.util.Map as a Substitution source.
+This allows you to use a HashMap or some other Map for
+the substitutions.
+
+Example:
+
+// Create the matcher needed for the substitution
+Perl5Matcher  matcher  = new Perl5Matcher()  ;
+
+// Create a pattern that will match words
+Perl5Compiler compiler = new Perl5Compiler() ;
+Pattern pattern = compiler.compile("\\b\\w+\\b") ;
+
+// Create and populate the map
+HashMap map = new HashMap() ;
+map.put("foo","bar") ;  // Replace 'foo' with 'bar'.
+map put("baz","quux") ; // Replace 'baz' with 'quux'.
+
+// Create the MapSubstitution
+MapSubstitution substitution = new MapSubstitution(map, false) ;
+
+// Create the input
+String input = "foo bar baz quux" ;
+
+// Do the substitution
+String output = Util.substitute(matcher, pattern, substitution, input) ;
+
+'output' will now contain "bar bar quux quux".
+
+**/
 public class MapSubstitution implements Substitution {
     private final static String CVS_REV = "$Revision$" ;
     private final static String CVS_DATE = "$Date$" ;
