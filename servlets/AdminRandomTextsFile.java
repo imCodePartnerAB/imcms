2aab47373bf051199001dc81066c4e634c99ddc6
peter <peter@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2002-02-20 15:05:27 +0000

    Bug 405 Removed some duplicated-code and did some minor changes to.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@1232 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- servlets/AdminRandomTextsFile.java
+++ servlets/AdminRandomTextsFile.java
@@ -5,324 +5,161 @@ import javax.servlet.http.*;
 import imcode.external.diverse.* ;
 import imcode.server.* ;
 import imcode.util.* ;
+import java.text.*;
 
 
 public class AdminRandomTextsFile extends Administrator {
 	private final static String CVS_REV = "$Revision$" ;
 	private final static String CVS_DATE = "$Date$" ;
-    String HTML_TEMPLATE ;
-
-    /**
-       The GET method creates the html page when this side has been
-       redirected from somewhere else.
-    **/
-
-    public void doGet(HttpServletRequest req, HttpServletResponse res)
-	throws ServletException, IOException 
-	{
-		this.doPost(req,res);
 	
-    } // End doGet
+	private final static String HTML_TEMPLATE 	= "fortunes/admin_random_texts_file.html" ;
+	private final static String OPTION_LINE 	= "fortunes/option_line.frag" ;
+	private final static String DATE_ERROR 		= "fortunes/date_err_msg.frag" ;
+	private final static String TEXT_ERROR 		= "fortunes/text_err_msg.frag" ;
 
-    /**
-       doPost
-    */
-    public void doPost(HttpServletRequest req, HttpServletResponse res)
-	throws ServletException, IOException 
-	{    
 
+	/**
+	The GET method creates the html page when this side has been
+	redirected from somewhere else.
+	**/
+	public void doGet(HttpServletRequest req, HttpServletResponse res)	throws ServletException, IOException{
+		this.doPost(req,res);
+
+	} // End doGet
+
+	/**
+	doPost
+	*/
+	public void doPost(HttpServletRequest req, HttpServletResponse res)	throws ServletException, IOException{ 
 		// Lets get the server this request was aimed for
 		String host = req.getHeader("Host") ;	
 		String imcServer = Utility.getDomainPref("adminserver",host) ;
-		
+
 		HttpSession session = req.getSession();
-		
+		imcode.server.User user ;
+
+		// Check if the user logged on
+		if ( (user = Check.userLoggedOn(req,res,"StartDoc" )) == null )	{
+			return ;
+		}
+
 		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
-		
+
 		String whichFile = (String)session.getAttribute("file") ;
-		
-		if (req.getParameter("back")!=null)
-		{
-			String url = MetaInfo.getServletPath(req) + "AdminRandomTexts";
-			res.sendRedirect(url) ;
+
+		if (req.getParameter("back")!=null)	{
+			res.sendRedirect("AdminRandomTexts") ;
 			return;
 		}	
+
+		Map lines = (Map)session.getAttribute("lines");
+		String date1 = "";
+		String date2 = "";
+		String text  = "";
+
+		if (req.getParameter("save")!=null)	{	
 		
-		String date1 = " ";
-			String date2 = " ";
-			String text = " ";
-		
-		if (req.getParameter("save")!=null)
-		{				
-			//out.println("okbuttonpushed <br> ");
-			//spara texten till filen
-			//String fortune_path = Utility.getDomainPref("FortunePath",host);
-			//hämta parametrar
-				
-			
-			
+			addLineToList(req,lines);			
+
 			File fortune_path = Utility.getDomainPrefPath("FortunePath",host);
 			String readFile = (String)session.getAttribute("file");
 			File file = new File(fortune_path,readFile + ".txt");
-			BufferedWriter fileW = new BufferedWriter( new FileWriter(file) );
-			//out.println("file: " + file + "<br>");
+			BufferedWriter fileW = new BufferedWriter( new FileWriter(file) );				
 			
-			//hämta nuvarande rader
-			Map lines = (Map)session.getAttribute("lines");
-			
-			date1 = (req.getParameter("date1")).trim();
-				date2 = (req.getParameter("date2")).trim();
-				text = (req.getParameter("text")).trim();
-								
-				if( checkDate(date1) && checkDate(date2) && text.length()>1 )
-				{
-					String fullLine = date1 + " " + date2 + " " + text;
-						//hitta högsta radnr
-					int last = 0;//( (Integer)lines.lastKey() ).intValue();
-					Set keys = lines.keySet();
-					Iterator rowI = keys.iterator();
-					while(rowI.hasNext())
-					{
-						int temp = (((Integer)rowI.next()).intValue());
-						last = temp>last?temp:last;
-					}
-					lines.put(new Integer(last+1),fullLine);
-				}
-				
+		
 			Set keyRows = lines.keySet();
 			Iterator rowIt = keyRows.iterator();
-			while(rowIt.hasNext())
-			{
+			while(rowIt.hasNext())	{
 				Integer row = (Integer)rowIt.next();
-			//	out.println(" lines2: " + lines.get(row) + "<br>" );
-				
+				//	out.println(" lines2: " + lines.get(row) + "<br>" );
+
 				//FIX så linjen blir ok med #
 				String fullLine = ((String)lines.get(row)).trim();
 				date1 = fullLine.substring(0,6);
-			 	date2 = fullLine.substring(7,13);
+				date2 = fullLine.substring(7,13);
 				text = fullLine.substring(14);//HTMLConv.toHTML(fullLine.substring(14));
-				
+
 				//out.println(date1 + "#" + date2 + "#" + text + "#" + "<br>");
 				fileW.write(date1 + "#" + date2 + "#" + text + "#" );
 				fileW.newLine();	
 			}
-				
+
 			fileW.flush();
 			fileW.close();
-			
+
 			//tillbaks till 
-			String url = MetaInfo.getServletPath(req) + "AdminRandomTexts" ;
-			res.sendRedirect(url) ;								  
+			res.sendRedirect("AdminRandomTexts") ;								  
 			return;
 		}
-		else
-		{
-			String options = "<option value=\"No_Choice\" selected>-- V&auml;lj Rad --</option>";
-			
-			
-			if ((req.getParameter("add")).equals("add"))
-			{
-				//hämta nuvarande rader
-				Map lines = (Map)session.getAttribute("lines");
-				
-				//hämta parametrar
-				date1 = (req.getParameter("date1")).trim();
-				date2 = (req.getParameter("date2")).trim();
-				text = (req.getParameter("text")).trim();
-								
-				boolean wrong = false;
-				
-				if( !checkDate(date1) )
-				{
-					date1="Fel datum!";
-					wrong = true;
-				}
-				
-				if( !checkDate(date2) )
-				{
-					date2="Fel datum!";
-					wrong = true;				
-				}
-				
-				if( text.length()<1 )
-				{
-					text="Fel: Du har inte angett texten!";
-					wrong = true;
-				}
-				
-				if( !wrong )
-				{
-					//lägg ihop den nya raden
-					String fullLine = date1 + " " + date2 + " " + text;
+		else{
+			String options = IMCServiceRMI.parseDoc(imcServer, null, OPTION_LINE , user.getLangPrefix());
+			String errMsgDate	= IMCServiceRMI.parseDoc(imcServer, null, DATE_ERROR , user.getLangPrefix());
+			String errMsgTxt	= IMCServiceRMI.parseDoc(imcServer, null, TEXT_ERROR , user.getLangPrefix());
 
-					//hitta högsta radnr
-					int last = 0;//( (Integer)lines.lastKey() ).intValue();
-					Set keys = lines.keySet();
-					Iterator rowI = keys.iterator();
-					while(rowI.hasNext())
-					{
-						int temp = (((Integer)rowI.next()).intValue());
-						last = temp>last?temp:last;
-					}
-					
-					//lägg till den nya raden
-					lines.put(new Integer(last+1),fullLine);
-					
-					//spara de nya raderna
-					session.setAttribute("lines",lines);
-					
-					date1 = " ";
-					date2 = " ";
-					text = " ";
+			if ((req.getParameter("add")).equals("add")){
+				//hämta parametrar
+				date1 = req.getParameter("date1").trim();
+				date2 = req.getParameter("date2").trim();
+				text  = req.getParameter("text").trim();
+				
+				boolean ok = true;
+				if( !checkDate(date1) )	{
+					date1=errMsgDate;
+					ok = false;
 				}
 				
-				Set keyRows = lines.keySet();
-				Iterator rowIt = keyRows.iterator();
-				while(rowIt.hasNext())
-				{
-					Integer row = (Integer)rowIt.next();
-				//	out.println(" lines2: " + lines.get(row) + "<br>" );
-				String fullLine = (String)lines.get(row);
-					
-			int stop = fullLine.length();
-			if (fullLine.indexOf("<BR>") != -1 ){	stop = fullLine.indexOf("<BR>");}
-			if (fullLine.indexOf("<br>") != -1 ){	stop = fullLine.indexOf("<br>");}
-			
-			options = options + "<option value=\""  + row + "\" > " + fullLine.substring(0,stop) + "</option>";
-		
-				//	options = options + "<option value=\""  + row + "\" > " + lines.get(row) + "</option>";
+				if( !checkDate(date2) )	{
+					date2=errMsgDate;
+					ok = false;				
 				}
+				
+				if( text.length()<1 ){
+					text=errMsgTxt;
+					ok = false;
+				}
+				
+				if( ok ){
+					addLineToList(req,lines);
+					date1 = "";
+					date2 = "";
+					text  = "";
+				}				
 			}	
-		
-			
-			if (req.getParameter("edit")!=null)
-			{
-				
-				
+
+
+			if (req.getParameter("edit")!=null){
 				//hämta raden som är markerad
 				String row = req.getParameter("AdminFile") ;
-				//out.println("row: " + row);
+			
+				//lägg till en eventuellt redan uppflyttad rad	
+				addLineToList(req,lines);
 				
-				Map lines = (Map)session.getAttribute("lines");
-		//lägg till en eventuellt redan uppflyttad rad		
-		if(req.getParameter("date1")!=null && req.getParameter("date2")!=null && req.getParameter("text")!=null )
-		{		//lägg till en eventuellt redan uppflyttad rad
-				//hämta parametrar
-				date1 = (req.getParameter("date1")).trim();
-				date2 = (req.getParameter("date2")).trim();
-				text = (req.getParameter("text")).trim();
-								
-				boolean wrong = false;
-				
-				if( !checkDate(date1) || !checkDate(date2) || text.length()<1 ){wrong = true;}
-				
-				if( !wrong )
-				{
-					//lägg ihop den nya raden
-					String fullLine = date1 + " " + date2 + " " + text;
-
-					//hitta högsta radnr
-					int last = 0;//( (Integer)lines.lastKey() ).intValue();
-					Set keys = lines.keySet();
-					Iterator rowI = keys.iterator();
-					while(rowI.hasNext())
-					{
-						int temp = (((Integer)rowI.next()).intValue());
-						last = temp>last?temp:last;
-					}
-					
-					//lägg till den nya raden
-					lines.put(new Integer(last+1),fullLine);
-					
-				}
-			}
-				if (!row.equals("No_Choice"))
-				{
-					Integer theRow = Integer.decode(row);
-				//	out.println("row: " + row + "therow: " + theRow + "<br>");
-					
+				if (!row.equals("No_Choice")){
+					Integer theRow = Integer.decode(row);					
 					String fullLine = ((String)lines.get(theRow)).trim();
 					date1 = fullLine.substring(0,6);
 					date2 = fullLine.substring(7,13);
 					text = fullLine.substring(14);
-					
-					lines.remove(theRow);
-				
-				}
-				
-				Set keyRows = lines.keySet();
-				Iterator rowIt = keyRows.iterator();
-				while(rowIt.hasNext())
-				{
-					//out.println(" lines2: " + lines.get(rowIt.next()) + "<br>" );
-					Integer rad = (Integer)rowIt.next();
-					String fullLine = (String)lines.get(rad);
-									
-					int stop = fullLine.length();
-					if (fullLine.indexOf("<BR>") != -1 ){	stop = fullLine.indexOf("<BR>");}
-					if (fullLine.indexOf("<br>") != -1 ){	stop = fullLine.indexOf("<br>");}
-							
-					options = options + "<option value=\""  + rad + "\" > " + fullLine.substring(0,stop) + "</option>";
-					
-					//options = options + "<option value=\""  + rad + "\" > " + lines.get(rad) + "</option>";
+					lines.remove(theRow);				
 				}
 			}	
-		
-			if (req.getParameter("remove")!=null)
-			{
-				//out.println("removebuttonpushed <br> ");
-				
-				//hämta nuvarande rader
-				Map lines = (Map)session.getAttribute("lines");
-				
-				if(req.getParameter("date1")!=null && req.getParameter("date2")!=null && req.getParameter("text")!=null )
-				{		//lägg till en eventuellt redan uppflyttad rad
-					//hämta parametrar
-					date1 = (req.getParameter("date1")).trim();
-					date2 = (req.getParameter("date2")).trim();
-					text = (req.getParameter("text")).trim();
-				}
-				
+
+			if (req.getParameter("remove")!=null){						
 				//hämta de rader som ska tas bort
 				String rows[] = req.getParameterValues("AdminFile") ;
-								
 				//ta bort de som ska raderas
-				for(int i=0;i<rows.length;i++)
-				{
-				//	out.println("rows: " + rows[i] + "<br>");
-					if (!rows[i].equals("No_Choice"))
-					{
+				for(int i=0;i<rows.length;i++){
+					if (!rows[i].equals("No_Choice")){
 						lines.remove(new Integer(rows[i]));	
 					}
 				}
-				
-				//spara de nya raderna
-				session.setAttribute("lines",lines);
-				
-				Set keyRows = lines.keySet();
-				Iterator rowIt = keyRows.iterator();
-				while(rowIt.hasNext())
-				{
-					Integer row = (Integer)rowIt.next();
-					
-					String fullLine = (String)lines.get(row);
-									
-							int stop = fullLine.length();
-							if (fullLine.indexOf("<BR>") != -1 ){	stop = fullLine.indexOf("<BR>");}
-							if (fullLine.indexOf("<br>") != -1 ){	stop = fullLine.indexOf("<br>");}
-							
-							options = options + "<option value=\""  + row + "\" > " + fullLine.substring(0,stop) + "</option>";
-					
-				//	out.println(" lines: " + lines.get(row) + "<br>" );
-					//options = options + "<option value=\""  + row + "\" > " + lines.get(row) + "</option>";
-				}
-				
-				
-				
 			}
-		
 			
+			session.setAttribute("lines",lines);
+			
+			StringBuffer buff = createOptionList(req,lines, imcServer, user );
+
 			//Add info for parsing to a Vector and parse it with a template to a htmlString that is printed
 			Vector values = new Vector();
 			values.add("#date1#");
@@ -334,49 +171,64 @@ public class AdminRandomTextsFile extends Administrator {
 			values.add("#file#");
 			values.add(whichFile);
 			values.add("#options#");
-			values.add(options);
-			
-			
+			values.add(buff.toString());
 
-			String parsed = IMCServiceRMI.parseExternalDoc(imcServer, values, "AdminRandomTextsFile.htm" , "se", "admin");
+			String parsed = IMCServiceRMI.parseDoc(imcServer, values, HTML_TEMPLATE  , user.getLangPrefix());
 			out.print(parsed);
 			return;
-		
 		}
-		
-		
-   } 
-   
-  
-   
-   public boolean checkDate(String date)
-   {
-   		boolean check = true;
-		
-   		// datumet måste bestå av 6 tecken
-		if( date.length()!=6 ){check = false;}
-		
-		//alla tecknen måste vara siffror
-		for(int i=0;i<date.length();i++)
+	} 
+	
+	private void addLineToList(HttpServletRequest req, Map lines){
+		String	date1 = (req.getParameter("date1")).trim();
+		String	date2 = (req.getParameter("date2")).trim();
+		String	text = (req.getParameter("text")).trim();
+
+		if( checkDate(date1) && checkDate(date2) && text.length()>1 ){
+			String fullLine = date1 + " " + date2 + " " + text;
+			//hitta högsta radnr
+			int last = 0;//( (Integer)lines.lastKey() ).intValue();
+			Set keys = lines.keySet();
+			Iterator rowI = keys.iterator();
+			while(rowI.hasNext()){
+				int temp = (((Integer)rowI.next()).intValue());
+				last = temp>last?temp:last;
+			}
+			lines.put(new Integer(last+1),fullLine);
+		}
+	}
+
+	private StringBuffer createOptionList(HttpServletRequest req, Map lines, String server, imcode.server.User user ) throws ServletException, IOException {
+		StringBuffer buff = new StringBuffer();
+		buff.append( IMCServiceRMI.parseDoc(server, null, OPTION_LINE , user.getLangPrefix()) );
+		Set keyRows = lines.keySet();
+		Iterator rowIt = keyRows.iterator();
+		while(rowIt.hasNext())
 		{
-			char tec = date.charAt(i);
-			if (!Character.isDigit(tec))
-			{check = false;}
+			Integer row = (Integer)rowIt.next();
+			String fullLine = (String)lines.get(row);			
+			buff.append( "<option value=\""  + row + "\" > " + fullLine + "</option>");
+		}		
+		return buff;
+	}
+
+	private boolean checkDate(String date){
+		DateFormat dateform = new SimpleDateFormat("yyMMdd") ;		
+		try
+		{
+			dateform.parse(date);	
+		}catch(java.text.ParseException pe){
+			return false;	
 		}
-				
-		return check;
-   }
-   
-   
-    /**
-       Log function, will work for both servletexec and Apache
-    **/
+		return true;
+	}
 
-    public void log( String str) {
-	super.log(str) ;
-	System.out.println("AdminManager: " + str ) ;	
-    }
-
-
+	/**
+	Log function, will work for both servletexec and Apache
+	**/
+	public void log( String str){
+		super.log(str) ;
+		System.out.println("AdminRandomTextsFile: " + str ) ;	
+	}
 
 } // End of class
