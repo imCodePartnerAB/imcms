e5dccc1b844020937f5b86a6fcf63e1033f0d037
lennart <lennart@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2002-03-19 12:52:08 +0000

    bug 506 changing name on sql-script for chat to chat_filename.sql
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@1349 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ sql/chat/chat_newdb.sql
@@ -0,0 +1,24 @@
+set IDENTITY_INSERT C_msg_type on
+insert into C_msg_type (msg_id,msg_string)
+values(1,'ignorerar')
+insert into C_msg_type (msg_id,msg_string)
+values(2,'tarbort')
+insert into C_msg_type (msg_id,msg_string)
+values(100,'säger till')
+insert into C_msg_type (msg_id,msg_string)
+values(101,'viskar till (privat)')
+insert into C_msg_type (msg_id,msg_string)
+values(102,'frågar')
+insert into C_msg_type (msg_id,msg_string)
+values(103,'ropar till')
+set IDENTITY_INSERT C_msg_type off
+
+
+
+insert into C_authorization_types(authorization_type,authorization_id)
+values('oregistrerad',1)
+insert into C_authorization_types(authorization_type,authorization_id)
+values('självregistrerad',2)
+insert into C_authorization_types(authorization_type,authorization_id)
+values('imCMSregistrerad',3)
+
