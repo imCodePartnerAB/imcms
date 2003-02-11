a00abc48b6fc623e6eb335a9894c7667b284c0a4
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2003-02-11 17:44:26 +0000

    Bug 207: Removed my xml-version-kludges from the main branch. Back to basics.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@1607 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/imcode/util/version/VersionParseException.java
+++
@@ -1,7 +0,0 @@
-package imcode.util.version ;
-
-public class VersionParseException extends Exception {
-    public VersionParseException (String message) {
-	super(message) ;
-    }
-}
--- servlets/Version.java
+++ servlets/Version.java
@@ -1,73 +1,29 @@
 import java.io.* ;
-import java.util.zip.* ;
-import java.util.Date ;
-import java.text.DateFormat ;
+
 import javax.servlet.* ;
 import javax.servlet.http.* ;
-
-import org.apache.oro.text.perl.* ;
-
-import imcode.util.version.* ;
-
-import org.w3c.dom.Document ;
-
-import org.apache.xml.serialize.* ;
 
 public class Version extends HttpServlet {
 
     private final static String CVS_REV =  "$Revision$" ;
     private final static String CVS_DATE = "$Date$" ;
 
+    private final static String VERSION_FILE = "version.txt" ;
+    private final static int BUFFER_LENGTH = 32768 ;
+
     public void init (ServletConfig config) throws ServletException {
 	super.init(config) ;
     }
 
     public void doGet (HttpServletRequest req, HttpServletResponse res) throws IOException {
+	InputStream in = this.getServletContext().getResourceAsStream("/WEB-INF/"+VERSION_FILE) ;
 
-	res.setContentType("text/xml; charset=UTF-8") ;
-	ServletOutputStream out = res.getOutputStream() ;
+	OutputStream out = res.getOutputStream() ;
 
-	try {
-	    if (req.getParameter("diff")!=null) {
-		diff(out) ;
-	    } else if (req.getParameter("version")!=null) {
-		version(out) ;
-	    } else {
-		now(out) ;
-	    }
-	} catch (Exception ex) {
-	    ex.printStackTrace(new PrintWriter(out)) ;
+	byte[] buffer = new byte[BUFFER_LENGTH] ;
+	int length ;
+	while (-1 != (length = in.read(buffer,0,BUFFER_LENGTH))) {
+	    out.write(buffer,0,length) ;
 	}
     }
-
-
-    private void diff(OutputStream out) throws Exception {
-	File webapproot = new File(this.getServletContext().getRealPath("/")) ;
-	File installedVersionFile    = new File(webapproot,"WEB-INF/version.xml") ;
-	XmlVersion installedVersion = new XmlVersion(installedVersionFile) ;
-	XmlVersion v2 = new XmlVersion(webapproot, installedVersion.getVersion()) ;
-	XmlVersionDiff d = new XmlVersionDiff(installedVersion.getDocument(),v2.getDocument()) ;
-	writeNode(out,d.getDiffDocument()) ;
-    }
-
-    private void version(OutputStream out) throws Exception {
-	File webapproot = new File(this.getServletContext().getRealPath("/")) ;
-	File version    = new File(webapproot,"WEB-INF/version.xml") ;
-	XmlVersion v = new XmlVersion(version) ;
-	writeNode(out,v.getDocument()) ;
-    }
-
-    private void now(OutputStream out) throws Exception {
-	File webapproot = new File(this.getServletContext().getRealPath("/")) ;
-	File installedVersionFile    = new File(webapproot,"WEB-INF/version.xml") ;
-	XmlVersion installedVersion = new XmlVersion(installedVersionFile) ;
-	XmlVersion v = new XmlVersion(webapproot,installedVersion.getVersion()) ;
-	writeNode(out,v.getDocument()) ;
-    }
-
-    private void writeNode(OutputStream out, Document document) throws Exception {
-	XMLSerializer xmlSerializer = new XMLSerializer() ;
-	xmlSerializer.writeNode(out,document) ;
-    }
-
 }
