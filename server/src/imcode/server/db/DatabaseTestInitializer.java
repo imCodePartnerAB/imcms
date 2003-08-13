840126e4db2afcc908092962240b43ccba923fef
hasse <hasse@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2003-08-13 13:46:00 +0000

    Bug 1288: Added both Christoffer and Hasses data.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@1936 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/imcode/server/db/DatabaseTestInitializer.java
+++ server/src/imcode/server/db/DatabaseTestInitializer.java
@@ -9,25 +9,26 @@ public class DatabaseTestInitializer {
     private static final int MIMER_PORT = 1360;
     private static final int MYSQL_PORT = 3306;
 
-    private static final File FILE_PATH = new File("/home/kreiger/work/imCMS-MAIN/1.3/sql/multipledatabases") ;
+    private static final File FILE_PATH = new File("E:/backuppas/projekt/imcode2003/imCMS/1.3/sql/multipledatabases/") ;
+    //private static final File FILE_PATH = new File("/home/kreiger/work/imCMS-MAIN/1.3/sql/multipledatabases") ;
 
     static DatabaseService static_initMimer() throws IOException {
-        DatabaseService dbService = new MimerDatabaseService("localhost", MIMER_PORT, "imcmstest", "sysadm", "trexus", FILE_PATH );
-        dbService.setupDatabaseWithTablesAndData();
+        DatabaseService dbService = new MimerDatabaseService("localhost", MIMER_PORT, "test", "sysadm", "admin", FILE_PATH );
+//        DatabaseService dbService = new MimerDatabaseService("localhost", MIMER_PORT, "imcmstest", "sysadm", "trexus", FILE_PATH );        dbService.setupDatabaseWithTablesAndData();
         dbService.createTestData();
         return dbService;
     }
 
     static DatabaseService static_initSqlServer() throws IOException {
-        DatabaseService dbService = new SQLServerDatabaseService( "ratatosk", SQLSERVER_PORT, "kreiger_imcmstest", "sa", "nonac", FILE_PATH );
-        dbService.setupDatabaseWithTablesAndData();
+        DatabaseService dbService = new SQLServerDatabaseService( "localhost", SQLSERVER_PORT, "test", "sa", "sa", FILE_PATH );
+//        DatabaseService dbService = new SQLServerDatabaseService( "ratatosk", SQLSERVER_PORT, "kreiger_imcmstest", "sa", "nonac", FILE_PATH );        dbService.setupDatabaseWithTablesAndData();
         dbService.createTestData();
         return dbService;
     }
 
     static DatabaseService static_initMySql() throws IOException {
-        DatabaseService dbService = new MySQLDatabaseServer( "localhost", MYSQL_PORT, "imcmstest", "root", "", FILE_PATH );
-        dbService.setupDatabaseWithTablesAndData();
+        DatabaseService dbService = new MySQLDatabaseServer( "localhost", MYSQL_PORT, "test", "root", "", FILE_PATH );
+//        DatabaseService dbService = new MySQLDatabaseServer( "localhost", MYSQL_PORT, "imcmstest", "root", "", FILE_PATH );        dbService.setupDatabaseWithTablesAndData();
         dbService.createTestData();
         return dbService;
     }
