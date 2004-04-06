41a5386f5fe096deb365f7b6b7c9b5bcb4a33ec2
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-04-06 12:36:04 +0000

    Bug 2097: Fixed intervals on MetaAdmin.
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/1.8-BRANCH@3341 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/servlet/superadmin/MetaAdmin.java
+++ server/src/com/imcode/imcms/servlet/superadmin/MetaAdmin.java
@@ -1,73 +1,78 @@
 package com.imcode.imcms.servlet.superadmin;
 
-import java.io.*;
-import java.util.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-
-import imcode.util.*;
-import imcode.server.*;
+import imcode.server.ApplicationServer;
+import imcode.server.IMCServiceInterface;
 import imcode.server.user.UserDomainObject;
-import imcode.server.document.*;
+import imcode.util.Utility;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MetaAdmin extends HttpServlet {
 
-    private final static int DEFAULT_META_START = 1001;
+    public static final String PARAMETER_BUTTON__SHOW_INTERVAL = "showinterval";
+    private static final String PARAMETER_BUTTON__SHOW_SPAN = "showspan";
+    private static final String PARAMETER__INTERVAL = "interval";
+    private static final String PARAMETER__START = "start";
+    private static final int ABBREVIATE_LENGTH = 80;
+    private final static int DEFAULT_START_DOCUMENT_ID = 1001;
+    private static final String PARAMETER__ENDMETA = "endmeta";
+    private static final String PARAMETER__STARTMETA = "startmeta";
 
     public void doGet( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
+
+        UserDomainObject user = Utility.getLoggedOnUser( req );
+
+        if (!user.isSuperAdmin()) {
+            return ;
+        }
 
         IMCServiceInterface imcref = ApplicationServer.getIMCServiceInterface();
 
         Utility.setDefaultHtmlContentType( res );
 
-        UserDomainObject user = Utility.getLoggedOnUser( req );
-
-        String[] pd = {
-            "&", "&amp;",
-            "<", "&lt;",
-            ">", "&gt;",
-            "\"", "&quot;",
-        };
-
         ServletOutputStream out = res.getOutputStream();
         int user_id = user.getId();
 
         boolean list = false; // show list if true.
-        if ( req.getParameter( "showinterval" ) != null || req.getParameter( "showspan" ) != null ) {
+        if ( req.getParameter( PARAMETER_BUTTON__SHOW_INTERVAL ) != null
+             || req.getParameter( PARAMETER_BUTTON__SHOW_SPAN ) != null ) {
             list = true;
         }
 
+        int min = getMinDocumentId( imcref );
+        int max = getMaxDocumentId( imcref );
+        int end = max;
         int interval;
         try {
-            interval = Integer.parseInt( req.getParameter( "interval" ) );
-
+            interval = Integer.parseInt( req.getParameter( PARAMETER__INTERVAL ) );
         } catch ( NumberFormatException ex ) {
-            interval = 1000;
+            interval = getMaxInterval( max-min ) ;
         }
         int start;
-        int min = Integer.parseInt( imcref.sqlQueryStr( "select min(meta_id) from meta", new String[0] ) );
-        int max = Integer.parseInt( imcref.sqlQueryStr( "select max(meta_id) from meta", new String[0] ) );
         try {
-            start = Integer.parseInt( req.getParameter( "start" ) );
+            start = Integer.parseInt( req.getParameter( PARAMETER__START ) );
             list = true;
         } catch ( NumberFormatException ex ) {
-            start = 1001;
-            //start = (Integer.parseInt(meta_id[0])-(Integer.parseInt(meta_id[0])%interval)) ;
+            start = DEFAULT_START_DOCUMENT_ID;
         }
 
-        int end = start + interval < max ? start + interval : max; // end will not bee > max meta_id
-
-        if ( req.getParameter( "showspan" ) != null ) {
-            end = Integer.parseInt( req.getParameter( "endmeta" ) );
-            start = Integer.parseInt( req.getParameter( "startmeta" ) );
+        if ( req.getParameter( PARAMETER_BUTTON__SHOW_SPAN ) != null ) {
+            end = Integer.parseInt( req.getParameter( PARAMETER__ENDMETA ) );
+            start = Integer.parseInt( req.getParameter( PARAMETER__STARTMETA ) );
         }
 
-        /*if ( !list ) {
-                start = 0 ;
-                end = 0 ;
-        }*/
-
-        Hashtable hash = new Hashtable();
+        Map hash = new HashMap();
         String[] meta_id = {""};
 
         if ( list ) {
@@ -82,30 +87,22 @@ public class MetaAdmin extends HttpServlet {
 
         }
 
-        Vector vec = new Vector();
+        List vec = new ArrayList();
         vec.add( "#start#" );
         vec.add( String.valueOf( start ) );
         vec.add( "#end#" );
         vec.add( String.valueOf( end ) );
 
         vec.add( "#intervals#" );
-        int max_min = max - min;
-        String tmp = "";
-        for ( int i = 10; i < ( max_min * 10 ); i *= 10 ) {
-            tmp += "<option value=\"" + i + "\" " + ( i == interval ? "selected" : "" ) + ">" + i + "</option>";
-        }
+        String tmp = createIntervalsOptionList( max-min, interval );
 
         if ( !list ) {
-            start = DEFAULT_META_START;
+            start = DEFAULT_START_DOCUMENT_ID;
         }
         vec.add( tmp );
         vec.add( "#starts#" );
-        tmp = "";
-        for ( int i = min; i <= max; i += interval ) {
-            tmp += "<option value=\"" + i + "\" " + ( i == start ? "selected" : "" ) + ">" + i + "</option>";
-        }
+        tmp = createStartsOptionList( min, max, start, interval );
         vec.add( tmp );
-        String lang_prefix = user.getLanguageIso639_2();
         out.println( imcref.getAdminTemplate( "MetaAdminControl.html", user, vec ) );
         if ( !list ) {
             return;
@@ -126,45 +123,47 @@ public class MetaAdmin extends HttpServlet {
                 break;
             }
             out.println( "<li>" );
-            if ( hl[i].length() > 80 ) {
-                hl[i] = hl[i].substring( 0, 77 ) + "...";
-            }
+            hl[i] = StringUtils.abbreviate( hl[i], ABBREVIATE_LENGTH );
 
-            HashMap docTypesIdAndNames = DocumentMapper.getDocumentTypeIdsAndNames( imcref, Integer.parseInt(meta_id[i]), user.getId(), lang_prefix );
-            String type = (String) docTypesIdAndNames.get( types[i] );
-
-            out.println( "<A name=\"" + meta_id[i] + "\" href=\"AdminDoc?meta_id=" + meta_id[i] + "\"><FONT COLOR=\"#FF0000\">" + meta_id[i] + "</FONT></A>&nbsp;<A name=\"" + meta_id[i] + "\" href=\"GetDoc?meta_id=" + meta_id[i] + "\">" + type + ",&nbsp;" + pc[i] + "&nbsp;parents&nbsp;:&nbsp;" + Parser.parseDoc( hl[i], pd ) + "</A>" );
+            out.println( "<A name=\"" + meta_id[i] + "\" href=\"AdminDoc?meta_id=" + meta_id[i]
+                         + "\"><FONT COLOR=\"#FF0000\">"
+                         + meta_id[i]
+                         + "</FONT></A>&nbsp;<A name=\""
+                         + meta_id[i]
+                         + "\" href=\"GetDoc?meta_id="
+                         + meta_id[i]
+                         + "\">&nbsp;"
+                         + pc[i]
+                         + "&nbsp;parents&nbsp;:&nbsp;"
+                         + StringEscapeUtils.escapeHtml( hl[i] )
+                         + "</A>" );
             if ( types[i].equals( "2" ) ) {
-                Hashtable h2 = imcref.sqlProcedureHash( "GetMenuDocChilds", new String[]{meta_id[i], "" + user_id} );
+                Map h2 = imcref.sqlProcedureHash( "GetMenuDocChilds", new String[]{meta_id[i], "" + user_id} );
                 String[] childs = (String[])h2.get( "to_meta_id" );
                 String[] hl2 = (String[])h2.get( "meta_headline" );
                 if ( childs != null && childs.length != 0 ) {
                     out.println( "<ul>" );
                     for ( int j = 0; j < childs.length; j++ ) {
                         String address = "MetaAdmin?start=" + childs[j] + "&interval=" + interval + "#" + childs[j];
-                        //int m_id = Integer.parseInt(childs[j]) ;
-                        //String address = "MetaAdmin?start="+(m_id - (m_id % interval))+"&interval="+interval+"#"+childs[j] ;
-                        if ( hl2[j].length() > 80 ) {
-                            hl2[j] = hl2[j].substring( 0, 77 ) + "...";
-                        }
-                        out.println( "<li><A href=\"" + address + "\">" + childs[j] + ":&nbsp;" + Parser.parseDoc( hl2[j], pd ) + "</A></li>" );
+                        hl2[j] = StringUtils.abbreviate( hl2[j], ABBREVIATE_LENGTH );
+                        out.println( "<li><A href=\"" + address + "\">" + childs[j] + ":&nbsp;"
+                                     + StringEscapeUtils.escapeHtml( hl2[j] )
+                                     + "</A></li>" );
                     }
                     out.println( "</ul>" );
                 }
             } else if ( types[i].equals( "6" ) ) {
-                Hashtable h2 = imcref.sqlProcedureHash( "GetBrowserDocChilds", new String[]{meta_id[i], "" + user_id} );
+                Map h2 = imcref.sqlProcedureHash( "GetBrowserDocChilds", new String[]{meta_id[i], "" + user_id} );
                 String[] childs = (String[])h2.get( "to_meta_id" );
                 String[] hl2 = (String[])h2.get( "meta_headline" );
                 if ( childs != null && childs.length != 0 ) {
                     out.println( "<ul>" );
                     for ( int j = 0; j < childs.length; j++ ) {
                         String address = "MetaAdmin?start=" + childs[j] + "&interval=" + interval + "#" + childs[j];
-                        //int m_id = Integer.parseInt(childs[j]) ;
-                        //String address = "MetaAdmin?start="+(m_id - (m_id % interval))+"&interval="+interval+"#" ;
-                        if ( hl2[j].length() > 80 ) {
-                            hl2[j] = hl2[j].substring( 0, 77 ) + "...";
-                        }
-                        out.println( "<li><A href=\"" + address + childs[j] + "\">" + childs[j] + ":&nbsp;" + Parser.parseDoc( hl2[j], pd ) + "</A></li>" );
+                        hl2[j] = StringUtils.abbreviate( hl2[j], ABBREVIATE_LENGTH );
+                        out.println( "<li><A href=\"" + address + childs[j] + "\">" + childs[j] + ":&nbsp;"
+                                     + StringEscapeUtils.escapeHtml( hl2[j] )
+                                     + "</A></li>" );
                     }
                     out.println( "</ul>" );
                 }
@@ -173,6 +172,43 @@ public class MetaAdmin extends HttpServlet {
             out.flush();
         }
         out.println( "</body></html>" );
+    }
+
+    private int getMaxInterval( int count ) {
+        if (0 == count) {
+            return 10 ;
+        }
+        int countLog10 = (int)log10(count);
+        return (int)Math.pow(10, 1+countLog10);
+    }
+
+    private double log10( int v ) {
+        return Math.log( v ) / Math.log(10) ;
+    }
+
+    private int getMaxDocumentId( IMCServiceInterface imcref ) {
+        return Integer.parseInt( imcref.sqlQueryStr( "select max(meta_id) from meta", new String[0] ) );
+    }
+
+    private int getMinDocumentId( IMCServiceInterface imcref ) {
+        return Integer.parseInt( imcref.sqlQueryStr( "select min(meta_id) from meta", new String[0] ) );
+    }
+
+    private String createStartsOptionList( int min, int max, int start, int interval ) {
+        String tmp;
+        tmp = "";
+        for ( int i = min; i <= max; i += interval ) {
+            tmp += "<option value=\"" + i + "\" " + ( i == start ? "selected" : "" ) + ">" + i + "</option>";
+        }
+        return tmp;
+    }
+
+    private String createIntervalsOptionList( int count, int interval ) {
+        String tmp = "";
+        for ( int i = 10; i <= getMaxInterval( count ); i *= 10 ) {
+            tmp += "<option value=\"" + i + "\" " + ( i == interval ? "selected" : "" ) + ">" + i + "</option>";
+        }
+        return tmp;
     }
 
 }
