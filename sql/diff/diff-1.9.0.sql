05bb31be365d53a64ffc0cbfad6b66f5e1f806c0
lennart <lennart@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-05-11 15:06:25 +0000

    bug 2246: Add diff script to rtrim those columns in table users that was convertet from char to varchar with no rtrim between version 1.6 to 1.7.0.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@3486 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/diff/diff-1.9.0.sql
+++ sql/diff/diff-1.9.0.sql
@@ -6,3 +6,9 @@ DROP PROCEDURE GetChilds
 
 -- 2004-05-04 Kreiger
 
+UPDATE users SET login_name=RTRIM(login_name), login_password=RTRIM(login_password),
+    first_name=RTRIM(first_name), last_name=RTRIM(last_name), title=RTRIM(title),
+    company=RTRIM(company), address=RTRIM(address), city=RTRIM(city),  zip=RTRIM(zip),
+    country=RTRIM(country),  county_council=RTRIM(county_council),  email=RTRIM(email)
+
+-- 2004-05-11  Lennart
