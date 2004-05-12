0b217e4a3f4eb227f1a7a110672622a930fa5254
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-05-12 16:58:06 +0000

    Bug 1799: Built test for superfluous language properties.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@3497 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ tools/src/imcode/buildtests/TestLanguagePropertiesInUse.java
@@ -0,0 +1,136 @@
+package imcode.buildtests;
+
+import imcode.util.FileFinder;
+import junit.framework.TestCase;
+import org.apache.commons.lang.UnhandledException;
+import org.apache.commons.lang.SystemUtils;
+import org.apache.oro.text.perl.Perl5Util;
+import org.apache.oro.text.regex.PatternMatcherInput;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Iterator;
+
+public class TestLanguagePropertiesInUse extends TestCase {
+
+    private Perl5Util perl5Util = new Perl5Util();
+
+    public void testLanguagePropertiesInUse() throws IOException {
+        File[] propertiesFiles = new File[]{new File("imcms_swe.properties"), new File("imcms_eng.properties")};
+
+        Set propertyKeysInUse = getPropertyKeysInUse();
+
+        checkPropertiesFiles( propertiesFiles, propertyKeysInUse );
+    }
+
+    private Set getPropertyKeysInUse() {
+        FileFinder fileFinder = new FileFinder();
+        StringCollectingFileFilter stringCollectingFilter = new StringCollectingFileFilter("m/<\\? (\\S+) \\?>/");
+        fileFinder.find( stringCollectingFilter, new File( "sql" ) );
+        fileFinder.find( stringCollectingFilter, new File( "web" ) );
+        stringCollectingFilter.setStringPattern( "m/\"(\\S+)\"/" );
+        fileFinder.find( stringCollectingFilter, new File( "server" ) );
+        Set propertyKeysInUse = stringCollectingFilter.getCollectedStrings();
+        return propertyKeysInUse;
+    }
+
+    private void checkPropertiesFiles( File[] propertiesFilenames, Set propertyKeysInUse ) throws IOException {
+        for ( int i = 0; i < propertiesFilenames.length; i++ ) {
+            File propertiesFile = propertiesFilenames[i] ;
+            checkPropertiesFile( propertiesFile, propertyKeysInUse );
+        }
+    }
+
+    private void checkPropertiesFile( File propertiesFile, Set propertyKeysInUse ) throws IOException {
+        Properties properties = loadPropertiesFile( propertiesFile );
+        Set propertyKeys = new HashSet( properties.keySet() );
+        propertyKeys.removeAll( propertyKeysInUse );
+        if ( !propertyKeys.isEmpty() ) {
+            String listOfLineNumbersOfPropertyKeysInFile = createListOfLineNumbersOfPropertyKeysInFile( propertyKeys, propertiesFile );
+            fail( propertyKeys.size() + " of " + properties.keySet().size() + " keys are superfluous: "+SystemUtils.LINE_SEPARATOR+listOfLineNumbersOfPropertyKeysInFile );
+        }
+    }
+
+    private Properties loadPropertiesFile( File propertiesFile ) throws IOException {
+        Properties properties = new Properties();
+        properties.load( new BufferedInputStream( new FileInputStream( propertiesFile ) ) );
+        return properties;
+    }
+
+    private String createListOfLineNumbersOfPropertyKeysInFile( Set propertyKeys, File propertiesFile ) throws IOException {
+        propertyKeys = new HashSet(propertyKeys) ;
+        String propertiesFileCanonicalPath = propertiesFile.getCanonicalPath();
+        StringBuffer listBuffer = new StringBuffer();
+        LineNumberReader reader = new LineNumberReader( new FileReader( propertiesFile ) );
+        for ( String line; null != ( line = reader.readLine() ); ) {
+            PatternMatcherInput lineToMatch = new PatternMatcherInput( line );
+            while ( perl5Util.match( "m/^(\\S+)\\s*[:=]/", lineToMatch ) ) {
+                String propertyKey = perl5Util.group( 1 );
+                if ( propertyKeys.remove( propertyKey ) ) {
+                    listBuffer.append( formatFileLineNumber( propertiesFileCanonicalPath, reader.getLineNumber(), propertyKey ) ) ;
+                }
+            }
+        }
+        for ( Iterator iterator = propertyKeys.iterator(); iterator.hasNext(); ) {
+            String propertyKey = (String)iterator.next();
+            listBuffer.append( formatFileLineNumber( propertiesFileCanonicalPath, 0, propertyKey ) ) ;
+
+        }
+        return listBuffer.toString() ;
+    }
+
+    private static String formatFileLineNumber( String canonicalFilePath, int lineNumber, String message ) {
+        return canonicalFilePath + ':' + lineNumber + ": " + message + SystemUtils.LINE_SEPARATOR;
+    }
+
+    private static class StringCollectingFileFilter implements FileFilter {
+
+        private Perl5Util perl5Util = new Perl5Util();
+        private Set collectedStrings = new HashSet();
+
+        private String stringPattern;
+
+        public StringCollectingFileFilter(String stringPattern) {
+            this.stringPattern = stringPattern ;
+        }
+
+        public boolean accept( File file ) {
+            try {
+                collectStringsInFile( file );
+            } catch ( IOException e ) {
+                throw new UnhandledException( e );
+            }
+            return true;
+        }
+
+        private void collectStringsInFile( File file ) throws IOException {
+            if ( !file.isFile() ) {
+                return;
+            }
+            LineNumberReader reader = new LineNumberReader( new FileReader( file ) );
+            for ( String line; null != ( line = reader.readLine() ); ) {
+                collectStringsInLine( line );
+            }
+        }
+
+        private void collectStringsInLine( String line ) {
+            PatternMatcherInput lineToMatch = new PatternMatcherInput( line );
+            while ( perl5Util.match( stringPattern, lineToMatch ) ) {
+                String propertyKey = perl5Util.group( 1 );
+                collectedStrings.add( propertyKey );
+            }
+        }
+
+        public Set getCollectedStrings() {
+            return collectedStrings;
+        }
+
+        public void setStringPattern( String stringPattern ) {
+            this.stringPattern = stringPattern;
+        }
+
+    }
+
+}
