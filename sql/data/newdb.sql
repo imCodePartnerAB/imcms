d5082c31ad03a80c234d3acd37ae21218f5b3bce
crister <crister@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-06-01 12:42:01 +0000

    bug 2302 Uppdaterat databasfilen.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@3587 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/data/newdb.sql
+++ sql/data/newdb.sql
@@ -46,7 +46,9 @@ INSERT INTO text_docs
 INSERT INTO roles_rights
  VALUES (2,1001,3)
 INSERT INTO texts
- VALUES( 1001, 1, '<h2>imCode imCMS</h2><br><a href="@rooturl@/login/"><? sql/sql/newdb.sql/2 ?>!</a><br><a href="@rooturl@/servlet/SearchDocuments"><? sql/sql/newdb.sql/4 ?></a><br><br><a href="@documentationwebappurl@/"><? sql/sql/newdb.sql/5 ?></a><br><br><a href="@rooturl@/imcms/docs/apisamples/"><? sql/sql/newdb.sql/6 ?></a><br>',1)
+ VALUES( 1001, 1, '<? sql/sql/newdb.sql/17 ?>',1)
+INSERT INTO texts
+ VALUES( 1001, 2, '<a href="@rooturl@/login/" title="<? sql/sql/newdb.sql/7 ?>"><b><? sql/sql/newdb.sql/2 ?>!</b></a><br><br><br><a href="@rooturl@/servlet/SearchDocuments" target="_blank" title="<? sql/sql/newdb.sql/8 ?>"><b><? sql/sql/newdb.sql/4 ?></b></a><br><? sql/sql/newdb.sql/9 ?> <i> <? sql/sql/newdb.sql/10 ?></i><br><br><a href="@documentationwebappurl@/" target="_blank" title="<? sql/sql/newdb.sql/11 ?>"><b><? sql/sql/newdb.sql/5 ?></b></a><br><? sql/sql/newdb.sql/12 ?><br><br><a href="http://www.imcms.net" target="_blank" title="<? sql/sql/newdb.sql/13 ?> www.imcms.net"><b><? sql/sql/newdb.sql/14 ?></b></a><br><? sql/sql/newdb.sql/15 ?><br><br><a href="@rooturl@/imcms/docs/apisamples/"  target="_blank" title="<? sql/sql/newdb.sql/13 ?>API-samples"><b><? sql/sql/newdb.sql/6 ?></b></a><br><? sql/sql/newdb.sql/16 ?>',1)
 INSERT INTO images ( meta_id , width , height , border , v_space , h_space , name , image_name , target , align , alt_text , low_scr , imgurl , linkurl )
 values (1001,100,29,0,0,0,3,'','_blank','top','','','imCMSpower.gif','http://www.imcms.net')
 
