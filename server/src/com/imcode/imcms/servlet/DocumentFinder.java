bc4773f08c1d0ca80f5584f89cbe18f437c4e5fc
hasse <hasse@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-06-15 14:25:26 +0000

    Bug 2348, 2128 : First version of images as File documents.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@3627 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ server/src/com/imcode/imcms/servlet/DocumentFinder.java
@@ -0,0 +1,49 @@
+package com.imcode.imcms.servlet;
+
+import imcode.server.document.DocumentDomainObject;
+import imcode.util.HttpSessionUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import java.io.IOException;
+
+public class DocumentFinder extends WebComponent {
+    private boolean documentSelected;
+    private DocumentDomainObject selectedDocument;
+    public static final String REQUEST_ATTRIBUTE_PARAMETER__SEARCH_DOCUMENTS = "searchDocuments";
+
+    public static DocumentFinder getInstance( HttpServletRequest request ) {
+        DocumentFinder documentFinder = (DocumentFinder)HttpSessionUtils.getObjectFromSessionWithKeyInRequest( request, DocumentFinder.REQUEST_ATTRIBUTE_PARAMETER__SEARCH_DOCUMENTS );
+        if ( null == documentFinder ) {
+            documentFinder = new DocumentFinder();
+        }
+        return documentFinder;
+    }
+
+
+    public boolean isDocumentSelected() {
+        return documentSelected;
+    }
+
+    public void setSelectedDocument( DocumentDomainObject selectedDocument ) {
+        documentSelected = true;
+        this.selectedDocument = selectedDocument;
+    }
+
+    public DocumentDomainObject getSelectedDocument() {
+        return selectedDocument;
+    }
+
+    public void forward( HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException {
+        HttpSessionUtils.addObjectToSessionAndSetSessionAttributeNameInRequest( this, request, DocumentFinder.REQUEST_ATTRIBUTE_PARAMETER__SEARCH_DOCUMENTS );
+
+        String SearchTargetString = "SearchDocuments?" +
+                SearchDocuments.PARAM__DOCUMENT_TYPE + "=" + DocumentDomainObject.DOCTYPE_FILE + "&" +
+                SearchDocuments.PARAM__SHOW_SELECT_LINK + "=" + "true" + "&" +
+                SearchDocuments.PARAM__CHOOSEN_URL + "=" + this.getForwardReturnUrl() ;
+        RequestDispatcher rd = request.getRequestDispatcher( SearchTargetString );
+        rd.forward( request, response );
+    }
+}
