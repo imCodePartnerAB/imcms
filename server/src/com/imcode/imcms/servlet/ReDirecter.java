4f8bfc7045519ab3066d285c3fbe14480c372baf
lennart <lennart@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-10-26 12:45:20 +0000

    bug 2633: - Add "[contextpath]/servlet" to the redirect url and change metaid_offset to get the right element for meta_id from url string in accesslog.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@3885 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/servlet/ReDirecter.java
+++ server/src/com/imcode/imcms/servlet/ReDirecter.java
@@ -9,16 +9,16 @@ import java.util.StringTokenizer;
 
 public class ReDirecter extends HttpServlet {
 
-    private final static int METAID_OFFSET = 1;
+    private final static int METAID_OFFSET = 2;
 
     public void doGet( HttpServletRequest req, HttpServletResponse res ) throws IOException, ServletException {
 
         String[] pathElements = req.getPathInfo().split("/") ;
 
         if ( pathElements.length > METAID_OFFSET ) {
-            res.sendRedirect( "GetDoc?meta_id=" + pathElements[METAID_OFFSET] );
+            res.sendRedirect( req.getContextPath() + "/servlet/GetDoc?meta_id=" + pathElements[METAID_OFFSET] );
         } else {
-            res.sendRedirect( "StartDoc" );
+            res.sendRedirect( req.getContextPath() + "/servlet/StartDoc" );
         }
     }
 
