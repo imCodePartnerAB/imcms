751bd3a9bb67ea1a8cab2a6a5c3e871fc82eaed9
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2004-11-25 17:16:54 +0000

    Bug 2694: Forgot to update tables
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@3958 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/data/newdb.sql
+++ sql/data/newdb.sql
@@ -19,12 +19,14 @@ SET IDENTITY_INSERT sys_data OFF
 INSERT INTO users
  VALUES (1,'admin', 'admin', 'Admin', 'Super','','','','','','','','@servermaster-email@',0,1001,0,<? sql/default_lang_id ?>,1,convert(char(10),getDate(),120))
 INSERT INTO users VALUES (2,'user', 'user', 'User', 'Extern','','','','','','','','',0,1001,0,<? sql/default_lang_id ?>,1,convert(char(10),getDate(),120))
+SET IDENTITY_INSERT roles ON
 INSERT INTO roles
  VALUES(0, 'Superadmin', 0, 1)
 INSERT INTO roles
  VALUES(1, 'Useradmin', 0, 2)
 INSERT INTO roles
  VALUES(2, 'Users', 1, 0)
+SET IDENTITY_INSERT roles OFF
 INSERT INTO user_roles_crossref
  VALUES(1,0)
 INSERT INTO user_roles_crossref
