0ca7f497e153a19c10119361c6b74d5b0e3c2f7d
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2005-01-03 11:31:11 +0000

    Bug 2776: Redirect back to AdminDoc after completing editing of text.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@4094 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/servlet/admin/SaveText.java
+++ server/src/com/imcode/imcms/servlet/admin/SaveText.java
@@ -10,19 +10,15 @@ import imcode.server.user.UserDomainObject;
 import imcode.util.Utility;
 import imcode.util.poll.PollHandlingSystem;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
 
-/**
- * Save textdocument in a document.
- */
 public final class SaveText extends HttpServlet {
 
-    public void doPost( HttpServletRequest req, HttpServletResponse res ) throws ServletException, IOException {
+    public void doPost( HttpServletRequest req, HttpServletResponse res ) throws IOException {
         req.setCharacterEncoding( WebAppGlobalConstants.DEFAULT_ENCODING_WINDOWS_1252 );
         Utility.setDefaultHtmlContentType( res );
         Writer out = res.getWriter();
@@ -31,42 +27,32 @@ public final class SaveText extends HttpServlet {
         ImcmsServices imcref = Imcms.getServices();
         int meta_id = Integer.parseInt( req.getParameter( "meta_id" ) );
         UserDomainObject user = Utility.getLoggedOnUser( req );
-        if ( !imcref.checkDocAdminRights( meta_id, user, imcode.server.ImcmsConstants.PERM_EDIT_TEXT_DOCUMENT_TEXTS ) ) {	// Checking to see if user may edit this
-            String output = AdminDoc.adminDoc( meta_id, user, req, res );
-            if ( output != null ) {
-                out.write( output );
+        if ( imcref.checkDocAdminRights( meta_id, user, imcode.server.ImcmsConstants.PERM_EDIT_TEXT_DOCUMENT_TEXTS )
+             && req.getParameter( "ok" ) != null ) {
+            // get text_no
+            int txt_no = Integer.parseInt( req.getParameter( "txt_no" ) );
+
+            // get textdocument
+            String text_string = req.getParameter( "text" );
+
+            int text_format = Integer.parseInt( req.getParameter( "format_type" ) );
+
+            String text_type = req.getParameter( "txt_type" ); // ex. pollrequest-1
+            if ( text_type == null ) {
+                text_type = "";
             }
-            return;
-        }
 
-        // get text_no
-        int txt_no = Integer.parseInt( req.getParameter( "txt_no" ) );
+            TextDomainObject text = new TextDomainObject( text_string, text_format );
 
-        // get textdocument
-        String text_string = req.getParameter( "text" );
-
-        int text_format = Integer.parseInt( req.getParameter( "format_type" ) );
-
-        String text_type = req.getParameter( "txt_type" ); // ex. pollrequest-1
-        if ( text_type == null ) {
-            text_type = "";
-        }
-
-        TextDomainObject text = new TextDomainObject( text_string, text_format );
-
-        user.put( "flags", new Integer( imcode.server.ImcmsConstants.PERM_EDIT_TEXT_DOCUMENT_TEXTS ) );
-
-        if ( req.getParameter( "ok" ) != null ) {
             DocumentMapper documentMapper = imcref.getDocumentMapper();
-            TextDocumentDomainObject document = (TextDocumentDomainObject)documentMapper.getDocument(meta_id);
+            TextDocumentDomainObject document = (TextDocumentDomainObject)documentMapper.getDocument( meta_id );
 
             saveText( documentMapper, text, document, txt_no, text_type, imcref, meta_id, user );
         }
 
-        String output = AdminDoc.adminDoc( meta_id, user, req, res );
-        if ( output != null ) {
-            out.write( output );
-        }
+        res.sendRedirect( "AdminDoc?meta_id=" + meta_id + "&flags="
+                          + imcode.server.ImcmsConstants.PERM_EDIT_TEXT_DOCUMENT_TEXTS );
+
     }
 
     private void saveText( DocumentMapper documentMapper, TextDomainObject text, TextDocumentDomainObject document,
@@ -83,6 +69,7 @@ public final class SaveText extends HttpServlet {
                 poll.savePollparameter( text_type, document.getId(), txt_no, text.getText() );
             }
         }
-        imcref.updateMainLog( "Text " + txt_no + " in [" + meta_id + "] modified by user: [" + user.getFullName() + "]");
+        imcref.updateMainLog( "Text " + txt_no + " in [" + meta_id + "] modified by user: [" + user.getFullName()
+                              + "]" );
     }
 }
