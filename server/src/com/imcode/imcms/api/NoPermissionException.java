74d46f77749f0999d9d124b7cc4e5f7eb8e14422
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2005-06-20 13:08:56 +0000

    Issue 3348: Made NoPermissionException a RuntimeException
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@4418 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/api/NoPermissionException.java
+++ server/src/com/imcode/imcms/api/NoPermissionException.java
@@ -4,7 +4,7 @@ package com.imcode.imcms.api;
  * This exception is thrown from almost all methods in the service classes. It is thrown when the current logged
  * in user dosen't have the apropiate rights to do that operation. See the message for futher information.
  */
-public class NoPermissionException extends Exception {
+public class NoPermissionException extends RuntimeException {
     public NoPermissionException( String message ) {
         super( message );
     }
