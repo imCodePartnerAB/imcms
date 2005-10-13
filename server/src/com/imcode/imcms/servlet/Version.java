3cf5952d8bd41f74dc44f014fc152f472c0d29f5
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2005-10-13 12:08:41 +0000

    Issue 3519: Output versions of java, servlet container, and database along with imCMS version.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@4543 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/servlet/Version.java
+++ server/src/com/imcode/imcms/servlet/Version.java
@@ -1,27 +1,67 @@
 package com.imcode.imcms.servlet;
 
+import com.imcode.db.DatabaseCommand;
+import com.imcode.db.DatabaseConnection;
+import com.imcode.db.DatabaseException;
+import imcode.server.Imcms;
+import org.apache.commons.io.IOUtils;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 public class Version extends HttpServlet {
 
-    private final static String VERSION_FILE = "version.txt";
-    private final static int BUFFER_LENGTH = 32768;
+    private final static String VERSION_FILE = "/WEB-INF/version.txt";
 
-    public void doGet( HttpServletRequest req, HttpServletResponse res ) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 
-        InputStream in = this.getServletContext().getResourceAsStream( "/WEB-INF/" + VERSION_FILE );
+        String imcmsVersion = getImcmsVersion();
+        String serverInfo = getServletContext().getServerInfo();
+        String databaseProductNameAndVersion = getDatabaseProductNameAndVersion();
+        String javaVersion = getJavaVersion();
 
-        OutputStream out = res.getOutputStream();
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        out.println(imcmsVersion);
+        out.println(javaVersion);
+        out.println(serverInfo);
+        out.println(databaseProductNameAndVersion);
+    }
 
-        byte[] buffer = new byte[BUFFER_LENGTH];
-        int length;
-        while ( -1 != ( length = in.read( buffer, 0, BUFFER_LENGTH ) ) ) {
-            out.write( buffer, 0, length );
+    private String getJavaVersion() {
+        return System.getProperty("java.vm.vendor")+" "+System.getProperty("java.vm.name")+" "+System.getProperty("java.vm.version");
+    }
+
+    private String getImcmsVersion() {
+        try {
+            Reader in = new InputStreamReader(getServletContext().getResourceAsStream(VERSION_FILE));
+            try {
+                return "imCMS "+IOUtils.toString(in).trim();
+            } finally {
+                in.close();
+            }
+        } catch ( Exception npe ) {
+            return "imCMS";
         }
+    }
+
+    private String getDatabaseProductNameAndVersion() {
+        return (String) Imcms.getServices().getDatabase().executeCommand(new DatabaseCommand() {
+            public Object executeOn(DatabaseConnection connection) throws DatabaseException {
+                try {
+                    DatabaseMetaData metaData = connection.getConnection().getMetaData();
+                    return metaData.getDatabaseProductName() + " " + metaData.getDatabaseProductVersion();
+                } catch ( SQLException e ) {
+                    throw DatabaseException.fromSQLException("", e);
+                }
+            }
+        });
     }
 }
