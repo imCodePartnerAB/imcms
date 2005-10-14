c9c7bb1963022f6f567706df6952994d25c89eb7
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2005-10-14 16:16:48 +0000

    Issue 3527: Provide a DataSource in DatabaseService
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@4551 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/api/DatabaseService.java
+++ server/src/com/imcode/imcms/api/DatabaseService.java
@@ -16,14 +16,19 @@ public class DatabaseService {
      * Get a databaseconnection from the connectionpool.
      *
      * <strong>IMPORTANT</strong>: Do not forget to make sure the connection is closed,
-     * else is won't be returned to the pool.
+     * otherwise it won't be returned to the pool.
      *
      * @return a databaseconnection from the connectionpool.
-
      * @throws SQLException if there was a problem getting the connection.
      */
-
     public Connection getConnection() throws SQLException {
         return dataSource.getConnection() ;
+    }
+    
+    /**
+     * @return A {@link DataSource}.
+     */
+    public DataSource getDataSource() {
+        return dataSource ;
     }
 }
