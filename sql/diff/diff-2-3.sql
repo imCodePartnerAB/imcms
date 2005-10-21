bf0b3ab7e0843ee39cc13ccc0d0426048e66faf0
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2005-10-21 15:04:11 +0000

    Issue 3383: Don't care about foreign key names when dropping them.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@4562 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/diff/diff-2-3.sql
+++ sql/diff/diff-2-3.sql
@@ -449,8 +449,40 @@ IF EXISTS(SELECT * FROM dbo.templategroups)
 GO
 SET IDENTITY_INSERT dbo.Tmp_templategroups OFF
 GO
-ALTER TABLE dbo.templates_cref
-        DROP CONSTRAINT templates_cref_FK_group_id_templategroups
+DECLARE @referenced_table_name VARCHAR(256)
+
+SET @referenced_table_name = 'templategroups'
+
+DECLARE constraints_cursor CURSOR FOR
+SELECT tables.name, constraints.name
+FROM   sysobjects constraints,
+       sysobjects tables,
+       sysobjects referenced_tables,
+       sysforeignkeys foreignkeys
+WHERE  foreignkeys.constid    = constraints.id
+AND    foreignkeys.fkeyid     = tables.id 
+AND    foreignkeys.rkeyid     = referenced_tables.id 
+AND    referenced_tables.name = @referenced_table_name
+
+OPEN constraints_cursor
+
+DECLARE @table_name VARCHAR(256)
+DECLARE @constraint_name VARCHAR(256)
+FETCH NEXT FROM constraints_cursor
+INTO @table_name, @constraint_name
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+
+    EXEC('ALTER TABLE '+@table_name+' DROP CONSTRAINT '+@constraint_name)
+
+    FETCH NEXT FROM constraints_cursor
+    INTO @table_name, @constraint_name
+
+END
+
+CLOSE constraints_cursor
+DEALLOCATE constraints_cursor
 GO
 DROP TABLE dbo.templategroups
 GO
