7297b80fdd07a2d9ebcd562e7370aeb2b46b05dd
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2006-06-13 09:04:59 +0000

    Issue 4500: Synchronized moveIntoPlace method.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@5839 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/imcode/util/TestFrequencyOrderedBag.java
+++ server/src/imcode/util/TestFrequencyOrderedBag.java
@@ -1,6 +1,9 @@
 package imcode.util;
 
+import junit.extensions.ActiveTestSuite;
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestResult;
 
 import java.util.List;
 
@@ -34,6 +37,37 @@ public class TestFrequencyOrderedBag extends TestCase {
         assertEquals("bar", list.get(1)) ;
         assertEquals(2, bag.size()) ;
         assertEquals(2, list.size()) ;
+    }
+
+    public void testConcurrency() throws InterruptedException {
+
+        ActiveTestSuite suite = new ActiveTestSuite();
+        Test test = new Test() {
+            private FrequencyOrderedBag bag = new FrequencyOrderedBag();
+            public int countTestCases() {
+                return 1;
+            }
+
+            public void run(TestResult testResult) {
+                try {
+                    for ( int j = 0; j < 10; ++j ) {
+                        for ( int i = 0; i < 10; ++i ) {
+                            Integer key = new Integer(i);
+                            bag.add(key);
+                            assertTrue(bag.getFrequency(key) > 0) ;
+                        }
+                    }
+                } catch ( Exception e ) {
+                    testResult.addError(this, e);
+                }
+            }
+        };
+        for ( int i = 0; i < 100; ++i ) {
+            suite.addTest(test);
+        }
+        TestResult testResult = new TestResult();
+        suite.run(testResult);
+        assertEquals(0, testResult.errorCount());
     }
 
 }
