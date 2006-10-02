99ceab9a51f43e7f0897ef64f2c29cd11e5e1f39
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2006-10-02 15:56:35 +0000

    Issue 4890: Handle parameters for forwards.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@6251 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/imcode/util/QueryStringDecodingHttpServletRequestWrapper.java
+++ server/src/imcode/util/QueryStringDecodingHttpServletRequestWrapper.java
@@ -1,6 +1,7 @@
 package imcode.util;
 
 import org.apache.commons.collections.MultiHashMap;
+import org.apache.commons.collections.MultiMap;
 import org.apache.commons.collections.iterators.IteratorEnumeration;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,77 +18,109 @@ public class QueryStringDecodingHttpServletRequestWrapper extends HttpServletReq
 
     private FallbackDecoder decoder;
     private Pattern parameterBytePattern = Pattern.compile("%[0-9a-fA-F][0-9a-fA-F]|.", Pattern.DOTALL);
-    private Map<String, String[]> parameterMap = new HashMap<String, String[]>();
+    private String previousForwardQueryString;
+    private Map<String, String[]> parameterMap;
 
     public QueryStringDecodingHttpServletRequestWrapper(HttpServletRequest request) {
         super(request);
         decoder = new FallbackDecoder(Charset.forName(Imcms.DEFAULT_ENCODING), Charset.forName(Imcms.ISO_8859_1_ENCODING));
-        decode(request);
     }
 
-    private void decode(HttpServletRequest request) {
-        String queryString = request.getQueryString();
-        if (null == queryString) {
-            return ;
+    public String getParameter(String parameterName) {
+        String[] parameterValues = getParameterValues(parameterName);
+        if ( null == parameterValues ) {
+            return null;
+        }
+        return parameterValues[0];
+    }
+
+    public Enumeration getParameterNames() {
+        return new IteratorEnumeration(getParameterMap().keySet().iterator());
+    }
+
+    public String[] getParameterValues(String parameterName) {
+        return getParameterMap().get(parameterName);
+    }
+
+    public Map<String, String[]> getParameterMap() {
+        String forwardQueryString = (String) getAttribute("javax.servlet.forward.query_string");
+        boolean shouldBuildMap = null == parameterMap 
+                                 || null == forwardQueryString ^ null == previousForwardQueryString 
+                                 || null != forwardQueryString && !forwardQueryString.equals(previousForwardQueryString);
+        if (shouldBuildMap) {
+            previousForwardQueryString = forwardQueryString ;
+            parameterMap = buildParameterMap();
+        }
+        return Collections.unmodifiableMap(parameterMap);
+    }
+
+    private Map<String, String[]> buildParameterMap() {
+        MultiHashMap result = new MultiHashMap();
+        addQueryStringParameters(previousForwardQueryString, result);
+        addQueryStringParameters(getQueryString(), result);
+        return convertMultiMapToArrayMap(result);
+    }
+
+    private void addQueryStringParameters(String queryString, MultiHashMap result) {
+        MultiMap queryStringParameterMap = decodeQueryString(queryString);
+        for ( Map.Entry<String, Collection<String>> entry : (Set<Map.Entry<String, Collection<String>>>)queryStringParameterMap.entrySet() ) {
+            String parameterName = entry.getKey();
+            Collection<String> parameterValues = entry.getValue();
+            for ( String parameterValue : parameterValues ) {
+                result.put(parameterName, parameterValue);
+            }
+        }
+    }
+
+    private MultiMap decodeQueryString(String queryString) {
+        if ( null == queryString ) {
+            return new MultiHashMap();
         }
         String[] parameterPairs = queryString.split("[&;]");
         MultiHashMap localParameterMap = new MultiHashMap();
-        for ( String parameterPair: parameterPairs ) {
-            if (0 == parameterPair.length()) {
+        for ( String parameterPair : parameterPairs ) {
+            if ( 0 == parameterPair.length() ) {
                 continue;
             }
-            String[] nameAndValue = parameterPair.split("=", 2) ;
-            String parameterName = decode(nameAndValue[0]) ;
+            String[] nameAndValue = parameterPair.split("=", 2);
+            String parameterName = decode(nameAndValue[0]);
             String parameterValue = decode(nameAndValue[1]);
-            localParameterMap.put(parameterName, parameterValue) ;
+            localParameterMap.put(parameterName, parameterValue);
         }
-        for ( Map.Entry<String, Collection<String>> entry : (Set<Map.Entry<String, Collection<String>>>)localParameterMap.entrySet() ) {
-            String parameterName = entry.getKey();
-            Collection<String> parameterValues = entry.getValue();
-            parameterMap.put(parameterName, parameterValues.toArray(new String[parameterValues.size()])) ;
-        }
+        return localParameterMap ;
     }
 
     private String decode(String encodedString) {
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         Matcher matcher = parameterBytePattern.matcher(encodedString);
-        while (matcher.find()) {
-            int c ;
+        while ( matcher.find() ) {
+            int c;
             String match = matcher.group();
-            if (3 == match.length()) {
-                c = Integer.parseInt(match.substring(1), 16) ;
-            } else if (1 == match.length()) {
+            if ( 3 == match.length() ) {
+                c = Integer.parseInt(match.substring(1), 16);
+            } else if ( 1 == match.length() ) {
                 c = match.charAt(0);
-                if (c == '+') {
+                if ( c == '+' ) {
                     c = ' ';
                 }
             } else {
-                throw new RuntimeException("Illegal match length.") ;
+                throw new RuntimeException("Illegal match length.");
             }
             bytes.write(c);
         }
-        encodedString = decoder.decodeBytes(bytes.toByteArray(), "\""+encodedString +"\"");
+        encodedString = decoder.decodeBytes(bytes.toByteArray(), "\"" + encodedString + "\"");
         return encodedString;
     }
 
-    public String getParameter(String parameterName) {
-        String[] parameterValues = getParameterValues(parameterName);
-        if (null == parameterValues) {
-            return null ;
+    private static Map<String,String[]> convertMultiMapToArrayMap(MultiMap multiMap) {
+        Map<String, String[]> parameterMap = new HashMap<String, String[]>();
+        for ( Map.Entry<String, Collection<String>> entry : (Set<Map.Entry<String, Collection<String>>>) multiMap.entrySet() )
+        {
+            String parameterName = entry.getKey();
+            Collection<String> parameterValues = entry.getValue();
+            parameterMap.put(parameterName, parameterValues.toArray(new String[parameterValues.size()]));
         }
-        return parameterValues[0] ;
-    }
-
-    public Map<String, String[]> getParameterMap() {
-        return Collections.unmodifiableMap(parameterMap) ;
-    }
-
-    public Enumeration getParameterNames() {
-        return new IteratorEnumeration(parameterMap.keySet().iterator());
-    }
-
-    public String[] getParameterValues(String parameterName) {
-        return parameterMap.get(parameterName) ;
+        return parameterMap ;        
     }
 
 }
