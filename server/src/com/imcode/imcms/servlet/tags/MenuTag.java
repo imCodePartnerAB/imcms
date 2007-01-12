f98b1ca90f94568482541532bccb135a190d3049
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2007-01-12 10:20:32 +0000

    Issue 4377: Make sure to show menu ui also when menu is empty.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@7464 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/servlet/tags/MenuTag.java
+++ server/src/com/imcode/imcms/servlet/tags/MenuTag.java
@@ -1,14 +1,16 @@
 package com.imcode.imcms.servlet.tags;
 
+import com.imcode.imcms.api.ContentManagementSystem;
+import com.imcode.imcms.api.TextDocument;
 import imcode.server.document.textdocument.MenuDomainObject;
 import imcode.server.document.textdocument.MenuItemDomainObject;
 import imcode.server.document.textdocument.TextDocumentDomainObject;
+import imcode.server.parser.MenuParser;
 import imcode.server.parser.ParserParameters;
 import imcode.server.parser.TagParser;
-import imcode.server.parser.MenuParser;
 import org.apache.commons.collections.iterators.ArrayIterator;
-import org.apache.commons.lang.UnhandledException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspException;
@@ -16,9 +18,6 @@ import javax.servlet.jsp.tagext.BodyTagSupport;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Properties;
-
-import com.imcode.imcms.api.TextDocument;
-import com.imcode.imcms.api.ContentManagementSystem;
 
 public class MenuTag extends BodyTagSupport {
 
@@ -42,13 +41,12 @@ public class MenuTag extends BodyTagSupport {
             nextMenuItem();
             return EVAL_BODY_BUFFERED;
         } else {
-            menuItemIterator = null ;
             return SKIP_BODY;
         }
     }
 
     public boolean nextMenuItem() {
-        if (null != menuItemIterator && menuItemIterator.hasNext()) {
+        if (menuItemIterator.hasNext()) {
             menuItem = menuItemIterator.next();
             pageContext.setAttribute("menuitem", new TextDocument.MenuItem(menuItem, ContentManagementSystem.fromRequest(pageContext.getRequest()))) ;
             return true ;
@@ -59,7 +57,7 @@ public class MenuTag extends BodyTagSupport {
     }
 
     public int doAfterBody() throws JspException {
-        if ( null != menuItemIterator && menuItemIterator.hasNext() ) {
+        if ( menuItemIterator.hasNext() ) {
             nextMenuItem();
             return EVAL_BODY_AGAIN;
         } else {
@@ -69,18 +67,20 @@ public class MenuTag extends BodyTagSupport {
 
     public int doEndTag() throws JspException {
         try {
-            if ( null != menuItemIterator ) {
-                HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
-                HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
-                ParserParameters parserParameters = ParserParameters.fromRequest(request);
-                String bodyContentString = getBodyContent().getString();
-                bodyContentString = MenuParser.addMenuAdmin(no,
-                                                            parserParameters.isMenuMode(),
-                                                            bodyContentString, menu, request,response,label);
-                bodyContentString = TagParser.addPreAndPost(attributes, bodyContentString);
-                pageContext.getOut().write(bodyContentString);
-            }
-        } catch ( Exception e ) {
+            String bodyContentString = null != bodyContent ? bodyContent.getString() : "";
+            HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+            HttpServletResponse response = (HttpServletResponse) pageContext.getResponse();
+            ParserParameters parserParameters = ParserParameters.fromRequest(request);
+            bodyContentString = MenuParser.addMenuAdmin(no,
+                                                        parserParameters.isMenuMode(),
+                                                        bodyContentString, menu, request,response,label);
+            bodyContentString = TagParser.addPreAndPost(attributes, bodyContentString);
+            pageContext.getOut().write(bodyContentString);
+        } catch ( IOException e ) {
+            throw new JspException(e);
+        } catch ( ServletException e ) {
+            throw new JspException(e);
+        } catch ( RuntimeException e ) {
             throw new JspException(e);
         }
         return EVAL_PAGE;
@@ -97,7 +97,7 @@ public class MenuTag extends BodyTagSupport {
     public void setMode(String mode) {
         attributes.setProperty("mode", mode) ;
     }
-    
+
     public void setLabel(String label) {
         this.label = label;
     }
