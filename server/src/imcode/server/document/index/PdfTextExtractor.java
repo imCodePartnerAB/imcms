e20ea7f46f1796d514d166bab588df34b985c293
kreiger <kreiger@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2007-06-20 10:02:27 +0000

    Issue 6132: Properly close PDF document after use.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@8634 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/imcode/server/document/index/PdfTextExtractor.java
+++ server/src/imcode/server/document/index/PdfTextExtractor.java
@@ -2,17 +2,20 @@ package imcode.server.document.index;
 
 import org.pdfbox.pdmodel.PDDocument;
 import org.pdfbox.util.PDFTextStripper;
-import org.apache.log4j.Logger;
 
-import java.io.InputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 class PdfTextExtractor implements StreamTextsExtractor {
 
     public String[] extractTexts(InputStream in) throws IOException {
         PDDocument pdf = PDDocument.load(in);
-        PDFTextStripper stripper = new PDFTextStripper();
-        String text = stripper.getText(pdf) ;
-        return new String[] {text} ;
+        try {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(pdf) ;
+            return new String[] {text} ;
+        } finally {
+            pdf.close();
+        }
     }
 }
