8152b5870cc8b770e20a08719cb91c0780b5ef74
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2009-09-18 09:03:58 +0000

    Issue 9341: Added dummy methods to SingleConnectionDataSource requred by java 1.5/1.6 API.
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@16842 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/db/SingleConnectionDataSource.java
+++ server/src/com/imcode/imcms/db/SingleConnectionDataSource.java
@@ -3,9 +3,12 @@ package com.imcode.imcms.db;
 import com.imcode.db.jdbc.ConnectionWrapper;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.lang.NotImplementedException;
 
 public class SingleConnectionDataSource implements DataSource {
 
@@ -20,7 +23,61 @@ public class SingleConnectionDataSource implements DataSource {
             public void close() throws SQLException {
 
             }
+
+            public <T> T unwrap(Class<T> iface) throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public boolean isWrapperFor(Class<?> iface) throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public Clob createClob() throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public Blob createBlob() throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public NClob createNClob() throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public SQLXML createSQLXML() throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public boolean isValid(int timeout) throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public void setClientInfo(String name, String value) throws SQLClientInfoException {
+                throw new NotImplementedException();
+            }
+
+            public void setClientInfo(Properties properties) throws SQLClientInfoException {
+                throw new NotImplementedException();
+            }
+
+            public String getClientInfo(String name) throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public Properties getClientInfo() throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public Array createArrayOf(String typeName, Object[] elements) throws SQLException {
+                throw new NotImplementedException();
+            }
+
+            public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
+                throw new NotImplementedException();
+            }
         };
+
+
     }
 
     public Connection getConnection(String username, String password) throws SQLException {
@@ -39,5 +96,13 @@ public class SingleConnectionDataSource implements DataSource {
 
     public int getLoginTimeout() throws SQLException {
         return 0;
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        throw new NotImplementedException();
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        throw new NotImplementedException();
     }
 }
