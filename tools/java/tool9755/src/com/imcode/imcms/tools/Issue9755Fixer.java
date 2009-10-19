0d036a6131100a47d7051e561b25b94ff66b59a3
dmikha <dmikha@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2009-10-19 07:23:49 +0000

    Issue 9755 - 14h
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@17180 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ tools/java/tool9755/src/com/imcode/imcms/tools/Issue9755Fixer.java
@@ -0,0 +1,150 @@
+package com.imcode.imcms.tools;
+
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.*;
+
+public class Issue9755Fixer {
+
+    private static final String NEW_LINE = System.getProperty("line.separator");
+
+    private Pattern localizationPattern = Pattern.compile("<\\?(.+?)\\?>",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+    private Pattern pagePattern = Pattern.compile("<%@.*page.+?%>",
+            Pattern.CASE_INSENSITIVE | Pattern.MULTILINE);
+
+    private String source;
+    private String destination;
+
+    private static final String[] structureToBeChecked = {
+        "web/imcms/lang/",
+        "web/WEB-INF/templates/lang/admin/",
+    };
+
+    public Issue9755Fixer (String source, String destination){
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public void initialize() throws IllegalArgumentException {
+        for(String path : structureToBeChecked) {
+            File f = new File(source, path);
+            if (!f.exists()) {
+                throw new IllegalArgumentException(
+                    String.format ("The required path '%s' not found. Please, specify the correct source directory.",
+                            f.getAbsolutePath()));
+            }
+        }
+    }
+
+    private void listFilesRecursively(File directory, FileFilter filter, List<File> result) {
+        File[] jspFiles = directory.listFiles(filter);
+        result.addAll(Arrays.asList(jspFiles));
+
+        File[] dirs = directory.listFiles(new FileFilter() {
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();  
+            }
+        });
+        for (File d: dirs)
+            listFilesRecursively(d, filter, result);
+    }
+
+    private String getFileContent(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        StringBuffer buffer = new StringBuffer();
+        String line;
+        do {
+            line = reader.readLine();
+            buffer.append(line).append(NEW_LINE);
+        }
+        while (line != null);
+
+        reader.close();
+
+        return buffer.toString();
+    }
+
+    private void saveToDestination(String path, String postDirs, String filename, String content) throws IOException {
+        File target = new File(destination, path);
+        target = new File(target, postDirs);
+        target.mkdirs();
+        File targetFile = new File(target, filename);
+        FileWriter writer = new FileWriter(targetFile);
+        writer.append(content);
+        writer.close();
+    }
+
+    private void processListOfFiles(List<File> list, String root, boolean isJsp) throws IOException {
+        for (File file: list) {
+            String content = getFileContent(file);
+            Matcher m = localizationPattern.matcher(content);
+
+            StringBuffer contentBuffer = new StringBuffer(content);
+            while (m.find()) {
+                String key = m.group(1).trim();
+                String message = String.format("<fmt:message key=\"%s\"/>", key);
+                contentBuffer.replace(m.start(), m.end(), message);
+                m.reset(contentBuffer.toString());
+            }
+
+            m = pagePattern.matcher(contentBuffer.toString());
+            int pageEndIndex = 0;
+            while (m.find()) {
+                pageEndIndex = m.end();
+            }
+            
+            contentBuffer.insert(pageEndIndex,
+                    NEW_LINE + "<%@taglib prefix=\"fmt\" uri=\"http://java.sun.com/jsp/jstl/fmt\" %>" + NEW_LINE);
+
+
+            if (!isJsp) {
+                contentBuffer.insert(0,
+                        "<%@ page contentType=\"text/html; charset=UTF-8\"%>" + NEW_LINE +
+                        "<%@taglib prefix=\"fmt\" uri=\"http://java.sun.com/jsp/jstl/fmt\" %>" + NEW_LINE);
+            }
+
+            String absolutePath = file.getAbsolutePath().replace('\\', '/');
+            int index = absolutePath.indexOf(root);
+            String filename = absolutePath.substring(index + root.length());
+            String postDirs = filename.replace(file.getName(), "");
+            String fn = file.getName();
+            if (!isJsp) {
+                int dotIndex = fn.lastIndexOf(".");
+                fn = fn.substring(0, dotIndex) + ".jsp";
+            }
+            saveToDestination(root, postDirs, fn, contentBuffer.toString());
+        }
+    }
+
+    public void fixJspPages() throws IOException {
+         List<File> result = new ArrayList<File>();
+         listFilesRecursively(new File (source, structureToBeChecked[0]), new FileFilter() {
+            public boolean accept(File file) {
+                return !file.isDirectory() && file.getName().toLowerCase().endsWith(".jsp");
+            }
+        }, result);
+
+        
+        processListOfFiles(result, structureToBeChecked[0], true);
+    }
+
+    public void fixHtmlTemplates() throws IOException {
+        List<File> result = new ArrayList<File>();
+        listFilesRecursively(new File (source, structureToBeChecked[1]), new FileFilter() {
+            public boolean accept(File file) {
+                String filename = file.getName().toLowerCase();
+                return !file.isDirectory() && (filename.endsWith(".html") ||
+                        filename.endsWith(".htm") || filename.endsWith(".txt"));
+            }
+        }, result);
+
+        processListOfFiles(result, structureToBeChecked[1], false);
+    }
+
+    public void fixReferences() {
+
+    }
+}
---
+++ tools/java/tool9755/src/com/imcode/imcms/tools/Tool9755Dialog.java
@@ -0,0 +1,121 @@
+package com.imcode.imcms.tools;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Tool9755Dialog extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonRun;
+    private JButton buttonClose;
+    private JTextField sourceField;
+    private JButton browseSourceButton;
+    private JTextField destinationField;
+    private JButton browseDestinationButton;
+    private JLabel statusLabel;
+    private JFileChooser directoryChooser;
+
+    public Tool9755Dialog() {
+        setTitle("Eliminate build time I18n templates");
+        setPreferredSize(new Dimension(500, 200));
+        setResizable(false);
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonRun);
+
+        buttonRun.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onRun();
+            }
+        });
+
+        buttonClose.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+        browseSourceButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                directoryChooser = new JFileChooser();
+                directoryChooser.setDialogTitle("Select source directory");
+                directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                if (directoryChooser.showDialog(Tool9755Dialog.this, "Select") == JFileChooser.APPROVE_OPTION)
+                {
+                    sourceField.setText(directoryChooser.getSelectedFile().getAbsolutePath());
+                }
+            }
+        });
+
+        browseDestinationButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                directoryChooser = new JFileChooser();
+                directoryChooser.setDialogTitle("Select destination directory");
+                directoryChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                if (directoryChooser.showDialog(Tool9755Dialog.this, "Select") == JFileChooser.APPROVE_OPTION)
+                {
+                    destinationField.setText(directoryChooser.getSelectedFile().getAbsolutePath());
+                }
+            }
+        });
+    }
+
+    private void onRun() {
+        try {
+            String source = sourceField.getText().trim();
+            String destination = destinationField.getText().trim();
+            boolean valid = true;
+            if (source == null || source.length() == 0) {
+                valid = false;
+                JOptionPane.showMessageDialog(this, "The source directory must be selected",
+                       "Validation error", JOptionPane.ERROR_MESSAGE);
+            }
+            else if (destination == null || destination.length() == 0) {
+                valid = false;
+                JOptionPane.showMessageDialog(this, "The destination directory must be selected",
+                       "Validation error", JOptionPane.ERROR_MESSAGE);
+            }
+
+            if (valid) {
+                statusLabel.setText("Migration has beed started");
+                setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+                Issue9755Fixer fixer = new Issue9755Fixer(source, destination);
+                fixer.initialize();
+                fixer.fixJspPages();
+                statusLabel.setText("Migration done!");
+//                fixer.fixHtmlTemplates();
+//                fixer.fixReferences();
+                setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+            }
+        }
+        catch(Throwable t){
+            JOptionPane.showMessageDialog(this, t.getMessage(),
+                       "Aplication error", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+    private void onCancel() {
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        Tool9755Dialog dialog = new Tool9755Dialog();
+        dialog.pack();
+        dialog.setVisible(true);
+        System.exit(0);
+    }
+}
