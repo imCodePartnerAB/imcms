5aa9c4dca04543d87fa2e286b1097e2770c3ac06
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2009-12-23 07:24:39 +0000

    Issue 9729: Deleted unused
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@18092 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/clojure/com/imcode/imcms/backdoor/controller.clj
+++
@@ -1,9 +0,0 @@
-(ns com.imcode.imcms.backdoor.controller
-  (:require [com.imcode.imcms.backdoor.cms :as cms]))
-
-(def index-page "/WEB-INF/backdoor/index.jsp")
-
-(defn handle [request response]
-  (-> request (.getRequestDispatcher ,, index-page) (.forward ,, request response)))
-
-
