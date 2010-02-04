8e9397e22f9e03ffe728a7e45f62b4e0934c3e0c
dmikha <dmikha@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2010-02-04 08:20:40 +0000

    Issue 10263, part 2 - (0,5h)
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@18475 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/servlet/XmlDoc.java
+++ server/src/com/imcode/imcms/servlet/XmlDoc.java
@@ -6,34 +6,53 @@ import imcode.server.document.DocumentDomainObject;
 import imcode.server.document.XmlDocumentBuilder;
 import imcode.util.Utility;
 import org.w3c.dom.Document;
+import org.apache.commons.lang.math.IntRange;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Iterator;
 
 import com.imcode.imcms.mapping.DocumentMapper;
 
 public class XmlDoc extends HttpServlet {
 
     public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-        int documentId = Integer.parseInt( request.getParameter( "meta_id" ) );
+        String metaId = request.getParameter( "meta_id" );
+        int index = metaId.indexOf('-');
+        IntRange idRange;
+        if (index > 0) {
+            String startMetaId = metaId.substring(0, index);
+            String endMetaId = metaId.substring(index + 1);
+            idRange = new IntRange(Integer.parseInt(startMetaId), Integer.parseInt(endMetaId));
+        }
+        else {
+            int documentId = Integer.parseInt( metaId );
+            idRange = new IntRange(documentId, documentId);     
+        }
+
 
         DocumentMapper documentMapper = Imcms.getServices().getDocumentMapper();
-        DocumentDomainObject document = documentMapper.getDocument( documentId );
+        Iterator documentsIterator = documentMapper.getDocumentsIterator(idRange);
         UserDomainObject currentUser = Utility.getLoggedOnUser(request);
-
-        if ( null == document ) {
-            response.sendError( HttpServletResponse.SC_NOT_FOUND ) ;
-        } else if (!currentUser.canAccess(document) || !document.isPublished() && !currentUser.canEdit(document) ) {
-            response.sendError( HttpServletResponse.SC_FORBIDDEN ) ;
-        } else {
-            XmlDocumentBuilder xmlDocumentBuilder = new XmlDocumentBuilder(currentUser);
-            xmlDocumentBuilder.addDocument( document);
-            Document xmlDocument = xmlDocumentBuilder.getXmlDocument() ;
-            Utility.outputXmlDocument( response, xmlDocument );
+        XmlDocumentBuilder xmlDocumentBuilder = new XmlDocumentBuilder(currentUser);
+        
+        while (documentsIterator.hasNext()) {
+            DocumentDomainObject document = (DocumentDomainObject)documentsIterator.next();
+            if ( null == document ) {
+                continue;
+            } else if (!currentUser.canAccess(document) || !document.isPublished() && !currentUser.canEdit(document) ) {
+                continue;
+            }
+            else {
+                xmlDocumentBuilder.addDocument( document);
+            }
         }
+
+        Document xmlDocument = xmlDocumentBuilder.getXmlDocument() ;
+        Utility.outputXmlDocument( response, xmlDocument );
     }
 
 }
