8b27919d63550da049b83d5d7df27d478c517416
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2010-06-29 16:48:10 +0000

    Issue 19614: Added build checkstyle and javadoc configuration.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@19965 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/resources/conf.clj
+++ src/main/resources/conf.clj
@@ -1,5 +1,5 @@
 ;;; Configuration file - consists of a single clojure record
-;;; Parameters such as base.dir, etc., must be defined ONLY at the first level ot the conf.
+;;; Parameters such as base.dir, etc., must be defined ONLY at the top level ot the conf.
 {
     ;; Application basedir
     ;; Replaced with a real basedir after this configuration has been read
@@ -11,12 +11,13 @@
     ;; Database configuration
     :db {      
         ;; Required database version
+        ;; If database version is lower than required it will be updated using diffs (see below).
         :version 6.2
 
         ;; Database init.
         ;; A db-init is a record of the following fields:
         ;;   :version - db version which is set after database is initialized.
-        ;;   :scripts - a vector of SQL scripts locations relative to the 'db.scripts.dir'.
+        ;;   :scripts - a vector of SQL scripts locations.
         :init {
             :version 4.11
             :scripts ["${db.scripts.dir}/imcms_rb4.sql"]
@@ -25,8 +26,8 @@
         ;; Database diffs - a set of diffs.
         ;; A diff is a record of the following fields:
         ;;   :from - db version to upgrade from
-        ;;   :to - db version to upgrade
-        ;;   :scripts - a vector of SQL scripts locations relative to the 'db.scripts.dir'.
+        ;;   :to - db version after upgrade
+        ;;   :scripts - a vector of SQL scripts locations.
         :diffs #{
             {
                 :from 4.11
