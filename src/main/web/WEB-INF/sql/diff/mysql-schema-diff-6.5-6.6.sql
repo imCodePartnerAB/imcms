77a61764de4a5525d2cacc34d08a84f7a8fe8675
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2010-11-15 11:36:44 +0000

    Issue 19614:  Added statements to sql upgrade scripts wich changes foreign keys.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@21270 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/web/WEB-INF/sql/diff/mysql-schema-diff-6.5-6.6.sql
+++ src/main/web/WEB-INF/sql/diff/mysql-schema-diff-6.5-6.6.sql
@@ -1,4 +1,5 @@
--- Adds last modified and modified by columns into the document version table
+-- Adds NOT NULL constraints to imcms_doc_versions created_XX, modified_XX.
+-- Replaces FK constraints to users table (removed 'ON DELETE SET NULL').
 
 SET @schema_version__major_new = 6;
 SET @schema_version__minor_new = 6;
@@ -21,8 +22,28 @@ ALTER TABLE imcms_doc_versions
    ADD CONSTRAINT `fk__imcms_doc_versions__user1` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`),
    ADD CONSTRAINT `fk__imcms_doc_versions__user2` FOREIGN KEY (`modified_by`) REFERENCES `users` (`user_id`);
 
+
+ALTER TABLE imcms_text_doc_texts_history
+    DROP FOREIGN KEY fk__imcms_text_doc_texts_history__users;
+
+ALTER TABLE imcms_text_doc_texts_history
+    MODIFY user_id int NOT NULL;
+
+ALTER TABLE imcms_text_doc_texts_history
+    ADD CONSTRAINT fk__imcms_text_doc_texts_history__users FOREIGN KEY (user_id) REFERENCES users (user_id);
+
+
+ALTER TABLE imcms_text_doc_images_history
+    DROP FOREIGN KEY fk__imcms_text_doc_images_history__users;
+
+ALTER TABLE imcms_text_doc_images_history
+    MODIFY user_id int NOT NULL;
+
+ALTER TABLE imcms_text_doc_images_history
+    ADD CONSTRAINT fk__imcms_text_doc_images_history__users FOREIGN KEY (user_id) REFERENCES users (user_id);
+
 --
--- Update schema version
+-- Update schema version                                                    û
 --
 UPDATE database_version
 SET
