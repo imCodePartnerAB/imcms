1a650f96c3a78a770ab957c90a303ba375db23c9
aleval <aleval@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2011-05-25 16:04:15 +0000

    Issue 11350: Added diff with new permissions' column for category and library roles.
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@23012 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ src/main/web/WEB-INF/sql/diff/mysql-schema-diff-6.8-6.9.sql
@@ -0,0 +1,19 @@
+SET @schema_version__major_new = 6;
+SET @schema_version__minor_new = 9;
+
+ALTER TABLE archive_category_roles ADD COLUMN canUse tinyint(1) NOT NULL;
+ALTER TABLE archive_category_roles ADD COLUMN canChange tinyint(1) NOT NULL;
+
+ALTER TABLE archive_library_roles ADD COLUMN canUse tinyint(1) NOT NULL;
+ALTER TABLE archive_library_roles ADD COLUMN canChange tinyint(1) NOT NULL;
+
+--
+-- Update schema version
+--
+UPDATE database_version
+SET
+  major = @schema_version__major_new,
+  minor = @schema_version__minor_new;
+
+
+
