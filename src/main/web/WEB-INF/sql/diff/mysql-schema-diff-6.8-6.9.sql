5a76bee60002df2e82fc729d195047f426a25e1a
aleval <aleval@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2011-05-26 07:43:39 +0000

    Issue 11350: Added missing schema column, fixed version in schema.xml
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@23015 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/web/WEB-INF/sql/diff/mysql-schema-diff-6.8-6.9.sql
+++ src/main/web/WEB-INF/sql/diff/mysql-schema-diff-6.8-6.9.sql
@@ -7,6 +7,8 @@ ALTER TABLE archive_category_roles ADD COLUMN canChange tinyint(1) NOT NULL;
 ALTER TABLE archive_library_roles ADD COLUMN canUse tinyint(1) NOT NULL;
 ALTER TABLE archive_library_roles ADD COLUMN canChange tinyint(1) NOT NULL;
 
+ALTER TABLE archive_images ADD COLUMN alt_text varchar(125);
+
 --
 -- Update schema version
 --
