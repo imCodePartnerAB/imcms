8091bdbf8a53aba4fa6ef061b894de0a07398d87
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2011-12-13 16:59:28 +0000

    Issue 11292: Updated project dependencies, fixed incompatible code
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@24053 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/java/com/imcode/imcms/addon/imagearchive/util/SQLServerDialect.java
+++ src/main/java/com/imcode/imcms/addon/imagearchive/util/SQLServerDialect.java
@@ -10,29 +10,29 @@ import org.hibernate.engine.SessionFactoryImplementor;
 import org.hibernate.type.Type;
 
 public class SQLServerDialect extends org.hibernate.dialect.SQLServerDialect {
-	public SQLServerDialect() {
-		registerFunction("current_date", new CurrentDateSQLFunction());
-	}
-	
-	private class CurrentDateSQLFunction implements SQLFunction {
-		public Type getReturnType(Type columnType, Mapping mapping) throws QueryException {
-			return Hibernate.DATE;
-		}
+    public SQLServerDialect() {
+        registerFunction("current_date", new CurrentDateSQLFunction());
+    }
 
-		public boolean hasArguments() {
-			return false;
-		}
+    private class CurrentDateSQLFunction implements SQLFunction {
+        public Type getReturnType(Type columnType, Mapping mapping) throws QueryException {
+            return org.hibernate.type.StandardBasicTypes.DATE;
+        }
 
-		public boolean hasParenthesesIfNoArguments() {
-			return true;
-		}
+        public boolean hasArguments() {
+            return false;
+        }
 
-		@SuppressWarnings("unchecked")
-        public String render(List args, SessionFactoryImplementor factory) throws QueryException {
-			// truncate current datetime to date part, 
-			// see this http://www.karaszi.com/SQLServer/info_datetime.asp
-			
-			return "DATEADD(DAY, DATEDIFF(DAY, '20080225', getdate()), '20080225')";
-		}
-	}
+        public boolean hasParenthesesIfNoArguments() {
+            return true;
+        }
+
+        @SuppressWarnings("unchecked")
+        public String render(Type firstArgumentType, List arguments, SessionFactoryImplementor factory) throws QueryException {
+            // truncate current datetime to date part,
+            // see this http://www.karaszi.com/SQLServer/info_datetime.asp
+
+            return "DATEADD(DAY, DATEDIFF(DAY, '20080225', getdate()), '20080225')";
+        }
+    }
 }
