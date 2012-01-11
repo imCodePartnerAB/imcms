42a080e40a74e37fb6ea71f0464cb3c6cfcfcd2f
aleval <aleval@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-01-11 19:59:15 +0000

    Issue 11875: More documented code. There's a method and I'm not sure how/where to use it, tried in a template as well as in imcms menu edit view, got null in the first case, couldn't obtain textdocumentviewing in the latter. The method is not used anywhere in imcms, and I can't think of a way it being helpful to a developer using public API. /**      * @return The index of the menu currently being edited, or null if none.      */     public Integer getEditedMenuIndex() {         return parserParameters.getEditingMenuIndex();     }
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@24206 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/api/TextDocumentViewing.java
+++ server/src/com/imcode/imcms/api/TextDocumentViewing.java
@@ -6,6 +6,8 @@ import imcode.server.parser.ParserParameters;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * Represents currently viewed TextDocument. Provides information if the current text document is in text, image, menu etc
+ * edit mode.
  * An instance of this class is fetchable in JSPs included in text documents (with &lt;?imcms:include path="..."?&gt; or
  * jsp:include) via {@link #fromRequest(javax.servlet.http.HttpServletRequest)},
  * and in velocity code in text templates as <code>$viewing</code>.
@@ -24,34 +26,67 @@ public class TextDocumentViewing {
         textDocument = new TextDocument( (TextDocumentDomainObject)parserParameters.getDocumentRequest().getDocument(), ContentManagementSystem.fromRequest( parserParameters.getDocumentRequest().getHttpServletRequest() ) );
     }
 
+    /**
+     * Returns TextDocumentViewing from the given request
+     * @param request HttpServletRequest to get TextDocumentViewing from
+     * @return TextDocumentViewing or null, if the request doesn't have any set
+     */
     public static TextDocumentViewing fromRequest( HttpServletRequest request ) {
         return (TextDocumentViewing)request.getAttribute( REQUEST_ATTRIBUTE__VIEWING );
     }
 
+    /**
+     * Returns currently viewed TextDocument
+     * @return currently viewed TextDocument
+     */
     public TextDocument getTextDocument() {
         return textDocument;
     }
 
+    /**
+     * Tests if current TextDocument is in edit mode(be it text, images, menus etc)
+     * @return true if current TextDocument is being edited, false otherwise
+     */
     public boolean isEditing() {
         return parserParameters.isAnyMode() ;
     }
 
+    /**
+     * Tests if current TextDocument is in text edit mode
+     * @return true if current TextDocument is in text edit mode, false otherwise
+     */
     public boolean isEditingTexts() {
         return parserParameters.isTextMode();
     }
 
+    /**
+     * Tests if current TextDocument is in image edit mode
+     * @return true if current TextDocument is in image edit mode, false otherwise
+     */
     public boolean isEditingImages() {
         return parserParameters.isImageMode();
     }
 
+    /**
+     * Tests if current TextDocument is in menu edit mode
+     * @return true if current TextDocument is in menu edit mode, false otherwise
+     */
     public boolean isEditingMenus() {
         return parserParameters.isMenuMode();
     }
 
+    /**
+     * Tests if current TextDocument is in include edit mode
+     * @return true if current TextDocument is in include edit mode, false otherwise
+     */
     public boolean isEditingIncludes() {
         return parserParameters.isIncludesMode();
     }
 
+    /**
+     * Tests if current TextDocument is in template edit mode
+     * @return true if current TextDocument is in template edit mode, false otherwise
+     */
     public boolean isEditingTemplate() {
         return parserParameters.isTemplateMode();
     }
@@ -63,6 +98,11 @@ public class TextDocumentViewing {
         return parserParameters.getEditingMenuIndex();
     }
 
+    /**
+     * Puts the given TextDocumentViewing object into request attribute and requests the one previously set.
+     * @param viewing TextDocumentViewing to put into request attribute.
+     * @return TextDocumentViewing that was previously in request attribute or null if none was before.
+     */
     public static TextDocumentViewing putInRequest( TextDocumentViewing viewing ) {
         HttpServletRequest httpServletRequest = viewing.parserParameters.getDocumentRequest().getHttpServletRequest();
         TextDocumentViewing previousViewing = fromRequest( httpServletRequest );
