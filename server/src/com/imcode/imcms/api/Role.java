d0d172230473645fd724c293a2c91bb11775d0c3
aleval <aleval@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-01-13 19:15:26 +0000

    Issue 11875: More documented code.
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@24232 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- server/src/com/imcode/imcms/api/Role.java
+++ server/src/com/imcode/imcms/api/Role.java
@@ -4,12 +4,24 @@ import imcode.server.user.RoleDomainObject;
 import imcode.server.user.RoleId;
 
 /**
+ * Represent a role in imcms.
  * @since 2.0
  */
 public class Role implements Comparable {
 
+    /**
+     * Super admin role id
+     */
     public static final int SUPERADMIN_ID = RoleId.SUPERADMIN_ID ;
+
+    /**
+     * User admin role id
+     */
     public static final int USERADMIN_ID = RoleId.USERADMIN_ID ;
+
+    /**
+     * Default user role id.
+     */
     public static final int USERS_ID = RoleId.USERS_ID ;
 
     private final RoleDomainObject internalRole ;
@@ -22,26 +34,52 @@ public class Role implements Comparable {
         return internalRole;
     }
 
+    /**
+     * Returns the id of this role
+     * @return id of this role
+     */
     public int getId() {
         return internalRole.getId().intValue();
     }
 
+    /**
+     * Returns the name of this role
+     * @return name of this role
+     */
     public String getName() {
         return internalRole.getName();
     }
 
+    /**
+     * Sets name of this role.
+     * @param name new name of this role
+     */
     public void setName( String name ) {
         internalRole.setName( name );
     }
 
+    /**
+     * Tests this role for equality with the given one.
+     * Two roles are the same if their ids are the same.
+     * @param o a {@link Role} to test with
+     * @return true if equal, false otherwise
+     */
     public boolean equals( Object o ) {
         return internalRole.equals( ((Role)o).internalRole );
     }
 
+    /**
+     * Returns hash code of this role
+     * @return hash code
+     */
     public int hashCode() {
         return internalRole.hashCode();
     }
 
+    /**
+     * Calls {@link com.imcode.imcms.api.Role#getName()}
+     * @return the String form of this role
+     */
     public String toString() {
         return getName() ;
     }
@@ -58,10 +96,20 @@ public class Role implements Comparable {
         }
     }
 
+    /**
+     * Tests whether this role owners have permission to get their passwords by email
+     * @return true if are allowed, false otherwise
+     */
     public boolean hasPasswordMailPermission() {
         return internalRole.hasPermission( RoleDomainObject.PASSWORD_MAIL_PERMISSION ) ;
     }
 
+    /**
+     * Compares two roles by name, the comparison is case insensitive.
+     * @param o another role to compare with
+     * @return 0 if equal lexicographically, less than 0 if this Role is lesser lexicographically than the argument,
+     * more than 0 if greater than the argument.
+     */
     public int compareTo( Object o ) {
         return internalRole.compareTo( ((Role)o).internalRole ) ;
     }
--- server/src/com/imcode/imcms/api/Template.java
+++ server/src/com/imcode/imcms/api/Template.java
@@ -2,14 +2,25 @@ package com.imcode.imcms.api;
 
 import imcode.server.document.TemplateDomainObject;
 
+/**
+ * Represents a template in imcms
+ */
 public class Template {
 
     private final TemplateDomainObject internalTemplate;
 
+    /**
+     * Constructs Template with the given TemplateDomainObject backing it.
+     * @param internalTemplate TemplateDomainObject to be used internally.
+     */
     public Template(TemplateDomainObject internalTemplate) {
         this.internalTemplate = internalTemplate;
     }
 
+    /**
+     * Returns the name of this template
+     * @return name of this template
+     */
     public String getName() {
         return internalTemplate.getName();
     }
@@ -19,6 +30,11 @@ public class Template {
         return 0;
     }
 
+    /**
+     * Compares two templates, uses {@link com.imcode.imcms.api.Template#getName()} as a criteria of equality.
+     * @param o template to compare with
+     * @return true if both templates are the same
+     */
     public boolean equals(Object o) {
         if ( this == o ) {
             return true;
@@ -33,6 +49,10 @@ public class Template {
 
     }
 
+    /**
+     * Hash code of this Template, calls hashCode() of Template's name.
+     * @return
+     */
     public int hashCode() {
         return internalTemplate.hashCode() ;
     }
@@ -41,6 +61,10 @@ public class Template {
         return internalTemplate;
     }
 
+    /**
+     * String representation of this template in the form of a name
+     * @return same as {@link com.imcode.imcms.api.Template#getName()}
+     */
     public String toString() {
         return getName();
     }
