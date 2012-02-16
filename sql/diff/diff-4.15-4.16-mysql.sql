2912e5f22793aded5270295808f25823eb0d9958
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-02-16 07:49:05 +0000

    Issue 11822: Added sql script
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@24431 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ sql/diff/diff-4.15-4.16-mysql.sql
@@ -0,0 +1,9 @@
+ALTER TABLE users
+  MODIFY COLUMN login_password VARCHAR(128) NOT NULL,
+  ADD COLUMN login_password_is_encrypted BOOLEAN DEFAULT FALSE,
+  ADD COLUMN login_password_reset_id VARCHAR(36),
+  ADD COLUMN login_password_reset_ts BIGINT,
+  ADD UNIQUE INDEX ux__users__login_password_reset_ts (login_password_reset_id);
+
+INSERT INTO sys_types (type_id, name) VALUES (9, "UserLoginPasswordResetExpirationInterval");
+INSERT INTO sys_data (sys_id, type_id, value) VALUES (9, 9, "24");
