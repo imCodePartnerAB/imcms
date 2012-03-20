035f76092766671edb0fdb682878c928f89dd463
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-03-20 14:09:35 +0000

    Issue 12019: Fully implemented password reset
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@24731 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/scala/com/imcode/imcms/db/db.scala
+++ src/main/scala/com/imcode/imcms/db/db.scala
@@ -14,17 +14,14 @@ class DB(ds: DataSource) extends Logger {
   
   val template = new JdbcTemplate(ds)
 
-  def tables = template.query("SHOW TABLES", new RowMapper[String] {
+  def tables() = template.query("SHOW TABLES", new RowMapper[String] {
     def mapRow(rs: ResultSet, rowNum: Int) = rs getString 1
   }).toList
 
-
-  def isEmpty = tables.isEmpty
-  
+  def isEmpty() = tables().isEmpty
 
   def version(): Version = template.queryForObject("""SELECT concat(major, '.', minor) FROM database_version""",
                                                    classOf[String])
-
 
   def updateVersion(newVersion: Version): Unit = synchronized {
     logger.info("Updating database version from %s to %s.".format(version(), newVersion))
@@ -32,16 +29,48 @@ class DB(ds: DataSource) extends Logger {
                                                                     Int box newVersion.minor)
   }
 
-
-  /**
-   * Throws an RuntimeException if database version is greater than required.
-   */
   def prepare(schema: Schema): Version = synchronized {
     def scriptFullPath(script: String) = "%s/%s".format(schema.scriptsDir, script)
 
+    def update(): Either[String, Version] = {
+      version() match {
+        case schema.version =>
+          logger.info("Database is up-to-date.");
+          Right(schema.version)
+
+        case dbVersion if dbVersion < schema.version =>
+          logger.info("Database have to be updated. Required version: %s, database version: %s."
+                      .format(schema.version, dbVersion))
+
+          schema diffsChain dbVersion match {
+            case Nil =>
+              Left("No diff is available for version %s." format dbVersion)
+
+            case diffsChain =>
+              for (diff <- diffsChain) {
+                logger.info("The following diff will be applied: %s." format diff)
+
+                runScripts(diff.scripts map scriptFullPath)
+                updateVersion(diff.to)
+              }
+
+              val updatedDbVersion = version()
+              logger.info("Database has been updated. Database version: %s." format updatedDbVersion)
+              Right(updatedDbVersion)
+          }
+
+        case unexpectedDbVersion =>
+          Left("Unexpected database version. Database version: %s is greater than required version: %s."
+                         .format(unexpectedDbVersion, schema.version))
+      }
+    } // update
+
     logger.info("Preparing databse.")
 
-    if (isEmpty) {
+                  // logger.error(errorMsg)
+                  // sys.error(errorMsg)
+
+    if (isEmpty()) {
       logger.info("Database is empty and need to be initialized.")
       logger.info("The following init will be applied: %s." format schema.init)
 
@@ -50,45 +79,17 @@ class DB(ds: DataSource) extends Logger {
 
       logger.info("Database has been initialized.")
     }
-    
-    version() match {
-      case schema.version =>
-        logger.info("Database is up-to-date.");
-        schema.version
-      
-      case dbVersion if dbVersion < schema.version =>
-        logger.info("Database have to be updated. Required version: %s, database version: %s."
-                    .format(schema.version, dbVersion))
 
-        schema diffsChain dbVersion match {
-          case Nil =>
-            val errorMsg = "No diff is available for version %s." format dbVersion
-            logger.error(errorMsg)
-            sys.error(errorMsg)
-
-          case diffsChain =>
-            for (diff <- diffsChain) {
-              logger.info("The following diff will be applied: %s." format diff)
-
-              runScripts(diff.scripts map scriptFullPath)
-              updateVersion(diff.to)
-            }
-
-            val updatedDbVersion = version()
-            logger.info("Database has been updated. Database version: %s." format updatedDbVersion)
-            updatedDbVersion
-        }
-
-      case unexpectedDbVersion =>
-        val errorMsg = "Unexpected database version. Database version: %s is greater than required version: %s."
-                       .format(unexpectedDbVersion, schema.version)
-
+    update() match {
+      case Left(errorMsg) =>
         logger.error(errorMsg)
         sys.error(errorMsg)
+
+      case Right(version) =>
+        version
     }
   }
 
-  
   def runScripts(scripts: Seq[String]): Unit = synchronized {
     template execute new ConnectionCallback[Unit] {
       def doInConnection(connection: Connection) {
