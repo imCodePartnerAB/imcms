e8df8f1944f5d1875b06a2784ebb01f1e15a6b3f
johlar <johlar@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-03-28 12:24:13 +0000

    issue 12028 fixed problem i t-sql
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@24795 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/diff/diff-4.8-4.9-mssql.sql
+++ sql/diff/diff-4.8-4.9-mssql.sql
@@ -3,17 +3,16 @@
 ALTER TABLE classification ALTER COLUMN code NVARCHAR(128);
 
 -- User's data columns
-ALTER TABLE users
-    ALTER COLUMN login_name     NVARCHAR(128),
-    ALTER COLUMN first_name     NVARCHAR(64),
-    ALTER COLUMN last_name      NVARCHAR(64),
-    ALTER COLUMN title          NVARCHAR(64),
-    ALTER COLUMN company        NVARCHAR(64),
-    ALTER COLUMN address        NVARCHAR(128),
-    ALTER COLUMN city           NVARCHAR(64),
-    ALTER COLUMN zip            NVARCHAR(64),
-    ALTER COLUMN country        NVARCHAR(64),
-    ALTER COLUMN county_council NVARCHAR(128),
-    ALTER COLUMN email          NVARCHAR(128);
+ALTER TABLE users ALTER COLUMN login_name     NVARCHAR(128);
+ALTER TABLE users ALTER COLUMN first_name     NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN last_name      NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN title          NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN company        NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN address        NVARCHAR(128);
+ALTER TABLE users ALTER COLUMN city           NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN zip            NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN country        NVARCHAR(64);
+ALTER TABLE users ALTER COLUMN county_council NVARCHAR(128);
+ALTER TABLE users ALTER COLUMN email          NVARCHAR(128);
 
 UPDATE database_version SET major = 4, minor = 9;
