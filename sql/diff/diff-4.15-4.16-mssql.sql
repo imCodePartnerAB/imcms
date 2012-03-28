f4f35e4f61bbc29d278ead3f643ad425d19fc238
johlar <johlar@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-03-28 13:51:33 +0000

    issue 12028 translated to t-sql
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@24796 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/diff/diff-4.15-4.16-mssql.sql
+++ sql/diff/diff-4.15-4.16-mssql.sql
@@ -1,15 +1,21 @@
--- todo: implement
--- UPDATE users SET email = concat('#', user_id) WHERE trim(email) = '';
+UPDATE users SET email = '#' + CONVERT(varchar(100),user_id) WHERE ltrim(rtrim(email)) = '';
 
--- ALTER TABLE users
---   MODIFY COLUMN login_password VARCHAR(128) NOT NULL,
---   ADD COLUMN login_password_is_encrypted BOOLEAN DEFAULT FALSE,
---   ADD COLUMN login_password_reset_id VARCHAR(36),
---   ADD COLUMN login_password_reset_ts BIGINT,
---   ADD UNIQUE INDEX ux__users__login_password_reset_id (login_password_reset_id),
---   ADD UNIQUE INDEX ux__users__email (email);
+ALTER TABLE users
+   ALTER COLUMN login_password VARCHAR(128) NOT NULL;
+ALTER TABLE users
+   ADD login_password_is_encrypted BIT DEFAULT 0;
 
--- INSERT INTO sys_types (type_id, name) VALUES (9, "UserLoginPasswordResetExpirationInterval");
--- INSERT INTO sys_data (sys_id, type_id, value) VALUES (9, 9, "24");
---
--- UPDATE database_version SET major = 4, minor = 16;
+ALTER TABLE users ADD login_password_reset_id VARCHAR(36) DEFAULT NEWID();
+GO
+UPDATE users SET login_password_reset_id = NEWID();
+GO
+CREATE UNIQUE NONCLUSTERED INDEX ux__users__login_password_reset_id ON users ( login_password_reset_id )
+ALTER TABLE users
+   ADD login_password_reset_ts BIGINT;
+
+CREATE UNIQUE NONCLUSTERED INDEX ux__users__email ON users ( email )
+
+INSERT INTO sys_types (type_id, name) VALUES (9, "UserLoginPasswordResetExpirationInterval");
+INSERT INTO sys_data (sys_id, type_id, value) VALUES (9, 9, "24");
+
+UPDATE database_version SET major = 4, minor = 16;
