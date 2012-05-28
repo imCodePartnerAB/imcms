625a7b8a6a9e1e750c21258de0693bc109da0860
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-05-28 13:42:33 +0000

    Issue 12028: Fixed MS-SQL diff
    git-svn-id: https://svn.imcode.com/imcode/imcms/branches/RB-4@25205 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- sql/diff/diff-4.15-4.16-mssql.sql
+++ sql/diff/diff-4.15-4.16-mssql.sql
@@ -1,7 +1,7 @@
 ALTER TABLE users
-   ALTER COLUMN login_password NVARCHAR(128) NOT NULL;
+   ALTER COLUMN login_password NVARCHAR(128) NOT NULL;
 ALTER TABLE users
-   ADD login_password_is_encrypted BIT DEFAULT 0;
+   ADD login_password_is_encrypted BIT DEFAULT 0;
 
 ALTER TABLE users ADD login_password_reset_id CHAR(36) DEFAULT NEWID();
 GO
@@ -9,9 +9,14 @@ UPDATE users SET login_password_reset_id = NEWID();
 GO
 CREATE UNIQUE NONCLUSTERED INDEX ux__users__login_password_reset_id ON users ( login_password_reset_id )
 ALTER TABLE users
-   ADD login_password_reset_ts BIGINT;
+   ADD login_password_reset_ts BIGINT;
 
-INSERT INTO sys_types (type_id, name) VALUES (9, "UserLoginPasswordResetExpirationInterval");
-INSERT INTO sys_data (sys_id, type_id, value) VALUES (9, 9, "24");
+SET IDENTITY_INSERT sys_types ON
+INSERT INTO sys_types (type_id, name) VALUES (9, 'UserLoginPasswordResetExpirationInterval');
+SET IDENTITY_INSERT sys_types OFF
+
+SET IDENTITY_INSERT sys_data ON
+INSERT INTO sys_data (sys_id, type_id, value) VALUES (9, 9, '24');
+SET IDENTITY_INSERT sys_data OFF
 
 UPDATE database_version SET major = 4, minor = 16;
