d78c31ebd3dfdc1f829137df69d6f8f1b32f0b78
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2012-07-13 02:09:36 +0000

    Issue 11278: Refactored
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@25511 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/scala/imcode/server/document/index/solr/DocumentIndexer.scala
+++ src/main/scala/imcode/server/document/index/solr/DocumentIndexer.scala
@@ -1,20 +1,23 @@
 package imcode.server.document.index.solr
 
 import com.imcode._
-//import java.text.DateFormat
 import scala.collection.JavaConverters._
 import imcode.server.document.DocumentDomainObject
 import imcode.server.document.RoleIdToDocumentPermissionSetTypeMappings
-
-import java.util.Date
-import imcode.util.DateConstants
 import org.apache.solr.common.SolrInputDocument
-import java.text.SimpleDateFormat
 import com.imcode.Log4jLoggerSupport
 import scala.reflect.BeanProperty
 import com.imcode.imcms.mapping.{CategoryMapper, DocumentMapper}
 import imcode.server.document.index.DocumentIndex
 
+/**
+ * Lucene & SOLr indexing differences:
+ * - properties - key - value not possible ???
+ * - text* dynamic field - add underscore | conflict with text | RE format ???
+ * - virtual field - phase
+ */
+// todo: ??? Truncate date fields to minute ???, check for null or insert default ("" - empty string) ???
+// todo: ??? null handling in SOLR ???
 class DocumentIndexer(
   @BeanProperty var documentMapper: DocumentMapper,
   @BeanProperty var categoryMapper: CategoryMapper,
@@ -24,16 +27,17 @@ class DocumentIndexer(
   def this() = this(null, null, null)
 
   /**
-   * Creates index document (SolrInputDocument) from doc.
+   * Creates SolrInputDocument based on provided document.
    *
-   * @return solr doc to be added into index.
+   * @return SolrInputDocument
    */
   def index(doc: DocumentDomainObject): SolrInputDocument = new SolrInputDocument |>> { indexDoc =>
+    def addFieldIfNotNull(name: String, value: AnyRef) = if (value != null) indexDoc.addField(name, value)
+
     val documentId = doc.getId
 
-    indexDoc.addField(DocumentIndex.FIELD__META_ID, documentId.toString)
-    // ??? WHY INDEX ???
-    indexDoc.addField(DocumentIndex.FIELD__META_ID_LEXICOGRAPHIC, documentId.toString)
+    indexDoc.addField(DocumentIndex.FIELD__META_ID, documentId)
+    indexDoc.addField(DocumentIndex.FIELD__META_ID_LEXICOGRAPHIC, documentId)
 
     doc.getI18nMeta |> { l =>
       val headline = l.getHeadline
@@ -44,97 +48,63 @@ class DocumentIndexer(
       indexDoc.addField(DocumentIndex.FIELD__META_TEXT, menuText)
     }
 
-    indexDoc.addField(DocumentIndex.FIELD__DOC_TYPE_ID, doc.getDocumentTypeId.toString)
-    indexDoc.addField(DocumentIndex.FIELD__CREATOR_ID, doc.getCreatorId.toString)
+    indexDoc.addField(DocumentIndex.FIELD__DOC_TYPE_ID, doc.getDocumentTypeId)
+    indexDoc.addField(DocumentIndex.FIELD__CREATOR_ID, doc.getCreatorId)
 
-    for (publisherId <- Option(doc.getPublisherId)) {
-        indexDoc.addField(DocumentIndex.FIELD__PUBLISHER_ID, publisherId.toString)
-    }
+    addFieldIfNotNull(DocumentIndex.FIELD__PUBLISHER_ID, doc.getPublisherId)
 
-    addDateFieldToIndexDocument(documentId, indexDoc, DocumentIndex.FIELD__CREATED_DATETIME, doc.getCreatedDatetime)
-    addDateFieldToIndexDocument(documentId, indexDoc, DocumentIndex.FIELD__MODIFIED_DATETIME, doc.getModifiedDatetime)
-    addDateFieldToIndexDocument(documentId, indexDoc, DocumentIndex.FIELD__ACTIVATED_DATETIME, doc.getPublicationStartDatetime)
-    addDateFieldToIndexDocument(documentId, indexDoc, DocumentIndex.FIELD__PUBLICATION_START_DATETIME, doc.getPublicationStartDatetime)
-    addDateFieldToIndexDocument(documentId, indexDoc, DocumentIndex.FIELD__PUBLICATION_END_DATETIME, doc.getPublicationEndDatetime)
-    addDateFieldToIndexDocument(documentId, indexDoc, DocumentIndex.FIELD__ARCHIVED_DATETIME, doc.getArchivedDatetime)
+    // todo: ??? or "" ???
+    indexDoc.addField(DocumentIndex.FIELD__CREATED_DATETIME, doc.getCreatedDatetime)
+    indexDoc.addField(DocumentIndex.FIELD__MODIFIED_DATETIME, doc.getModifiedDatetime)
+    indexDoc.addField(DocumentIndex.FIELD__ACTIVATED_DATETIME, doc.getPublicationStartDatetime)
+    indexDoc.addField(DocumentIndex.FIELD__PUBLICATION_START_DATETIME, doc.getPublicationStartDatetime)
+    indexDoc.addField(DocumentIndex.FIELD__PUBLICATION_END_DATETIME, doc.getPublicationEndDatetime)
+    indexDoc.addField(DocumentIndex.FIELD__ARCHIVED_DATETIME, doc.getArchivedDatetime)
 
-    indexDoc.addField(DocumentIndex.FIELD__STATUS, doc.getPublicationStatus.toString)
+    indexDoc.addField(DocumentIndex.FIELD__STATUS, doc.getPublicationStatus.asInt())
 
     for (category <- categoryMapper.getCategories(doc.getCategoryIds).asScala) {
       indexDoc.addField(DocumentIndex.FIELD__CATEGORY, category.getName)
-      indexDoc.addField(DocumentIndex.FIELD__CATEGORY_ID, category.getId.toString)
+      indexDoc.addField(DocumentIndex.FIELD__CATEGORY_ID, category.getId)
 
       val categoryType = category.getType
       indexDoc.addField(DocumentIndex.FIELD__CATEGORY_TYPE, categoryType.getName)
-      indexDoc.addField(DocumentIndex.FIELD__CATEGORY_TYPE_ID, categoryType.getId.toString)
+      indexDoc.addField(DocumentIndex.FIELD__CATEGORY_TYPE_ID, categoryType.getId)
     }
 
     for (documentKeyword <- doc.getKeywords.asScala) {
         indexDoc.addField(DocumentIndex.FIELD__KEYWORD, documentKeyword)
     }
 
-
     documentMapper.getDocumentMenuPairsContainingDocument(doc).map(p => p.getDocument.getId -> p.getMenuIndex) |> {
       parentDocumentAndMenuIds =>
 
         for ((parentId, menuId) <- parentDocumentAndMenuIds) {
-            indexDoc.addField(DocumentIndex.FIELD__PARENT_ID, parentId.toString)
-            indexDoc.addField(DocumentIndex.FIELD__PARENT_MENU_ID, parentId + "_" + menuId)
+          indexDoc.addField(DocumentIndex.FIELD__PARENT_ID, parentId)
+          indexDoc.addField(DocumentIndex.FIELD__PARENT_MENU_ID, parentId + "_" + menuId)
         }
 
-        // WHY INDEX ???
-        indexDoc.addField(DocumentIndex.FIELD__HAS_PARENTS, parentDocumentAndMenuIds.nonEmpty.toString)
+        indexDoc.addField(DocumentIndex.FIELD__HAS_PARENTS, parentDocumentAndMenuIds.nonEmpty)
     }
 
-    for (alias <- Option(doc.getAlias)) {
-      indexDoc.addField(DocumentIndex.FIELD__ALIAS, alias)
-    }
-
-    // HOW TO INDEX???
-    // remove not needed???
-//    for ((name, value) <- doc.getProperties.asScala) {
-//        indexDoc.addField(name, value)
-//    }
+    addFieldIfNotNull(DocumentIndex.FIELD__ALIAS, doc.getAlias)
 
     for ((key, value) <- doc.getProperties.asScala) {
-      indexDoc.addField(DocumentIndex.FIELD__PROPERTY_PREFIX + key, value.toString)
+      indexDoc.addField(DocumentIndex.FIELD__PROPERTY_PREFIX + key, value)
     }
 
-
-    // ??? WHY INDEX ???
     val roleIdMappings: RoleIdToDocumentPermissionSetTypeMappings = doc.getRoleIdsMappedToDocumentPermissionSetTypes
     for (mapping <- roleIdMappings.getMappings) {
-      indexDoc.addField(DocumentIndex.FIELD__ROLE_ID, mapping.getRoleId.intValue.toString)
+      indexDoc.addField(DocumentIndex.FIELD__ROLE_ID, mapping.getRoleId.intValue)
     }
 
     try {
       contentIndexer.index(doc, indexDoc)
     } catch {
-      case re => logger.error("Error indexing doc-type-specific data of doc " + documentId, re)
+      case e =>
+        logger.error("Failed to index doc's content. Doc id: %d, language: %s, type: %s".
+          format(documentId, doc.getLanguage, doc.getDocumentType), e
+        )
     }
   }
-
-  // todo: refactor
-  private def addDateFieldToIndexDocument(documentId: Int, indexDocument: SolrInputDocument, fieldName: String,
-                                          date: Date) {
-    if (null != date) {
-      try {
-        indexDocument.addField(fieldName, date)
-        return
-      } catch {
-        case re =>
-          val dateFormat = new SimpleDateFormat(DateConstants.DATETIME_FORMAT_STRING)
-          logger.warn("Failed to index datetime '" + dateFormat.format(date) + "' in field '" + fieldName + "' of doc " + documentId, re)
-      }
-    }
-//        indexDocument.addField(fieldName, "")
-  }
-
-//    static Field unStoredKeyword(String fieldName, String fieldValue) {
-//        return new Field(fieldName, fieldValue.toLowerCase(), Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO)
-//    }
-//
-//    private static Field unStoredKeyword(String fieldName, Date fieldValue) {
-//        return new Field(fieldName, DateTools.dateToString(fieldValue, DateTools.Resolution.MINUTE), Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO)
-//    }
 }
