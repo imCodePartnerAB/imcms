15fc94993deba77f5ad63d109fafbcb4c1f00237
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2013-04-03 14:40:09 +0000

    Issue 12024: Added adapter for request redirect for legacy request handlers such as ChangeMenu
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@27661 bd66a97b-2aff-0310-9095-89ca5cabf5a6

---
+++ src/main/java/com/imcode/imcms/servlet/AdapterServlet.java
@@ -0,0 +1,36 @@
+package com.imcode.imcms.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
+
+public class AdapterServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uri = request.getRequestURI();
+
+        if (uri.endsWith("/servlet/ChangeMenu")) {
+            String url = String.format("%s/docadmin/menu?docId=%s&menuNo=%s&%s",
+                                    request.getContextPath(),
+                                    request.getParameter("documentId"),
+                                    request.getParameter("menuIndex"),
+                                    request.getQueryString());
+
+            response.sendRedirect(url);
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+}
