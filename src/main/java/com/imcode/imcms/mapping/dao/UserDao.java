58f83869b72cc88b81bd1865219f4db7913c133b
antjos <antjos@bd66a97b-2aff-0310-9095-89ca5cabf5a6>
2014-02-27 00:13:59 +0000

    Issue 12024: NB! Current build is broken - work in progress. Fully separated document's content management (create, save, update, delete) layer from the database (hibernate/jpa) layer
    git-svn-id: https://svn.imcode.com/imcode/imcms/trunk@30374 bd66a97b-2aff-0310-9095-89ca5cabf5a6

--- src/main/java/com/imcode/imcms/mapping/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.imcode.imcms.mapping.dao;
-
-import com.imcode.imcms.mapping.orm.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDao extends JpaRepository<User, Integer> {
-}
