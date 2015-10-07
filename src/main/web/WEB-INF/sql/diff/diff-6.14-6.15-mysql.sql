7f2596a4791737ad7e1523fc7bab0e0e3dd6c9de
Shadowgun <shadowgun@i.ua>
2015-10-07 16:27:37 +0300

    update migrations

--- src/main/web/WEB-INF/sql/diff/diff-6.14-6.15-mysql.sql
+++ src/main/web/WEB-INF/sql/diff/diff-6.14-6.15-mysql.sql
@@ -4,44 +4,141 @@
 SET @schema_version__major_new = 6;
 SET @schema_version__minor_new = 15;
 
-ALTER TABLE imcms_text_doc_content_loops
-    DROP COLUMN version;
+DROP PROCEDURE IF EXISTS migration_6_15;
+DROP PROCEDURE IF EXISTS dropForeignKeysFromTable;
 
-CREATE TABLE imcms_text_doc_contents_new(
-    id serial,
-    loop_id int NOT NULL,
-    no int NOT NULL,
-    ix int NOT NULL,
-    enabled boolean NOT NULL DEFAULT true,
-    CONSTRAINT fk__content_loop FOREIGN KEY (loop_id) REFERENCES imcms_text_doc_content_loops (id),
-    KEY ix__content_order (ix)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DELIMITER ///
 
-INSERT INTO imcms_text_doc_contents_new
-    SELECT c.id, l.id, c.no, c.ix, c.enabled
-    FROM imcms_text_doc_content_loops l JOIN
-         imcms_text_doc_contents c
-         ON l.doc_id = c.doc_id AND
-            l.doc_version_no = c.doc_version_no AND
-            l.no = c.loop_no;
+CREATE PROCEDURE dropForeignKeysFromTable()
+  BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE dropCommand VARCHAR(255);
+    DECLARE createCommand VARCHAR(255);
+    DECLARE dropCur CURSOR FOR
+      SELECT DISTINCT
+        CONCAT("ALTER TABLE `", `K`.`TABLE_NAME`, "` DROP FOREIGN KEY `", `K`.`CONSTRAINT_NAME`, "`;") 'DROP'
+      FROM `information_schema`.`KEY_COLUMN_USAGE` `K`
+        LEFT JOIN `information_schema`.`REFERENTIAL_CONSTRAINTS` `C` USING (`CONSTRAINT_NAME`)
+      WHERE `K`.`REFERENCED_TABLE_SCHEMA` = SCHEMA()
+            AND `K`.`REFERENCED_TABLE_NAME` = "imcms_text_doc_contents";
 
--- An attempt to drop both foreign key and an index in one statement leads to server crash and data corruption.
-ALTER TABLE imcms_text_doc_images_history
-    DROP FOREIGN KEY fk__imcms_text_doc_images_history__content;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
--- An attempt to drop both indexed and key in one statement leads to server crash and data corruption.
-ALTER TABLE imcms_text_doc_texts_history
-    DROP FOREIGN KEY fk__imcms_text_doc_texts_history__content;
+      OPEN dropCur;
 
+      read_loop: LOOP
+        FETCH dropCur
+        INTO dropCommand;
+        IF done
+        THEN
+          LEAVE read_loop;
+        END IF;
 
-DROP TABLE imcms_text_doc_contents;
-RENAME TABLE imcms_text_doc_contents_new TO imcms_text_doc_contents;
+        SET @sdropCommand = dropCommand;
+
+        PREPARE dropClientUpdateKeyStmt FROM @sdropCommand;
+
+        EXECUTE dropClientUpdateKeyStmt;
+
+        DEALLOCATE PREPARE dropClientUpdateKeyStmt;
+      END LOOP;
+
+      CLOSE dropCur;
+  END///
+
+DELIMITER #
+
+CREATE PROCEDURE migration_6_15
+  (
+  )
+  BEGIN
+    IF exists(SELECT *
+              FROM information_schema.columns
+              WHERE table_name = 'imcms_text_doc_content_loops' AND column_name = 'version')
+    THEN
+      ALTER TABLE imcms_text_doc_content_loops
+      DROP COLUMN version;
+
+      DROP TABLE IF EXISTS imcms_text_doc_contents_new;
+
+      CREATE TABLE imcms_text_doc_contents_new (
+        id      SERIAL,
+        loop_id INT     NOT NULL,
+        no      INT     NOT NULL,
+        ix      INT     NOT NULL,
+        enabled BOOLEAN NOT NULL DEFAULT TRUE,
+        CONSTRAINT fk__content_loop FOREIGN KEY (loop_id) REFERENCES imcms_text_doc_content_loops (id),
+        KEY ix__content_order (ix)
+      )
+        ENGINE = InnoDB
+        DEFAULT CHARSET = utf8;
+
+      INSERT INTO imcms_text_doc_contents_new
+        SELECT
+          c.id,
+          l.id,
+          c.no,
+          c.ix,
+          c.enabled
+        FROM imcms_text_doc_content_loops l JOIN
+          imcms_text_doc_contents c
+            ON l.doc_id = c.doc_id AND
+               l.doc_version_no = c.doc_version_no AND
+               l.no = c.loop_no;
+
+      DROP TABLE imcms_text_doc_contents;
+      RENAME TABLE imcms_text_doc_contents_new TO imcms_text_doc_contents;
+    END IF;
+
+    IF exists(SELECT *
+              FROM information_schema.TABLE_CONSTRAINTS
+              WHERE
+                CONSTRAINT_TYPE = 'FOREIGN KEY'
+                AND TABLE_SCHEMA = SCHEMA()
+                AND TABLE_NAME = 'imcms_text_doc_images_history'
+                AND CONSTRAINT_NAME = 'fk__imcms_text_doc_images_history__content')
+    THEN
+      -- An attempt to drop both foreign key and an index in one statement leads to server crash and data corruption.
+      ALTER TABLE imcms_text_doc_images_history
+      DROP FOREIGN KEY fk__imcms_text_doc_images_history__content;
+    END IF;
+
+    IF exists(SELECT *
+              FROM information_schema.TABLE_CONSTRAINTS
+              WHERE
+                CONSTRAINT_TYPE = 'FOREIGN KEY'
+                AND TABLE_SCHEMA = SCHEMA()
+                AND TABLE_NAME = 'imcms_text_doc_texts_history'
+                AND CONSTRAINT_NAME = 'fk__imcms_text_doc_texts_history__content')
+    THEN
+      -- An attempt to drop both indexed and key in one statement leads to server crash and data corruption.
+      ALTER TABLE imcms_text_doc_texts_history
+      DROP FOREIGN KEY fk__imcms_text_doc_texts_history__content;
+    END IF;
+
+  END# -- end of stored procedure block
+
+DELIMITER ;
+-- switch delimiters again
+CALL dropForeignKeysFromTable();
+CALL migration_6_15();
+
+DROP PROCEDURE IF EXISTS dropForeignKeysFromTable;
+DROP PROCEDURE IF EXISTS migration_6_15;
+
+DROP VIEW IF EXISTS imcms_text_doc_contents_v;
 
 CREATE VIEW imcms_text_doc_contents_v AS
-    SELECT l.doc_id, l.doc_version_no, l.no AS loop_no, c.no AS content_no, c.ix AS content_ix, c.enabled as content_enabled
-    FROM imcms_text_doc_content_loops l JOIN
-         imcms_text_doc_contents c
-         ON l.id = c.loop_id;
+  SELECT
+    l.doc_id,
+    l.doc_version_no,
+    l.no      AS loop_no,
+    c.no      AS content_no,
+    c.ix      AS content_ix,
+    c.enabled AS content_enabled
+  FROM imcms_text_doc_content_loops l JOIN
+    imcms_text_doc_contents c
+      ON l.id = c.loop_id;
 
 --
 -- Update schema version
--- src/main/web/WEB-INF/sql/diff/diff-6.15-6.16-mysql.sql
+++ src/main/web/WEB-INF/sql/diff/diff-6.15-6.16-mysql.sql
@@ -5,15 +5,164 @@ SET @schema_version__major_new = 6;
 SET @schema_version__minor_new = 16;
 
 
-ALTER TABLE fileupload_docs
-DROP FOREIGN KEY fk__fileupload_docs__doc_version;
-ALTER TABLE fileupload_docs
-CHANGE COLUMN meta_id doc_id INT(11) NOT NULL ;
-ALTER TABLE fileupload_docs
-ADD CONSTRAINT fk__fileupload_docs__doc_version
-  FOREIGN KEY (doc_id , doc_version_no)
-  REFERENCES imcms_doc_versions (doc_id , no)
-  ON DELETE CASCADE;
+DROP PROCEDURE IF EXISTS dropForeignKeysFromTable;
+DROP PROCEDURE IF EXISTS migration_6_16;
+
+DELIMITER ///
+
+CREATE PROCEDURE dropForeignKeysFromTable()
+  BEGIN
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE dropCommand VARCHAR(255);
+    DECLARE createCommand VARCHAR(255);
+    DECLARE dropCur CURSOR FOR
+      SELECT DISTINCT
+        CONCAT("ALTER TABLE `", `K`.`TABLE_NAME`, "` DROP FOREIGN KEY `", `K`.`CONSTRAINT_NAME`, "`;") 'DROP'
+      FROM `information_schema`.`KEY_COLUMN_USAGE` `K`
+        LEFT JOIN `information_schema`.`REFERENTIAL_CONSTRAINTS` `C` USING (`CONSTRAINT_NAME`)
+      WHERE `K`.`REFERENCED_TABLE_SCHEMA` = "haninge-dport"
+            AND `K`.`REFERENCED_TABLE_NAME` = "fileupload_docs";
+
+
+    DECLARE createCur CURSOR FOR
+      SELECT DISTINCT *
+      FROM `temp_fk_view`;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    IF exists(SELECT *
+              FROM information_schema.columns
+              WHERE table_name = 'fileupload_docs' AND column_name = 'meta_id')
+    THEN
+
+
+      SET @query = "DROP TABLE IF EXISTS temp_fk_view;";
+      SELECT @query;
+      PREPARE stmt FROM @query;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      SET @query = "CREATE TABLE temp_fk_view (`query` varchar(21844));";
+      SELECT @query;
+      PREPARE stmt FROM @query;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+      SET @query = "INSERT INTO temp_fk_view SELECT DISTINCT CONCAT('ALTER TABLE `',`K`.`TABLE_NAME`,'` ADD CONSTRAINT ','`fk_',`K`.`TABLE_NAME`,'_',`K`.`REFERENCED_TABLE_NAME`,'1','` FOREIGN KEY (`',`K`.`COLUMN_NAME`,'`) REFERENCES ','`',`K`.`REFERENCED_TABLE_SCHEMA`,'`.`',`K`.`REFERENCED_TABLE_NAME`,'` (`',IF(`K`.`REFERENCED_COLUMN_NAME`='meta_id', 'doc_id', `K`.`REFERENCED_COLUMN_NAME`),'`) ON DELETE ',`C`.`DELETE_RULE`,' ON UPDATE ',`C`.`UPDATE_RULE`,';') 'CREATE' FROM `information_schema`.`KEY_COLUMN_USAGE` `K` LEFT JOIN `information_schema`.`REFERENTIAL_CONSTRAINTS` `C` USING (`CONSTRAINT_NAME`) WHERE `K`.`REFERENCED_TABLE_SCHEMA` = 'haninge-dport' AND `K`.`REFERENCED_TABLE_NAME` = 'fileupload_docs';";
+      SELECT @query;
+      PREPARE stmt FROM @query;
+      EXECUTE stmt;
+      DEALLOCATE PREPARE stmt;
+
+      OPEN dropCur;
+
+      read_loop: LOOP
+        FETCH dropCur
+        INTO dropCommand;
+        IF done
+        THEN
+          LEAVE read_loop;
+        END IF;
+
+        SET @sdropCommand = dropCommand;
+
+        PREPARE dropClientUpdateKeyStmt FROM @sdropCommand;
+
+        EXECUTE dropClientUpdateKeyStmt;
+
+        DEALLOCATE PREPARE dropClientUpdateKeyStmt;
+      END LOOP;
+
+      CLOSE dropCur;
+
+      DROP TABLE IF EXISTS `fileupload_docs_temporary`;
+
+      CREATE TABLE `fileupload_docs_temporary` (
+        `id`               INT(11)      NOT NULL AUTO_INCREMENT,
+        `doc_id`           INT(11)      NOT NULL,
+        `doc_version_no`   INT(11)      NOT NULL,
+        `variant_name`     VARCHAR(100) NOT NULL,
+        `filename`         VARCHAR(255) NOT NULL,
+        `mime`             VARCHAR(50)  NOT NULL,
+        `created_as_image` INT(11)      NOT NULL,
+        `default_variant`  TINYINT(1)   NOT NULL DEFAULT '0',
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `uk__fileupload_docs__1` (`doc_id`, `doc_version_no`, `variant_name`),
+        CONSTRAINT `fk__fileupload_docs__doc_version` FOREIGN KEY (`doc_id`, `doc_version_no`)
+        REFERENCES `imcms_doc_versions` (`doc_id`, `no`)
+          ON DELETE CASCADE
+      )
+        ENGINE = INNODB
+        AUTO_INCREMENT = 46
+        DEFAULT CHARSET = UTF8;
+
+
+      INSERT INTO fileupload_docs_temporary
+        SELECT
+          fd.id,
+          (fd.meta_id) 'doc_id',
+          fd.doc_version_no,
+          fd.variant_name,
+          fd.filename,
+          fd.mime,
+          fd.created_as_image,
+          fd.default_variant
+        FROM fileupload_docs fd;
+
+      DROP TABLE fileupload_docs;
+
+      RENAME TABLE fileupload_docs_temporary TO fileupload_docs;
+
+      SET done = FALSE;
+
+
+      OPEN createCur;
+
+      write_loop: LOOP
+        FETCH createCur
+        INTO createCommand;
+        IF done
+        THEN
+          LEAVE write_loop;
+        END IF;
+
+        SET @screateCommand = createCommand;
+
+        PREPARE createClientUpdateKeyStmt FROM @screateCommand;
+
+        EXECUTE createClientUpdateKeyStmt;
+
+        DEALLOCATE PREPARE createClientUpdateKeyStmt;
+      END LOOP;
+
+      CLOSE createCur;
+    END IF;
+  END///
+
+
+CREATE PROCEDURE migration_6_16
+  (
+  )
+  BEGIN
+    IF exists(SELECT *
+              FROM information_schema.TABLE_CONSTRAINTS
+              WHERE
+                CONSTRAINT_TYPE = 'FOREIGN KEY'
+                AND TABLE_SCHEMA = SCHEMA()
+                AND TABLE_NAME = 'fileupload_docs'
+                AND CONSTRAINT_NAME = 'fk__fileupload_docs__doc_version')
+    THEN
+      ALTER TABLE fileupload_docs
+      DROP FOREIGN KEY fk__fileupload_docs__doc_version;
+    END IF;
+  END/// -- end of stored procedure block
+
+DELIMITER ;
+-- switch delimiters again
+
+CALL migration_6_16();
+CALL dropForeignKeysFromTable();
+
+DROP PROCEDURE IF EXISTS migration_6_16;
+DROP PROCEDURE IF EXISTS dropForeignKeysFromTable;
 
 --
 -- Update schema version
@@ -22,6 +171,3 @@ UPDATE database_version
 SET
   major = @schema_version__major_new,
   minor = @schema_version__minor_new;
-
-
-
