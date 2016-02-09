5a87767c17f3cdbb474f9ac4c463185bc7ca1306
Serg-Maximchuk <serg.maximchuk@gmail.com>
2016-02-09 11:55:33 +0200

    Fixed bug with loop saving.

--- src/main/java/com/imcode/imcms/servlet/apis/LoopController.java
+++ src/main/java/com/imcode/imcms/servlet/apis/LoopController.java
@@ -16,10 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -73,15 +70,17 @@ public class LoopController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	protected Object doPost(@RequestParam("data") Map<String, Object> params) throws ServletException, IOException {
+	protected Object doPost(@RequestParam("entries") String params,
+							@RequestParam("loopId") Integer loopId,
+							@RequestParam("meta") Integer metaId) throws ServletException, IOException {
+		// I can't make this method take a List or an array of Map<String, Object>, also wrapper class not work too.
+		// So I wrote genEntries() parser.
 		Map<String, Object> result = new HashMap<>();
 		try {
-			Integer metaId = Integer.parseInt(params.get("meta").toString());
-			Integer loopId = Integer.parseInt(params.get("loopId").toString());
-			List<Map<String, Object>> entries = (ArrayList<Map<String, Object>>) params.get("entries");
+			List<Map<String, String>> entries = genEntries(params);
 			TextDocumentDomainObject document = Imcms.getServices().getDocumentMapper().getDocument(metaId);
 			TextDocumentContentSaver textDocumentContentSaver = Imcms.getServices().getManagedBean(TextDocumentContentSaver.class);
-			Map<Integer, Boolean> loopMap = entries.stream().collect(Collectors.mapping((item) -> Integer.parseInt(item.get("no").toString()),
+			Map<Integer, Boolean> loopMap = entries.stream().collect(Collectors.mapping((item) -> Integer.parseInt(item.get("no")),
 					Collectors.toMap((key) -> key, (value) -> true)));
 
 			textDocumentContentSaver.saveLoop(new TextDocLoopContainer(document.getVersionRef(), loopId, Loop.of(loopMap)));
@@ -93,5 +92,54 @@ public class LoopController {
 		}
 
 		return result;
+	}
+
+	// params is smth like [{"no":1,"text":"content is not defined"},{"no":2,"text":""},{"no":3,"text":""},{"no":4,"text":""}]
+	protected List<Map<String, String>> genEntries(String params) {
+		List<Map<String, String>> result = new LinkedList<>();
+		if (isValid(params)) {
+			LinkedList<String> s = new LinkedList<>(Arrays.asList(params.split("\"")));
+			// one loop looks like "[{"no":1,"text":"content is not defined"}]", so after split by " we have (6n + 1) elements
+			s.removeFirst();
+			if (numberIsGood(s)) {
+				ListIterator<String> iter = s.listIterator();
+				while (iter.hasNext()) {
+					Map<String, String> loop = new HashMap<>();
+					// working with "no":1,"
+					String no = iter.next();
+					if (!"no".equals(no)) {
+						throw new NullPointerException("Something goes wrong! The 'no' attribute not found!");
+					}
+					String noVal = iter.next();
+					// ":1," - we need remove first and last symbols to get the number
+					String noValue = noVal.substring(1, noVal.length() - 1);
+					loop.put(no, noValue);
+
+					// "text":"content is not defined"
+					String text = iter.next();
+					if (!"text".equals(text)) {
+						throw new NullPointerException("Something goes wrong! The 'text' attribute not found!");
+					}
+					if (!":".equals(iter.next())) {
+						throw new NullPointerException("Something goes wrong! The ':' attribute not found!");
+					}
+					String textValue = iter.next();
+					iter.next();
+					loop.put(text, textValue);
+
+					result.add(loop);
+				}
+			}
+		}
+		return result;
+	}
+
+	protected boolean isValid(String params) {
+		// check if String params is an array, else smth wrong
+		return params.startsWith("[") && params.endsWith("]");
+	}
+
+	protected boolean numberIsGood(LinkedList<String> str) {
+		return (str.size() % 6) == 0;
 	}
 }
--- src/main/web/imcms/lang/scripts/imcms_loop.js
+++ src/main/web/imcms/lang/scripts/imcms_loop.js
@@ -1,217 +1,213 @@
-/**
- * Created by Shadowgun on 11.03.2015.
- */
 Imcms.Loop = {};
 Imcms.Loop.API = function () {
 
 };
 Imcms.Loop.API.prototype = {
-    path: Imcms.contextPath + "/api/loop",
-    create: function (request, response) {
-        $.ajax({
-            url: this.path,
-            type: "POST",
-            data: request,
-            success: response
-        })
-    },
-    read: function (request, response) {
-        $.ajax({
-            url: this.path,
-            type: "GET",
-            data: request,
-            success: response
-        });
-    },
-    create2: function (request, response) {
-        $.ajax({
-            url: this.path,
-            type: "PUT",
-            data: request,
-            success: response
-        })
-    },
-    update: function (request, response) {
-        $.ajax({
-            url: this.path,
-            type: "POST",
-            data: request,
-            success: response
-        })
-    }
-
+	path: Imcms.contextPath + "/api/loop",
+	create: function (request, response) {
+		$.ajax({
+			url: this.path,
+			type: "POST",
+			data: request,
+			success: response
+		})
+	},
+	read: function (request, response) {
+		$.ajax({
+			url: this.path,
+			type: "GET",
+			data: request,
+			success: response
+		});
+	},
+	create2: function (request, response) {
+		$.ajax({
+			url: this.path,
+			type: "PUT",
+			data: request,
+			success: response
+		})
+	},
+	update: function (request, response) {
+		$.ajax({
+			url: this.path,
+			type: "POST",
+			data: request,
+			success: response
+		})
+	}
 };
 
 Imcms.Loop.Loader = function () {
-    this.init();
+	this.init();
 };
 Imcms.Loop.Loader.prototype = {
-    _api: new Imcms.Loop.API(),
-    _editorList: [],
-    init: function () {
-        jQuery(".editor-loop").each(function (pos, element) {
-            this._editorList[pos] = new Imcms.Loop.Editor(element, this);
-        }.bind(this));
-    },
-    create: function (name) {
-        var that = this;
-        this._api.create({name: name}, function (data) {
-            if (!data.result) return;
-            that.redirect(data.id);
-        })
-    },
-    update: function (data, loopId, callback) {
-        this._api.update({data: JSON.stringify({entries: data, loopId: loopId, meta: Imcms.document.meta})}, callback);
-    },
-    entriesList: function (data, callback) {
-        this._api.read(Imcms.Utils.margeObjectsProperties(Imcms.document, data), function (response) {
-            if (response && response.result) {
-                callback(response.data)
-            }
-            else {
-                callback({});
-            }
-        });
-    }
+	_api: new Imcms.Loop.API(),
+	_editorList: [],
+	init: function () {
+		jQuery(".editor-loop").each(function (pos, element) {
+			this._editorList[pos] = new Imcms.Loop.Editor(element, this);
+		}.bind(this));
+	},
+	create: function (name) {
+		var that = this;
+		this._api.create({name: name}, function (data) {
+			if (!data.result) return;
+			that.redirect(data.id);
+		})
+	},
+	update: function (data, loopId, callback) {
+		this._api.update({entries: JSON.stringify(data), loopId: loopId, meta: Imcms.document.meta}, callback);
+	},
+	entriesList: function (data, callback) {
+		this._api.read(Imcms.Utils.margeObjectsProperties(Imcms.document, data), function (response) {
+			if (response && response.result) {
+				callback(response.data)
+			}
+			else {
+				callback({});
+			}
+		});
+	}
 };
 
 Imcms.Loop.Editor = function (element, loader) {
-    this._loader = loader;
-    this._target = element;
-    this.init();
+	this._loader = loader;
+	this._target = element;
+	this.init();
 };
 Imcms.Loop.Editor.prototype = {
-    _builder: {},
-    _target: {},
-    _loader: {},
-    _frame: {},
-    _loopListAdapter: {},
-    init: function () {
-        return this.buildView().buildLoopsList().buildExtra();
-    },
-    buildView: function () {
-        this._builder = new JSFormBuilder("<DIV>")
-            .form()
-            .div()
-            .class("imcms-header")
-            .div()
-            .html("Loop Editor")
-            .class("imcms-title")
-            .end()
-            .button()
-            .reference("closeButton")
-            .class("imcms-close-button")
-            .on("click", $.proxy(this.close, this))
-            .end()
-            /*
-             .button()
-             .html("Close without saving")
-             .class("imcms-neutral close-without-saving")
-             .on("click", $.proxy(this.close, this))
-             .end()*/
-            .end()
-            .div()
-            .class("imcms-content")
-            .table()
-            .reference("entriesList")
-            .end()
-            .end()
-            .div()
-            .class("imcms-footer")
-            .button()
-            .reference("createNew")
-            .class("imcms-neutral create-new")
-            .html("Create new")
-            .end()
-            .button()
-            .html("Save and close")
-            .class("imcms-positive imcms-save-and-close")
-            .on("click", $.proxy(this.save, this))
-            .end()
-            .div()
-            .class("clear")
-            .end()
-            .end()
-            .end();
-        $(this._builder[0])
-            .appendTo("body")
-            .addClass("editor-form loop-viewer reset");
-        return this;
-    },
-    buildLoopsList: function (data) {
-        if (!data) {
-            this._loader.entriesList({loopId: $(this._target).data().prettify().no}, $.proxy(this.buildLoopsList, this));
-            return this;
-        }
-        this._loopListAdapter =
-            new Imcms.Loop.ListAdapter(
-                this._builder.ref("entriesList"),
-                data
-            );
-        this._builder.ref("createNew").on("click", $.proxy(this._loopListAdapter.addLoop, this._loopListAdapter))
-        return this;
-    },
-    buildExtra: function () {
-        this._frame = new Imcms.FrameBuilder()
-            .title("Loop Editor")
-            .click($.proxy(this.open, this))
-            .build()
-            .prependTo(this._target);
-        return this;
-    },
-    save: function () {
-        this._loader.update(
-            this._loopListAdapter.collect(),
-            $(this._target).data().prettify().no,
-            Imcms.BackgroundWorker.createTask({refreshPage: true})
-        );
-        this.close();
-    },
-    open: function () {
-        $(this._builder[0]).fadeIn("fast").find(".imcms-content").css({height: $(window).height() - 95});
-    },
-    close: function () {
-        $(this._builder[0]).fadeOut("fast");
-    }
+	_builder: {},
+	_target: {},
+	_loader: {},
+	_frame: {},
+	_loopListAdapter: {},
+	init: function () {
+		return this.buildView().buildLoopsList().buildExtra();
+	},
+	buildView: function () {
+		this._builder = new JSFormBuilder("<DIV>")
+			.form()
+			.div()
+			.class("imcms-header")
+			.div()
+			.html("Loop Editor")
+			.class("imcms-title")
+			.end()
+			.button()
+			.reference("closeButton")
+			.class("imcms-close-button")
+			.on("click", $.proxy(this.close, this))
+			.end()
+			/*
+			 .button()
+			 .html("Close without saving")
+			 .class("imcms-neutral close-without-saving")
+			 .on("click", $.proxy(this.close, this))
+			 .end()*/
+			.end()
+			.div()
+			.class("imcms-content")
+			.table()
+			.reference("entriesList")
+			.end()
+			.end()
+			.div()
+			.class("imcms-footer")
+			.button()
+			.reference("createNew")
+			.class("imcms-neutral create-new")
+			.html("Create new")
+			.end()
+			.button()
+			.html("Save and close")
+			.class("imcms-positive imcms-save-and-close")
+			.on("click", $.proxy(this.save, this))
+			.end()
+			.div()
+			.class("clear")
+			.end()
+			.end()
+			.end();
+		$(this._builder[0])
+			.appendTo("body")
+			.addClass("editor-form loop-viewer reset");
+		return this;
+	},
+	buildLoopsList: function (data) {
+		if (!data) {
+			this._loader.entriesList({loopId: $(this._target).data().prettify().no}, $.proxy(this.buildLoopsList, this));
+			return this;
+		}
+		this._loopListAdapter =
+			new Imcms.Loop.ListAdapter(
+				this._builder.ref("entriesList"),
+				data
+			);
+		this._builder.ref("createNew").on("click", $.proxy(this._loopListAdapter.addLoop, this._loopListAdapter));
+		return this;
+	},
+	buildExtra: function () {
+		this._frame = new Imcms.FrameBuilder()
+			.title("Loop Editor")
+			.click($.proxy(this.open, this))
+			.build()
+			.prependTo(this._target);
+		return this;
+	},
+	save: function () {
+		this._loader.update(
+			this._loopListAdapter.collect(),
+			$(this._target).data().prettify().no,
+			Imcms.BackgroundWorker.createTask({refreshPage: true})
+		);
+		this.close();
+	},
+	open: function () {
+		$(this._builder[0]).fadeIn("fast").find(".imcms-content").css({height: $(window).height() - 95});
+	},
+	close: function () {
+		$(this._builder[0]).fadeOut("fast");
+	}
 };
 
 Imcms.Loop.ListAdapter = function (container, data) {
-    this._container = container;
-    this._data = data;
-    this.init();
+	this._container = container;
+	this._data = data;
+	this.init();
 };
 Imcms.Loop.ListAdapter.prototype = {
-    _container: {},
-    _ul: {},
-    _data: {},
-    init: function () {
-        this.buildList(this._data);
-    },
-    buildList: function (data) {
-        $.each(data, $.proxy(this.addLoopToList, this));
-    },
-    addLoopToList: function (position, data) {
-        var deleteButton = $("<button>");
-        this._container.row(data.no, data.text, deleteButton
-                .addClass("imcms-negative")
-                .attr("type", "button")[0]
-        );
-        var row = this._container.row(position);
-        deleteButton
-            .click($.proxy(this.deleteLoop, this, data, row));
-    },
-    deleteLoop: function (data, row) {
-        $(row).remove();
-        this._data.remove(data);
-    },
-    addLoop: function () {
-        var length = this._data.length;
-        this._data.push({no: length ? this._data[length - 1].no + 1 : 1, text: ""});
-        this.addLoopToList(length, this._data[length]);
-    },
-    collect: function () {
-        return this._data;
-    }
+	_container: {},
+	_ul: {},
+	_data: {},
+	init: function () {
+		this.buildList(this._data);
+	},
+	buildList: function (data) {
+		$.each(data, $.proxy(this.addLoopToList, this));
+	},
+	addLoopToList: function (position, data) {
+		var deleteButton = $("<button>");
+		this._container.row(data.no, data.text, deleteButton
+			.addClass("imcms-negative")
+			.attr("type", "button")[0]
+		);
+		var row = this._container.row(position);
+		deleteButton
+			.click($.proxy(this.deleteLoop, this, data, row));
+	},
+	deleteLoop: function (data, row) {
+		$(row).remove();
+		this._data.remove(data);
+	},
+	addLoop: function () {
+		var length = this._data.length;
+		this._data.push({no: length ? this._data[length - 1].no + 1 : 1, text: ""});
+		this.addLoopToList(length, this._data[length]);
+	},
+	collect: function () {
+		return this._data;
+	}
 };
 
 
