62a941f61192a23acec6559c1981e64825b3be45
3emluk <3emluk@gmail.com>
2016-07-29 17:00:47 +0300

    IMCMS-95: Updated StringLink POJO.

--- src/main/java/com/imcode/imcms/api/linker/StringLink.java
+++ src/main/java/com/imcode/imcms/api/linker/StringLink.java
@@ -1,7 +1,36 @@
 package com.imcode.imcms.api.linker;
 
+import java.util.List;
+
 /**
  * Created by 3emluk on 29.07.16.
  */
 public class StringLink {
+    private String name;
+    private String url;
+    private List<String> args;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getArgs() {
+        return args;
+    }
+
+    public void setArgs(List<String> args) {
+        this.args = args;
+    }
 }
