c0ff55c70038d55872186fa272ebe2d759c43c12
3emluk <3emluk@gmail.com>
2016-07-29 17:34:48 +0300

    IMCMS-95: Added controller to JSON from file.

--- src/main/java/com/imcode/imcms/servlet/apis/LinkController.java
+++ src/main/java/com/imcode/imcms/servlet/apis/LinkController.java
@@ -1,7 +1,38 @@
 package com.imcode.imcms.servlet.apis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Created by 3emluk on 29.07.16.
  */
+
+@RestController
 public class LinkController {
+
+    @Autowired
+    ServletContext servletContext;
+
+    @RequestMapping(value = "/links", method = RequestMethod.GET)
+    @ResponseBody
+    public String getLinksConfigPath(){
+        String fileString = "";
+        try {
+            fileString = new String(Files.readAllBytes(Paths.get(servletContext.getRealPath("/WEB-INF/conf/links.json"))), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileString;
+    }
+
 }
