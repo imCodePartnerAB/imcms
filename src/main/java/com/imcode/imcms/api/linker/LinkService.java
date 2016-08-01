aa3e1aa7bee962fa2252c9dc870bcdb1b53bbf99
3emluk <3emluk@gmail.com>
2016-08-01 13:48:23 +0300

    IMCMS-95: Added check for argument count.

--- src/main/java/com/imcode/imcms/api/linker/LinkService.java
+++ src/main/java/com/imcode/imcms/api/linker/LinkService.java
@@ -1,6 +1,7 @@
 package com.imcode.imcms.api.linker;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.sun.javaws.exceptions.InvalidArgumentException;
 import org.apache.commons.collections.map.HashedMap;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
@@ -11,6 +12,7 @@ import org.codehaus.jackson.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.naming.NameNotFoundException;
 import javax.servlet.ServletContext;
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +20,8 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by 3emluk on 29.07.16.
@@ -35,11 +39,8 @@ public class LinkService {
     //TODO must be private
     private void initializeLinksMap() {
         ObjectMapper mapper = new ObjectMapper();
-//        mapper.configure(DeserializationConfig.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         try {
-
             // Convert JSON string from file to Object
-
             List<StringLink> links = mapper.readValue(new File(String.valueOf(Paths.get(servletContext.getRealPath("/WEB-INF/conf/links.json")))), new TypeReference<List<StringLink>>() {
             });
             for (StringLink stringLink : links) {
@@ -68,20 +69,41 @@ public class LinkService {
         }
     }
 
-    public static String find(String... args) {
+    public static String find(String... args) throws InvalidArgumentException, NameNotFoundException {
         String link = linksMap.get(args[0]);
         if (link == null) {
             for (Map.Entry<String, String> e : linksMap.entrySet()) {
                 if (e.getKey().startsWith(args[0])) {
-                    link = e.getValue();
+                    if (findArgsAmout(e.getValue()) != args.length - 1) {
+                        continue;
+                    } else {
+                        link = e.getValue();
+                    }
                     break;
                 }
             }
+        }
+
+        if (link == null) {
+            throw new NameNotFoundException();
+        }
+        if (findArgsAmout(link) != args.length - 1) {
+            throw new InvalidArgumentException(new String[]{"Arguments count doesn't match with pattern"});
         }
         for (int i = 1; i < args.length; i++) {
             link.replace("{" + i + "}", args[i]);
         }
         return link;
+    }
+
+    private static int findArgsAmout(String urlPattern) {
+        int count = 0;
+        Pattern pattern = Pattern.compile("\\{\\d{1,3}\\}");
+        Matcher matcher = pattern.matcher(urlPattern);
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
     }
 
 }
