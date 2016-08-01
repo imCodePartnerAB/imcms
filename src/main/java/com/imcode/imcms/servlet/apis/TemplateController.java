dc2582367df64c3129a0bee9d6842c48ab8fdbf1
3emluk <3emluk@gmail.com>
2016-08-01 17:05:22 +0300

    IMCMS-95: Cleanup of testing stuff .

--- src/main/java/com/imcode/imcms/servlet/apis/TemplateController.java
+++ src/main/java/com/imcode/imcms/servlet/apis/TemplateController.java
@@ -1,14 +1,12 @@
 package com.imcode.imcms.servlet.apis;
 
 import com.imcode.imcms.imagearchive.service.Facade;
-import com.sun.org.apache.xpath.internal.functions.WrongNumberArgsException;
 import imcode.server.Imcms;
 import imcode.server.document.TemplateDomainObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.naming.NameNotFoundException;
 import java.util.stream.Collectors;
 
 /**
@@ -24,15 +22,10 @@ public class TemplateController {
     @RequestMapping
     protected Object getTemplatesList() {
     //TODO REMOVE Testing stuff
-        try {
             String str1 = facade.getLinkService().get("readDoc","2331");
-            String str2 = facade.getLinkService().redirect("readDoc","2331");
             String str3 = facade.getLinkService().forward("readDoc","2331");
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (WrongNumberArgsException e) {
-            e.printStackTrace();
-        }
+            String str4 = facade.getLinkService().forward("readDoc","2331","draft");
+            String str5 = facade.getLinkService().forward("readDoc","2331","draft");
 
 
         return Imcms.getServices().getTemplateMapper().getAllTemplates()
