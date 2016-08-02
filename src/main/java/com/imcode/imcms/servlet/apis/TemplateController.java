9af745b20e322ed47dcb6c017aa28b0e90912ef0
3emluk <3emluk@gmail.com>
2016-08-02 18:06:40 +0300

    IMCMS-95: Clean up.

--- src/main/java/com/imcode/imcms/servlet/apis/TemplateController.java
+++ src/main/java/com/imcode/imcms/servlet/apis/TemplateController.java
@@ -1,9 +1,7 @@
 package com.imcode.imcms.servlet.apis;
 
-import com.imcode.imcms.imagearchive.service.Facade;
 import imcode.server.Imcms;
 import imcode.server.document.TemplateDomainObject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
