58bd498de8ef3b0cab589e251a8ac6da2523ed7f
Serg-Maximchuk <serg.maximchuk@gmail.com>
2016-08-04 17:25:36 +0300

    IMCMS-95 - Create new feature Imcms.Linker: - Linker was downgraded to ES5 because of low translator's speed.

--- src/main/web/imcms/lang/scripts/imcms_linker.js
+++ src/main/web/imcms/lang/scripts/imcms_linker.js
@@ -1,7 +1,3 @@
-Array.prototype.flatMap = function(lambda) {
-    return Array.prototype.concat.apply([], this.map(lambda));
-};
-
 /**
  * Created by Serhii from Ubrainians for Imcode
  * on 28.07.16.
@@ -12,30 +8,30 @@ Array.prototype.flatMap = function(lambda) {
  *
  * @author Serhii
  */
-class Linker {
-    constructor() {
-        // tag 'base' should be on every page and should hold context path
-        this._contextPath = $("base").attr("href");
-        var linksPath = this._contextPath + "/api/links";
+var Linker = function () {
+    this._contextPath = $("base").attr("href"); // tag 'base' should be on every page and should hold context path
+    var linksPath = this._contextPath + "/api/links";
 
-        $.ajax({
-            url: linksPath,
-            type: "GET",
-            async: false,
-            success: (response) => this._links = response
-        });
-    }
+    $.ajax({
+        url: linksPath,
+        type: "GET",
+        async: false,
+        success: function (response) {
+            this._links = response;
+        }.bind(this)
+    });
+};
+
+Linker.prototype = {
+    _links: [],
+    _contextPath: "",
 
     /**
      * @returns {Array} all links
      */
-    get links() {
+    getLinks: function () {
         return this._links;
-    }
-
-    get contextPath() {
-        return this._contextPath;
-    }
+    },
 
     /**
      * Use it to get some link with context path.
@@ -43,9 +39,10 @@ class Linker {
      * @param {...string} [args] - arguments for url in correct order
      * @returns {string} built link
      */
-    get(name, ...args) {
-        return this._contextPath + this.getRelative(name, args);
-    }
+    get: function (name, args) {
+        var urlArgs = this._prepareArgs(arguments);
+        return this._contextPath + this._getLinkUrl(name, urlArgs);
+    },
 
     /**
      * Use it to get some link without context path.
@@ -53,21 +50,53 @@ class Linker {
      * @param {...string} [args] - arguments for url in correct order
      * @returns {string} built link
      */
-    getRelative(name, ...args) {
-        args = args.flatMap((arg) => arg); // [[]] -> []
-        args = args.filter((arg) => typeof arg !== 'undefined' && arg !== null);
+    getRelative: function (name, args) {
+        var urlArgs = this._prepareArgs(arguments);
+        return this._getLinkUrl(name, urlArgs);
+    },
 
+    /**
+     * Returns link's url by it's name and arguments.
+     * @param {string} name - the name of url, from links.json file.
+     * @param {string[]} [args] - arguments for url in correct order
+     * @returns {string} built link's url
+     */
+    _getLinkUrl: function (name, args) {
         var result = this._links
-            .find((link) => link.name == name && link.args.length == args.length)
+            .find(function (link) {
+                return (link.name == name && link.args.length == args.length);
+            })
             .url;
 
-        args.forEach((arg, index) => {
+        args.forEach(function (arg, index) {
             result = result.replace("{" + (index + 1) + "}", arg);
         });
 
         return result;
+    },
+
+    /**
+     * Function get all arguments and returns only url's arguments.
+     * First is name, and as we don't need it here we check all others args and return prepared array.
+     * @param args
+     * @returns {*}
+     * @private
+     */
+    _prepareArgs: function (args) {
+        return Array.prototype.slice
+            .call(arguments, 1) // 0 argument is link's name, 1.. is args to url so we start from 1
+            .flatMap(function (arg) { // [[]] -> []
+                return arg;
+            })
+            .filter(function (arg) {
+                return (typeof arg !== 'undefined' && arg !== null);
+            });
     }
-}
+};
+
+Array.prototype.flatMap = function (lambda) {
+    return Array.prototype.concat.apply([], this.map(lambda));
+};
 
 var Imcms = {};
 Imcms.Linker = new Linker();
