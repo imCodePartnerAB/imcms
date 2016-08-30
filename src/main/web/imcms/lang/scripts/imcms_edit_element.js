ff5e93f302aa1a1306c783dab83b2313edccaf26
Serg-Maximchuk <serg.maximchuk@gmail.com>
2016-08-30 17:54:18 +0300

    IMCMS-122 - Direct link to menu, textfield and image: - Added skeleton for JS functionality.

--- src/main/web/imcms/lang/scripts/imcms_edit_element.js
+++ src/main/web/imcms/lang/scripts/imcms_edit_element.js
@@ -2,3 +2,19 @@
  * Created by Serhii from Ubrainians for Imcode
  * on 30.08.16.
  */
+Imcms.SingleEdit = {};
+Imcms.SingleEdit.Text = {};
+Imcms.SingleEdit.Image = {};
+Imcms.SingleEdit.Menu = {};
+
+Imcms.SingleEdit.Text.init = function () {
+
+};
+
+Imcms.SingleEdit.Image.init = function () {
+
+};
+
+Imcms.SingleEdit.Menu.init = function () {
+
+};
