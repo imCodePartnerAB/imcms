839e92564c9e477c3f07fbd4c0e617e5d5681432
Serg-Maximchuk <serg.maximchuk@gmail.com>
2016-08-31 15:17:06 +0300

    IMCMS-122 - Direct link to menu, textfield and image: Refactoring

--- src/main/web/imcms/lang/scripts/imcms_edit_element.js
+++ src/main/web/imcms/lang/scripts/imcms_edit_element.js
@@ -3,72 +3,76 @@
  * Created by Serhii from Ubrainians for Imcode
  * on 30.08.16.
  */
-Imcms.SingleEdit = {};
-Imcms.SingleEdit.Text = {};
-Imcms.SingleEdit.Image = {};
-Imcms.SingleEdit.Menu = {};
+Imcms.SingleEdit = {
+    /**
+     * Count fails to prevent recycling
+     * @type {number}
+     */
+    failCount: 0,
 
-/**
- * Count fails to prevent recycling
- * @type {number}
- */
-Imcms.SingleEdit.failCount = 0;
+    Text: {
+        /**
+         * Indicates that CKEditor was switched (or not) to full-screen
+         * @type {boolean}
+         */
+        fired: false,
 
-/**
- * Unified editor calling
- */
-Imcms.SingleEdit.openEditor = function () {
-    // as there are no any event for Imcms.Editors that it is initialized, we should use setTimeout
-    setTimeout(function () {
-        try {
-            $('.editor-frame').click();
-        } catch (e) {
-            if (Imcms.SingleEdit.failCount < 20) { // to prevent recycling
-                Imcms.SingleEdit.failCount++;
-                console.log("SingleEdit::init : Waiting for Imcms.Editors initializing first.");
-                Imcms.SingleEdit.openEditor();
-                return;
-            }
+        /**
+         * When CKEditor loads, switches it to full-screen
+         */
+        init: function () {
+            CKEDITOR.on('instanceReady', function (event) {
+                if (!Imcms.SingleEdit.Text.fired) {
+                    var editor = event.editor;
+
+                    if (editor.elementMode == 3) { // only if not full screen to prevent recycled switching
+                        editor.execCommand('toolbarswitch');
+                    }
+
+                    Imcms.SingleEdit.Text.fired = true;
+                }
+            })
         }
+    },
+    Image: {
+        /**
+         * Runs Image editor when it is initialized
+         */
+        init: function () {
+            Imcms.SingleEdit.openEditor();
+        }
+    },
+    Menu: {
+        /**
+         * Runs Menu editor when it is initialized
+         */
+        init: function () {
+            Imcms.SingleEdit.openEditor();
+        }
+    },
 
+    /**
+     * Unified editor calling
+     */
+    openEditor: function () {
+        // as there are no any event for Imcms.Editors that it is initialized, we should use setTimeout
         setTimeout(function () {
-            $("#tagWrap").removeClass("hidden");
-        }, 500);
-    }, 10);
-};
-
-/**
- * Indicates that CKEditor was switched (or not) to full-screen
- * @type {boolean}
- */
-Imcms.SingleEdit.Text.fired = false;
-
-/**
- * When CKEditor loads, switches it to full-screen
- */
-Imcms.SingleEdit.Text.init = function () {
-    CKEDITOR.on('instanceReady', function (event) {
-        if (!Imcms.SingleEdit.Text.fired) {
-            var editor = event.editor;
-
-            if (editor.elementMode == 3) { // only if not full screen to prevent recycled switching
-                editor.execCommand('toolbarswitch');
+            try {
+                $('.editor-frame').click();
+            } catch (e) {
+                if (Imcms.SingleEdit.failCount < 50) { // to prevent recycling
+                    Imcms.SingleEdit.failCount++;
+                    console.log("SingleEdit::init : Waiting for Imcms.Editors initializing first.");
+                    Imcms.SingleEdit.openEditor();
+                    return;
+                }
             }
 
-            Imcms.SingleEdit.Text.fired = true;
-        }
-    })
-};
-
-/**
- * Runs Image editor when it is initialized.
- */
-Imcms.SingleEdit.Image.init = function () {
-    Imcms.SingleEdit.openEditor();
-};
-
-Imcms.SingleEdit.Menu.init = function () {
-    Imcms.SingleEdit.openEditor();
+            setTimeout(function () {
+                $("#tagWrap").removeClass("hidden");
+            }, 500);
+        }, 10);
+    }
 };
 
 $(document).ready(function () {
