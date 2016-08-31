78500d04df639696bc19f5f21252a6f0f803aa85
Serg-Maximchuk <serg.maximchuk@gmail.com>
2016-08-31 10:55:24 +0300

    IMCMS-122 - Direct link to menu, textfield and image: Now automatic full-screen runs once.

--- src/main/web/imcms/lang/scripts/imcms_edit_element.js
+++ src/main/web/imcms/lang/scripts/imcms_edit_element.js
@@ -8,11 +8,18 @@ Imcms.SingleEdit.Text = {};
 Imcms.SingleEdit.Image = {};
 Imcms.SingleEdit.Menu = {};
 
+Imcms.SingleEdit.Text.fired = false;
+
 Imcms.SingleEdit.Text.init = function () {
     CKEDITOR.on('instanceReady', function (event) {
-        var editor = event.editor;
-        if (editor.elementMode == 3) { // only if not full screen to prevent recycled switching
-            editor.execCommand('toolbarswitch');
+        if (!Imcms.SingleEdit.Text.fired) {
+            var editor = event.editor;
+
+            if (editor.elementMode == 3) { // only if not full screen to prevent recycled switching
+                editor.execCommand('toolbarswitch');
+            }
+
+            Imcms.SingleEdit.Text.fired = true;
         }
     })
 };
