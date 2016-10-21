6ace58bbec6e25408ac24ba432abef5de79b6481
Serg-Maximchuk <serg.maximchuk@gmail.com>
2016-10-21 14:23:18 +0300

    IMCMS-166 - Missed publication date set in some cases: - Fixed case of new publication version date when versioning is turned on.

--- src/main/java/com/imcode/imcms/mapping/DocumentMapper.java
+++ src/main/java/com/imcode/imcms/mapping/DocumentMapper.java
@@ -1052,6 +1052,10 @@ public class DocumentMapper implements DocumentGetter {
             final DocumentMapper mapper = Imcms.getServices().getDocumentMapper();
             final DocumentVersion newVersion = mapper.makeDocumentVersion(document.getId(), user);
             mapper.changeDocumentDefaultVersion(document.getId(), newVersion.getNo(), user);
+
+            document.setPublicationStartDatetime(new Date());
+            document.setPublisher(user);
+            Imcms.getServices().getDocumentMapper().saveDocument(document, user);
         }
     }
 
