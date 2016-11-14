e8b6c0025f7bdb347d7d4d97d692cff5a747514e
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2016-11-14 15:35:35 +0200

    IMCMS-150 - Rewrite js hell in imCMS and use AMD mechanism: - Added more polyfils to imcms_addons.

--- src/main/web/imcms/scripts/imcms_addons.js
+++ src/main/web/imcms/scripts/imcms_addons.js
@@ -1,9 +1,56 @@
+/**
+ * Flatmap function, as in Java Stream API
+ * @param {function} lambda - lambda to be used for each element
+ * @returns {Array} - resulted array
+ */
+Array.prototype.flatMap = function (lambda) {
+    return Array.prototype.concat.apply([], this.map(lambda));
+};
+
+/**
+ * Extension for Array to made convenient remove
+ * @param {*} value to remove from array
+ */
+Array.prototype.remove = function (value) {
+    while (true) {
+        var index = this.indexOf(value);
+        if (index < 0) return;
+        this.splice(index, 1);
+    }
+};
+
+if (!Array.prototype.find) {
+    /**
+     * Find element from array using predicate
+     */
+    Array.prototype.find = function (predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
 if (!String.prototype.startsWith) {
     Object.defineProperty(String.prototype, 'startsWith', {
         enumerable: false,
         configurable: false,
         writable: false,
-        value: function(searchString, position) {
+        value: function (searchString, position) {
             position = position || 0;
             return this.lastIndexOf(searchString, position) === position;
         }
