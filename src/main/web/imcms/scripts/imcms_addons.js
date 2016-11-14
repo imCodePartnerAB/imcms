286d8d10107945cf06bd77bc9f4ad3ee81288594
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2016-11-14 12:10:11 +0200

    IMCMS-187 - Image editor fixes: - Added String.prototype.startsWith polyfill for old browsers support.

--- src/main/web/imcms/scripts/imcms_addons.js
+++ src/main/web/imcms/scripts/imcms_addons.js
@@ -1,3 +1,15 @@
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function(searchString, position) {
+            position = position || 0;
+            return this.lastIndexOf(searchString, position) === position;
+        }
+    });
+}
+
 (function (old) {
     /**
      * Cool extension that provide possibility to call $(element).attr()
