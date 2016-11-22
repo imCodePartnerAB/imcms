be03b429764e27b8a8de6025d458659862338612
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2016-11-22 16:35:15 +0200

    IMCMS-208 - Refactor package com.imcode.imcms.mapping.container using OOP: - Created unified interface Container for all containers.

---
+++ src/main/java/com/imcode/imcms/mapping/container/Container.java
@@ -0,0 +1,12 @@
+package com.imcode.imcms.mapping.container;
+
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 22.11.16.
+ */
+public interface Container {
+
+    int getDocId();
+
+    int getVersionNo();
+}
