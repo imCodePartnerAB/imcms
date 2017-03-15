9474fc9502ed103094e0adf49b57921e24391775
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-03-15 19:26:32 +0200

    IMCMS-150 - Rewrite js hell in imCMS and use AMD mechanism: - Permission module refactored.

--- src/main/web/imcms/scripts/imcms_permission.js
+++ src/main/web/imcms/scripts/imcms_permission.js
@@ -1,31 +1,24 @@
 /**
  * Created by Shadowgun on 14.04.2015.
+ *
+ * Refactored by Serhii Maksymchuk, 2017
  */
+(function (Imcms) {
+    var permissionUrl = Imcms.Linker.get("permission"),
+        api = {};
 
-Imcms.Permission = {};
-Imcms.Permission.Loader = function () {
-    this.init();
-};
-Imcms.Permission.Loader.prototype = {
-    _api: {},
-    init: function () {
-        this._api = new Imcms.Permission.API();
-    },
-    read: function (callback) {
-        this._api.read({}, callback);
-    }
-};
+    Imcms.Permission = {};
+    Imcms.Permission.Loader = function () {
+        this.init();
+    };
+    Imcms.Permission.Loader.prototype = {
+        init: function () {
+            api = new Imcms.REST.API(permissionUrl);
+        },
+        read: function (callback) {
+            api.read({}, callback);
+        }
+    };
 
-Imcms.Permission.API = function () {
-
-};
-Imcms.Permission.API.prototype = {
-    read: function (request, response) {
-        $.ajax({
-            url: Imcms.Linker.get("permission"),
-            type: "GET",
-            data: request,
-            success: response
-        });
-    }
-};
+    return Imcms;
+})(Imcms);
