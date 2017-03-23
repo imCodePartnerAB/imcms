aae610ff2869b593e36a924f2859f7a641b9e9c9
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-03-23 17:36:24 +0200

    IMCMS-150 - Rewrite js hell in imCMS and use AMD mechanism: - Refactored Role API as module.

--- src/main/web/imcms/scripts/imcms_role.js
+++ src/main/web/imcms/scripts/imcms_role.js
@@ -1,31 +1,24 @@
 /**
  * Created by Shadowgun on 14.04.2015.
+ *
+ * Refactored by Serhii Maksymchuk, 2017
  */
+(function (Imcms) {
+    var roleUrl = Imcms.Linker.get("role"),
+        api = {};
 
-Imcms.Role = {};
-Imcms.Role.Loader = function () {
-    this.init();
-};
-Imcms.Role.Loader.prototype = {
-    _api: {},
-    init: function () {
-        this._api = new Imcms.Role.API();
-    },
-    read: function (callback) {
-        this._api.read({}, callback);
-    }
-};
+    Imcms.Role = {};
+    Imcms.Role.Loader = function () {
+        this.init();
+    };
+    Imcms.Role.Loader.prototype = {
+        init: function () {
+            api = new Imcms.REST.API(roleUrl);
+        },
+        read: function (callback) {
+            api.read({}, callback);
+        }
+    };
 
-Imcms.Role.API = function () {
-
-};
-Imcms.Role.API.prototype = {
-    read: function (request, response) {
-        $.ajax({
-            url: Imcms.Linker.get("role"),
-            type: "GET",
-            data: request,
-            success: response
-        });
-    }
-};
+    return Imcms;
+})(Imcms);
