38081424ac22b4bc3a7a1f97cab2f0aa2230a4e5
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-03-24 17:59:33 +0200

    IMCMS-150 - Rewrite js hell in imCMS and use AMD mechanism: - Rewritten File and Folder functionality as modules.

--- src/main/web/imcms/scripts/imcms_file.js
+++ src/main/web/imcms/scripts/imcms_file.js
@@ -1,105 +1,108 @@
-Imcms.File = {};
-Imcms.File.API = function () {
-};
-Imcms.File.API.prototype = {
-    create: function (request, response) {
-        Imcms.Logger.log("File.API::create :",
-            $.ajax({
-                url: Imcms.Linker.get("files", request.folder, request.filename),
-                contentType: false,
-                processData: false,
-                type: "POST",
-                data: request.data,
-                success: response
-            }), request);
-    },
-    read: function (request, response) {
-        Imcms.Logger.log("File.API::read :",
-            $.ajax.bind($, {
-                url: Imcms.Linker.get("files", request.folder, request.file),
-                type: "GET",
-                success: response
-            }), request);
-    },
-    update: function (request, response) {
-        Imcms.Logger.log("File.API::update :",
-            $.ajax.bind($, {
-                url: Imcms.Linker.get("files", request.folder, request.file),
-                type: "PATCH",
-                data: request,
-                success: response
-            }), request);
-    },
-    remove: function (request, response) {
-        Imcms.Logger.log("File.API::remove :",
-            $.ajax.bind($, {
-                url: Imcms.Linker.get("files", request.folder, request.file),
-                type: "DELETE",
-                success: response
-            }), request);
-    }
-};
+(function (Imcms) {
+    Imcms.File = {};
+    Imcms.File.API = function () {
+    };
+    Imcms.File.API.prototype = {
+        create: function (request, response) {
+            Imcms.Logger.log("File.API::create :",
+                $.ajax({
+                    url: Imcms.Linker.get("files", request.folder, request.filename),
+                    contentType: false,
+                    processData: false,
+                    type: "POST",
+                    data: request.data,
+                    success: response
+                }), request);
+        },
+        read: function (request, response) {
+            Imcms.Logger.log("File.API::read :",
+                $.ajax.bind($, {
+                    url: Imcms.Linker.get("files", request.folder, request.file),
+                    type: "GET",
+                    success: response
+                }), request);
+        },
+        update: function (request, response) {
+            Imcms.Logger.log("File.API::update :",
+                $.ajax.bind($, {
+                    url: Imcms.Linker.get("files", request.folder, request.file),
+                    type: "PATCH",
+                    data: request,
+                    success: response
+                }), request);
+        },
+        remove: function (request, response) {
+            Imcms.Logger.log("File.API::remove :",
+                $.ajax.bind($, {
+                    url: Imcms.Linker.get("files", request.folder, request.file),
+                    type: "DELETE",
+                    success: response
+                }), request);
+        }
+    };
 
-Imcms.File.Loader = function () {
+    Imcms.File.Loader = function () {
+    };
+    Imcms.File.Loader.prototype = {
+        _api: new Imcms.File.API(),
+        getAllPictures: function (folder, callback) {
+            this._api.read(
+                {folder: folder || "", file: "*.jpg"},
+                Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
+            );
+            this._api.read(
+                {folder: folder || "", file: "*.jpeg"},
+                Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
+            );
+            this._api.read(
+                {folder: folder || "", file: "*.png"},
+                Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
+            );
+            this._api.read(
+                {folder: folder || "", file: "*.gif"},
+                Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
+            )
+        },
+        getAll: function (folder, callback) {
+            this._api.read(
+                {folder: folder || "", file: "*.*"},
+                Imcms.Logger.log.bind(this, "File::getAll : ", callback)
+            )
+        },
+        get: function (folder, filename, callback) {
+            this._api.read(
+                {folder: folder || "", file: filename || "*.*"},
+                Imcms.Logger.log.bind(this, "File::get : ", callback)
+            )
+        },
+        move: function (folder, filename, folderTo, callback) {
+            this._api.update(
+                {folder: folder || "", file: filename || "*.*", to: folderTo || ""},
+                Imcms.Logger.log.bind(this, "File::move : ", callback)
+            )
+        },
+        removeAll: function (folder, callback) {
+            this._api.remove(
+                {folder: folder || "", file: "*.*"},
+                Imcms.Logger.log.bind(this, "File::removeAll : ", callback)
+            )
+        },
+        remove: function (folder, filename, callback) {
+            this._api.remove(
+                {folder: folder || "", file: filename || "*.*"},
+                Imcms.Logger.log.bind(this, "File::remove : ", callback)
+            )
+        },
+        addPictureFile: function (file, folder, callback) {
+            var data = new FormData();
+            data.append("file", file);
 
-};
-Imcms.File.Loader.prototype = {
-    _api: new Imcms.File.API(),
-    getAllPictures: function (folder, callback) {
-        this._api.read(
-            {folder: folder || "", file: "*.jpg"},
-            Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
-        );
-        this._api.read(
-            {folder: folder || "", file: "*.jpeg"},
-            Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
-        );
-        this._api.read(
-            {folder: folder || "", file: "*.png"},
-            Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
-        );
-        this._api.read(
-            {folder: folder || "", file: "*.gif"},
-            Imcms.Logger.log.bind(this, "File::getAllPictures : ", callback)
-        )
-    },
-    getAll: function (folder, callback) {
-        this._api.read(
-            {folder: folder || "", file: "*.*"},
-            Imcms.Logger.log.bind(this, "File::getAll : ", callback)
-        )
-    },
-    get: function (folder, filename, callback) {
-        this._api.read(
-            {folder: folder || "", file: filename || "*.*"},
-            Imcms.Logger.log.bind(this, "File::get : ", callback)
-        )
-    },
-    move: function (folder, filename, folderTo, callback) {
-        this._api.update(
-            {folder: folder || "", file: filename || "*.*", to: folderTo || ""},
-            Imcms.Logger.log.bind(this, "File::move : ", callback)
-        )
-    },
-    removeAll: function (folder, callback) {
-        this._api.remove(
-            {folder: folder || "", file: "*.*"},
-            Imcms.Logger.log.bind(this, "File::removeAll : ", callback)
-        )
-    },
-    remove: function (folder, filename, callback) {
-        this._api.remove(
-            {folder: folder || "", file: filename || "*.*"},
-            Imcms.Logger.log.bind(this, "File::remove : ", callback)
-        )
-    },
-    addPictureFile: function (file, folder, callback) {
-        var data = new FormData();
-        data.append("file", file);
+            this._api.create(
+                {folder: folder || "", filename: file.name, data: data},
+                Imcms.Logger.log.bind(this, "File::addPictureFile : ", callback)
+            )
+        }
+    };
 
-        this._api.create(
-            {folder: folder || "", filename: file.name, data: data},
-            Imcms.Logger.log.bind(this, "File::addPictureFile : ", callback)
-        )
-    }
-};
+    return Imcms;
+})(Imcms);
