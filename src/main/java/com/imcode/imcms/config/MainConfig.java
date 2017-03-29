014a7240e5a9845e71de7ab24650a768f9d9491a
3emluk <3emluk@gmail.com>
2017-03-29 13:48:32 +0300

    IMCMS-159 - Move XML configuration to Java config: - Beatify MainConfig

--- src/main/java/com/imcode/imcms/config/MainConfig.java
+++ src/main/java/com/imcode/imcms/config/MainConfig.java
@@ -4,23 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by zemluk on 13.10.16.
  */
 @Configuration
-@Import({DBConfig.class, WebConfig.class, ApplicationConfig.class})
-//@Import({ WebConfig.class})
-
-//@Import({WebConfig.class, AppConfig.class})
-@ComponentScan({"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis", "com.imcode.imcms.config"})
-//@ComponentScan(basePackages = {"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.imcode.imcms.mapping.jpa.*") )
-//@ComponentScan(basePackages = {"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.imcode.imcms.mapping.jpa") )
-//@PropertySource("/WEB-INF/conf/server.properties" )
 @PropertySources({@PropertySource(value = "/WEB-INF/conf/server.properties", ignoreResourceNotFound = true)})
-//@Import({ AppConfig.class, DBConfig.class, WebConfig.class})
-//@Import({ WebConfig.class, ApplicationConfig.class})
+@Import({DBConfig.class, WebConfig.class, ApplicationConfig.class})
+@ComponentScan({"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis"})
+//@ComponentScan(basePackages = {"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.imcode.imcms.mapping.jpa*") )
 public class MainConfig {
 
     @Autowired
