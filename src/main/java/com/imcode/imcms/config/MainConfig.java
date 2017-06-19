cc8b8e9c17c0c81d993f5a663701eee210d00598
3emluk <3emluk@gmail.com>
2017-06-19 18:05:02 +0300

    Revert: IMCMS-159 - Move XML configuration to Java config: - Ignoring system properties

--- src/main/java/com/imcode/imcms/config/MainConfig.java
+++ src/main/java/com/imcode/imcms/config/MainConfig.java
@@ -13,6 +13,7 @@ import org.springframework.core.env.Environment;
 @PropertySources({@PropertySource(value = "/WEB-INF/conf/server.properties", ignoreResourceNotFound = true)})
 @Import({DBConfig.class, WebConfig.class, ApplicationConfig.class})
 @ComponentScan({"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis", "com.imcode.imcms.document.text"})
+//@ComponentScan(basePackages = {"com.imcode.imcms.mapping", "com.imcode.imcms.imagearchive", "com.imcode.imcms.api.linker", "imcode.util", "com.imcode.imcms.servlet.apis"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASPECTJ, pattern = "com.imcode.imcms.mapping.jpa*") )
 public class MainConfig {
 
     @Autowired
@@ -21,8 +22,6 @@ public class MainConfig {
     //    Required to be able to access properties file from environment at other configs
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        propertySourcesPlaceholderConfigurer.setLocalOverride(true);
-        return propertySourcesPlaceholderConfigurer;
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
