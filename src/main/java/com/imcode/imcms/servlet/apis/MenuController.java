d144373f4f095bd22af80f46de47c96acca09ee1
RuslanPopenko <ruslanpopenko@ukr.net>
2017-09-22 15:02:29 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - Added menu service in which controller logic was moved.

--- src/main/java/com/imcode/imcms/servlet/apis/MenuController.java
+++ src/main/java/com/imcode/imcms/servlet/apis/MenuController.java
@@ -1,39 +1,27 @@
 package com.imcode.imcms.servlet.apis;
 
 import com.imcode.imcms.mapping.dto.MenuElementDTO;
-import com.imcode.imcms.mapping.DocumentMapper;
-import com.imcode.imcms.mapping.mappers.Mappable;
-import imcode.server.document.textdocument.MenuItemDomainObject.TreeMenuItemDomainObject;
-import imcode.server.document.textdocument.TextDocumentDomainObject;
+import com.imcode.imcms.service.MenuElementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/menu")
 public class MenuController {
 
-    private final DocumentMapper documentMapper;
-    private final Mappable<TreeMenuItemDomainObject, MenuElementDTO> mapper;
+    private final MenuElementService menuElementService;
 
     @Autowired
-    public MenuController(DocumentMapper documentMapper, Mappable<TreeMenuItemDomainObject, MenuElementDTO> mapper) {
-        this.documentMapper = documentMapper;
-        this.mapper = mapper;
+    public MenuController(MenuElementService menuElementService) {
+        this.menuElementService = menuElementService;
     }
 
     @GetMapping(params = {"docId", "menuId"})
     public List<MenuElementDTO> getMenuItems(@RequestParam("menuId") int menuNo,
                                              @RequestParam("docId") int documentId) {
-        return documentMapper
-                .<TextDocumentDomainObject>getWorkingDocument(documentId)
-                .getMenu(menuNo)
-                .getMenuItemsAsTree()
-                .stream()
-                .map(mapper::map)
-                .collect(Collectors.toList());
+        return menuElementService.getMenuElements(menuNo, documentId);
     }
 
 }
