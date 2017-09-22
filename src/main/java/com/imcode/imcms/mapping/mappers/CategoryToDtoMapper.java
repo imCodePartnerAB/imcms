d9380a5be71fdfc6c998469e139ff9a03fddaf37
RuslanPopenko <ruslanpopenko@ukr.net>
2017-09-22 15:22:35 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - Renamed CategoryDomainObjectToDtoMapper to CategoryToDtoMapper.

--- src/main/java/com/imcode/imcms/mapping/mappers/CategoryToDtoMapper.java
+++ src/main/java/com/imcode/imcms/mapping/mappers/CategoryToDtoMapper.java
@@ -5,7 +5,7 @@ import com.imcode.imcms.mapping.jpa.doc.Category;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CategoryDomainObjectToDtoMapper implements Mappable<Category, CategoryDTO> {
+public class CategoryToDtoMapper implements Mappable<Category, CategoryDTO> {
 
     @Override
     public CategoryDTO map(Category category) {
