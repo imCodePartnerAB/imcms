55601417524a478697d1cda206179a05657352cc
RuslanPopenko <ruslanpopenko@ukr.net>
2017-09-22 17:25:17 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - Refactoring DTO with lombok usages.

--- src/main/java/com/imcode/imcms/mapping/dto/CategoryDTO.java
+++ src/main/java/com/imcode/imcms/mapping/dto/CategoryDTO.java
@@ -1,19 +1,18 @@
 package com.imcode.imcms.mapping.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.io.Serializable;
 
-public class CategoryDTO extends AbstractIdDTO implements Serializable{
+@Getter
+@AllArgsConstructor
+public class CategoryDTO implements Serializable{
 
     private static final long serialVersionUID = -848500041308510098L;
 
+    private Integer id;
+
     private String name;
 
-    public CategoryDTO(Integer id, String name) {
-        super(id);
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
