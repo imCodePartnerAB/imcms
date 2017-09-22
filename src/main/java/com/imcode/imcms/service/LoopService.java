a054d141f9a18f15c108beae23771284f1f08eb4
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-22 14:42:26 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Created loop service class.

---
+++ src/main/java/com/imcode/imcms/service/LoopService.java
@@ -0,0 +1,12 @@
+package com.imcode.imcms.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Serhii Maksymchuk from Ubrainians for imCode
+ * 22.09.17.
+ */
+@Service
+public class LoopService {
+
+}
