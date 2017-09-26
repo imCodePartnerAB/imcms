ca8321fe915f80a8316b43b4162e66e8161963b8
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-26 10:39:51 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Test config updated.

--- src/test/java/com/imcode/imcms/test/EnvironmentConfig.java
+++ src/test/java/com/imcode/imcms/test/EnvironmentConfig.java
@@ -2,7 +2,6 @@ package com.imcode.imcms.test;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.core.env.StandardEnvironment;
 
 import javax.inject.Inject;
@@ -11,15 +10,8 @@ import javax.inject.Inject;
 @PropertySource("classpath:server.properties")
 public class EnvironmentConfig {
 
-    private Environment env;
-
-    public Environment getEnv() {
-        return env;
-    }
-
     @Inject
     public void setEnv(StandardEnvironment env) {
         env.getPropertySources().remove(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME);
-        this.env = env;
     }
 }
--- src/test/java/com/imcode/imcms/test/TestConfig.java
+++ src/test/java/com/imcode/imcms/test/TestConfig.java
@@ -1,21 +1,60 @@
 package com.imcode.imcms.test;
 
+import com.imcode.imcms.api.linker.LinkService;
+import com.imcode.imcms.api.linker.StringLink;
+import com.imcode.imcms.config.ApplicationConfig;
+import com.imcode.imcms.config.DBConfig;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.StandardServletEnvironment;
 
 import javax.inject.Inject;
+import javax.servlet.*;
+import javax.servlet.descriptor.JspConfigDescriptor;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @Configuration
-@Import(EnvironmentConfig.class)
+@PropertySource(value = "classpath:server.properties", name = "imcms.properties")
+@Import({
+        DBConfig.class,
+        ApplicationConfig.class
+})
+@ComponentScan({
+        "com.imcode.imcms.mapping",
+        "com.imcode.imcms.imagearchive",
+        "com.imcode.imcms.api.linker",
+        "imcode.util",
+        "com.imcode.imcms.service",
+        "com.imcode.imcms.document.text"
+})
 public class TestConfig {
 
-    @Inject
-    private Environment env;
+    private final AbstractEnvironment env;
+
+    @Autowired
+    public TestConfig(AbstractEnvironment env) {
+        this.env = env;
+    }
+
+    //    Required to be able to access properties file from environment at other configs
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Scope("prototype")
     @Bean(destroyMethod = "close")
@@ -30,5 +69,52 @@ public class TestConfig {
         ds.setMaxTotal(1);
 
         return ds;
+    }
+
+    @Bean
+    public Properties imcmsProperties() {
+        return (Properties) env.getPropertySources().get("imcms.properties").getSource();
+    }
+
+    @Bean
+    public ServletContext servletContext() {
+        return new MockServletContext();
+    }
+
+    @Autowired
+    @Bean
+    public MockMvc mockMvc(WebApplicationContext wac) {
+        return webAppContextSetup(wac).build();
+    }
+
+    @Autowired
+    @Bean
+    public LinkService linkService(ServletContext servletContext) {
+        return new LinkService(servletContext) {
+            @Override
+            public void initializeLinksMap(String realPathToJSON) throws IOException {
+                // do nothing!!!1
+            }
+
+            @Override
+            public String get(String... args) {
+                return "";
+            }
+
+            @Override
+            public String forward(String... args) {
+                return "forward:";
+            }
+
+            @Override
+            public String redirect(String... args) {
+                return "redirect:";
+            }
+
+            @Override
+            public List<StringLink> getJSON() {
+                return Collections.emptyList();
+            }
+        };
     }
 }
