e13d7c59db2c2aa21bbf80754bc68ba2ce657b30
RuslanPopenko <ruslanpopenko@ukr.net>
2017-09-27 14:58:39 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - Refactoring.

--- src/test/java/com/imcode/imcms/mapping/mapper/AbstractMapperTest.java
+++ src/test/java/com/imcode/imcms/mapping/mapper/AbstractMapperTest.java
@@ -1,7 +1,6 @@
 package com.imcode.imcms.mapping.mapper;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
