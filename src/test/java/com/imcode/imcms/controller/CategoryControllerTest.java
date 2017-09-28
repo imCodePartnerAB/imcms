3de962a380454e2eb894c91a8806a159f9a29af1
RuslanPopenko <ruslanpopenko@ukr.net>
2017-09-28 15:44:37 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - CategoryControllerTest.

---
+++ src/test/java/com/imcode/imcms/controller/CategoryControllerTest.java
@@ -0,0 +1,61 @@
+package com.imcode.imcms.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imcode.imcms.config.TestConfig;
+import com.imcode.imcms.config.WebTestConfig;
+import com.imcode.imcms.util.datainitializer.CategoryDataInitializer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class, WebTestConfig.class})
+@WebAppConfiguration
+public class CategoryControllerTest {
+
+    @Autowired
+    private CategoryDataInitializer categoryDataInitializer;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    @After
+    public void cleanRepos() {
+        categoryDataInitializer.cleanRepositories();
+    }
+
+    @Test
+    public void getAllExpectedOkAndCategoriesJsonResponseTest() throws Exception {
+        categoryDataInitializer.init(4);
+        final String expectedCategories = objectMapper.writeValueAsString(categoryDataInitializer.getCategoriesAsDTO());
+
+        mockMvc.perform(get("/categories"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json(expectedCategories));
+    }
+
+    @Test
+    public void getAllExpectedEmptyArrayOnNonExistingCategories() throws Exception {
+        mockMvc.perform(get("/categories"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(content().json("[]"));
+    }
+
+}
