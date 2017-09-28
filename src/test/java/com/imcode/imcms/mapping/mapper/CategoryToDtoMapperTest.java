a0d544bbcd19b851bf22914293094fc776d5afd8
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-09-28 16:25:55 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Moved mapping functions to their own config.

--- src/test/java/com/imcode/imcms/mapping/mapper/CategoryToDtoMapperTest.java
+++ src/test/java/com/imcode/imcms/mapping/mapper/CategoryToDtoMapperTest.java
@@ -7,9 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * @see CategoryToDtoMapper
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 public class CategoryToDtoMapperTest extends AbstractMapperTest<Category, CategoryDTO> {
