a2177722a34bf79dc2bf99ef9ac5009aef1c098a
RuslanPopenko <ruslanpopenko@ukr.net>
2017-10-02 11:28:11 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - Category and CategoryType with lombok.

--- src/main/java/com/imcode/imcms/persistence/entity/Category.java
+++ src/main/java/com/imcode/imcms/persistence/entity/Category.java
@@ -1,8 +1,15 @@
 package com.imcode.imcms.persistence.entity;
 
-import javax.persistence.*;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "categories")
 public class Category implements Cloneable {
@@ -12,6 +19,7 @@ public class Category implements Cloneable {
     @Column(name = "category_id")
     private Integer id;
 
+    @NotNull
     private String name;
 
     private String description = "";
@@ -23,19 +31,8 @@ public class Category implements Cloneable {
     @JoinColumn(name = "category_type_id", nullable = false)
     private CategoryType type;
 
-    public Category() {
-    }
-
     public Category(String name, String description, String imageUrl, CategoryType type) {
         this(null, name, description, imageUrl, type);
-    }
-
-    public Category(Integer id, String name, String description, String imageUrl, CategoryType type) {
-        this.id = id;
-        this.description = description;
-        this.type = type;
-        this.name = name;
-        this.imageUrl = imageUrl;
     }
 
     @Override
@@ -50,74 +47,4 @@ public class Category implements Cloneable {
         }
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, description, type, name, imageUrl);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj == this || (obj instanceof Category && equals((Category) obj));
-    }
-
-    private boolean equals(Category that) {
-        return Objects.equals(id, that.id)
-                && Objects.equals(description, that.description)
-                && Objects.equals(type, that.type)
-                && Objects.equals(name, that.name)
-                && Objects.equals(imageUrl, that.imageUrl);
-
-
-    }
-
-    @Override
-    public String toString() {
-        return com.google.common.base.Objects.toStringHelper(this)
-                .add("id", id)
-                .add("description", description)
-                .add("type", type)
-                .add("name", name)
-                .add("imageUrl", imageUrl)
-                .toString();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public CategoryType getType() {
-        return type;
-    }
-
-    public void setType(CategoryType type) {
-        this.type = type;
-    }
 }
