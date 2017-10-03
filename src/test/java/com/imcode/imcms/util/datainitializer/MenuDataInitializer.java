ee7045a11715a3e72525dafcdf6055c599f583b4
RuslanPopenko <ruslanpopenko@ukr.net>
2017-10-03 20:03:44 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors:  - Tests fixes.

--- src/test/java/com/imcode/imcms/util/datainitializer/MenuDataInitializer.java
+++ src/test/java/com/imcode/imcms/util/datainitializer/MenuDataInitializer.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
-public class MenuDataInitializer implements RepositoryCleaner {
+public class MenuDataInitializer extends RepositoryCleaner {
 
     private final MenuRepository menuRepository;
     private final VersionDataInitializer versionDataInitializer;
