748af77c5001b09489b67cb8a917571126ad58e6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-03 17:51:35 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Refactoring.

--- src/test/java/com/imcode/imcms/util/RepositoryCleaner.java
+++ src/test/java/com/imcode/imcms/util/RepositoryCleaner.java
@@ -2,11 +2,11 @@ package com.imcode.imcms.util;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RepositoryCleaner {
+public abstract class RepositoryCleaner {
 
-    void cleanRepositories();
+    public abstract void cleanRepositories();
 
-    default void cleanRepositories(JpaRepository... jpaRepositories) {
+    public void cleanRepositories(JpaRepository... jpaRepositories) {
         for (JpaRepository jpaRepository : jpaRepositories) {
             jpaRepository.deleteAll();
             jpaRepository.flush();
