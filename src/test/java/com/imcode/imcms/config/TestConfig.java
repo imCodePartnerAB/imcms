eb71fa1e3cddea661988ad68859c88d2a88249ac
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-05 16:42:45 +0300

    IMCMS-256 - Upgrade loop functionality to work with new loop API on client side: - Test config fully used main config.

--- src/test/java/com/imcode/imcms/config/TestConfig.java
+++ src/test/java/com/imcode/imcms/config/TestConfig.java
@@ -1,15 +1,8 @@
 package com.imcode.imcms.config;
 
-import com.imcode.imcms.db.DB;
-import com.imcode.imcms.db.Schema;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-import java.io.InputStream;
 
 @Configuration
 @Import({MainConfig.class})
@@ -17,20 +10,4 @@ import java.io.InputStream;
         "com.imcode.imcms.util.datainitializer"
 })
 public class TestConfig {
-
-    private final DataSource dataSource;
-
-    @Autowired
-    public TestConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @PostConstruct
-    public void createImcmsDatabaseStructure() {
-        final InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("schema.xml");
-        final Schema schema = Schema.fromInputStream(inputStream);
-        final DB db = new DB(dataSource);
-        db.prepare(schema.setScriptsDir("src/main/webapp/WEB-INF/sql"));
-    }
-
 }
