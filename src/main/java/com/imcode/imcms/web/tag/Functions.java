947358cca1596fa2b72cea7b01812b280595dd20
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-05 16:50:32 +0300

    IMCMS-256 - Upgrade loop functionality to work with new loop API on client side: - Removed unused function.

--- src/main/java/com/imcode/imcms/web/tag/Functions.java
+++ src/main/java/com/imcode/imcms/web/tag/Functions.java
@@ -2,13 +2,11 @@ package com.imcode.imcms.web.tag;
 
 import com.imcode.imcms.api.TextDocument;
 import imcode.server.document.DocumentDomainObject;
-import imcode.server.document.textdocument.TextDocumentDomainObject;
 import imcode.server.user.UserDomainObject;
 import imcode.util.Utility;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
-import java.util.Map;
 
 public class Functions {
 
@@ -26,12 +24,5 @@ public class Functions {
 
     public static TextDocument getDocument(String docIdentifier, PageContext pageContext) {
         return Utility.getCMS(pageContext).getDocumentService().getTextDocument(docIdentifier);
-    }
-
-    @SuppressWarnings("unchecked")
-    public static TextDocument.LoopItem createLoopItem(Object entry, int loopNo,
-                                                       TextDocumentDomainObject document) {
-        // this cast have to be done since JSP processor doesn't understand interface in interface or something
-        return new TextDocument.LoopItem(((Map.Entry<Integer, Boolean>) entry), loopNo, document);
     }
 }
