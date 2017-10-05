b2d662963041d9cebdfd83750d413ecee05205b7
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-05 16:33:26 +0300

    IMCMS-256 - Upgrade loop functionality to work with new loop API on client side: - Old tag classes commented as first step for removal.

--- src/main/java/com/imcode/imcms/servlet/tags/LoopTag.java
+++ src/main/java/com/imcode/imcms/servlet/tags/LoopTag.java
@@ -1,176 +1,176 @@
-package com.imcode.imcms.servlet.tags;
-
-import com.imcode.imcms.api.Loop;
-import com.imcode.imcms.api.TextDocument;
-import com.imcode.imcms.mapping.container.LoopEntryRef;
-import com.imcode.imcms.servlet.tags.Editor.LoopEditor;
-import imcode.server.document.textdocument.TextDocumentDomainObject;
-import imcode.server.parser.ParserParameters;
-import imcode.server.parser.TagParser;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.BodyContent;
-import javax.servlet.jsp.tagext.BodyTagSupport;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Properties;
-
-public class LoopTag extends BodyTagSupport implements IEditableTag {
-
-    /**
-     * Loop number in a TextDocument.
-     */
-    private int no;
-
-    private Loop loop;
-
-    private Iterator<Map.Entry<Integer, Boolean>> loopIterator;
-
-    private volatile Map.Entry<Integer, Boolean> currentEntry;
-
-    private Properties attributes = new Properties();
-
-    /**
-     * Label - common imcms attribute.
-     */
-    private String label;
-
-    private boolean editMode;
-
-    private HttpServletRequest request;
-
-    private TextDocumentDomainObject document;
-
-    private ParserParameters parserParameters;
-
-    @Override
-    public int doStartTag() throws JspException {
-
-        request = (HttpServletRequest) pageContext.getRequest();
-        parserParameters = ParserParameters.fromRequest(request);
-        document = parserParameters.getDocumentRequest().getDocument();
-        editMode = TagParser.isEditable(attributes, parserParameters.isContentLoopMode());
-
-        loop = document.getLoop(no);
-
-        if (loop == null) {
-            loop = Loop.singleEntry();
-
-            document.setLoop(no, loop);
-        }
-        loopIterator = loop.getEntries().entrySet().iterator();
-
-        return doAfterBody();
-    }
-
-    @Override
-    public int doAfterBody() throws JspException {
-        while (loopIterator.hasNext()) {
-            currentEntry = loopIterator.next();
-
-            // check is current entry enabled
-            if (currentEntry.getValue()) {
-                final TextDocument.LoopItem loopItem = new TextDocument.LoopItem(currentEntry, no, document);
-                pageContext.setAttribute("loopItem", loopItem);
-                request.setAttribute("loopEntryRef", loopItem.getLoopEntryRef()); // for nested tags usage
-                return EVAL_BODY_AGAIN;
-            }
-        }
-
-        currentEntry = null;
-        return SKIP_BODY;
-    }
-
-    private String getCurrentBodyContent() {
-        BodyContent bodyContent = getBodyContent();
-        Map<Integer, Boolean> loopEntries = loop.getEntries();
-
-        boolean hasBodyContent = (null != bodyContent);
-        boolean hasEntries = (!loopEntries.isEmpty());
-        boolean hasEnabledEntries = (hasEntries && loopEntries.values().stream().anyMatch(Boolean::booleanValue));
-
-        return (hasBodyContent && hasEnabledEntries)
-                ? bodyContent.getString()
-                : "";
-    }
-
-    @Override
-    public int doEndTag() throws JspException {
-        try {
-            String bodyContentString = getCurrentBodyContent();
-
-            if (editMode) {
-                LoopEditor editor = createEditor().setNo(no);
-                bodyContentString = editor.wrap(bodyContentString);
-            }
-
-            bodyContentString = TagParser.addPreAndPost(attributes, bodyContentString);
-
-            if (editMode) {
-                try {
-                    request.setAttribute("loopNo", no);
-                    request.setAttribute("loop", loop);
-                    request.setAttribute("content", bodyContentString);
-                    request.setAttribute("document", document);
-                    request.setAttribute("label", label);
-                    request.setAttribute("flags", parserParameters.getFlags());
-                } catch (Exception e) {
-                    throw new JspException(e);
-                }
-            }
-            pageContext.getOut().write(bodyContentString);
-        } catch (IOException | RuntimeException e) {
-            throw new JspException(e);
-        }
-        return EVAL_PAGE;
-    }
-
-    public int getNo() {
-        return no;
-    }
-
-    public void setNo(int no) {
-        this.no = no;
-    }
-
-    public void setMode(String mode) {
-        attributes.setProperty("mode", mode);
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public void setPre(String pre) {
-        attributes.setProperty("pre", pre);
-    }
-
-    public void setPost(String post) {
-        attributes.setProperty("post", post);
-    }
-
-    public Loop getLoop() {
-        return loop;
-    }
-
-    public void setLoop(Loop loop) {
-        this.loop = loop;
-    }
-
-    public LoopEntryRef getLoopEntryRef() {
-        return LoopEntryRef.of(no, currentEntry.getKey());
-    }
-
-    // @Override
-    public LoopEditor createEditor() {
-        return new LoopEditor();
-    }
-
-    void invalidateCurrentEntry() {
-        currentEntry = null;
-        pageContext.removeAttribute("loopItem");
-        request.removeAttribute("loopEntryRef");
-    }
-}
+//package com.imcode.imcms.servlet.tags;
+//
+//import com.imcode.imcms.api.Loop;
+//import com.imcode.imcms.api.TextDocument;
+//import com.imcode.imcms.mapping.container.LoopEntryRef;
+//import com.imcode.imcms.servlet.tags.Editor.LoopEditor;
+//import imcode.server.document.textdocument.TextDocumentDomainObject;
+//import imcode.server.parser.ParserParameters;
+//import imcode.server.parser.TagParser;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.jsp.JspException;
+//import javax.servlet.jsp.tagext.BodyContent;
+//import javax.servlet.jsp.tagext.BodyTagSupport;
+//import java.io.IOException;
+//import java.util.Iterator;
+//import java.util.Map;
+//import java.util.Properties;
+//
+//public class LoopTag extends BodyTagSupport implements IEditableTag {
+//
+//    /**
+//     * Loop number in a TextDocument.
+//     */
+//    private int no;
+//
+//    private Loop loop;
+//
+//    private Iterator<Map.Entry<Integer, Boolean>> loopIterator;
+//
+//    private volatile Map.Entry<Integer, Boolean> currentEntry;
+//
+//    private Properties attributes = new Properties();
+//
+//    /**
+//     * Label - common imcms attribute.
+//     */
+//    private String label;
+//
+//    private boolean editMode;
+//
+//    private HttpServletRequest request;
+//
+//    private TextDocumentDomainObject document;
+//
+//    private ParserParameters parserParameters;
+//
+//    @Override
+//    public int doStartTag() throws JspException {
+//
+//        request = (HttpServletRequest) pageContext.getRequest();
+//        parserParameters = ParserParameters.fromRequest(request);
+//        document = parserParameters.getDocumentRequest().getDocument();
+//        editMode = TagParser.isEditable(attributes, parserParameters.isContentLoopMode());
+//
+//        loop = document.getLoop(no);
+//
+//        if (loop == null) {
+//            loop = Loop.singleEntry();
+//
+//            document.setLoop(no, loop);
+//        }
+//        loopIterator = loop.getEntries().entrySet().iterator();
+//
+//        return doAfterBody();
+//    }
+//
+//    @Override
+//    public int doAfterBody() throws JspException {
+//        while (loopIterator.hasNext()) {
+//            currentEntry = loopIterator.next();
+//
+//            // check is current entry enabled
+//            if (currentEntry.getValue()) {
+//                final TextDocument.LoopItem loopItem = new TextDocument.LoopItem(currentEntry, no, document);
+//                pageContext.setAttribute("loopItem", loopItem);
+//                request.setAttribute("loopEntryRef", loopItem.getLoopEntryRef()); // for nested tags usage
+//                return EVAL_BODY_AGAIN;
+//            }
+//        }
+//
+//        currentEntry = null;
+//        return SKIP_BODY;
+//    }
+//
+//    private String getCurrentBodyContent() {
+//        BodyContent bodyContent = getBodyContent();
+//        Map<Integer, Boolean> loopEntries = loop.getEntries();
+//
+//        boolean hasBodyContent = (null != bodyContent);
+//        boolean hasEntries = (!loopEntries.isEmpty());
+//        boolean hasEnabledEntries = (hasEntries && loopEntries.values().stream().anyMatch(Boolean::booleanValue));
+//
+//        return (hasBodyContent && hasEnabledEntries)
+//                ? bodyContent.getString()
+//                : "";
+//    }
+//
+//    @Override
+//    public int doEndTag() throws JspException {
+//        try {
+//            String bodyContentString = getCurrentBodyContent();
+//
+//            if (editMode) {
+//                LoopEditor editor = createEditor().setNo(no);
+//                bodyContentString = editor.wrap(bodyContentString);
+//            }
+//
+//            bodyContentString = TagParser.addPreAndPost(attributes, bodyContentString);
+//
+//            if (editMode) {
+//                try {
+//                    request.setAttribute("loopNo", no);
+//                    request.setAttribute("loop", loop);
+//                    request.setAttribute("content", bodyContentString);
+//                    request.setAttribute("document", document);
+//                    request.setAttribute("label", label);
+//                    request.setAttribute("flags", parserParameters.getFlags());
+//                } catch (Exception e) {
+//                    throw new JspException(e);
+//                }
+//            }
+//            pageContext.getOut().write(bodyContentString);
+//        } catch (IOException | RuntimeException e) {
+//            throw new JspException(e);
+//        }
+//        return EVAL_PAGE;
+//    }
+//
+//    public int getNo() {
+//        return no;
+//    }
+//
+//    public void setNo(int no) {
+//        this.no = no;
+//    }
+//
+//    public void setMode(String mode) {
+//        attributes.setProperty("mode", mode);
+//    }
+//
+//    public void setLabel(String label) {
+//        this.label = label;
+//    }
+//
+//    public void setPre(String pre) {
+//        attributes.setProperty("pre", pre);
+//    }
+//
+//    public void setPost(String post) {
+//        attributes.setProperty("post", post);
+//    }
+//
+//    public Loop getLoop() {
+//        return loop;
+//    }
+//
+//    public void setLoop(Loop loop) {
+//        this.loop = loop;
+//    }
+//
+//    public LoopEntryRef getLoopEntryRef() {
+//        return LoopEntryRef.of(no, currentEntry.getKey());
+//    }
+//
+//    // @Override
+//    public LoopEditor createEditor() {
+//        return new LoopEditor();
+//    }
+//
+//    void invalidateCurrentEntry() {
+//        currentEntry = null;
+//        pageContext.removeAttribute("loopItem");
+//        request.removeAttribute("loopEntryRef");
+//    }
+//}
--- src/main/java/com/imcode/imcms/servlet/tags/MenuTag.java
+++ src/main/java/com/imcode/imcms/servlet/tags/MenuTag.java
@@ -1,161 +1,161 @@
-package com.imcode.imcms.servlet.tags;
-
-import com.imcode.imcms.api.TextDocument;
-import com.imcode.imcms.servlet.tags.Editor.MenuEditor;
-import imcode.server.Imcms;
-import imcode.server.document.DocumentDomainObject;
-import imcode.server.document.DocumentReference;
-import imcode.server.document.textdocument.MenuItemDomainObject;
-import imcode.server.document.textdocument.TextDocumentDomainObject;
-import imcode.server.parser.ParserParameters;
-import imcode.server.parser.TagParser;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.tagext.BodyTagSupport;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Properties;
-
-public class MenuTag extends BodyTagSupport implements IEditableTag {
-
-    private volatile int no;
-    private volatile int docId = 1001;
-    private volatile Properties attributes = new Properties();
-    private volatile LinkedList<MenuItemDomainObject.TreeMenuItemDomainObject> menuItemsCollection;
-    private volatile MenuItemDomainObject menuItem;
-    private volatile String label;
-    private volatile String template;
-    private boolean editMode;
-    private HttpServletRequest request;
-
-    public int doStartTag() throws JspException {
-        request = (HttpServletRequest) pageContext.getRequest();
-        ParserParameters parserParameters = ParserParameters.fromRequest(request);
-
-        TextDocumentDomainObject document = (docId >= 1001)
-                ? Imcms.getServices().getDocumentMapper().getVersionedDocument(docId, request)
-                : parserParameters.getDocumentRequest().getDocument();
-
-        menuItemsCollection = document.getMenu(no).getMenuItemsVisibleToUserAsTree();
-        editMode = TagParser.isEditable(attributes, parserParameters.isMenuMode());
-
-        return (menuItemsCollection.size() > 0)
-                ? EVAL_BODY_BUFFERED
-                : SKIP_BODY;
-    }
-
-    public boolean nextMenuItem(MenuItemDomainObject menuItem) {
-        if (menuItem != null) {
-            DocumentDomainObject document = DocumentDomainObject.asDefaultUser(menuItem.getDocument());
-            DocumentReference docIdentity = Imcms.getServices()
-                    .getDocumentMapper()
-                    .getDocumentReference(document);
-
-            menuItem.setDocumentReference(docIdentity);
-
-            this.menuItem = menuItem;
-            TextDocument.MenuItem item = new TextDocument.MenuItem(
-                    menuItem, document, Imcms.fromRequest(request)
-            );
-            pageContext.setAttribute("menuitem", item);
-            return true;
-        } else {
-            invalidateMenuItem();
-            return false;
-        }
-    }
-
-    public int doAfterBody() throws JspException {
-        return (menuItemsCollection.size() > 0)
-                ? EVAL_BODY_AGAIN
-                : SKIP_BODY;
-    }
-
-    public int doEndTag() throws JspException {
-        try {
-            String bodyContentString = (null != getBodyContent())
-                    ? getBodyContent().getString()
-                    : "";
-            bodyContent = null;
-
-            if (editMode) {
-                bodyContentString = createEditor().setNo(no)
-                        .setDocumentId(docId)
-                        .wrap(bodyContentString);
-            }
-
-            bodyContentString = TagParser.addPreAndPost(attributes, bodyContentString);
-            pageContext.getOut().write(bodyContentString);
-
-        } catch (IOException | RuntimeException e) {
-            throw new JspException(e);
-        }
-        return EVAL_PAGE;
-    }
-
-    public LinkedList<MenuItemDomainObject.TreeMenuItemDomainObject> getList() {
-        return menuItemsCollection;
-    }
-
-    public int getNo() {
-        return no;
-    }
-
-    public void setNo(int no) {
-        this.no = no;
-    }
-
-    public void setMode(String mode) {
-        attributes.setProperty("mode", mode);
-    }
-
-    public void setPre(String pre) {
-        attributes.setProperty("pre", pre);
-    }
-
-    public void setPost(String post) {
-        attributes.setProperty("post", post);
-    }
-
-    public MenuItemDomainObject getMenuItem() {
-        if (null == menuItem) {
-            nextMenuItem(null); //fixme: Hey, something wrong here
-        }
-        return menuItem;
-    }
-
-    public void invalidateMenuItem() {
-        menuItem = null;
-        pageContext.removeAttribute("menuitem");
-    }
-
-    public String getLabel() {
-        return label;
-    }
-
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
-    public String getTemplate() {
-        return template;
-    }
-
-    public void setTemplate(String template) {
-        this.template = template;
-    }
-
-    @Override
-    public MenuEditor createEditor() {
-        return new MenuEditor();
-    }
-
-    public int getDocId() {
-        return docId;
-    }
-
-    public void setDocId(int docId) {
-        this.docId = docId;
-    }
-}
+//package com.imcode.imcms.servlet.tags;
+//
+//import com.imcode.imcms.api.TextDocument;
+//import com.imcode.imcms.servlet.tags.Editor.MenuEditor;
+//import imcode.server.Imcms;
+//import imcode.server.document.DocumentDomainObject;
+//import imcode.server.document.DocumentReference;
+//import imcode.server.document.textdocument.MenuItemDomainObject;
+//import imcode.server.document.textdocument.TextDocumentDomainObject;
+//import imcode.server.parser.ParserParameters;
+//import imcode.server.parser.TagParser;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.jsp.JspException;
+//import javax.servlet.jsp.tagext.BodyTagSupport;
+//import java.io.IOException;
+//import java.util.LinkedList;
+//import java.util.Properties;
+//
+//public class MenuTag extends BodyTagSupport implements IEditableTag {
+//
+//    private volatile int no;
+//    private volatile int docId = 1001;
+//    private volatile Properties attributes = new Properties();
+//    private volatile LinkedList<MenuItemDomainObject.TreeMenuItemDomainObject> menuItemsCollection;
+//    private volatile MenuItemDomainObject menuItem;
+//    private volatile String label;
+//    private volatile String template;
+//    private boolean editMode;
+//    private HttpServletRequest request;
+//
+//    public int doStartTag() throws JspException {
+//        request = (HttpServletRequest) pageContext.getRequest();
+//        ParserParameters parserParameters = ParserParameters.fromRequest(request);
+//
+//        TextDocumentDomainObject document = (docId >= 1001)
+//                ? Imcms.getServices().getDocumentMapper().getVersionedDocument(docId, request)
+//                : parserParameters.getDocumentRequest().getDocument();
+//
+//        menuItemsCollection = document.getMenu(no).getMenuItemsVisibleToUserAsTree();
+//        editMode = TagParser.isEditable(attributes, parserParameters.isMenuMode());
+//
+//        return (menuItemsCollection.size() > 0)
+//                ? EVAL_BODY_BUFFERED
+//                : SKIP_BODY;
+//    }
+//
+//    public boolean nextMenuItem(MenuItemDomainObject menuItem) {
+//        if (menuItem != null) {
+//            DocumentDomainObject document = DocumentDomainObject.asDefaultUser(menuItem.getDocument());
+//            DocumentReference docIdentity = Imcms.getServices()
+//                    .getDocumentMapper()
+//                    .getDocumentReference(document);
+//
+//            menuItem.setDocumentReference(docIdentity);
+//
+//            this.menuItem = menuItem;
+//            TextDocument.MenuItem item = new TextDocument.MenuItem(
+//                    menuItem, document, Imcms.fromRequest(request)
+//            );
+//            pageContext.setAttribute("menuitem", item);
+//            return true;
+//        } else {
+//            invalidateMenuItem();
+//            return false;
+//        }
+//    }
+//
+//    public int doAfterBody() throws JspException {
+//        return (menuItemsCollection.size() > 0)
+//                ? EVAL_BODY_AGAIN
+//                : SKIP_BODY;
+//    }
+//
+//    public int doEndTag() throws JspException {
+//        try {
+//            String bodyContentString = (null != getBodyContent())
+//                    ? getBodyContent().getString()
+//                    : "";
+//            bodyContent = null;
+//
+//            if (editMode) {
+//                bodyContentString = createEditor().setNo(no)
+//                        .setDocumentId(docId)
+//                        .wrap(bodyContentString);
+//            }
+//
+//            bodyContentString = TagParser.addPreAndPost(attributes, bodyContentString);
+//            pageContext.getOut().write(bodyContentString);
+//
+//        } catch (IOException | RuntimeException e) {
+//            throw new JspException(e);
+//        }
+//        return EVAL_PAGE;
+//    }
+//
+//    public LinkedList<MenuItemDomainObject.TreeMenuItemDomainObject> getList() {
+//        return menuItemsCollection;
+//    }
+//
+//    public int getNo() {
+//        return no;
+//    }
+//
+//    public void setNo(int no) {
+//        this.no = no;
+//    }
+//
+//    public void setMode(String mode) {
+//        attributes.setProperty("mode", mode);
+//    }
+//
+//    public void setPre(String pre) {
+//        attributes.setProperty("pre", pre);
+//    }
+//
+//    public void setPost(String post) {
+//        attributes.setProperty("post", post);
+//    }
+//
+//    public MenuItemDomainObject getMenuItem() {
+//        if (null == menuItem) {
+//            nextMenuItem(null); //fixme: Hey, something wrong here
+//        }
+//        return menuItem;
+//    }
+//
+//    public void invalidateMenuItem() {
+//        menuItem = null;
+//        pageContext.removeAttribute("menuitem");
+//    }
+//
+//    public String getLabel() {
+//        return label;
+//    }
+//
+//    public void setLabel(String label) {
+//        this.label = label;
+//    }
+//
+//    public String getTemplate() {
+//        return template;
+//    }
+//
+//    public void setTemplate(String template) {
+//        this.template = template;
+//    }
+//
+//    @Override
+//    public MenuEditor createEditor() {
+//        return new MenuEditor();
+//    }
+//
+//    public int getDocId() {
+//        return docId;
+//    }
+//
+//    public void setDocId(int docId) {
+//        this.docId = docId;
+//    }
+//}
