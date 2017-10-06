94dc3423d4fdffbef243f1208eefe9eb297fcd6a
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-06 12:31:22 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Refactored Image entity.

--- src/main/java/com/imcode/imcms/mapping/jpa/doc/content/textdoc/Image.java
+++ src/main/java/com/imcode/imcms/mapping/jpa/doc/content/textdoc/Image.java
@@ -1,15 +1,70 @@
 package com.imcode.imcms.mapping.jpa.doc.content.textdoc;
 
+import com.imcode.imcms.mapping.jpa.doc.content.VersionedI18nContent;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "imcms_text_doc_images")
-@NamedQuery(name = "Image.allImages",
-        query = "SELECT DISTINCT i FROM Image i")
+@Getter
+@Setter
+public class Image extends VersionedI18nContent {
 
-public class Image extends ImageBase {
+    private Integer no;
+
+    private int width;
+
+    private int height;
+
+    private int border;
+
+    private String align = "";
+
+    @Column(name = "alt_text")
+    private String alternateText = "";
+
+    @Column(name = "low_scr")
+    private String lowResolutionUrl = "";
+
+    @Column(name = "v_space")
+    private int verticalSpace;
+
+    @Column(name = "h_space")
+    private int horizontalSpace;
+
+    private String target = "";
+
+    @Column(name = "linkurl")
+    private String linkUrl = "";
+
+    @Column(name = "imgurl")
+    private String url = "";
+
+    @Column(name = "image_name")
+    private String name = "";
+
+    private Integer type;
+
+    @Column(name = "format", nullable = false, columnDefinition = "smallint")
+    private int format;
+
+    private ImageCropRegion cropRegion = new ImageCropRegion();
+
+    @Column(name = "rotate_angle", nullable = false, columnDefinition = "smallint")
+    private int rotateAngle;
+
+    @Column(name = "gen_file")
+    private String generatedFilename;
+
+    private LoopEntryRef loopEntryRef;
+
+    private int resize;
+
+    @Column(name = "archive_image_id")
+    private Long archiveImageId;
 
 }
-
