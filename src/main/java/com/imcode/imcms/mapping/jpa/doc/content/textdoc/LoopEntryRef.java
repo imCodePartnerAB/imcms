147bc122af637f1bac86d19234085f44454f528b
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-06 14:12:52 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Refactored LoopEntryRef class.

--- src/main/java/com/imcode/imcms/mapping/jpa/doc/content/textdoc/LoopEntryRef.java
+++ src/main/java/com/imcode/imcms/mapping/jpa/doc/content/textdoc/LoopEntryRef.java
@@ -1,12 +1,18 @@
 package com.imcode.imcms.mapping.jpa.doc.content.textdoc;
 
-import com.google.common.base.Objects;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoopEntryRef implements Serializable, Cloneable {
 
     @Column(name = "content_loop_no")
@@ -14,39 +20,4 @@ public class LoopEntryRef implements Serializable, Cloneable {
 
     @Column(name = "content_no")
     private int entryNo;
-
-    public LoopEntryRef() {
-    }
-
-    public LoopEntryRef(int loopNo, int entryNo) {
-        this.loopNo = loopNo;
-        this.entryNo = entryNo;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        return this == o || (o instanceof LoopEntryRef && equals((LoopEntryRef) o));
-    }
-
-    private boolean equals(LoopEntryRef that) {
-        return loopNo == that.loopNo && entryNo == that.entryNo;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(loopNo, entryNo);
-    }
-
-    @Override
-    public String toString() {
-        return Objects.toStringHelper(this).add("entryNo", entryNo).add("loopNo", loopNo).toString();
-    }
-
-    public int getLoopNo() {
-        return loopNo;
-    }
-
-    public int getEntryNo() {
-        return entryNo;
-    }
 }
