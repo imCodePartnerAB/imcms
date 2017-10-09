716d14bd81a8ccd1616ccf2181cd7005279af9ba
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-09 18:21:59 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Added constructor to ImageDTO.

--- src/main/java/com/imcode/imcms/domain/dto/ImageDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/ImageDTO.java
@@ -1,9 +1,11 @@
 package com.imcode.imcms.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ImageDTO {
     private Integer index;
