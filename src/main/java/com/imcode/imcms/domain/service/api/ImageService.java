c0dbaebe65dc5cf248df5efac0b256d5e9f8af06
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-10 11:59:44 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Implemented get image method with test coverage in ImageService.

--- src/main/java/com/imcode/imcms/domain/service/api/ImageService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/ImageService.java
@@ -1,31 +1,53 @@
 package com.imcode.imcms.domain.service.api;
 
+import com.imcode.imcms.domain.dto.ImageDTO;
+import com.imcode.imcms.domain.service.core.CommonContentService;
 import com.imcode.imcms.domain.service.core.VersionService;
+import com.imcode.imcms.mapping.jpa.doc.Language;
 import com.imcode.imcms.mapping.jpa.doc.Version;
 import com.imcode.imcms.persistence.entity.Image;
 import com.imcode.imcms.persistence.repository.ImageRepository;
+import imcode.server.Imcms;
+import imcode.server.user.UserDomainObject;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 public class ImageService {
 
     private final ImageRepository imageRepository;
     private final VersionService versionService;
+    private final CommonContentService commonContentService;
+    private final Function<Image, ImageDTO> imageToImageDTO;
 
-    public ImageService(ImageRepository imageRepository, VersionService versionService) {
+    public ImageService(ImageRepository imageRepository,
+                        VersionService versionService,
+                        CommonContentService commonContentService,
+                        Function<Image, ImageDTO> imageToImageDTO) {
+
         this.imageRepository = imageRepository;
         this.versionService = versionService;
+        this.commonContentService = commonContentService;
+        this.imageToImageDTO = imageToImageDTO;
     }
 
     public Collection<Image> getAllGeneratedImages() {
         return imageRepository.findAllGeneratedImages();
     }
 
-    public Image getImage(int docId, int index) {
-        final Version documentWorkingVersion = versionService.getDocumentWorkingVersion(docId);
-        //todo: implement!
-        return null;
+    public ImageDTO getImage(int docId, int index) {
+        final Version version = versionService.getDocumentWorkingVersion(docId);
+        final int versionIndex = version.getNo();
+        final UserDomainObject user = Imcms.getUser();
+        final Language language = commonContentService.findByDocIdAndVersionNoAndUser(docId, versionIndex, user)
+                .getLanguage();
+
+        final Image image = imageRepository.findByVersionAndLanguageAndIndexWhereLoopEntryRefIsNull(version, language, index);
+        return Optional.ofNullable(image)
+                .map(imageToImageDTO)
+                .orElse(new ImageDTO(index));
     }
 }
--- src/test/java/com/imcode/imcms/domain/service/api/ImageServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/ImageServiceTest.java
@@ -1,7 +1,12 @@
 package com.imcode.imcms.domain.service.api;
 
+import com.imcode.imcms.components.datainitializer.CommonContentDataInitializer;
 import com.imcode.imcms.config.TestConfig;
+import com.imcode.imcms.domain.dto.ImageDTO;
 import com.imcode.imcms.domain.exception.DocumentNotExistException;
+import imcode.server.Imcms;
+import imcode.server.user.UserDomainObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +14,43 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestConfig.class)
 @Transactional
 public class ImageServiceTest {
 
+    private static final int TEST_DOC_ID = 1001;
     private static final int TEST_IMAGE_INDEX = 1;
+    private static final ImageDTO TEST_IMAGE_DTO = new ImageDTO(TEST_IMAGE_INDEX);
 
     @Autowired
     private ImageService imageService;
 
+    @Autowired
+    private CommonContentDataInitializer commonContentDataInitializer;
+
+    @Before
+    public void setUp() throws Exception {
+        commonContentDataInitializer.cleanRepositories();
+        commonContentDataInitializer.createData(TEST_DOC_ID, 0);
+    }
 
     @Test(expected = DocumentNotExistException.class)
     public void getImage_When_DocumentNotExist_Expect_Exception() {
         int nonExistingDocId = 0;
         imageService.getImage(nonExistingDocId, TEST_IMAGE_INDEX);// should throw exception
+    }
+
+    @Test
+    public void getImage_When_NotExist_Expect_EmptyDTO() {
+        final UserDomainObject user = new UserDomainObject(1);
+        user.setLanguageIso639_2("en");
+        Imcms.setUser(user);
+
+        final ImageDTO image = imageService.getImage(TEST_DOC_ID, TEST_IMAGE_INDEX);
+
+        assertEquals(image, TEST_IMAGE_DTO);
     }
 }
