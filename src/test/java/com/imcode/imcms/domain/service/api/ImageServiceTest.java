c3b5cfeefd380b34f253eab8389c9ab43c6027ac
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-10 16:18:47 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Covered by test use case when requesting existing image in service.

--- src/test/java/com/imcode/imcms/domain/service/api/ImageServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/ImageServiceTest.java
@@ -4,8 +4,14 @@ import com.imcode.imcms.components.datainitializer.CommonContentDataInitializer;
 import com.imcode.imcms.config.TestConfig;
 import com.imcode.imcms.domain.dto.ImageDTO;
 import com.imcode.imcms.domain.exception.DocumentNotExistException;
+import com.imcode.imcms.mapping.jpa.doc.LanguageRepository;
+import com.imcode.imcms.mapping.jpa.doc.VersionRepository;
+import com.imcode.imcms.persistence.entity.Image;
+import com.imcode.imcms.persistence.repository.ImageRepository;
+import com.imcode.imcms.util.Value;
 import imcode.server.Imcms;
 import imcode.server.user.UserDomainObject;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +19,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
@@ -31,10 +39,31 @@ public class ImageServiceTest {
     @Autowired
     private CommonContentDataInitializer commonContentDataInitializer;
 
+    @Autowired
+    private LanguageRepository languageRepository;
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @Autowired
+    private VersionRepository versionRepository;
+
+    @Autowired
+    private Function<Image, ImageDTO> imageToImageDTO;
+
     @Before
     public void setUp() throws Exception {
         commonContentDataInitializer.cleanRepositories();
         commonContentDataInitializer.createData(TEST_DOC_ID, 0);
+
+        final UserDomainObject user = new UserDomainObject(1);
+        user.setLanguageIso639_2("en"); // user lang should exist in common content
+        Imcms.setUser(user);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Imcms.removeUser();
     }
 
     @Test(expected = DocumentNotExistException.class)
@@ -52,5 +81,20 @@ public class ImageServiceTest {
         final ImageDTO image = imageService.getImage(TEST_DOC_ID, TEST_IMAGE_INDEX);
 
         assertEquals(image, TEST_IMAGE_DTO);
+    }
+
+    @Test
+    public void getImage_When_ImageExist_Expect_EqualResult() {
+        final Image image = Value.with(new Image(), img -> {
+            img.setIndex(TEST_IMAGE_INDEX);
+            img.setLanguage(languageRepository.findByCode("en"));
+            img.setVersion(versionRepository.findWorking(TEST_DOC_ID));
+        });
+        imageRepository.save(image);
+
+        final ImageDTO imageDTO = imageToImageDTO.apply(image);
+        final ImageDTO resultImage = imageService.getImage(TEST_DOC_ID, TEST_IMAGE_INDEX);
+
+        assertEquals(imageDTO, resultImage);
     }
 }
