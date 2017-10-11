01a50c5de33e29a2bcf6b678cfe4e8820ae4353f
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-11 11:47:47 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Unused method removed.

--- src/main/java/com/imcode/imcms/persistence/repository/LanguageRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/LanguageRepository.java
@@ -2,8 +2,6 @@ package com.imcode.imcms.persistence.repository;
 
 import com.imcode.imcms.persistence.entity.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,7 +9,4 @@ public interface LanguageRepository extends JpaRepository<Language, Integer> {
 
     Language findByCode(String code);
 
-    @Modifying
-    @Query("DELETE FROM Language l WHERE l.code = ?1")
-    int deleteByCode(String code);
 }
