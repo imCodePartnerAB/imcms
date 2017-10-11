d63e8013f482c250add45c7a42c8a9be53317ce6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-11 15:49:53 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Removed unused code.

--- src/main/java/com/imcode/imcms/web/tag/Functions.java
+++ src/main/java/com/imcode/imcms/web/tag/Functions.java
@@ -1,26 +1,11 @@
 package com.imcode.imcms.web.tag;
 
 import com.imcode.imcms.api.TextDocument;
-import imcode.server.document.DocumentDomainObject;
-import imcode.server.user.UserDomainObject;
 import imcode.util.Utility;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.PageContext;
 
 public class Functions {
-
-    public static boolean canEditDocument(DocumentDomainObject document, PageContext pageContext) {
-        UserDomainObject user = Utility.getLoggedOnUser((HttpServletRequest) pageContext.getRequest());
-
-        return user.canEdit(document);
-    }
-
-    public static boolean canEditDocumentInformation(DocumentDomainObject document, PageContext pageContext) {
-        UserDomainObject user = Utility.getLoggedOnUser((HttpServletRequest) pageContext.getRequest());
-
-        return user.canEditDocumentInformationFor(document);
-    }
 
     public static TextDocument getDocument(String docIdentifier, PageContext pageContext) {
         return Utility.getCMS(pageContext).getDocumentService().getTextDocument(docIdentifier);
