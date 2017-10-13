18f086d6b7f3f547d1b81a8ef66a3b8e0272db37
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-13 19:22:43 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Implemented image generating after save in editor.

--- src/main/java/com/imcode/imcms/domain/service/api/ImageService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/ImageService.java
@@ -10,6 +10,12 @@ import com.imcode.imcms.persistence.entity.Language;
 import com.imcode.imcms.persistence.entity.LoopEntryRef;
 import com.imcode.imcms.persistence.repository.ImageRepository;
 import com.imcode.imcms.util.function.TernaryFunction;
+import imcode.server.document.textdocument.ImageSource;
+import imcode.server.document.textdocument.ImagesPathRelativePathImageSource;
+import imcode.server.document.textdocument.NullImageSource;
+import imcode.util.ImcmsImageUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -22,6 +28,7 @@ public class ImageService {
     private final ImageRepository imageRepository;
     private final VersionService versionService;
     private final LanguageService languageService;
+    private final String imagesPath;
     private final Function<LoopEntryRefDTO, LoopEntryRef> loopEntryRefDtoToLoopEntryRef;
     private final TernaryFunction<ImageDTO, Version, Language, Image> imageDtoToImage;
     private final Function<Image, ImageDTO> imageToImageDTO;
@@ -29,6 +36,7 @@ public class ImageService {
     public ImageService(ImageRepository imageRepository,
                         VersionService versionService,
                         LanguageService languageService,
+                        @Value("${ImageUrl}") String imagesPath,
                         Function<LoopEntryRefDTO, LoopEntryRef> loopEntryRefDtoToLoopEntryRef,
                         TernaryFunction<ImageDTO, Version, Language, Image> imageDtoToImage,
                         Function<Image, ImageDTO> imageToImageDTO) {
@@ -36,6 +44,7 @@ public class ImageService {
         this.imageRepository = imageRepository;
         this.versionService = versionService;
         this.languageService = languageService;
+        this.imagesPath = imagesPath;
         this.loopEntryRefDtoToLoopEntryRef = loopEntryRefDtoToLoopEntryRef;
         this.imageDtoToImage = imageDtoToImage;
         this.imageToImageDTO = imageToImageDTO;
@@ -60,8 +69,8 @@ public class ImageService {
     public void saveImage(ImageDTO imageDTO) {
         final Version version = versionService.getDocumentWorkingVersion(imageDTO.getDocId());
         final Language language = languageService.findByCode(imageDTO.getLangCode());
+        final Image image = generateImage(imageDTO, version, language);
         final Integer imageId = getImageId(imageDTO, version, language);
-        final Image image = imageDtoToImage.apply(imageDTO, version, language);
 
         image.setId(imageId);
         imageRepository.save(image);
@@ -87,6 +96,28 @@ public class ImageService {
         }
 
         return image.getId();
+    }
+
+    private Image generateImage(ImageDTO imageDTO, Version version, Language language) {
+        ImageSource imageSource = new NullImageSource();
+        String imagePath = imageDTO.getPath();
+
+        if (StringUtils.isNotBlank(imagePath)) {
+
+            if (imagePath.startsWith(imagesPath)) {
+                imagePath = imagePath.substring(imagesPath.length());
+            }
+
+            if (StringUtils.isNotBlank(imagePath)) {
+                imageSource = new ImagesPathRelativePathImageSource(imagePath);
+            }
+        }
+
+        imageDTO.setSource(imageSource);
+        imageDTO.generateFilename();
+        ImcmsImageUtils.generateImage(imageDTO, true);
+
+        return imageDtoToImage.apply(imageDTO, version, language);
     }
 
     private Image getImage(int index, Version version, Language language, LoopEntryRefDTO loopEntryRefDTO) {
