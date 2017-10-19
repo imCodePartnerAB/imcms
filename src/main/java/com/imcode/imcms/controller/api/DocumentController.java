04ce27d561e414da2475e1b1e49a43181509bab8
3emluk <3emluk@gmail.com>
2017-10-19 11:53:29 +0300

    IMCMS-260 - Upgrade document functionality to work with new document editor API on client side: - Add basic DocumentController.

---
+++ src/main/java/com/imcode/imcms/controller/api/DocumentController.java
@@ -0,0 +1,13 @@
+package com.imcode.imcms.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dmizem from Ubrainians for imCode on 19.10.17.
+ */
+@RestController
+@RequestMapping("/documents")
+public class DocumentController {
+
+}
