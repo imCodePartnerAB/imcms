d00dd31cf72d5a7066a89f4b88fd261f22053b67
RuslanPopenko <ruslanpopenko@ukr.net>
2017-10-19 11:25:54 +0300

    IMCMS-260 - Upgrade document functionality to work with new document editor API on client side:  - Created DocumentDTO. Fixed MenuDTO.

--- src/main/java/com/imcode/imcms/domain/dto/MenuDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/MenuDTO.java
@@ -3,11 +3,12 @@ package com.imcode.imcms.domain.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
-public class MenuDTO {
+public class MenuDTO implements Serializable {
 
     private Integer menuId;
 
