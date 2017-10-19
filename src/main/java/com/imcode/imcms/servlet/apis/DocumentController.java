5d6ba8c54910918575ba5bff7aac04f09699ae54
3emluk <3emluk@gmail.com>
2017-10-19 11:56:20 +0300

    IMCMS-260 - Upgrade document functionality to work with new document editor API on client side: - Remove Controller annotation from old Document controller.

--- src/main/java/com/imcode/imcms/servlet/apis/DocumentController.java
+++ src/main/java/com/imcode/imcms/servlet/apis/DocumentController.java
@@ -41,7 +41,6 @@ import static imcode.util.DateConstants.*;
  * @see RestController
  */
 @SuppressWarnings("unused")
-@RestController
 @RequestMapping("/document")
 public class DocumentController {
 
