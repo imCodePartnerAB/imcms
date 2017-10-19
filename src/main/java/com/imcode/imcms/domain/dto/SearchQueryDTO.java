23a212d2ac3a866cb9726d4bf3e19bb7e18f3732
3emluk <3emluk@gmail.com>
2017-10-19 11:53:01 +0300

    IMCMS-260 - Upgrade document functionality to work with new document editor API on client side: - Add SearchQueryDTO.

---
+++ src/main/java/com/imcode/imcms/domain/dto/SearchQueryDTO.java
@@ -0,0 +1,26 @@
+package com.imcode.imcms.domain.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by dmizem from Ubrainians for imCode on 19.10.17.
+ */
+@Data
+@NoArgsConstructor
+public class SearchQueryDTO implements Serializable {
+
+    private static final long serialVersionUID = -3236215724457387659L;
+
+    private String term;
+
+    private Integer userId;
+
+    private List<Integer> categoriesId;
+
+    private PageRequest page;
+}
