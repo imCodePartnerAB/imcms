5fb6fe7caaf4baf0b665b925114407c96530423c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-25 11:19:25 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Upgraded import.

--- src/main/java/com/imcode/imcms/persistence/repository/MenuRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/MenuRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository("com.imcode.imcms.persistence.repository.MenuRepository")
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
 
@@ -13,5 +15,10 @@ public interface MenuRepository extends JpaRepository<Menu, Integer> {
             "left join fetch menu.menuItems " +
             "where menu.no = ?1 and menu.version = ?2")
     Menu findByNoAndVersionAndFetchMenuItemsEagerly(Integer menuNo, Version version);
+
+    @Query("SELECT m FROM com.imcode.imcms.persistence.entity.Menu m " +
+            "left join fetch m.menuItems " +
+            "WHERE m.version = ?1")
+    List<Menu> findByVersion(Version version);
 
 }
