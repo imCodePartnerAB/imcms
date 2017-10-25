cd1420ad4c8d01625c9ffbded13064cef94473fd
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-25 11:41:55 +0300

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Added tests and load time logging to imcms start module.

--- src/main/webapp/js/imcms_new/imcms_start.js
+++ src/main/webapp/js/imcms_new/imcms_start.js
@@ -4,11 +4,15 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 07.08.17.
  */
-Imcms.require(["imcms-admin-panel-builder", "imcms"], function (panelBuilder, imcms) {
+Imcms.require(["imcms-admin-panel-builder", "imcms", "imcms-tests"], function (panelBuilder, imcms, tests) {
+    Imcms.tests = tests;
+    console.info("%c Tests loaded.", "color: green");
+
     panelBuilder.buildPanel({
         active: imcms.isEditMode ? 'edit' : 'public'
     });
     imcms.isEditMode && imcms.require("imcms-editors-initializer", function (editorsInit) {
         editorsInit.initEditors();
+        console.timeEnd("imCMS JS loaded");
     });
 });
