b5a23ca226638220b1788014447257006fafb586
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-30 12:06:37 +0200

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Current tag set for editor builders.

--- src/main/webapp/js/imcms_new/editor_initializer/imcms_editor_init_strategy.js
+++ src/main/webapp/js/imcms_new/editor_initializer/imcms_editor_init_strategy.js
@@ -6,8 +6,9 @@ Imcms.define("imcms-editor-init-strategy", ["jquery"], function ($) {
     return {
         initEditor: function (editorInitData) {
             var openEditor = function () {
-                var editorData = $(this).parents(editorInitData.EDIT_AREA_SELECTOR).data();
-                editorInitData.editorBuilder.build(editorData);
+                var $editedTag = $(this).parents(editorInitData.EDIT_AREA_SELECTOR);
+                var editorData = $editedTag.data();
+                editorInitData.editorBuilder.setTag($editedTag).build(editorData);
             };
 
             $(editorInitData.EDIT_AREA_SELECTOR).find(editorInitData.CONTROL_SELECTOR).click(openEditor);
