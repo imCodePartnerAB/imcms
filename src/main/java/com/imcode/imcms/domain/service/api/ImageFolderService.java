848f83ed73f6a5b5c507e1406b4ef2c2938203ba
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-30 18:28:52 +0200

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Introduced new Image Folder Service and it's test class.

---
+++ src/main/java/com/imcode/imcms/domain/service/api/ImageFolderService.java
@@ -0,0 +1,14 @@
+package com.imcode.imcms.domain.service.api;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service for Images Content Manager.
+ * CRUD operations with image folders and content.
+ *
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 30.10.17.
+ */
+@Service
+public class ImageFolderService {
+}
---
+++ src/test/java/com/imcode/imcms/domain/service/api/ImageFolderServiceTest.java
@@ -0,0 +1,21 @@
+package com.imcode.imcms.domain.service.api;
+
+import com.imcode.imcms.config.TestConfig;
+import com.imcode.imcms.config.WebTestConfig;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class, WebTestConfig.class})
+@WebAppConfiguration
+@Transactional
+public class ImageFolderServiceTest {
+
+    @Autowired
+    private ImageFolderService imageFolderService;
+
+}
