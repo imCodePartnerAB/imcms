de41645cf7479b447595b20b52a824254b6d7f02
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-31 12:24:10 +0200

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Added more logging to REST API calls on client side.

--- src/main/webapp/js/imcms_new/rest/imcms_rest_api.js
+++ src/main/webapp/js/imcms_new/rest/imcms_rest_api.js
@@ -2,34 +2,47 @@ Imcms.define("imcms-rest-api", ["imcms", "jquery"], function (imcms, $) {
 
     var API_PREFIX = "/api";
 
-    function logAjaxCall(type, url, data) {
-        console.log("AJAX " + type + " call: " + url + " with data: ");
+    function logAjaxRequest(type, url, data) {
+        console.log("%c AJAX " + type + " call: " + url + " with request: ", "color: blue;");
         console.log(data);
+    }
+
+    function logAjaxResponse(type, url, response) {
+        console.log("%c AJAX " + type + " call: " + url + " response: ", "color: blue;");
+        console.log(response);
     }
 
     function ajax(data, callback) {
         var url = imcms.contextPath + API_PREFIX + this.url;
-        logAjaxCall(this.type, url, data);
+        var type = this.type;
+        logAjaxRequest(type, url, data);
 
         return $.ajax({
             url: url,
             type: this.type,
             contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
             data: data,
-            success: callback
+            success: function (response) {
+                logAjaxResponse(type, url, response);
+                callback && callback(response);
+            }
         });
     }
 
     function ajaxWithBody(data, callback) {
         var url = imcms.contextPath + API_PREFIX + this.url;
-        logAjaxCall(this.type, url, data);
+        var type = this.type;
+        logAjaxRequest(type, url, data);
 
         return $.ajax({
             url: url,
-            type: this.type,
+            type: type,
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data),
-            success: callback
+            success: function (response) {
+                logAjaxResponse(type, url, response);
+                callback && callback(response);
+            }
         });
     }
 
