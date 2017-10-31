3de9c201a2df8b2c8ead54c6423c520f72528930
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-10-31 19:06:43 +0200

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Implemented functionality for saving image files with test coverage.

---
+++ src/main/java/com/imcode/imcms/domain/service/api/ImageFileService.java
@@ -0,0 +1,61 @@
+package com.imcode.imcms.domain.service.api;
+
+import com.imcode.imcms.domain.dto.ImageFileDTO;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Service for Images Content Manager.
+ * CRUD operations with image files.
+ *
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 31.10.17.
+ */
+@Service
+@Transactional
+public class ImageFileService {
+
+    private final Function<File, ImageFileDTO> fileToImageFileDTO;
+
+    @Value("${ImagePath}")
+    private File imagesPath;
+
+    public ImageFileService(Function<File, ImageFileDTO> fileToImageFileDTO) {
+        this.fileToImageFileDTO = fileToImageFileDTO;
+    }
+
+    public List<ImageFileDTO> saveNewImageFiles(String folder, List<MultipartFile> files) throws IOException {
+
+        final File targetFolder = ((folder == null) || folder.isEmpty()) ? imagesPath : new File(imagesPath, folder);
+        final ArrayList<ImageFileDTO> imageFileDTOS = new ArrayList<>();
+        final Function<File, Boolean> mapAndAddToList = fileToImageFileDTO.andThen(imageFileDTOS::add);
+
+        // do not rewrite using Java Stream API, file transfer can be long operation. in cycle.
+        for (MultipartFile file : files) {
+            int copiesCount = 1;
+            final String originalFilename = file.getOriginalFilename();
+            File destination = new File(targetFolder, originalFilename);
+
+            while (destination.exists()) {
+                final String baseName = FilenameUtils.getBaseName(originalFilename);
+                final String newName = baseName + copiesCount + "." + FilenameUtils.getExtension(originalFilename);
+                destination = new File(targetFolder, newName);
+                copiesCount++;
+            }
+
+            file.transferTo(destination);
+            mapAndAddToList.apply(destination);
+        }
+
+        return imageFileDTOS;
+    }
+}
---
+++ src/test/java/com/imcode/imcms/domain/service/api/ImageFileServiceTest.java
@@ -0,0 +1,53 @@
+package com.imcode.imcms.domain.service.api;
+
+import com.imcode.imcms.config.TestConfig;
+import com.imcode.imcms.config.WebTestConfig;
+import com.imcode.imcms.domain.dto.ImageFileDTO;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@Transactional
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class, WebTestConfig.class})
+public class ImageFileServiceTest {
+
+    @Autowired
+    private ImageFileService imageFileService;
+
+    @Value("classpath:img1.jpg")
+    private File testImageFile;
+
+    @Value("${ImagePath}")
+    private File imagesPath;
+
+    @Test
+    public void saveNewImageFiles_When_FolderIsNotSet_Expect_CorrectDTO() throws IOException {
+        final byte[] imageFileBytes = FileUtils.readFileToByteArray(testImageFile);
+
+        final MockMultipartFile file = new MockMultipartFile("file", "img1-test.jpg", null, imageFileBytes);
+        final List<MultipartFile> files = Collections.singletonList(file);
+        final List<ImageFileDTO> imageFileDTOS = imageFileService.saveNewImageFiles(null, files);
+
+        assertNotNull(imageFileDTOS);
+        assertEquals(files.size(), imageFileDTOS.size());
+
+        imageFileDTOS.forEach(imageFileDTO -> assertTrue(new File(imagesPath.getParentFile(), imageFileDTO.getPath()).delete()));
+    }
+}
