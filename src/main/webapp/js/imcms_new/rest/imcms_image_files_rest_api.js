4f9b2c2caf4fdd4089a7b68c25d5641df57c0899
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-01 12:38:20 +0200

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - New rest api module for working with image folders created.

--- src/main/webapp/js/imcms_new/rest/imcms_image_files_rest_api.js
+++ src/main/webapp/js/imcms_new/rest/imcms_image_files_rest_api.js
@@ -4,7 +4,7 @@
  */
 Imcms.define("imcms-image-files-rest-api", ["imcms-rest-api"], function (rest) {
 
-    var api = new rest.API("/images/folders");
+    var api = new rest.API("/images/files");
 
     //mock data
     api.create = function (data) {
