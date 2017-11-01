0f36c2ff80aba9cc16b686d072e23d5c3734ff59
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-01 11:09:45 +0200

    IMCMS-258 - Upgrade image functionality to work with new image editor API on client side: - Added more functionality to Abstract Controller Test class.

--- src/test/java/com/imcode/imcms/controller/AbstractControllerTest.java
+++ src/test/java/com/imcode/imcms/controller/AbstractControllerTest.java
@@ -1,6 +1,7 @@
 package com.imcode.imcms.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -54,6 +55,10 @@ public abstract class AbstractControllerTest {
 
     protected <T> T fromJson(String json, Class<T> resultClass) throws IOException {
         return objectMapper.readValue(json, resultClass);
+    }
+
+    protected <T> T fromJson(String json, TypeReference<T> resultTypeReference) throws IOException {
+        return objectMapper.readValue(json, resultTypeReference);
     }
 
     protected <T> void performPostWithContentExpectException(Object contentObject,
