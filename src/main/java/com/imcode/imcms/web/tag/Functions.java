350495bac0df742dc316a261ce1108b79b702181
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-07 12:36:07 +0200

    IMCMS-255 - Upgrade server side to work with new client: - No more used function removed.

--- src/main/java/com/imcode/imcms/web/tag/Functions.java
+++ src/main/java/com/imcode/imcms/web/tag/Functions.java
@@ -1,16 +1,11 @@
 package com.imcode.imcms.web.tag;
 
-import com.imcode.imcms.api.TextDocument;
 import imcode.server.document.textdocument.TextDocumentDomainObject;
 import imcode.util.Utility;
 
 import javax.servlet.jsp.PageContext;
 
 public class Functions {
-
-    public static TextDocument getDocument(String docIdentifier, PageContext pageContext) {
-        return Utility.getCMS(pageContext).getDocumentService().getTextDocument(docIdentifier);
-    }
 
     public static TextDocumentDomainObject getTextDocumentDomainObject(String docIdentifier, PageContext pageContext) {
         return Utility.getCMS(pageContext).getDocumentService().getTextDocument(docIdentifier).getInternal();
