e01e28d677cee62472e5c527ebff994da31eaf90
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-07 16:58:39 +0200

    IMCMS-259 - Upgrade text functionality to work with new text editor API on client side: - Implemented getText method in domain layer with test coverage.

--- src/main/java/com/imcode/imcms/domain/service/api/TextService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/TextService.java
@@ -1,15 +1,72 @@
 package com.imcode.imcms.domain.service.api;
 
+import com.imcode.imcms.domain.dto.LoopEntryRefDTO;
+import com.imcode.imcms.domain.dto.TextDTO;
+import com.imcode.imcms.domain.service.core.VersionService;
+import com.imcode.imcms.persistence.entity.Language;
+import com.imcode.imcms.persistence.entity.LoopEntryRef;
+import com.imcode.imcms.persistence.entity.Text;
+import com.imcode.imcms.persistence.entity.Version;
 import com.imcode.imcms.persistence.repository.TextRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.function.Function;
 
 @Service
+@Transactional
 public class TextService {
 
     private final TextRepository textRepository;
+    private final Function<LoopEntryRefDTO, LoopEntryRef> loopEntryRefDtoToLoopEntryRef;
+    private final LanguageService languageService;
+    private final VersionService versionService;
+    private final Function<Text, TextDTO> textToTextDTO;
 
-    public TextService(TextRepository textRepository) {
+    public TextService(TextRepository textRepository,
+                       Function<LoopEntryRefDTO, LoopEntryRef> loopEntryRefDtoToLoopEntryRef,
+                       LanguageService languageService,
+                       VersionService versionService,
+                       Function<Text, TextDTO> textToTextDTO) {
+
         this.textRepository = textRepository;
+        this.loopEntryRefDtoToLoopEntryRef = loopEntryRefDtoToLoopEntryRef;
+        this.languageService = languageService;
+        this.versionService = versionService;
+        this.textToTextDTO = textToTextDTO;
     }
 
+    public TextDTO getText(TextDTO textRequestData) {
+        return getText(
+                textRequestData.getDocId(),
+                textRequestData.getIndex(),
+                textRequestData.getLangCode(),
+                textRequestData.getLoopEntryRef()
+        );
+    }
+
+    public TextDTO getText(int docId, int index, String langCode, LoopEntryRefDTO loopEntryRef) {
+        return getText(docId, index, langCode, loopEntryRef, versionService::getDocumentWorkingVersion);
+    }
+
+    private TextDTO getText(int docId, int index, String langCode, LoopEntryRefDTO loopEntryRefDTO,
+                            Function<Integer, Version> versionReceiver) {
+
+        final Version version = versionReceiver.apply(docId);
+        final Language language = languageService.findByCode(langCode);
+        final Text text = getText(index, version, language, loopEntryRefDTO);
+
+        return Optional.ofNullable(text)
+                .map(textToTextDTO)
+                .orElse(new TextDTO(index, docId, langCode, loopEntryRefDTO));
+    }
+
+    private Text getText(int index, Version version, Language language, LoopEntryRefDTO loopEntryRefDTO) {
+        final LoopEntryRef loopEntryRef = loopEntryRefDtoToLoopEntryRef.apply(loopEntryRefDTO);
+
+        return (loopEntryRef == null)
+                ? textRepository.findByVersionAndLanguageAndIndexWhereLoopEntryRefIsNull(version, language, index)
+                : textRepository.findByVersionAndLanguageAndIndexAndLoopEntryRef(version, language, index, loopEntryRef);
+    }
 }
--- src/test/java/com/imcode/imcms/domain/service/api/TextServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/TextServiceTest.java
@@ -3,10 +3,13 @@ package com.imcode.imcms.domain.service.api;
 import com.imcode.imcms.components.datainitializer.VersionDataInitializer;
 import com.imcode.imcms.config.TestConfig;
 import com.imcode.imcms.config.WebTestConfig;
+import com.imcode.imcms.domain.dto.TextDTO;
 import com.imcode.imcms.persistence.entity.Language;
+import com.imcode.imcms.persistence.entity.Text;
 import com.imcode.imcms.persistence.entity.Version;
 import com.imcode.imcms.persistence.repository.LanguageRepository;
 import com.imcode.imcms.persistence.repository.TextRepository;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,9 +18,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
+import static com.imcode.imcms.persistence.entity.TextBase.Type.PLAIN_TEXT;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,6 +36,8 @@ public class TextServiceTest {
     private static final int VERSION_NO = 0;
     private static final String ENG_CODE = "en";
     private static final String SWE_CODE = "sv";
+    private static final int MIN_TEXT_INDEX = 1;
+    private static final int MAX_TEXT_INDEX = 10;
 
     @Autowired
     private TextService textService;
@@ -42,14 +51,25 @@ public class TextServiceTest {
     @Autowired
     private TextRepository textRepository;
 
+    @Autowired
+    private Function<Text, TextDTO> textToTextDTO;
+
     private Version version;
     private List<Language> languages;
 
     @Before
     public void setUp() throws Exception {
+        clearRepos();
+
         version = versionDataInitializer.createData(VERSION_NO, DOC_ID);
         languages = Arrays.asList(languageRepository.findByCode(ENG_CODE), languageRepository.findByCode(SWE_CODE));
         // both langs should already be created
+    }
+
+    @After
+    public void clearRepos() {
+        textRepository.deleteAll();
+        textRepository.flush();
     }
 
     @Test
@@ -59,8 +79,26 @@ public class TextServiceTest {
 
     @Test
     public void getText_When_NotInLoop_Expect_CorrectDTO() {
-//        new Text
-//        textRepository.save()
+        final List<TextDTO> textDTOS = new ArrayList<>();
+
+        for (Language language : languages) {
+            for (int index = MIN_TEXT_INDEX; index <= MAX_TEXT_INDEX; index++) {
+                final Text text = new Text();
+                text.setIndex(index);
+                text.setLanguage(language);
+                text.setText("test");
+                text.setType(PLAIN_TEXT);
+                text.setVersion(version);
+
+                textRepository.save(text);
+                textDTOS.add(textToTextDTO.apply(text));
+            }
+        }
+
+        for (TextDTO textDTO : textDTOS) {
+            final TextDTO savedText = textService.getText(textDTO);
+            assertEquals(savedText, textDTO);
+        }
     }
 
 }
