07cf0c68256db4f631e4d9fcc85b64d954bf177c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-07 13:48:06 +0200

    IMCMS-259 - Upgrade text functionality to work with new text editor API on client side: - Refactored TextHistory entity.

--- src/main/java/com/imcode/imcms/mapping/jpa/doc/content/textdoc/TextHistory.java
+++ src/main/java/com/imcode/imcms/mapping/jpa/doc/content/textdoc/TextHistory.java
@@ -1,12 +1,19 @@
 package com.imcode.imcms.mapping.jpa.doc.content.textdoc;
 
 import com.imcode.imcms.mapping.jpa.User;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
-import java.util.Objects;
 
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
 @Entity
+@NoArgsConstructor
 @Table(name = "imcms_text_doc_texts_history")
 public class TextHistory extends TextBase {
 
@@ -17,9 +24,6 @@ public class TextHistory extends TextBase {
     @Column(name = "modified_dt")
     @Temporal(TemporalType.TIMESTAMP)
     private Date modifiedDt;
-
-    public TextHistory() {
-    }
 
     public TextHistory(Text text, User modifiedBy) {
         setVersion(text.getVersion());
@@ -32,41 +36,4 @@ public class TextHistory extends TextBase {
         setModifiedDt(new Date());
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return obj == this || (obj instanceof TextHistory && equals((TextHistory) obj));
-    }
-
-    private boolean equals(TextHistory that) {
-        return Objects.equals(getId(), that.getId())
-                && Objects.equals(getVersion(), that.getVersion())
-                && Objects.equals(getLanguage(), that.getLanguage())
-                && Objects.equals(getType(), that.getType())
-                && Objects.equals(getIndex(), that.getIndex())
-                && Objects.equals(getLoopEntryRef(), that.getLoopEntryRef())
-                && Objects.equals(modifiedBy, that.modifiedBy)
-                && Objects.equals(modifiedDt, that.modifiedDt);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getVersion(), getLanguage(), getText(), getType(), getIndex(),
-                getLoopEntryRef(), modifiedBy, modifiedDt);
-    }
-
-    public User getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(User createdBy) {
-        this.modifiedBy = createdBy;
-    }
-
-    public Date getModifiedDt() {
-        return modifiedDt;
-    }
-
-    public void setModifiedDt(Date modifiedDt) {
-        this.modifiedDt = modifiedDt;
-    }
 }
