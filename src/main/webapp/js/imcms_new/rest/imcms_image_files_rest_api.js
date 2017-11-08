e4ff218f97383fbba2f0c498833c7a78bc4d0209
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-08 16:52:39 +0200

    IMCMS-259 - Upgrade text functionality to work with new text editor API on client side: - Refactored image files rest API using updated REST API module.

--- src/main/webapp/js/imcms_new/rest/imcms_image_files_rest_api.js
+++ src/main/webapp/js/imcms_new/rest/imcms_image_files_rest_api.js
@@ -2,40 +2,13 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 17.08.17.
  */
-Imcms.define("imcms-image-files-rest-api", ["imcms-rest-api", "imcms", "jquery"], function (rest, imcms, $) {
+Imcms.define("imcms-image-files-rest-api", ["imcms-rest-api"], function (rest) {
 
     var apiPath = "/images/files";
     var api = new rest.API(apiPath);
 
-    var API_PREFIX = "/api";
-
-    function logAjaxRequest(type, url, data) {
-        console.log("%c AJAX " + type + " call: " + url + " with request: ", "color: blue;");
-        console.log(data);
-    }
-
-    function logAjaxResponse(type, url, response) {
-        console.log("%c AJAX " + type + " call: " + url + " response: ", "color: blue;");
-        console.log(response);
-    }
-
-    api.create = function ajaxFilesPostCall(data, callback) {
-        var url = imcms.contextPath + API_PREFIX + apiPath;
-        var type = "POST";
-        logAjaxRequest(type, url, data);
-
-        return $.ajax({
-            url: url,
-            type: type,
-            contentType: false, // this is the reason of custom function
-            processData: false, // this is the reason of custom function
-            data: data,
-            success: function (response) {
-                logAjaxResponse(type, url, response);
-                callback && callback(response);
-            }
-        });
-    };
+    // custom non-json POST call for files
+    api.create = rest.ajax.bind({url: apiPath, type: "POST", json: false, contentType: false, processData: false});
 
     return api;
 });
