2ad9dcf72313306698fad00d73b69aba2b0c7458
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-10 13:23:57 +0200

    IMCMS-260 - Upgrade document functionality to work with new document editor API on client side: - Added util method to AuditDTO.

--- src/main/java/com/imcode/imcms/domain/dto/AuditDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/AuditDTO.java
@@ -1,9 +1,14 @@
 package com.imcode.imcms.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.util.Date;
+
+import static imcode.util.DateConstants.DATETIME_DOC_FORMAT;
 
 @Data
 @NoArgsConstructor
@@ -18,5 +23,14 @@ public class AuditDTO implements Serializable {
     private String date;
 
     private String time;
+
+    @JsonIgnore
+    public Date getFormattedDate() {
+        try {
+            return DATETIME_DOC_FORMAT.parse(date + " " + time);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
 
 }
