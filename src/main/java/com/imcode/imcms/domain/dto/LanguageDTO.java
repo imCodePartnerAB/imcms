dbe37938e075a37a186e0e697467bffd5e4d44f9
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-16 10:43:52 +0200

    IMCMS-233 - Upgrade server side to work with new client: - Added comment for language DTO code field to know which ISO is it.

--- src/main/java/com/imcode/imcms/domain/dto/LanguageDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/LanguageDTO.java
@@ -11,6 +11,9 @@ public class LanguageDTO implements Serializable {
 
     private static final long serialVersionUID = -3433592782831228045L;
 
+    /**
+     * Two-letter ISO-639-1 code, like "en" or "sv"
+     */
     private String code;
 
     private String name;
