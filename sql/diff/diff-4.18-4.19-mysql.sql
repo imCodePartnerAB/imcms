1cddc161e1546c49781962d95a2bfe8688ce24f5
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-21 12:19:55 +0200

    IMCMS-265 - Introduce IP whitelist in superadmin - for login: - Fixed sql diff.

--- sql/diff/diff-4.18-4.19-mysql.sql
+++ sql/diff/diff-4.18-4.19-mysql.sql
@@ -1,9 +1,8 @@
 CREATE TABLE `imcms_ip_white_list` (
-  role_id INT NOT NULL,
+  is_admin TINYINT(1) NOT NULL,
   ip_range_from VARCHAR(15) NOT NULL,
   ip_range_to VARCHAR(15) NOT NULL,
-  UNIQUE (role_id, ip_range_from, ip_range_to),
-  CONSTRAINT role_id_to_white_list_range FOREIGN KEY (role_id) REFERENCES roles(role_id)
+  UNIQUE (is_admin, ip_range_from, ip_range_to)
 );
 
 UPDATE database_version SET major = 4, minor = 19;
