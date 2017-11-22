ffccddc8a141f20b6a4c49bf02f64a3fbc35ed40
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-22 10:38:12 +0200

    IMCMS-265 - Introduce IP whitelist in superadmin - for login: - Implemented receiving IP whitelists from DB.

--- server/src/com/imcode/imcms/servlet/superadmin/AdminIpWhiteList.java
+++ server/src/com/imcode/imcms/servlet/superadmin/AdminIpWhiteList.java
@@ -1,6 +1,10 @@
 package com.imcode.imcms.servlet.superadmin;
 
+import com.imcode.db.DatabaseCommand;
 import com.imcode.db.commands.InsertIntoTableDatabaseCommand;
+import com.imcode.db.commands.SqlQueryDatabaseCommand;
+import com.imcode.db.handlers.CollectionHandler;
+import com.imcode.db.handlers.RowTransformer;
 import com.imcode.imcms.util.l10n.ImcmsPrefsLocalizedMessageProvider;
 import imcode.server.Imcms;
 import imcode.server.user.UserDomainObject;
@@ -13,6 +17,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Feature allows to define IP white list per user role.
@@ -22,10 +30,28 @@ import java.io.IOException;
  */
 public class AdminIpWhiteList extends HttpServlet {
 
-    private final static Logger log = Logger.getLogger(AdminIpWhiteList.class);
+    private static final Logger log = Logger.getLogger(AdminIpWhiteList.class);
 
     private static final String WHITE_LIST_TEMPLATE = "AdminIpWhiteList.jsp";
     private static final String WHITE_LIST_ADD_TEMPLATE = "AdminIpWhiteList_Add.jsp";
+    private static final String TABLE_NAME = "imcms_ip_white_list";
+
+    private final InetAddressValidator ipValidator = InetAddressValidator.getInstance();
+
+    private final RowTransformer<RoleIpRange> rowTransformer = new RowTransformer<RoleIpRange>() {
+        @Override
+        public RoleIpRange createObjectFromResultSetRow(ResultSet resultSet) throws SQLException {
+            final boolean isAdmin = resultSet.getBoolean(1);
+            final String ipFrom = resultSet.getString(2);
+            final String ipTo = resultSet.getString(3);
+            return new RoleIpRange(isAdmin, ipFrom, ipTo);
+        }
+
+        @Override
+        public Class<RoleIpRange> getClassOfCreatedObjects() {
+            return RoleIpRange.class;
+        }
+    };
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -37,12 +63,18 @@ public class AdminIpWhiteList extends HttpServlet {
             return;
         }
 
+        final DatabaseCommand<List<RoleIpRange>> queryCommand = new SqlQueryDatabaseCommand<List<RoleIpRange>>(
+                "SELECT is_admin, ip_range_from, ip_range_to FROM " + TABLE_NAME, new Object[]{},
+                new CollectionHandler<RoleIpRange, List<RoleIpRange>>(new ArrayList<RoleIpRange>(), rowTransformer)
+        );
+
+        final List<RoleIpRange> roleIpRanges = Imcms.getServices().getDatabase().execute(queryCommand);
         final String templatePath = getAdminTemplatePath(WHITE_LIST_TEMPLATE, user);
 
         response.setContentType("text/html");
-
         request.setAttribute("contextPath", request.getContextPath());
         request.setAttribute("language", language);
+        request.setAttribute("roleIpRanges", roleIpRanges);
         request.getRequestDispatcher(templatePath).forward(request, response);
     }
 
@@ -64,11 +96,9 @@ public class AdminIpWhiteList extends HttpServlet {
         } else if (request.getParameter("ADD_NEW_IP_RANGE") != null) {
             final String ipFrom = request.getParameter("IP_START");
             final String ipTo = request.getParameter("IP_END");
-            final InetAddressValidator ipValidator = InetAddressValidator.getInstance();
 
             if (ipValidator.isValidInet4Address(ipFrom) && ipValidator.isValidInet4Address(ipTo)) {
                 final String isAdmin = String.valueOf(request.getParameter("IS_ADMIN"));
-                final String tableName = "imcms_ip_white_list";
                 final String[][] commandParams = new String[][]{
                         {"is_admin", isAdmin},
                         {"ip_range_from", ipFrom},
@@ -78,7 +108,7 @@ public class AdminIpWhiteList extends HttpServlet {
                 log.info("Adding new IP range from " + ipFrom + " to " + ipTo + " for "
                         + ("1".equals(isAdmin) ? "super" : "non") + "-admin roles.");
 
-                Imcms.getServices().getDatabase().execute(new InsertIntoTableDatabaseCommand(tableName, commandParams));
+                Imcms.getServices().getDatabase().execute(new InsertIntoTableDatabaseCommand(TABLE_NAME, commandParams));
 
                 final String templatePath = getAdminTemplatePath(WHITE_LIST_TEMPLATE, user);
 
@@ -97,6 +127,50 @@ public class AdminIpWhiteList extends HttpServlet {
 
     private String getAdminTemplatePath(String templateFileName, UserDomainObject user) {
         return "/WEB-INF/templates/" + user.getLanguageIso639_2() + "/admin/" + templateFileName;
+    }
+
+    public final class RoleIpRange {
+        private final boolean isAdmin;
+        private final String ipFrom;
+        private final String ipTo;
+
+        private RoleIpRange(boolean isAdmin, String ipFrom, String ipTo) {
+            this.isAdmin = isAdmin;
+            this.ipFrom = ipFrom;
+            this.ipTo = ipTo;
+        }
+
+        public boolean isAdmin() {
+            return isAdmin;
+        }
+
+        public String getIpFrom() {
+            return ipFrom;
+        }
+
+        public String getIpTo() {
+            return ipTo;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            RoleIpRange that = (RoleIpRange) o;
+
+            if (isAdmin != that.isAdmin) return false;
+            if (!ipFrom.equals(that.ipFrom)) return false;
+            return ipTo.equals(that.ipTo);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = (isAdmin ? 1 : 0);
+            result = 31 * result + ipFrom.hashCode();
+            result = 31 * result + ipTo.hashCode();
+            return result;
+        }
     }
 
 }
