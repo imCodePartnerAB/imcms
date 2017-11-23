796c89c57c6c05c87f87317ec23964360c6d9d7a
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-23 16:15:53 +0200

    IMCMS-265 - Introduce IP whitelist in superadmin - for login: - Cleanup before changes.

--- server/src/com/imcode/imcms/servlet/VerifyUser.java
+++ server/src/com/imcode/imcms/servlet/VerifyUser.java
@@ -21,20 +21,18 @@ import java.io.IOException;
 
 public class VerifyUser extends HttpServlet {
 
-    private static final Log log = LogFactory.getLog(VerifyUser.class);
-
     public static final String SESSION_ATTRIBUTE__NEXT_URL = "next_url";
     public static final String REQUEST_PARAMETER__NEXT_URL = SESSION_ATTRIBUTE__NEXT_URL;
     public static final String REQUEST_PARAMETER__NEXT_META = "next_meta";
     public static final String SESSION_ATTRIBUTE__NEXT_META = "next_meta";
-    private static final String SESSION_ATTRIBUTE__LOGIN_TARGET = "login.target";
     public static final String REQUEST_PARAMETER__EDIT_USER = "edit_user";
     public static final String REQUEST_PARAMETER__USERNAME = "name";
     public static final String REQUEST_PARAMETER__PASSWORD = "passwd";
     public static final String REQUEST_ATTRIBUTE__ERROR = "error";
-
+    private static final Log log = LogFactory.getLog(VerifyUser.class);
+    private static final String SESSION_ATTRIBUTE__LOGIN_TARGET = "login.target";
     private final static LocalizedMessage ERROR__LOGIN_FAILED = new LocalizedMessage("templates/login/access_denied.html/4");
-    
+
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         doPost(req, res);
     }
@@ -47,9 +45,9 @@ public class VerifyUser extends HttpServlet {
 
         final ContentManagementSystem cms = ContentManagementSystem.login(req, res, name, passwd);
 
-        if ( null != cms ) {
+        if (null != cms) {
             final User currentUser = cms.getCurrentUser();
-            if ( req.getParameter(REQUEST_PARAMETER__EDIT_USER) != null && !currentUser.isDefaultUser() ) {
+            if (req.getParameter(REQUEST_PARAMETER__EDIT_USER) != null && !currentUser.isDefaultUser()) {
                 goToEditUserPage(currentUser, res, req);
             } else {
                 goToLoginSuccessfulPage(req, res);
@@ -62,7 +60,7 @@ public class VerifyUser extends HttpServlet {
     private void goToLoginFailedPage(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         req.setAttribute(REQUEST_ATTRIBUTE__ERROR, ERROR__LOGIN_FAILED);
         req.getRequestDispatcher("/imcms/" + Utility.getLoggedOnUser(req).getLanguageIso639_2()
-                                 + "/login/index.jsp").forward(req, res);
+                + "/login/index.jsp").forward(req, res);
     }
 
     private void goToLoginSuccessfulPage(HttpServletRequest req,
@@ -72,7 +70,7 @@ public class VerifyUser extends HttpServlet {
     }
 
     private void goToEditUserPage(User user, HttpServletResponse res, HttpServletRequest req) throws IOException, ServletException {
-        final UserDomainObject internalUser = Imcms.getServices().getImcmsAuthenticatorAndUserAndRoleMapper().getUser(user.getId()) ;
+        final UserDomainObject internalUser = Imcms.getServices().getImcmsAuthenticatorAndUserAndRoleMapper().getUser(user.getId());
         final DispatchCommand returnCommand = new GoToLoginSuccessfulPageCommand();
         final UserEditorPage userEditorPage = new UserEditorPage(internalUser, new AdminUser.SaveUserAndReturnCommand(internalUser, returnCommand), returnCommand);
         userEditorPage.forward(req, res);
@@ -83,17 +81,17 @@ public class VerifyUser extends HttpServlet {
                              final HttpServletResponse response) throws IOException, ServletException {
             String nexturl = "/servlet/StartDoc";
             final HttpSession session = request.getSession(true);
-            if ( session.getAttribute(SESSION_ATTRIBUTE__NEXT_META) != null ) {
+            if (session.getAttribute(SESSION_ATTRIBUTE__NEXT_META) != null) {
                 nexturl = "/servlet/GetDoc?meta_id=" + session.getAttribute(SESSION_ATTRIBUTE__NEXT_META);
                 session.removeAttribute(SESSION_ATTRIBUTE__NEXT_META);
-            } else if ( session.getAttribute(SESSION_ATTRIBUTE__NEXT_URL) != null ) {
+            } else if (session.getAttribute(SESSION_ATTRIBUTE__NEXT_URL) != null) {
                 nexturl = (String) session.getAttribute(SESSION_ATTRIBUTE__NEXT_URL);
                 session.removeAttribute(SESSION_ATTRIBUTE__NEXT_URL);
-            } else if ( request.getParameter(REQUEST_PARAMETER__NEXT_URL) != null ) {
+            } else if (request.getParameter(REQUEST_PARAMETER__NEXT_URL) != null) {
                 nexturl = request.getParameter(REQUEST_PARAMETER__NEXT_URL);
-            } else if ( request.getParameter(REQUEST_PARAMETER__NEXT_META) != null ) {
+            } else if (request.getParameter(REQUEST_PARAMETER__NEXT_META) != null) {
                 nexturl = "/servlet/GetDoc?meta_id=" + request.getParameter(REQUEST_PARAMETER__NEXT_META);
-            } else if ( session.getAttribute(SESSION_ATTRIBUTE__LOGIN_TARGET) != null ) {
+            } else if (session.getAttribute(SESSION_ATTRIBUTE__LOGIN_TARGET) != null) {
                 nexturl = (String) session.getAttribute(SESSION_ATTRIBUTE__LOGIN_TARGET);
                 session.removeAttribute(SESSION_ATTRIBUTE__LOGIN_TARGET);
             }
