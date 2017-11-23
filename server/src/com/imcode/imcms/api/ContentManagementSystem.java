0e731bb7b524b80a1ab723259e7f0eb4f394d771
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-23 11:16:11 +0200

    IMCMS-265 - Introduce IP whitelist in superadmin - for login: - Cleanup before changes.

--- server/src/com/imcode/imcms/api/ContentManagementSystem.java
+++ server/src/com/imcode/imcms/api/ContentManagementSystem.java
@@ -18,65 +18,22 @@ import javax.servlet.http.HttpServletResponse;
 public abstract class ContentManagementSystem {
 
     /**
-     * Returns {@link UserService}
-     * @return UserService
-     */
-    public abstract UserService getUserService();
-
-    /**
-     * Returns {@link DocumentService}
-     * @return DocumentService
-     */
-    public abstract DocumentService getDocumentService();
-
-    /**
-     * Returns current {@link User}
-     * @return current user
-     */
-    public abstract User getCurrentUser();
-
-    /**
-     * Returns {@link DatabaseService}
-     * @return DatabaseService
-     */
-    public abstract DatabaseService getDatabaseService();
-
-    /**
-     * Returns {@link TemplateService}
-     * @return TemplateService
-     */
-    public abstract TemplateService getTemplateService();
-
-    /**
-     * Returns {@link MailService}
-     * @return MailService
-     */
-    public abstract MailService getMailService() ;
-
-    /**
-     * Runs {@link ContentManagementSystemRunnable#runWith(ContentManagementSystem)} with passed ContentManagementSystem's
-     * user having super admin privileges.
-     * @param runnable class implementing ContentManagementSystemRunnable
-     * @throws NoPermissionException if ContentManagementSystemRunnable implementer is not signed with the key store
-     */
-    public abstract void runAsSuperadmin(ContentManagementSystemRunnable runnable) throws NoPermissionException;
-
-    /**
      * Get a ContentManagementSystem for the given username and password.
+     *
      * @param userName user's login name
      * @param password user's password
-    **/
-    public static ContentManagementSystem getContentManagementSystem( String userName, String password ) {
+     **/
+    public static ContentManagementSystem getContentManagementSystem(String userName, String password) {
         ImcmsServices imcref = Imcms.getServices();
-        UserDomainObject user = imcref.verifyUser( userName, password );
-        return DefaultContentManagementSystem.create( imcref, user, Imcms.getApiDataSource());
+        UserDomainObject user = imcref.verifyUser(userName, password);
+        return DefaultContentManagementSystem.create(imcref, user, Imcms.getApiDataSource());
     }
 
     /**
      * Attempts to login the current user with the given name and password. Returns ContentManagementSystem for that user
      * if login is successful.
      *
-     * @param request HttpServletRequest request
+     * @param request  HttpServletRequest request
      * @param response HttpServletResponse response
      * @param username user's login name
      * @param password user's password
@@ -87,10 +44,10 @@ public abstract class ContentManagementSystem {
         ImcmsServices services = Imcms.getServices();
         UserDomainObject user = services.verifyUser(username, password);
 
-        if ( null == user || user.isDefaultUser()) {
-            return null ;
+        if (null == user || user.isDefaultUser()) {
+            return null;
         }
-        
+
         ContentManagementSystem cms = Utility.initRequestWithApi(request, user);
 
         if (services.getConfig().isDenyMultipleUserLogin()) {
@@ -98,13 +55,13 @@ public abstract class ContentManagementSystem {
             currentUser.setSessionId(request.getSession().getId());
             cms.getUserService().updateUserSession(currentUser);
         }
-        
+
         String rememberCd = user.getRememberCd();
         if (StringUtils.isEmpty(rememberCd)) {
-        	cms.getUserService().updateUserRememberCd(user);
+            cms.getUserService().updateUserRememberCd(user);
         }
         Utility.setRememberCdCookie(request, response, user.getRememberCd());
-        
+
         Utility.makeUserLoggedIn(request, user);
 
         return cms;
@@ -112,14 +69,66 @@ public abstract class ContentManagementSystem {
 
     /**
      * Returns ContentManagementSystem set in the given request.
+     *
      * @param request ServletRequest to get ContentManagementSystem from
      * @return The ContentManagementSystem for the request
      * @since 2.0
      */
     public static ContentManagementSystem fromRequest(ServletRequest request) {
-        return Utility.getContentManagementSystemFromRequest(request) ;
+        return Utility.getContentManagementSystemFromRequest(request);
     }
 
-    abstract ImcmsServices getInternal() ;
+    /**
+     * Returns {@link UserService}
+     *
+     * @return UserService
+     */
+    public abstract UserService getUserService();
+
+    /**
+     * Returns {@link DocumentService}
+     *
+     * @return DocumentService
+     */
+    public abstract DocumentService getDocumentService();
+
+    /**
+     * Returns current {@link User}
+     *
+     * @return current user
+     */
+    public abstract User getCurrentUser();
+
+    /**
+     * Returns {@link DatabaseService}
+     *
+     * @return DatabaseService
+     */
+    public abstract DatabaseService getDatabaseService();
+
+    /**
+     * Returns {@link TemplateService}
+     *
+     * @return TemplateService
+     */
+    public abstract TemplateService getTemplateService();
+
+    /**
+     * Returns {@link MailService}
+     *
+     * @return MailService
+     */
+    public abstract MailService getMailService();
+
+    /**
+     * Runs {@link ContentManagementSystemRunnable#runWith(ContentManagementSystem)} with passed ContentManagementSystem's
+     * user having super admin privileges.
+     *
+     * @param runnable class implementing ContentManagementSystemRunnable
+     * @throws NoPermissionException if ContentManagementSystemRunnable implementer is not signed with the key store
+     */
+    public abstract void runAsSuperadmin(ContentManagementSystemRunnable runnable) throws NoPermissionException;
+
+    abstract ImcmsServices getInternal();
 
 }
