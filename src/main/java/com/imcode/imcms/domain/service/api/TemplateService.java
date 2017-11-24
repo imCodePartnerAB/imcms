5ef4c0cfb77a34af5138e056985eb9c46b47c64e
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-24 14:36:09 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Saving new templates to DB on startup if found in directory.

--- src/main/java/com/imcode/imcms/domain/service/api/TemplateService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/TemplateService.java
@@ -1,24 +1,29 @@
 package com.imcode.imcms.domain.service.api;
 
 import com.imcode.imcms.domain.dto.TemplateDTO;
+import com.imcode.imcms.persistence.entity.Template;
 import com.imcode.imcms.persistence.entity.TemplateJPA;
 import com.imcode.imcms.persistence.repository.TemplateRepository;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class TemplateService {
 
     private final TemplateRepository templateRepository;
     private final Function<TemplateJPA, TemplateDTO> templateToTemplateDTO;
     private final File templateDirectory;
     private final Function<TemplateDTO, TemplateDTO> templateSaver;
+    private final Set<String> templateExtensions = new HashSet<>(Arrays.asList("jsp", "jspx", "html"));
 
     public TemplateService(TemplateRepository templateRepository,
                            Function<TemplateJPA, TemplateDTO> templateToTemplateDTO,
@@ -29,6 +34,37 @@ public class TemplateService {
         this.templateToTemplateDTO = templateToTemplateDTO;
         this.templateDirectory = templateDirectory;
         this.templateSaver = templateDtoToTemplate.andThen(templateRepository::save).andThen(templateToTemplateDTO);
+    }
+
+    /**
+     * Will save to DB all templates if not yet for files in directory.
+     */
+    @PostConstruct
+    private void saveTemplatesInFolder() {
+        final Set<String> savedTemplateNames = templateRepository.findAll()
+                .stream()
+                .map(Template::getName)
+                .collect(Collectors.toSet());
+
+        final String[] templateNamesToBeSaved = templateDirectory.list((dir, name) -> {
+            final String extension = FilenameUtils.getExtension(name);
+
+            if (!templateExtensions.contains(extension)) {
+                return false;
+            }
+
+            final String templateName = FilenameUtils.getBaseName(name);
+            return !savedTemplateNames.contains(templateName);
+        });
+
+        if (templateNamesToBeSaved == null) {
+            return; // strange situation, should not happen at all
+        }
+
+        Arrays.stream(templateNamesToBeSaved)
+                .map(FilenameUtils::getBaseName)
+                .map(templateName -> new TemplateJPA(null, templateName, false))
+                .forEach(templateRepository::save);
     }
 
     public List<TemplateDTO> getAll() {
@@ -57,7 +93,7 @@ public class TemplateService {
     }
 
     private boolean isTemplateFileExist(String templateName) {
-        for (String extension : new String[]{"jsp", "jspx", "html"}) {
+        for (String extension : templateExtensions) {
             final String templateFileName = templateName + "." + extension;
             final File templateFile = new File(templateDirectory, templateFileName);
 
