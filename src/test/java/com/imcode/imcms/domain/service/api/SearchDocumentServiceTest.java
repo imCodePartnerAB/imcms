2aac2ee15bdb48168759a5461eb03cd417b0ee89
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-11-28 17:51:11 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Fixed SearchDocumentServiceTest.

--- src/test/java/com/imcode/imcms/domain/service/api/SearchDocumentServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/SearchDocumentServiceTest.java
@@ -7,6 +7,7 @@ import com.imcode.imcms.domain.dto.SearchQueryDTO;
 import imcode.server.Imcms;
 import imcode.server.ImcmsServices;
 import imcode.server.document.index.DocumentIndex;
+import imcode.util.io.FileUtility;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -21,8 +22,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by dmizem from Ubrainians for imCode on 20.10.17.
@@ -45,6 +48,9 @@ public class SearchDocumentServiceTest {
     @Autowired
     private ImcmsServices imcmsServices;
 
+    @Autowired
+    private Imcms imcms;
+
     @Before
     public void setUp() throws Exception {
         versionDataInitializer.cleanRepositories();
@@ -65,12 +71,15 @@ public class SearchDocumentServiceTest {
 
     @After
     public void tearDown() throws Exception {
+        imcms.stop();
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2)); // to let solr shut down, not sure 2 sec is exact time
         versionDataInitializer.cleanRepositories();
-        testSolrFolder.delete();
+        assertTrue(FileUtility.forceDelete(testSolrFolder));
     }
 
     @Test
     public void searchDocuments() throws Exception {
+        Thread.sleep(TimeUnit.SECONDS.toMillis(2)); // to let solr init, not sure 2 sec is exact time
         PageRequest pageRequest = new PageRequest(0, 10, new Sort(new Sort.Order(DocumentIndex.FIELD__META_ID)));
 
         SearchQueryDTO searchQueryDTO = new SearchQueryDTO();
