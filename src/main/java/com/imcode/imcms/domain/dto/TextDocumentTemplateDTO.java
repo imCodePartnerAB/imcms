4d0bae632f9c24c78957f332a293a1d65ecbbd9b
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-01 18:53:54 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Cleanup.

--- src/main/java/com/imcode/imcms/domain/dto/TextDocumentTemplateDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/TextDocumentTemplateDTO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TextDocumentTemplateDTO extends TextDocumentTemplate {
+
     private Integer docId;
 
     private String templateName;
