f029d780ef1a727985416ef42d76332344051cd6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-05 13:06:45 +0200

    IMCMS-255 - Upgrade server side to work with new client: - No more mock "delete" method in document REST API.

--- src/main/webapp/js/imcms_new/rest/imcms_documents_rest_api.js
+++ src/main/webapp/js/imcms_new/rest/imcms_documents_rest_api.js
@@ -1,22 +1,3 @@
 Imcms.define("imcms-documents-rest-api", ["imcms-rest-api"], function (rest) {
-
-    var api = new rest.API("/documents");
-
-    api.remove = function (documentId) {
-        return {
-            done: function (callback) {
-                var responses = [200, 500],
-                    responseCode = responses[Math.floor(Math.random() * responses.length)];
-                if (responseCode === 200) {
-                    console.log("%c Document " + documentId + " was removed (not really)", "color: blue;");
-                } else {
-                    console.log("%c Document " + documentId + " wasn't removed due to some mock circumstances", "color: red;");
-
-                }
-                callback(responseCode);
-            }
-        }
-    };
-
-    return api;
+    return new rest.API("/documents");
 });
