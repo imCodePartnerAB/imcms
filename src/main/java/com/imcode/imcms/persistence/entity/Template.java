0f347bb3f1c0822d0144c1a00af80469282cd4e4
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-06 16:37:29 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Removed doc.

--- src/main/java/com/imcode/imcms/persistence/entity/Template.java
+++ src/main/java/com/imcode/imcms/persistence/entity/Template.java
@@ -2,12 +2,6 @@ package com.imcode.imcms.persistence.entity;
 
 import lombok.NoArgsConstructor;
 
-/**
- * Super class for both JPA and DTO realisation
- *
- * @author Serhii Maksymchuk from Ubrainians for imCode
- * 23.11.17.
- */
 @NoArgsConstructor
 public abstract class Template {
 
