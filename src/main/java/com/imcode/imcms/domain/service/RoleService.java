54fcbcfd8ac36681bd5e3690a347761cf6303be7
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-07 11:55:52 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Cleanup.

--- src/main/java/com/imcode/imcms/domain/service/RoleService.java
+++ src/main/java/com/imcode/imcms/domain/service/RoleService.java
@@ -4,14 +4,12 @@ import com.imcode.imcms.domain.dto.RoleDTO;
 
 import java.util.List;
 
-/**
- * @author Serhii Maksymchuk from Ubrainians for imCode
- * 07.12.17.
- */
 public interface RoleService {
+
     RoleDTO getById(int id);
 
     List<RoleDTO> getAll();
 
     RoleDTO save(RoleDTO saveMe);
+
 }
