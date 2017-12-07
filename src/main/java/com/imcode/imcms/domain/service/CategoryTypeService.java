0706ee70bb47295e9d39407e96693b0ad69e93d6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-07 11:46:01 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Cleanup.

--- src/main/java/com/imcode/imcms/domain/service/CategoryTypeService.java
+++ src/main/java/com/imcode/imcms/domain/service/CategoryTypeService.java
@@ -4,10 +4,8 @@ import com.imcode.imcms.domain.dto.CategoryTypeDTO;
 
 import java.util.List;
 
-/**
- * @author Serhii Maksymchuk from Ubrainians for imCode
- * 07.12.17.
- */
 public interface CategoryTypeService {
+
     List<CategoryTypeDTO> getAll();
+
 }
