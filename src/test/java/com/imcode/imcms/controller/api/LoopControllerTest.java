262fb662a498aa4782756afe71d10d9fc0319ba5
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-07 19:04:38 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Test case with loop entries saving covered on controller layer.

--- src/test/java/com/imcode/imcms/controller/api/LoopControllerTest.java
+++ src/test/java/com/imcode/imcms/controller/api/LoopControllerTest.java
@@ -5,8 +5,10 @@ import com.imcode.imcms.config.TestConfig;
 import com.imcode.imcms.config.WebTestConfig;
 import com.imcode.imcms.controller.AbstractControllerTest;
 import com.imcode.imcms.domain.dto.LoopDTO;
+import com.imcode.imcms.domain.dto.LoopEntryDTO;
 import com.imcode.imcms.domain.exception.DocumentNotExistException;
 import com.imcode.imcms.mapping.jpa.doc.VersionRepository;
+import com.imcode.imcms.model.Loop;
 import com.imcode.imcms.persistence.entity.LoopJPA;
 import com.imcode.imcms.persistence.entity.Version;
 import com.imcode.imcms.persistence.repository.LoopRepository;
@@ -25,7 +27,9 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Transactional
 @WebAppConfiguration
@@ -35,7 +39,7 @@ public class LoopControllerTest extends AbstractControllerTest {
     private static final int TEST_DOC_ID = 1001;
     private static final int TEST_LOOP_INDEX = 1;
 
-    private static final LoopDTO TEST_LOOP_DTO = new LoopDTO(TEST_DOC_ID, TEST_LOOP_INDEX, Collections.emptyList());
+    private static final Loop TEST_LOOP = new LoopDTO(TEST_DOC_ID, TEST_LOOP_INDEX, Collections.emptyList());
 
     @Autowired
     private LoopDataInitializer loopDataInitializer;
@@ -46,7 +50,7 @@ public class LoopControllerTest extends AbstractControllerTest {
 
     @Before
     public void createData() {
-        loopDataInitializer.createData(TEST_LOOP_DTO);
+        loopDataInitializer.createData(TEST_LOOP);
 
         final UserDomainObject user = new UserDomainObject(1);
         user.addRoleId(RoleId.SUPERADMIN);
@@ -60,7 +64,7 @@ public class LoopControllerTest extends AbstractControllerTest {
 
     @Test
     public void getLoop_Expect_OkAndResponseEqualTestData() throws Exception {
-        final String expectedJsonData = asJson(TEST_LOOP_DTO);
+        final String expectedJsonData = asJson(TEST_LOOP);
         final MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(controllerPath())
                 .param("index", String.valueOf(TEST_LOOP_INDEX))
                 .param("docId", String.valueOf(TEST_DOC_ID));
@@ -73,20 +77,20 @@ public class LoopControllerTest extends AbstractControllerTest {
         final UserDomainObject user = new UserDomainObject(2);
         Imcms.setUser(user); // means current user is default user
 
-        performPostWithContentExpectException(TEST_LOOP_DTO, NoPermissionToEditDocumentException.class);
+        performPostWithContentExpectException(TEST_LOOP, NoPermissionToEditDocumentException.class);
     }
 
     @Test
     public void postLoop_When_UserIsAdmin_Expect_Ok() throws Exception {
-        performPostWithContentExpectOk(TEST_LOOP_DTO);
+        performPostWithContentExpectOk(TEST_LOOP);
     }
 
     @Test
     public void postLoop_When_UserIsAdminAndDocNotExist_Expect_DocumentNotExistException() throws Exception {
         final int nonExistingDocId = -13;
-        final LoopDTO loopDTO = new LoopDTO(nonExistingDocId, TEST_LOOP_INDEX, Collections.emptyList());
+        final Loop loop = new LoopDTO(nonExistingDocId, TEST_LOOP_INDEX, Collections.emptyList());
 
-        performPostWithContentExpectException(loopDTO, DocumentNotExistException.class);
+        performPostWithContentExpectException(loop, DocumentNotExistException.class);
     }
 
     @Test
@@ -99,7 +103,27 @@ public class LoopControllerTest extends AbstractControllerTest {
             loopRepository.delete(shouldNotExist);
         }
 
-        final LoopDTO loopDTO = new LoopDTO(TEST_DOC_ID, nonExistingLoopIndex, Collections.emptyList());
-        performPostWithContentExpectOk(loopDTO);
+        final Loop loop = new LoopDTO(TEST_DOC_ID, nonExistingLoopIndex, Collections.emptyList());
+        performPostWithContentExpectOk(loop);
+    }
+
+    @Test
+    public void postLoop_When_NotEmptyEntries_Expect_Saved() throws Exception {
+        final List<LoopEntryDTO> entries = Arrays.asList(
+                new LoopEntryDTO(1, true),
+                new LoopEntryDTO(2, false),
+                new LoopEntryDTO(3, true)
+        );
+
+        final Loop loop = new LoopDTO(TEST_DOC_ID, TEST_LOOP_INDEX, entries);
+
+        performPostWithContentExpectOk(loop);
+
+        final String expectedJsonData = asJson(loop);
+        final MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(controllerPath())
+                .param("index", String.valueOf(TEST_LOOP_INDEX))
+                .param("docId", String.valueOf(TEST_DOC_ID));
+
+        performRequestBuilderExpectedOkAndJsonContentEquals(requestBuilder, expectedJsonData);
     }
 }
