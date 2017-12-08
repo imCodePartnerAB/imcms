86ea91e413b271480f891976b423d26621b29e45
RuslanPopenko <ruslanpopenko@ukr.net>
2017-12-08 16:57:28 +0200

    IMCMS-233 - Apply new UI to the admin panel and editors: - Version service cover with tests.

--- src/test/java/com/imcode/imcms/domain/service/core/VersionServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/core/VersionServiceTest.java
@@ -4,6 +4,7 @@ import com.imcode.imcms.components.datainitializer.VersionDataInitializer;
 import com.imcode.imcms.config.TestConfig;
 import com.imcode.imcms.config.WebTestConfig;
 import com.imcode.imcms.domain.service.VersionService;
+import com.imcode.imcms.mapping.jpa.doc.VersionRepository;
 import com.imcode.imcms.persistence.entity.Version;
 import org.junit.After;
 import org.junit.Before;
@@ -14,6 +15,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -30,16 +34,21 @@ public class VersionServiceTest {
     private VersionService versionService;
 
     @Autowired
+    private VersionRepository versionRepository;
+
+    @Autowired
     private VersionDataInitializer versionDataInitializer;
+    private List<Version> versions;
 
     @Before
     public void setUpVersions() {
         versionDataInitializer.cleanRepositories();
 
         versionDataInitializer.createData(0, docId);
-        versionDataInitializer.createData(1, docId);
-        versionDataInitializer.createData(2, docId);
-        versionDataInitializer.createData(3, docId);
+        versions = Arrays.asList(
+                versionDataInitializer.createData(1, docId),
+                versionDataInitializer.createData(2, docId),
+                versionDataInitializer.createData(3, docId));
     }
 
     @After
@@ -57,6 +66,26 @@ public class VersionServiceTest {
     }
 
     @Test
+    public void hasNewerVersion_When_VersionCreatedAfterDocModification() {
+        assertFalse(versionService.hasNewerVersion(docId));
+    }
+
+    @Test
+    public void hasNewerVersion_When_VersionCreatedBeforeDocModification() {
+        final Version documentWorkingVersion = versionService.getDocumentWorkingVersion(docId);
+        documentWorkingVersion.setModifiedDt(getTomorrowDate());
+        versionRepository.save(documentWorkingVersion);
+
+        assertTrue(versionService.hasNewerVersion(docId));
+    }
+
+    @Test
+    public void hasNewerVersion_When_OnlyWorkingVersion() {
+        versionRepository.delete(versions);
+        assertTrue(versionService.hasNewerVersion(docId));
+    }
+
+    @Test
     public void delete() {
         final List<Version> versions = versionService.findByDocId(docId);
         assertTrue(versions.size() > 0);
@@ -65,5 +94,12 @@ public class VersionServiceTest {
 
         final List<Version> emptyList = versionService.findByDocId(docId);
         assertTrue(emptyList.isEmpty());
+    }
+
+    private Date getTomorrowDate() {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, 1);
+        return calendar.getTime();
     }
 }
