e008c10c5906ae1b937490b389ca4ad8c6e6b8ce
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-11 11:52:52 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Cleanup.

--- src/main/java/com/imcode/imcms/domain/service/SearchDocumentService.java
+++ src/main/java/com/imcode/imcms/domain/service/SearchDocumentService.java
@@ -5,10 +5,8 @@ import com.imcode.imcms.domain.dto.SearchQueryDTO;
 
 import java.util.List;
 
-/**
- * @author Serhii Maksymchuk from Ubrainians for imCode
- * 07.12.17.
- */
 public interface SearchDocumentService {
+
     List<DocumentDTO> searchDocuments(SearchQueryDTO searchQuery);
+
 }
