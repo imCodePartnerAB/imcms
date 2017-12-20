31595b0766b22d1831711cde528ec8bd5c767aab
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-20 16:28:17 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Refactored window builder.

--- src/main/webapp/js/imcms_new/builders/windows/imcms_window_builder.js
+++ src/main/webapp/js/imcms_new/builders/windows/imcms_window_builder.js
@@ -8,6 +8,19 @@ Imcms.define("imcms-window-builder", ["imcms-window-components-builder", "jquery
         $("body").css("overflow", overflowValue);
     }
 
+    function enableBackgroundPageScrolling(pageOverflow, scrollTop) {
+        setBodyScrollingRule(pageOverflow);
+        $(window).scrollTop(scrollTop);
+    }
+
+    function getScrollTopAndDisable() {
+        var $window = $(window);
+        var prevScrollTop = $window.scrollTop();
+        $window.scrollTop(0);
+
+        return prevScrollTop;
+    }
+
     var WindowBuilder = function (opts) {
         this.factory = opts.factory;
         this.loadDataStrategy = opts.loadDataStrategy;
@@ -18,21 +31,10 @@ Imcms.define("imcms-window-builder", ["imcms-window-components-builder", "jquery
     WindowBuilder.prototype = {
         _scrollTop: 0,
         _pageOverflow: "auto",
-        _disableBackgroundPageScrolling: function () {
-            var $window = $(window);
-            this._scrollTop = $window.scrollTop();
-            $window.scrollTop(0);
-
+        buildWindow: function (windowInitData) {
+            this._scrollTop = getScrollTopAndDisable();
             this._pageOverflow = $("body").css("overflow") || "auto";
             setBodyScrollingRule("hidden");
-        },
-        _enableBackgroundPageScrolling: function () {
-            setBodyScrollingRule(this._pageOverflow);
-            $(window).scrollTop(this._scrollTop);
-            this._scrollTop = 0;
-        },
-        buildWindow: function (windowInitData) {
-            this._disableBackgroundPageScrolling();
 
             if (!this.$editor) {
                 this.$editor = this.factory(windowInitData).appendTo("body");
@@ -42,7 +44,8 @@ Imcms.define("imcms-window-builder", ["imcms-window-components-builder", "jquery
             this.$editor.css("display", "block");
         },
         closeWindow: function () {
-            this._enableBackgroundPageScrolling();
+            enableBackgroundPageScrolling(this._pageOverflow, this._scrollTop);
+            this._scrollTop = 0;
             this.$editor.css("display", "none");
 
             if (this.clearDataStrategy) {
