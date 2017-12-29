d4a8ac36cca764df04d8d3f03271dce2052bc381
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2017-12-29 18:04:37 +0200

    IMCMS-255 - Upgrade server side to work with new client: - FileDocumentDTO implemented.

---
+++ src/main/java/com/imcode/imcms/domain/dto/FileDocumentDTO.java
@@ -0,0 +1,35 @@
+package com.imcode.imcms.domain.dto;
+
+import com.imcode.imcms.persistence.entity.Meta;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class FileDocumentDTO extends DocumentDTO implements Serializable {
+
+    private static final long serialVersionUID = -8104577284192730444L;
+
+    private DocumentFileDTO file;
+
+    {
+        super.type = Meta.DocumentType.FILE;
+    }
+
+    public FileDocumentDTO(DocumentDTO from) {
+        super(from);
+    }
+
+    public static FileDocumentDTO createEmpty(DocumentDTO from) {
+        final FileDocumentDTO fileDocumentDTO = new FileDocumentDTO(from);
+        fileDocumentDTO.file = new DocumentFileDTO();
+
+        return fileDocumentDTO;
+    }
+}
