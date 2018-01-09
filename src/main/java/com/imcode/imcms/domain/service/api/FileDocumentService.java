afd9ed9485b7ed9e2154d0c11b65e11a6d52ebc9
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-01-09 16:31:20 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Added business logic for get/save in FileDocumentService.

--- src/main/java/com/imcode/imcms/domain/service/api/FileDocumentService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/FileDocumentService.java
@@ -1,12 +1,18 @@
 package com.imcode.imcms.domain.service.api;
 
 import com.imcode.imcms.domain.dto.DocumentDTO;
+import com.imcode.imcms.domain.dto.DocumentFileDTO;
 import com.imcode.imcms.domain.dto.FileDocumentDTO;
+import com.imcode.imcms.domain.service.DocumentFileService;
 import com.imcode.imcms.domain.service.DocumentService;
+import com.imcode.imcms.model.DocumentFile;
 import com.imcode.imcms.persistence.entity.Meta;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Service for work with File Documents only.
@@ -19,9 +25,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class FileDocumentService implements DocumentService<FileDocumentDTO> {
 
     private final DocumentService<DocumentDTO> defaultDocumentService;
+    private final DocumentFileService documentFileService;
 
-    FileDocumentService(@Qualifier("defaultDocumentService") DocumentService<DocumentDTO> documentService) {
-        defaultDocumentService = documentService;
+    FileDocumentService(@Qualifier("defaultDocumentService") DocumentService<DocumentDTO> documentService,
+                        DocumentFileService documentFileService) {
+
+        this.defaultDocumentService = documentService;
+        this.documentFileService = documentFileService;
     }
 
     @Override
@@ -31,11 +41,32 @@ public class FileDocumentService implements DocumentService<FileDocumentDTO> {
 
     @Override
     public FileDocumentDTO get(int docId) {
-        return new FileDocumentDTO(defaultDocumentService.get(docId));
+        final FileDocumentDTO fileDocument = new FileDocumentDTO(defaultDocumentService.get(docId));
+
+        final List<DocumentFileDTO> documentFiles = documentFileService.getByDocId(docId)
+                .stream()
+                .map(DocumentFileDTO::new)
+                .collect(Collectors.toList());
+
+        fileDocument.setFiles(documentFiles);
+
+        return fileDocument;
     }
 
     public int save(FileDocumentDTO saveMe) {
-        return defaultDocumentService.save(saveMe);
+        final int savedDocId = defaultDocumentService.save(saveMe);
+
+        final List<DocumentFile> saveMeFiles = saveMe.getFiles()
+                .stream()
+                .map(documentFileDTO -> {
+                    documentFileDTO.setDocId(savedDocId);
+                    return ((DocumentFile) documentFileDTO);
+                })
+                .collect(Collectors.toList());
+
+        documentFileService.saveAll(saveMeFiles);
+
+        return savedDocId;
     }
 
     @Override
