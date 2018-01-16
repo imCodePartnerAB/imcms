9c84f1e34a39c642dad739ccb5c7d28153efab96
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-01-16 16:55:17 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Refactored sending files through ajax for wider usage.

--- src/main/webapp/js/imcms_new/rest/imcms_image_files_rest_api.js
+++ src/main/webapp/js/imcms_new/rest/imcms_image_files_rest_api.js
@@ -3,12 +3,5 @@
  * 17.08.17.
  */
 Imcms.define("imcms-image-files-rest-api", ["imcms-rest-api"], function (rest) {
-
-    var apiPath = "/images/files";
-    var api = new rest.API(apiPath);
-
-    // custom non-json POST call for files
-    api.create = rest.ajax.bind({url: apiPath, type: "POST", json: false, contentType: false, processData: false});
-
-    return api;
+    return new rest.API("/images/files");
 });
