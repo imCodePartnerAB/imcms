c1ecc48d959392d71ca8b57e0d16c1d9a78132f6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-01-19 11:18:19 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Fixed Page Info tabs appearance.

--- src/main/webapp/js/imcms_new/builders/windows/page_info_tabs/imcms_page_info_tab_form_builder.js
+++ src/main/webapp/js/imcms_new/builders/windows/page_info_tabs/imcms_page_info_tab_form_builder.js
@@ -7,10 +7,10 @@ Imcms.define("imcms-page-info-tab-form-builder", ["imcms-bem-builder", "jquery"]
 
     return {
         showTab: function (tabIndex) {
-            $("[data-window-id=" + tabIndex + "]").css("display", "block");
+            $(".imcms-tabs__tab[data-window-id=" + tabIndex + "]").css("display", "block");
         },
         hideTab: function (tabIndex) {
-            $("[data-window-id=" + tabIndex + "]").css("display", "none");
+            $(".imcms-tabs__tab[data-window-id=" + tabIndex + "]").css("display", "none");
         },
         buildFormBlock: function (elements, index) {
             return formsBEM.buildBlock("<div>", elements, {"data-window-id": index}, "field");
