75a68fcdec22cfad8accad36ed5c601be0290427
Ivan Kohut <kogut.iwan@gmail.com>
2018-01-24 11:56:22 +0200

    IMCMS-233 - Apply new UI to the admin panel and editors: - Added DocumentURL abstract class and two its realizations (DTO and JPA).

--- src/main/java/com/imcode/imcms/persistence/entity/DocumentUrlJPA.java
+++ src/main/java/com/imcode/imcms/persistence/entity/DocumentUrlJPA.java
@@ -1,18 +1,24 @@
 package com.imcode.imcms.persistence.entity;
 
+import com.imcode.imcms.model.DocumentURL;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 /**
  * URL target and frame name are legacy fields and never used.
  */
 @Data
+@NoArgsConstructor
 @Entity
 @Table(name = "imcms_url_docs")
-public class DocumentUrlJPA extends VersionedContent {
+public class DocumentUrlJPA extends DocumentURL {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
     @Column(name = "frame_name")
     private String urlFrameName;
@@ -28,4 +34,16 @@ public class DocumentUrlJPA extends VersionedContent {
 
     @Column(name = "lang_prefix")
     private String urlLanguagePrefix;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumns({
+            @JoinColumn(name = "doc_id", referencedColumnName = "doc_id"),
+            @JoinColumn(name = "doc_version_no", referencedColumnName = "no")
+    })
+    private Version version;
+
+    public DocumentUrlJPA(DocumentURL from) {
+        super(from);
+    }
 }
