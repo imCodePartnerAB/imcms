ee1efe211fc44fa582fdaa5223cec23817866985
Ivan Kohut <kogut.iwan@gmail.com>
2018-01-26 12:40:27 +0200

    IMCMS-233 - Apply new UI to the admin panel and editors: - Added tests for checking url document saving.

--- src/test/java/com/imcode/imcms/domain/service/api/UrlDocumentServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/UrlDocumentServiceTest.java
@@ -4,7 +4,15 @@ import com.imcode.imcms.config.TestConfig;
 import com.imcode.imcms.domain.dto.DocumentUrlDTO;
 import com.imcode.imcms.domain.dto.UrlDocumentDTO;
 import com.imcode.imcms.domain.service.DocumentService;
+import com.imcode.imcms.domain.service.DocumentUrlService;
+import com.imcode.imcms.model.DocumentURL;
+import com.imcode.imcms.persistence.entity.DocumentUrlJPA;
 import com.imcode.imcms.persistence.entity.Meta;
+import com.imcode.imcms.persistence.repository.DocumentUrlRepository;
+import com.imcode.imcms.persistence.repository.MetaRepository;
+import imcode.server.Imcms;
+import imcode.server.user.UserDomainObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +20,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -24,10 +34,25 @@ public class UrlDocumentServiceTest {
     @Autowired
     private DocumentService<UrlDocumentDTO> urlDocumentService;
 
+    @Autowired
+    private MetaRepository metaRepository;
+
+    @Autowired
+    private DocumentUrlRepository documentUrlRepository;
+
+    @Autowired
+    private DocumentUrlService documentUrlService;
+
+    private UrlDocumentDTO emptyUrlDocumentDTO;
+
+    @Before
+    public void setUp() {
+        Imcms.setUser(new UserDomainObject(1));
+        this.emptyUrlDocumentDTO = urlDocumentService.createEmpty();
+    }
+
     @Test
     public void createEmpty() {
-        final UrlDocumentDTO emptyUrlDocumentDTO = urlDocumentService.createEmpty();
-
         final DocumentUrlDTO expectedDefaultDocumentUrlDTO = new DocumentUrlDTO();
         expectedDefaultDocumentUrlDTO.setId(null);
         expectedDefaultDocumentUrlDTO.setDocId(null);
@@ -43,5 +68,36 @@ public class UrlDocumentServiceTest {
         assertEquals(emptyUrlDocumentDTO.getType(), Meta.DocumentType.URL);
         assertNotNull(actualDefaultDocumentUrlDTO);
         assertEquals(expectedDefaultDocumentUrlDTO, actualDefaultDocumentUrlDTO);
+    }
+
+    @Test
+    public void saveUrlDocument_When_documentDoesNotExist_Expect_Saved() {
+        emptyUrlDocumentDTO.setDocumentUrlDTO(null);
+        urlDocumentService.save(emptyUrlDocumentDTO);
+
+        assertEquals(2, metaRepository.findAll().size()); // one doc with 1001 id
+    }
+
+    @Test
+    public void saveUrlDocument_When_DocumentUrlDTOIsNotSet_Expect_DocumentUrlDtoIsNotSaved() {
+        emptyUrlDocumentDTO.setDocumentUrlDTO(null);
+        urlDocumentService.save(emptyUrlDocumentDTO);
+
+        assertEquals(0, documentUrlRepository.findAll().size());
+    }
+
+    @Test
+    public void saveUrlDocument_When_DocumentUrlDtoIsSet_Expect_DocumentUrlDtoIsSaved() {
+        final int savedDocId = urlDocumentService.save(emptyUrlDocumentDTO);
+        final List<DocumentUrlJPA> all = documentUrlRepository.findAll();
+
+        assertEquals(1, all.size());
+
+        final DocumentURL actualDocumentUrl = documentUrlService.getByDocId(savedDocId);
+
+        final DocumentUrlDTO expectedDocumentUrl = emptyUrlDocumentDTO.getDocumentUrlDTO();
+        expectedDocumentUrl.setId(actualDocumentUrl.getId());
+
+        assertEquals(expectedDocumentUrl, expectedDocumentUrl);
     }
 }
