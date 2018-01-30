2e78db2b7eb271f886ac20e38dad7a7597c34070
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-01-30 16:28:55 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Implemented client part of Text History feature with mock data.

--- src/main/webapp/js/imcms_new/builders/windows/imcms_text_history_window_builder.js
+++ src/main/webapp/js/imcms_new/builders/windows/imcms_text_history_window_builder.js
@@ -5,13 +5,19 @@
  * 29.01.18
  */
 Imcms.define("imcms-text-history-window-builder",
-    ["imcms-window-builder", "imcms-bem-builder", "imcms-components-builder", "jquery"],
-    function (WindowBuilder, BEM, components, $) {
+    [
+        "imcms-window-builder", "imcms-bem-builder", "imcms-components-builder", "jquery",
+        "imcms-texts-history-rest-api", "tinyMCE"
+    ],
+    function (WindowBuilder, BEM, components, $, textsHistoryRestAPI, tinyMCE) {
 
-        var textData;
+        var $historyListContainer, $textHistoryView;
 
         function onWriteToTextField() {
-            console.log(textData);
+            var textButton = textHistoryWindowBuilder.$editor.find(".view-text-button")[0];
+            viewText.call(textButton);
+
+            tinyMCE.activeEditor.setContent($textHistoryView.html());
             textHistoryWindowBuilder.closeWindow();
         }
 
@@ -32,8 +38,52 @@ Imcms.define("imcms-text-history-window-builder",
             ]);
         }
 
+        function isClickAllowed(element) {
+            var $element = $(element);
+
+            if ($element.hasClass("imcms-button--disabled")) {
+                return false;
+            }
+
+            textHistoryWindowBuilder.$editor.find(".imcms-button--disabled")
+                .removeClass("imcms-button--disabled");
+
+            $element.addClass("imcms-button--disabled");
+
+            return true;
+        }
+
+        function viewSource() {
+            isClickAllowed(this) && $textHistoryView.text($textHistoryView.html());
+        }
+
+        function viewText() {
+            isClickAllowed(this) && $textHistoryView.html($textHistoryView.text());
+        }
+
+        function buildTextHistoryControlButtons() {
+            return components.buttons.buttonsContainer("<div>", [
+                components.buttons.negativeButton({
+                    "class": "view-source-button",
+                    text: "View Source",
+                    click: viewSource
+                }),
+                components.buttons.negativeButton({
+                    "class": "view-text-button",
+                    text: "View Text",
+                    click: viewText
+                })
+            ]);
+        }
+
         function buildHistoryView() {
-            return $("<div>", {"class": "imcms-right-side text-history-view"});
+            return new BEM({
+                block: "imcms-right-side",
+                elements: {
+                    "text-history-view": $textHistoryView = $("<div>"),
+                    "buttons": buildTextHistoryControlButtons()
+                }
+            }).buildBlockStructure("<div>");
         }
 
         function buildHistoryListContainer() {
@@ -45,19 +95,97 @@ Imcms.define("imcms-text-history-window-builder",
                 block: "imcms-pop-up-modal",
                 elements: {
                     "head": textHistoryWindowBuilder.buildHead("Text history"),
-                    "left-side": buildHistoryListContainer(),
+                    "left-side": $historyListContainer = buildHistoryListContainer(),
                     "right-side": buildHistoryView(),
                     "footer": buildFooter()
                 }
             }).buildBlockStructure("<div>", {"class": "text-history"});
         }
 
+        function buildTextHistoriesForDateContainer(date) {
+            var $date = $("<div>", {
+                "class": "text-history-date",
+                text: date
+            });
+
+            var $separator = $("<div>", {"class": "text-history-date-separator"});
+
+            return new BEM({
+                block: "text-history-date-container",
+                elements: {
+                    "date": $date,
+                    "separator": $separator
+                }
+            }).buildBlockStructure("<div>");
+        }
+
+        function showTextHistoryUnit(text) {
+            $textHistoryView.html(text);
+        }
+
+        function onTextHistoryUnitClicked(unit) {
+            $(".text-history-unit").removeClass("text-history-date-unit__unit--active");
+            $(unit).addClass("text-history-date-unit__unit--active");
+
+            textHistoryWindowBuilder.$editor.find(".imcms-buttons")
+                .find(".imcms-button")
+                .css("display", "block");
+
+            textHistoryWindowBuilder.$editor.find(".imcms-button--disabled")
+                .removeClass("imcms-button--disabled");
+
+            textHistoryWindowBuilder.$editor.find(".view-text-button")
+                .addClass("imcms-button--disabled");
+        }
+
+        function buildTextHistoriesForDate(textHistoriesForDate) {
+            return textHistoriesForDate.map(function (textHistory) {
+                var $textHistoryUnit = $("<div>", {
+                    "class": "text-history-unit",
+                    text: textHistory.modified.time + " | " + textHistory.modifiedBy.username,
+                    click: function () {
+                        onTextHistoryUnitClicked(this);
+                        showTextHistoryUnit(textHistory.text);
+                    }
+                });
+
+                return {"unit": $textHistoryUnit};
+            });
+        }
+
+        function buildTextHistoryUnit(date, textHistoriesForDate) {
+            var elements = [{"date": buildTextHistoriesForDateContainer(date)}];
+
+            new BEM({
+                block: "text-history-date-unit",
+                elements: elements.concat(buildTextHistoriesForDate(textHistoriesForDate))
+            }).buildBlockStructure("<div>").appendTo($historyListContainer);
+        }
+
         function loadData(textDTO) {
-            textData = textDTO;
+            textsHistoryRestAPI.read(textDTO).done(function (textsHistory) {
+                var dateToTextHistoryUnits = {};
+
+                textsHistory.forEach(function (textHistory) {
+                    var date = textHistory.modified.date;
+                    dateToTextHistoryUnits[date] = (dateToTextHistoryUnits[date] || []);
+                    dateToTextHistoryUnits[date].push(textHistory);
+                });
+
+                $.each(dateToTextHistoryUnits, buildTextHistoryUnit);
+
+                $historyListContainer.find(".text-history-date-unit__unit")
+                    .first()
+                    .click();
+            });
         }
 
         function clearData() {
-
+            $historyListContainer.empty();
+            $textHistoryView.html('');
+            textHistoryWindowBuilder.$editor.find(".imcms-buttons")
+                .find(".imcms-button")
+                .css("display", "none");
         }
 
         var textHistoryWindowBuilder = new WindowBuilder({
