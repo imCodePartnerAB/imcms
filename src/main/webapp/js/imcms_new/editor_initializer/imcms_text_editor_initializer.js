af349419fc0809e7d99736fe7d2ffac8fb725e4d
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-02-09 15:48:16 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Correct saving text content.

--- src/main/webapp/js/imcms_new/editor_initializer/imcms_text_editor_initializer.js
+++ src/main/webapp/js/imcms_new/editor_initializer/imcms_text_editor_initializer.js
@@ -21,6 +21,7 @@ Imcms.define("imcms-text-editor-initializer",
 
             textsRestApi.create(textDTO).success(function () {
                 events.trigger("imcms-version-modified");
+                tinyMCE.activeEditor.startContent = tinyMCE.activeEditor.getContent();
                 editor.setDirty(false);
             });
         }
