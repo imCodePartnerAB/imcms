bf3cea14b4f5861f74aa756bba04b499d3b55530
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-02-15 17:19:05 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Added status to Document class.

--- src/main/java/com/imcode/imcms/model/Document.java
+++ src/main/java/com/imcode/imcms/model/Document.java
@@ -1,9 +1,6 @@
 package com.imcode.imcms.model;
 
-import com.imcode.imcms.domain.dto.AuditDTO;
-import com.imcode.imcms.domain.dto.CategoryDTO;
-import com.imcode.imcms.domain.dto.CommonContentDTO;
-import com.imcode.imcms.domain.dto.RestrictedPermissionDTO;
+import com.imcode.imcms.domain.dto.*;
 import com.imcode.imcms.persistence.entity.Meta.DisabledLanguageShowMode;
 import com.imcode.imcms.persistence.entity.Meta.DocumentType;
 import com.imcode.imcms.persistence.entity.Meta.Permission;
@@ -13,10 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * @author Serhii Maksymchuk from Ubrainians for imCode
@@ -69,6 +63,50 @@ public abstract class Document {
 
     public Set<RestrictedPermissionDTO> getRestrictedPermissions() {
         return (this.restrictedPermissions == null) ? null : new TreeSet<>(this.restrictedPermissions);
+    }
+
+    @SuppressWarnings("unused") // used on client side
+    public DocumentStatus getDocumentStatus() {
+        if (PublicationStatus.NEW.equals(publicationStatus)) {
+            return DocumentStatus.IN_PROCESS;
+
+        } else if (PublicationStatus.DISAPPROVED.equals(publicationStatus)) {
+            return DocumentStatus.DISAPPROVED;
+
+        } else if (isAuditDateInPast(archived)) {
+            return DocumentStatus.ARCHIVED;
+
+        } else if (isAuditDateInPast(publicationEnd)) {
+            return DocumentStatus.PASSED;
+
+        } else if (PublicationStatus.APPROVED.equals(publicationStatus) && isAuditDateInPast(published)) {
+            return DocumentStatus.PUBLISHED;
+
+        } else if (PublicationStatus.APPROVED.equals(publicationStatus) && isAuditDateInFuture(published)) {
+            return DocumentStatus.PUBLISHED_WAITING;
+
+        } else if (PublicationStatus.APPROVED.equals(publicationStatus) && isNullAuditDate(published)) {
+            return DocumentStatus.IN_PROCESS;
+
+        } else { // should newer happen
+            return DocumentStatus.PUBLISHED;
+        }
+    }
+
+    private boolean isNullAuditDate(AuditDTO auditToCheck) {
+        return (auditToCheck == null) || (auditToCheck.getFormattedDate() == null);
+    }
+
+    private boolean isAuditDateInPast(AuditDTO auditToCheck) {
+        return (auditToCheck != null)
+                && (auditToCheck.getFormattedDate() != null)
+                && new Date().after(auditToCheck.getFormattedDate());
+    }
+
+    private boolean isAuditDateInFuture(AuditDTO auditToCheck) {
+        return (auditToCheck != null)
+                && (auditToCheck.getFormattedDate() != null)
+                && new Date().before(auditToCheck.getFormattedDate());
     }
 
 }
