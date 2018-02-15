bf2912e5b9f11fc887038ca59d08c7d04778aa26
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-02-15 17:18:19 +0200

    IMCMS-255 - Upgrade server side to work with new client: - Cleanup.

--- src/main/java/imcode/server/document/index/DocumentStoredFields.java
+++ src/main/java/imcode/server/document/index/DocumentStoredFields.java
@@ -4,7 +4,6 @@ package imcode.server.document.index;
 import com.imcode.imcms.domain.dto.DocumentStatus;
 import com.imcode.imcms.persistence.entity.Meta.DocumentType;
 import com.imcode.imcms.persistence.entity.Meta.PublicationStatus;
-import com.jcabi.aspects.Cacheable;
 import org.apache.solr.common.SolrDocument;
 
 import java.util.Date;
@@ -20,69 +19,56 @@ public class DocumentStoredFields {
         this.solrDocument = solrDocument;
     }
 
-    @Cacheable
     public int id() {
         return Integer.parseInt(solrDocument.getFieldValue(DocumentIndex.FIELD__META_ID).toString());
     }
 
-    @Cacheable
     public int versionNo() {
         return (Integer) solrDocument.getFieldValue(DocumentIndex.FIELD__VERSION_NO);
     }
 
-    @Cacheable
     public String headline() {
         return (String) solrDocument.getFieldValue(DocumentIndex.FIELD__META_HEADLINE);
     }
 
-    @Cacheable
     public String alias() {
         return (String) solrDocument.getFieldValue(DocumentIndex.FIELD__ALIAS);
     }
 
-    @Cacheable
     public String languageCode() {
         return (String) solrDocument.getFieldValue(DocumentIndex.FIELD__LANGUAGE_CODE);
     }
 
-    @Cacheable
     public DocumentType documentType() {
         final Integer typeOrdinal = (Integer) solrDocument.getFieldValue(DocumentIndex.FIELD__DOC_TYPE_ID);
         return DocumentType.values()[typeOrdinal];
     }
 
-    @Cacheable
     public PublicationStatus publicationStatus() {
         final Integer statusOrdinal = (Integer) solrDocument.getFieldValue(DocumentIndex.FIELD__STATUS);
         return PublicationStatus.values()[statusOrdinal];
     }
 
-    @Cacheable
     public Date created() {
         return (Date) solrDocument.getFieldValue(DocumentIndex.FIELD__CREATED_DATETIME);
     }
 
-    @Cacheable
     public Date modified() {
         return (Date) solrDocument.getFieldValue(DocumentIndex.FIELD__MODIFIED_DATETIME);
     }
 
-    @Cacheable
     public Date publicationStart() {
         return (Date) solrDocument.getFieldValue(DocumentIndex.FIELD__PUBLICATION_START_DATETIME);
     }
 
-    @Cacheable
     public Date archived() {
         return (Date) solrDocument.getFieldValue(DocumentIndex.FIELD__ARCHIVED_DATETIME);
     }
 
-    @Cacheable
     public Date publicationEndDt() {
         return (Date) solrDocument.getFieldValue(DocumentIndex.FIELD__PUBLICATION_END_DATETIME);
     }
 
-    @Cacheable
     public DocumentStatus documentStatus() {
         final PublicationStatus publicationStatus = publicationStatus();
 
