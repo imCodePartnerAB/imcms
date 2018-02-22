76cb4bfb31d949c0e4ec74dde9bfb23b34a5fab2
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-02-22 17:24:14 +0200

    IMCMS-277 - Profiles: - Introduced DocumentValidatingService interface.

---
+++ src/main/java/com/imcode/imcms/domain/service/DocumentValidatingService.java
@@ -0,0 +1,11 @@
+package com.imcode.imcms.domain.service;
+
+/**
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 22.02.18.
+ */
+public interface DocumentValidatingService {
+
+    boolean isTextDocument(String documentIdentifier);
+
+}
