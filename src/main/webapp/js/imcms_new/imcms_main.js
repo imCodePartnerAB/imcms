27bef9d2bb8855fb00f6821055e76b5bfe458ef9
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-03-01 17:44:27 +0200

    IMCMS-279 - Support text tag attributes: - Added polyfills for js.

--- src/main/webapp/js/imcms_new/imcms_main.js
+++ src/main/webapp/js/imcms_new/imcms_main.js
@@ -6,6 +6,30 @@ Function.prototype.bindArgs = function () {
 Function.prototype.applyAsync = function (args, context) {
     setTimeout(this.apply.bind(this, context, args));
 };
+if (!String.prototype.endsWith) {
+    Object.defineProperty(String.prototype, 'endsWith', {
+        value: function (searchString, position) {
+            var subjectString = this.toString();
+            if (position === undefined || position > subjectString.length) {
+                position = subjectString.length;
+            }
+            position -= searchString.length;
+            var lastIndex = subjectString.indexOf(searchString, position);
+            return lastIndex !== -1 && lastIndex === position;
+        }
+    });
+}
+if (!String.prototype.startsWith) {
+    Object.defineProperty(String.prototype, 'startsWith', {
+        enumerable: false,
+        configurable: false,
+        writable: false,
+        value: function (searchString, position) {
+            position = position || 0;
+            return this.indexOf(searchString, position) === position;
+        }
+    });
+}
 (function (imcms) {
     function registerModule(id, module) {
         if (imcms.modules[id]) {
