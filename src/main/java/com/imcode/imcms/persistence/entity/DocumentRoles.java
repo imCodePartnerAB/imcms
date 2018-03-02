e9b0a4bae39a42c128cabdacd061acbee2b09eb3
Ivan Kohut <kogut.iwan@gmail.com>
2018-03-02 19:15:00 +0200

    IMCMS-233 - Apply new UI to the admin panel and editors: - Created entity for "roles_rights" table.

---
+++ src/main/java/com/imcode/imcms/persistence/entity/DocumentRoles.java
@@ -0,0 +1,36 @@
+package com.imcode.imcms.persistence.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "roles_rights")
+public class DocumentRoles {
+
+    @EmbeddedId
+    private DocumentRoleId id;
+
+    @ManyToOne
+    @JoinColumn(name = "meta_id", insertable = false, updatable = false)
+    private Meta document;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", insertable = false, updatable = false)
+    private RoleJPA role;
+
+    @Column(name = "permission", nullable = false, columnDefinition = "VARCHAR(16)")
+    @Enumerated(EnumType.STRING)
+    private Meta.Permission permission;
+
+    public DocumentRoles(Meta document, RoleJPA role, Meta.Permission permission) {
+        this.document = document;
+        this.role = role;
+        this.permission = permission;
+
+        this.id = new DocumentRoleId(document.getId(), role.getId());
+    }
+}
