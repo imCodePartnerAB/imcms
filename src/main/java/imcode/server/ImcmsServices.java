48d75c52358dbd3c3997defa3dcc613c40745f3e
Ivan Kohut <kogut.iwan@gmail.com>
2018-03-16 15:50:28 +0200

    IMCMS-281 - Make latest version compatible with old API: - Added getter for menu service.

--- src/main/java/imcode/server/ImcmsServices.java
+++ src/main/java/imcode/server/ImcmsServices.java
@@ -5,6 +5,7 @@ import com.imcode.imcms.api.DatabaseService;
 import com.imcode.imcms.api.DocumentLanguages;
 import com.imcode.imcms.api.MailService;
 import com.imcode.imcms.db.ProcedureExecutor;
+import com.imcode.imcms.domain.service.MenuService;
 import com.imcode.imcms.domain.service.TemplateService;
 import com.imcode.imcms.mapping.CategoryMapper;
 import com.imcode.imcms.mapping.DocumentMapper;
@@ -95,4 +96,6 @@ public interface ImcmsServices {
     MailService getMailService();
 
     TemplateService getTemplateService();
+
+    MenuService getMenuService();
 }
