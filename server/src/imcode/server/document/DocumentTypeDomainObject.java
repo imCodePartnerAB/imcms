7b317af54557dd950b75ff28c8b3b20d3803cf5c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-03-21 12:16:25 +0200

    IMCMS-283 - Add return_url also on Change Menu: - Fixed constants order.

--- server/src/imcode/server/document/DocumentTypeDomainObject.java
+++ server/src/imcode/server/document/DocumentTypeDomainObject.java
@@ -13,6 +13,15 @@ public class DocumentTypeDomainObject extends IdLocalizedNamePair {
     public static final int BROWSER_ID = 6;
     public static final int HTML_ID = 7;
     public static final int FILE_ID = 8;
+
+    private static final String DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX = "document_type/name/";
+
+    public final static DocumentTypeDomainObject BROWSER = new DocumentTypeDomainObject(BROWSER_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "browser"));
+    public final static DocumentTypeDomainObject TEXT = new DocumentTypeDomainObject(TEXT_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "text"));
+    public final static DocumentTypeDomainObject URL = new DocumentTypeDomainObject(URL_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "url"));
+    public final static DocumentTypeDomainObject HTML = new DocumentTypeDomainObject(HTML_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "html"));
+    public final static DocumentTypeDomainObject FILE = new DocumentTypeDomainObject(FILE_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "file"));
+
     final static DocumentTypeDomainObject[] ALL_DOCUMENT_TYPES = {
             BROWSER,
             FILE,
@@ -20,12 +29,6 @@ public class DocumentTypeDomainObject extends IdLocalizedNamePair {
             TEXT,
             URL,
     };
-    private static final String DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX = "document_type/name/";
-    public final static DocumentTypeDomainObject TEXT = new DocumentTypeDomainObject(TEXT_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "text"));
-    public final static DocumentTypeDomainObject URL = new DocumentTypeDomainObject(URL_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "url"));
-    public final static DocumentTypeDomainObject BROWSER = new DocumentTypeDomainObject(BROWSER_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "browser"));
-    public final static DocumentTypeDomainObject HTML = new DocumentTypeDomainObject(HTML_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "html"));
-    public final static DocumentTypeDomainObject FILE = new DocumentTypeDomainObject(FILE_ID, new LocalizedMessage(DOCUMENT_TYPE_NAME_LOCALIZED_MESSAGE_PREFIX + "file"));
 
     public DocumentTypeDomainObject(int id, LocalizedMessage name) {
         super(id, name);
