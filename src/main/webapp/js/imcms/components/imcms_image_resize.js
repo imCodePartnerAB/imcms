fbbfda9420c0503225b83d95bd818ee23556064a
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-03-27 18:34:49 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Created image resize module.

---
+++ src/main/webapp/js/imcms/components/imcms_image_resize.js
@@ -0,0 +1,74 @@
+/**
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 27.03.18
+ */
+Imcms.define(
+    "imcms-image-resize",
+    ["imcms-image-crop-angles", "imcms-image-cropping-elements", "imcms-events"],
+    function (croppingAngles, cropElements, events) {
+        return {
+            resize: function (opts, imageDataContainers, isImageProportionsInverted) {
+                var angleHeight = croppingAngles.getHeight();
+                var angleWidth = croppingAngles.getWidth();
+                var angleBorderSize = croppingAngles.getBorderSize();
+                var doubleAngleBorderSize = croppingAngles.getDoubleBorderSize();
+                var newWidth = opts.image.width;
+                var newHeight = opts.image.height;
+                var cropArea = opts.cropArea;
+
+                cropElements.$image.animate(opts.image, 200);
+
+                imageDataContainers.$shadow.animate({
+                    "width": ((isImageProportionsInverted) ? newHeight : newWidth) + doubleAngleBorderSize,
+                    "height": ((isImageProportionsInverted) ? newWidth : newHeight) + doubleAngleBorderSize
+                }, 200);
+
+                cropElements.$cropImg.animate({
+                    "left": -cropArea.left + angleBorderSize,
+                    "top": -cropArea.top + angleBorderSize,
+                    "width": newWidth,
+                    "height": newHeight
+                }, 200);
+
+                cropElements.$cropArea.animate(cropArea, 200);
+
+                var positionTopForAngelsTop = cropArea.top - angleBorderSize;
+                var positionTopForAngelsBottom = cropArea.top + cropArea.height - angleHeight;
+                var positionLeftForAngelsLeft = cropArea.left - angleBorderSize;
+                var positionLeftForAngelsRight = cropArea.left + cropArea.width - angleWidth;
+
+                if (cropArea.left <= angleBorderSize) {
+                    positionLeftForAngelsLeft = 0;
+                }
+
+                if (cropArea.top <= angleBorderSize) {
+                    positionTopForAngelsTop = 0;
+                }
+
+                croppingAngles.topRight.animate({
+                    "top": positionTopForAngelsTop,
+                    "left": positionLeftForAngelsRight
+                }, 200);
+
+                croppingAngles.bottomRight.animate({
+                    "top": positionTopForAngelsBottom,
+                    "left": positionLeftForAngelsRight
+                }, 200);
+
+                croppingAngles.topLeft.animate({
+                    "top": positionTopForAngelsTop,
+                    "left": positionLeftForAngelsLeft
+                }, 200);
+
+                croppingAngles.bottomLeft.animate({
+                    "top": positionTopForAngelsBottom,
+                    "left": positionLeftForAngelsLeft
+                }, 200);
+
+                setTimeout(function () {
+                    events.trigger("update cropArea");
+                }, 250);
+            }
+        };
+    }
+);
