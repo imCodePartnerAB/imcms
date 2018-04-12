b654552156a8c3eca921de1d233487bf21eab106
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-04-12 14:27:18 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Created validator interface.

---
+++ src/main/java/com/imcode/imcms/components/Validator.java
@@ -0,0 +1,13 @@
+package com.imcode.imcms.components;
+
+import javax.validation.ValidationException;
+
+/**
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 12.04.18.
+ */
+public interface Validator<T> {
+
+    void validate(T validateMe) throws ValidationException;
+
+}
