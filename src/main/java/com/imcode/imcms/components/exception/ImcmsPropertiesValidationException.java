5bbcbc5b3ea010d07717821d60bdad63b8241365
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-04-12 14:28:31 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Implemented properties validation exception.

---
+++ src/main/java/com/imcode/imcms/components/exception/ImcmsPropertiesValidationException.java
@@ -0,0 +1,18 @@
+package com.imcode.imcms.components.exception;
+
+import javax.validation.ValidationException;
+
+/**
+ * Indicates that something wrong with required imcms properties.
+ *
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 12.04.18.
+ */
+public class ImcmsPropertiesValidationException extends ValidationException {
+
+    private static final long serialVersionUID = -8063618949892864259L;
+
+    public ImcmsPropertiesValidationException(String message) {
+        super(message);
+    }
+}
