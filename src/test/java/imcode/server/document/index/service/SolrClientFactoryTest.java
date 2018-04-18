735e68d9ab4d9e7b6ea2f1b8c23fe5be4acd848a
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-04-19 00:26:04 +0300

    IMCMS-233: Apply new UI to the admin panel and editors: - SolrClientFactoryTest refactored.

--- src/test/java/imcode/server/document/index/service/SolrClientFactoryTest.java
+++ src/test/java/imcode/server/document/index/service/SolrClientFactoryTest.java
@@ -3,6 +3,7 @@ package imcode.server.document.index.service;
 import com.imcode.imcms.components.datainitializer.VersionDataInitializer;
 import com.imcode.imcms.config.TestConfig;
 import imcode.server.document.index.service.impl.DocumentIndexServiceOps;
+import org.apache.http.conn.HttpHostConnectException;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -20,7 +21,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.net.ConnectException;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -48,58 +51,75 @@ public class SolrClientFactoryTest {
         solrClient = new HttpSolrClient.Builder(SOLR_URL).build();
     }
 
-    @Test
-    public void createHttpSolrClient_When_SpecifiedCoreDoesNotExist_Expect_CoreAndSolrClientAreCreated() {
+    @After
+    public void deleteCore() throws IOException {
         try {
-            final SolrClient httpSolrClient = SolrClientFactory
-                    .createHttpSolrClient(SOLR_URL + "/" + CORE, false);
+            CoreAdminRequest.unloadCore(CORE, true, true, solrClient);
 
-            assertNotNull(httpSolrClient);
-            coreExists(CORE);
-        } catch (Exception e) {
-            // solr is not setup
+        } catch (SolrServerException e) {
+            // solr is not set up, ok.
+            checkException(e);
         }
     }
 
     @Test
-    public void createHttpSolrClient_When_SpecifiedCoreExist_Expect_SolrClientAreCreated() {
+    public void createHttpSolrClient_When_SpecifiedCoreDoesNotExist_Expect_CoreAndSolrClientAreCreated() throws IOException, SolrServerException {
+        try {
+            final SolrClient httpSolrClient = SolrClientFactory.createHttpSolrClient(
+                    SOLR_URL + "/" + CORE, false
+            );
+
+            assertNotNull(httpSolrClient);
+            coreExists(CORE);
+
+        } catch (IllegalArgumentException e) {
+            // solr is not set up, ok.
+            checkException(e);
+        }
+    }
+
+    @Test
+    public void createHttpSolrClient_When_SpecifiedCoreExist_Expect_SolrClientAreCreated() throws IOException {
         try {
             CoreAdminRequest.createCore(CORE, CORE, solrClient);
             coreExists(CORE);
 
-            final SolrClient httpSolrClient = SolrClientFactory
-                    .createHttpSolrClient(SOLR_URL + "/" + CORE, false);
+            final SolrClient httpSolrClient = SolrClientFactory.createHttpSolrClient(
+                    SOLR_URL + "/" + CORE, false
+            );
 
             assertNotNull(httpSolrClient);
-        } catch (Exception e) {
-            // solr is not setup
+
+        } catch (SolrServerException e) {
+            // solr is not set up, ok.
+            checkException(e);
         }
     }
 
     @Test
-    public void createHttpSolrClient_When_RecreateDataDirIsSet_Expect_Recreated() {
+    public void createHttpSolrClient_When_RecreateDataDirIsSet_Expect_Recreated() throws IOException, SolrServerException {
         try {
-            SolrClient httpSolrClient = SolrClientFactory
-                    .createHttpSolrClient(SOLR_URL + "/" + CORE, false);
+            SolrClient httpSolrClient = SolrClientFactory.createHttpSolrClient(
+                    SOLR_URL + "/" + CORE, false
+            );
 
             versionDataInitializer.createData(0, DOC_ID);
 
             documentIndexServiceOps.addDocsToIndex(httpSolrClient, DOC_ID);
 
-            final SolrQuery solrQuery = new SolrQuery("*:*");
+            final SolrQuery query = new SolrQuery("*:*");
 
-            assertTrue(
-                    documentIndexServiceOps.query(httpSolrClient, solrQuery).getResults().getNumFound() == 1
+            assertEquals(1, documentIndexServiceOps.query(httpSolrClient, query).getResults().getNumFound());
+
+            httpSolrClient = SolrClientFactory.createHttpSolrClient(
+                    SOLR_URL + "/" + CORE, true
             );
 
-            httpSolrClient = SolrClientFactory
-                    .createHttpSolrClient(SOLR_URL + "/" + CORE, true);
+            assertEquals(0, documentIndexServiceOps.query(httpSolrClient, query).getResults().getNumFound());
 
-            assertTrue(
-                    documentIndexServiceOps.query(httpSolrClient, solrQuery).getResults().getNumFound() == 0
-            );
-        } catch (Exception e) {
-            // solr is not setup
+        } catch (IllegalArgumentException e) {
+            // solr is not set up, ok.
+            checkException(e);
         }
     }
 
@@ -111,12 +131,18 @@ public class SolrClientFactoryTest {
         assertNotNull(coreAdminRequest.process(solrClient).getCoreStatus(coreName));
     }
 
-    @After
-    public void deleteCore() {
-        try {
-            CoreAdminRequest.unloadCore(CORE, true, true, solrClient);
-        } catch (Exception e) {
-            // solr is not setup
-        }
+    private void checkException(IllegalArgumentException e) {
+        final Throwable cause = e.getCause();
+        assertTrue(cause instanceof SolrServerException);
+        checkException(((SolrServerException) cause));
+    }
+
+    private void checkException(SolrServerException e) {
+        Throwable cause = e.getCause();
+        assertTrue(cause instanceof HttpHostConnectException);
+
+        cause = cause.getCause();
+        assertTrue(cause instanceof ConnectException);
+        assertEquals(cause.getMessage(), "Connection refused: connect");
     }
 }
