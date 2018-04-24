7ce6d9ef9f68557a52257bfdc769e1cee3bedc26
Ivan Kohut <kogut.iwan@gmail.com>
2018-04-24 18:00:32 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Showing alert message and redirect to login page when session time is expired.

--- src/main/webapp/js/imcms/imcms_start.js
+++ src/main/webapp/js/imcms/imcms_start.js
@@ -5,8 +5,8 @@
  * 07.08.17.
  */
 Imcms.require(
-    ["imcms-admin-panel-builder", "imcms", "imcms-tests", "imcms-events"],
-    function (panelBuilder, imcms, tests, events) {
+    ["imcms-admin-panel-builder", "imcms", "imcms-tests", "imcms-events", "imcms-i18n-texts"],
+    function (panelBuilder, imcms, tests, events, texts) {
         Imcms.tests = tests;
         console.info("%c Tests loaded.", "color: green");
 
@@ -17,6 +17,11 @@ Imcms.require(
         events.on("imcms-publish-new-version-current-doc", function () {
             window.location.href = imcms.contextPath + "/api/publish-document/" + imcms.document.id;
         });
+
+        setTimeout(function () {
+            alert(texts.sessionExpiredMessage);
+            window.location.href = imcms.contextPath + "/login";
+        }, imcms.expiredSessionTime);
 
         function detectActivePanelButton() {
             if (imcms.isEditMode) {
