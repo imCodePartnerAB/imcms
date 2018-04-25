6c2d914294341de5fb83004492c8d3f026c5c182
Ivan Kohut <kogut.iwan@gmail.com>
2018-04-25 13:53:21 +0300

    IMCMS-233 - Apply new UI to the admin panel and editors: - Changed expired session messages.

--- src/main/webapp/js/imcms/i18n/imcms_i18n_texts.js
+++ src/main/webapp/js/imcms/i18n/imcms_i18n_texts.js
@@ -272,7 +272,7 @@ Imcms.define("imcms-i18n-texts", ["imcms"], function (imcms) {
                 errors: " Fel",
                 warnings: " Varningar"
             },
-            sessionExpiredMessage: "Du har blivit utloggad p\u00e5 grund av inaktivitet."
+            sessionExpiredMessage: "Du har blivit utloggad p\u00e5 grund av inaktivitet. Omdirigera till inloggningssidan?"
         },
         "en": {
             panel: {
@@ -539,7 +539,7 @@ Imcms.define("imcms-i18n-texts", ["imcms"], function (imcms) {
                 errors: " Errors",
                 warnings: " Warnings"
             },
-            sessionExpiredMessage: "You have been logged out due to inactivity."
+            sessionExpiredMessage: "You have been logged out due to inactivity. Redirect to login page?"
         }
     };
 
