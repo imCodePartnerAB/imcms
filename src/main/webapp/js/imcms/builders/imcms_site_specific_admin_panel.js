80b042c93686b63c90b319df2a372121b6364c1c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-05-03 19:05:38 +0300

    IMCMS-268 - Client specific area: - Added cookies to remember special panel position.

--- src/main/webapp/js/imcms/builders/imcms_site_specific_admin_panel.js
+++ src/main/webapp/js/imcms/builders/imcms_site_specific_admin_panel.js
@@ -4,8 +4,8 @@
  */
 Imcms.define(
     "imcms-site-specific",
-    ["imcms-admin-panel-builder", "imcms-i18n-texts", "jquery"],
-    function (panelBuilder, texts, $) {
+    ["imcms-admin-panel-builder", "imcms-i18n-texts", "imcms-cookies", "jquery"],
+    function (panelBuilder, texts, cookies, $) {
 
         texts = texts.panel;
 
@@ -27,23 +27,41 @@ Imcms.define(
             });
 
             $link.insertAfter('.imcms-admin-panel .imcms-panel__item--page-info:first');
+            var $collapsible = $('#imcmsAdminSpecial.imcms-collapsible');
 
             $link.click(function () {
-                var $collapsible = $('#imcmsAdminSpecial.imcms-collapsible');
-
                 if ($collapsible.hasClass('imcms-collapsible-hidden')) {
                     $collapsible.removeClass('imcms-collapsible-hidden');
                     $link.addClass('imcms-panel__item--active');
+                    cookies.setCookie("imcms-client-special-area", "opened", {expires: 30});
 
                 } else {
                     $collapsible.addClass('imcms-collapsible-hidden');
                     $link.removeClass('imcms-panel__item--active');
+                    cookies.setCookie("imcms-client-special-area", "closed", {expires: 30});
                 }
 
                 window.setTimeout(function () {
                     $('body').css('top', $('#imcms-admin').height() + 'px');
                 }, 300);
             });
+
+            if (cookies.getCookie("imcms-client-special-area") === "opened") {
+                var $imcms = $('#imcms-admin');
+                $imcms.css('top', "-10000px"); // dummy value
+
+                // holy shit code! rewrite client-special panel to behave as separated part of regular admin panel,
+                // no hiding with height = 0, just set top = -height with next falling down when needs!
+
+                window.setTimeout(function () {
+                    $link.addClass('imcms-panel__item--active');
+                    $collapsible.removeClass('imcms-collapsible-hidden');
+
+                    window.setTimeout(function () {
+                        $imcms.css('top', "-" + $imcms.height() + 'px');
+                    }, 100);
+                }, 100);
+            }
         }
 
         return {
