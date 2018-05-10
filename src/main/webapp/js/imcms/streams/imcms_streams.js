97af56066c9a881aa8f667ebd3e908660cb0b815
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-05-10 23:17:15 +0300

    IMCMS-290: Add a "pin" icon to imCMS's panel: - Implemented streams module.

--- src/main/webapp/js/imcms/streams/imcms_streams.js
+++ src/main/webapp/js/imcms/streams/imcms_streams.js
@@ -4,8 +4,85 @@
  * @author Serhii Maksymchuk from Ubrainians for imCode
  * 10.05.18
  */
-Imcms.define('imcms-streams', [], function () {
+Imcms.define('imcms-streams', ['imcms-displacing-array'], function (DisplacingArray) {
 
+    var Subscriber = function (topic) {
+        (this.topic = topic).addSubscriber(this);
+    };
 
-    return {};
+    Subscriber.prototype = {
+        onPublish: function (doOnPublish) {
+            this.doOnPublish = doOnPublish;
+            return this;
+        },
+        runFromStart: function () {
+            this.topic.forEachPublication(this.reactOnPublication.bind(this))
+        },
+        getReactionOnPublication: function (content) {
+            return function () {
+                this.doOnPublish(content);
+            }.bind(this);
+        },
+        reactOnPublication: function (content) {
+            setTimeout(this.getReactionOnPublication(content));
+        }
+    };
+
+    var Topic = function (name) {
+        this.name = name;
+        this.publications = new DisplacingArray(1024);
+        this.subscribers = new DisplacingArray(1024);
+    };
+
+    Topic.prototype = {
+        forEachPublication: function (doOnPublication) {
+            this.publications.forEach(doOnPublication);
+        },
+        addSubscriber: function (subscriber) {
+            this.subscribers.push(subscriber);
+        },
+        getAllSubscribers: function () {
+            return this.subscribers;
+        },
+        savePublication: function (content) {
+            this.publications.push(content);
+        },
+        publish: function (content) {
+            this.savePublication(content);
+
+            this.getAllSubscribers().forEach(function (subscriber) {
+                subscriber.reactOnPublication(content);
+            });
+        }
+    };
+
+    var namePerTopic = {};
+
+    function getOrCreateTopic(topicName) {
+        return namePerTopic[topicName] || (namePerTopic[topicName] = new Topic(topicName));
+    }
+
+    var Publisher = function (topic) {
+        this.topic = topic;
+    };
+
+    Publisher.prototype = {
+        publish: function (content) {
+            this.topic.publish(content);
+        }
+    };
+
+    return {
+        createSubscriberOnTopic: function (topicName) {
+            return new Subscriber(getOrCreateTopic(topicName));
+        },
+        subscribeFromStart: function (topicName, doOnPublish) {
+            this.createSubscriberOnTopic(topicName)
+                .onPublish(doOnPublish)
+                .runFromStart();
+        },
+        createPublisherOnTopic: function (topicName) {
+            return new Publisher(getOrCreateTopic(topicName));
+        }
+    }
 });
