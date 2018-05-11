c5fc5998548174e3a19358cb6663eb9d5728f705
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-05-12 00:50:58 +0300

    IMCMS-290: Add a "pin" icon to imCMS's panel: - Refactored displacing array.

--- src/main/webapp/js/imcms/components/imcms_displacing_array.js
+++ src/main/webapp/js/imcms/components/imcms_displacing_array.js
@@ -1,20 +1,21 @@
 /**
- * Array with fixed max length and displacing first element on oversize.
+ * List with fixed max length and displacing first element on oversize.
  *
  * @author Serhii Maksymchuk from Ubrainians for imCode
  * 10.05.18
  */
-Imcms.define('imcms-displacing-array', [], function () {
+Imcms.define('imcms-displacing-array', [], function () { // todo: rename "array"->"list" in module name and file
 
-    var DisplacingArray = function (size) {
+    var DisplacingList = function (size) {
         if (typeof size !== 'number') throw new Error("Size should be integer!");
         if (size < 1) throw new Error("Size should be >= 1");
 
         this.size = size;
         this.elements = [];
+        this.length = 0;
     };
 
-    DisplacingArray.prototype = {
+    DisplacingList.prototype = {
         push: function (content) {
             if (this.elements.length > this.size) {
                 this.elements.splice(0, 1); // removes first element
@@ -25,8 +26,11 @@ Imcms.define('imcms-displacing-array', [], function () {
         },
         forEach: function (doForEach) {
             this.elements.forEach(doForEach);
+        },
+        get: function (index) {
+            return this.elements[index];
         }
     };
 
-    return DisplacingArray;
+    return DisplacingList;
 });
