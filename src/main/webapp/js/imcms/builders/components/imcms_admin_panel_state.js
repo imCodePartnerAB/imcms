f5058c939332724ad39b6b522277ab414fe62d14
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-05-12 01:13:16 +0300

    IMCMS-290: Add a "pin" icon to imCMS's panel: - Implemented panel state module.

--- src/main/webapp/js/imcms/builders/components/imcms_admin_panel_state.js
+++ src/main/webapp/js/imcms/builders/components/imcms_admin_panel_state.js
@@ -1,3 +1,25 @@
-Imcms.define("imcms-admin-panel-state", [], function () {
-    return {}
+Imcms.define("imcms-admin-panel-state", ["jquery"], function ($) {
+
+    var PanelState = function () {
+        this.isSpecialPanelHidingPrevented = false;
+    };
+
+    PanelState.prototype = {
+        refreshSpecialPanelPosition: function () {
+            var $imcmsAdminSpecial = $('#imcmsAdminSpecial');
+
+            if ($imcmsAdminSpecial.length) {
+                var adminPanelHeight = this.isSpecialPanelHidingPrevented ? 0 : $('#imcms-admin-panel').outerHeight();
+                $imcmsAdminSpecial.css("padding-top", adminPanelHeight);
+            }
+        },
+        disableSpecialPanelHiding: function () {
+            this.isSpecialPanelHidingPrevented = true;
+        },
+        enableSpecialPanelHiding: function () {
+            this.isSpecialPanelHidingPrevented = false;
+        }
+    };
+
+    return new PanelState();
 });
