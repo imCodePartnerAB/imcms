5cea6e184078bc021edb8110bed0ff2f8bb16d19
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-05-15 18:32:38 +0300

    IMCMS-294 - ENTER (RETURN) as SAVE: - Window Builder wired with windows key controller.

--- src/main/webapp/js/imcms/builders/windows/imcms_window_builder.js
+++ src/main/webapp/js/imcms/builders/windows/imcms_window_builder.js
@@ -2,101 +2,125 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 04.09.17
  */
-Imcms.define("imcms-window-builder", ["imcms-window-components-builder", "jquery"], function (windowComponents, $) {
+Imcms.define(
+    "imcms-window-builder",
+    ["imcms-window-components-builder", "imcms-window-keys-controller", "jquery"],
+    function (windowComponents, windowKeysController, $) {
 
-    function setBodyScrollingRule(overflowValue) {
-        $("body").css("overflow", overflowValue);
-    }
+        function setBodyScrollingRule(overflowValue) {
+            $("body").css("overflow", overflowValue);
+        }
 
-    function enableBackgroundPageScrolling(pageOverflow, scrollTop) {
-        setBodyScrollingRule(pageOverflow);
-        $(window).scrollTop(scrollTop);
-    }
+        function enableBackgroundPageScrolling(pageOverflow, scrollTop) {
+            setBodyScrollingRule(pageOverflow);
+            $(window).scrollTop(scrollTop);
+        }
 
-    function getScrollTopAndDisable() {
-        var $window = $(window);
-        var prevScrollTop = $window.scrollTop();
-        $window.scrollTop(0);
+        function getScrollTopAndDisable() {
+            var $window = $(window);
+            var prevScrollTop = $window.scrollTop();
+            $window.scrollTop(0);
 
-        return prevScrollTop;
-    }
+            return prevScrollTop;
+        }
 
-    var WindowBuilder = function (opts) {
-        this.factory = opts.factory;
-        this.loadDataStrategy = opts.loadDataStrategy;
-        this.clearDataStrategy = opts.clearDataStrategy;
-        this.$editor = undefined;
-    };
+        var WindowBuilder = function (opts) {
+            this.factory = opts.factory;
+            this.loadDataStrategy = opts.loadDataStrategy;
+            this.clearDataStrategy = opts.clearDataStrategy;
+            this.onEscKeyPressed = opts.onEscKeyPressed;
+            this.onEnterKeyPressed = opts.onEnterKeyPressed;
+            this.disableKeyBindings = opts.disableKeyBindings;
+            this.$editor = undefined;
+        };
 
-    WindowBuilder.prototype = {
-        _scrollTop: 0,
-        _pageOverflow: "auto",
-        buildWindowWithShadow: function (windowInitData) {
-            this.shadowBuilder = this.shadowBuilder || new WindowBuilder({
-                factory: function () {
-                    return $("<div>", {"class": "imcms-modal-layout"});
-                }
-            });
+        WindowBuilder.prototype = {
+            _scrollTop: 0,
+            _pageOverflow: "auto",
+            buildWindowWithShadow: function (windowInitData) {
+                this.shadowBuilder = this.shadowBuilder || new WindowBuilder({
+                    disableKeyBindings: true,
+                    factory: function () {
+                        return $("<div>", {"class": "imcms-modal-layout"});
+                    }
+                });
 
-            this.shadowBuilder.buildWindow();
-            this.buildWindow.applyAsync(arguments, this);
-        },
-        buildWindow: function (windowInitData) {
-            try {
-                this._scrollTop = getScrollTopAndDisable();
-                this._pageOverflow = $("body").css("overflow") || "auto";
-                setBodyScrollingRule("hidden");
-
-                if (!this.$editor) {
-                    this.$editor = this.factory.apply(null, arguments).appendTo("body");
-                }
-
-                this.loadDataStrategy && this.loadDataStrategy.applyAsync(arguments);
-                this.$editor.css("display", "block");
-
-            } catch (e) {
-                console.error(e);
-                alert("Error in window builder! Stacktrace in console.");
-                this.shadowBuilder.closeWindow();
-            }
-        },
-        closeWindow: function () {
-            enableBackgroundPageScrolling(this._pageOverflow, this._scrollTop);
-            this._scrollTop = 0;
-            this.$editor.css("display", "none");
-
-            if (this.shadowBuilder) {
-                setTimeout(this.shadowBuilder.closeWindow.bind(this.shadowBuilder));
-            }
-
-            if (this.clearDataStrategy) {
+                this.shadowBuilder.buildWindow();
+                this.buildWindow.applyAsync(arguments, this);
+            },
+            buildWindow: function (windowInitData) {
                 try {
-                    this.clearDataStrategy.call();
+                    this._scrollTop = getScrollTopAndDisable();
+                    this._pageOverflow = $("body").css("overflow") || "auto";
+                    setBodyScrollingRule("hidden");
 
+                    if (!this.$editor) {
+                        this.$editor = this.factory.apply(null, arguments).appendTo("body");
+                    }
+
+                    this.loadDataStrategy && this.loadDataStrategy.applyAsync(arguments);
+                    this.$editor.css("display", "block");
+
+                    this.disableKeyBindings || windowKeysController.registerWindow(
+                        this.onEscKeyPressed, this.onEnterKeyPressed
+                    );
                 } catch (e) {
                     console.error(e);
-                }
-            }
-        },
-        /**
-         * Builds head with specified title and close button
-         * @param {string} title
-         * @param {function} onCloseClick
-         * @returns {*} head as jQuery element
-         */
-        buildHead: function (title, onCloseClick) {
-            onCloseClick = onCloseClick || this.closeWindow.bind(this);
-            return windowComponents.buildHead(title, onCloseClick);
-        },
-        /**
-         * Builds footer with specified buttons as it's block elements
-         * @param {[]} buttons - array of jQuery elements
-         * @returns {*} footer as jQuery element
-         */
-        buildFooter: function (buttons) {
-            return windowComponents.buildFooter.apply(windowComponents, arguments);
-        }
-    };
+                    alert("Error in window builder! Stacktrace in console.");
 
-    return WindowBuilder;
-});
+                    this.$editor && this.$editor.detach();
+                    this.shadowBuilder && this.shadowBuilder.closeWindow();
+                    this.disableKeyBindings || windowKeysController.unRegister();
+                }
+            },
+            closeWindow: function () {
+                try {
+                    this.disableKeyBindings || windowKeysController.unRegister();
+                    enableBackgroundPageScrolling(this._pageOverflow, this._scrollTop);
+
+                    this._scrollTop = 0;
+                    this.$editor.css("display", "none");
+
+                    if (this.shadowBuilder) {
+                        setTimeout(this.shadowBuilder.closeWindow.bind(this.shadowBuilder));
+                    }
+
+                    if (this.clearDataStrategy && this.clearDataStrategy.call) {
+                        try {
+                            this.clearDataStrategy.call();
+
+                        } catch (e) {
+                            console.error(e);
+                        }
+                    }
+                } catch (e) {
+                    console.error(e);
+                    alert("Error in window builder! Stacktrace in console.");
+
+                    this.$editor && this.$editor.detach();
+                    this.shadowBuilder && this.shadowBuilder.closeWindow();
+                }
+            },
+            /**
+             * Builds head with specified title and close button
+             * @param {string} title
+             * @param {function} onCloseClick
+             * @returns {*} head as jQuery element
+             */
+            buildHead: function (title, onCloseClick) {
+                onCloseClick = onCloseClick || this.closeWindow.bind(this);
+                return windowComponents.buildHead(title, onCloseClick);
+            },
+            /**
+             * Builds footer with specified buttons as it's block elements
+             * @param {[]} buttons - array of jQuery elements
+             * @returns {*} footer as jQuery element
+             */
+            buildFooter: function (buttons) {
+                return windowComponents.buildFooter.apply(windowComponents, arguments);
+            }
+        };
+
+        return WindowBuilder;
+    }
+);
