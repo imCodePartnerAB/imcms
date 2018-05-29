426975ad6a6fdd47558944a0b29d6913cb6683c3
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-05-29 16:10:38 +0300

    IMCMS-316: Page /servlet/ListDocuments doesn't work properly: - Handled file and url types in ListDocuments.

--- src/main/java/com/imcode/imcms/controller/core/EditDocumentController.java
+++ src/main/java/com/imcode/imcms/controller/core/EditDocumentController.java
@@ -1,7 +1,8 @@
 package com.imcode.imcms.controller.core;
 
+import com.imcode.imcms.domain.service.DelegatingByTypeDocumentService;
 import com.imcode.imcms.domain.service.VersionService;
-import com.imcode.imcms.persistence.entity.Version;
+import com.imcode.imcms.model.Document;
 import com.imcode.imcms.servlet.DocumentHistory;
 import imcode.util.Utility;
 import org.springframework.stereotype.Controller;
@@ -17,27 +18,39 @@ public class EditDocumentController {
     private static final String PARAMETER__META_ID = "meta_id";
 
     private final VersionService versionService;
+    private final DelegatingByTypeDocumentService documentService;
 
-    EditDocumentController(final VersionService versionService) {
+    EditDocumentController(final VersionService versionService,
+                           final DelegatingByTypeDocumentService documentService) {
+
         this.versionService = versionService;
+        this.documentService = documentService;
     }
 
     @RequestMapping({"", "/"})
     public void editDocument(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
         final int metaId = Integer.parseInt(request.getParameter(PARAMETER__META_ID));
 
-        final Version documentWorkingVersion = versionService.findWorking(metaId);
-
-        if (documentWorkingVersion == null) {
+        if (versionService.findWorking(metaId) == null) {
             response.sendError(HttpServletResponse.SC_NOT_FOUND);
             return;
         }
 
-        Utility.setDefaultHtmlContentType(response);
-        DocumentHistory.from(request.getSession()).pushIfNotYet(metaId);
+        Utility.setDefaultHtmlContentType(response);                        // not sure it's still needed
+        DocumentHistory.from(request.getSession()).pushIfNotYet(metaId);    // not sure it's still needed
 
-        request.setAttribute("isEditMode", "true");
+        final Document document = documentService.get(metaId);
 
-        request.getRequestDispatcher("/api/viewDoc/" + metaId).forward(request, response);
+        switch (document.getType()) {
+            case TEXT: {
+                request.setAttribute("isEditMode", "true");
+                request.getRequestDispatcher("/api/viewDoc/" + metaId).forward(request, response);
+                return;
+            }
+            case URL:
+            case FILE:
+                response.sendRedirect(request.getContextPath() + "/api/admin/page-info?meta-id=" + metaId);
+        }
     }
 }
