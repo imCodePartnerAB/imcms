c7ee87252d5b404c1bbd105fc6e48b0e82b29167
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-06-27 12:04:59 +0300

    IMCMS-300 - New design to superadmin-pages: - Created new interface for future service.

---
+++ src/main/java/com/imcode/imcms/domain/service/UserCreationService.java
@@ -0,0 +1,17 @@
+package com.imcode.imcms.domain.service;
+
+import com.imcode.imcms.api.UserAlreadyExistsException;
+import com.imcode.imcms.domain.dto.UserData;
+import com.imcode.imcms.domain.exception.UserValidationException;
+
+/**
+ * For users creation, obviously.
+ *
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 27.06.18.
+ */
+public interface UserCreationService {
+
+    void createUser(final UserData userData) throws UserValidationException, UserAlreadyExistsException;
+
+}
