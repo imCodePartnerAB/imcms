e48475f981515288d05520f133fdc29638baa320
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-06-27 19:03:16 +0300

    IMCMS-300 - New design to superadmin-pages: - Constructor for future stuff.

--- src/main/java/com/imcode/imcms/domain/exception/UserValidationException.java
+++ src/main/java/com/imcode/imcms/domain/exception/UserValidationException.java
@@ -1,5 +1,7 @@
 package com.imcode.imcms.domain.exception;
 
+import com.imcode.imcms.domain.component.UserValidationResult;
+
 /**
  * When something wrong with user fields while create/edit user
  *
@@ -8,4 +10,8 @@ package com.imcode.imcms.domain.exception;
  */
 public class UserValidationException extends RuntimeException {
     private static final long serialVersionUID = 7799856979816003123L;
+
+    public UserValidationException(UserValidationResult validationResult) {
+
+    }
 }
