4628e4014360442bc2fbbe110f87dce7762c2f29
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-06-27 12:14:25 +0300

    IMCMS-300 - New design to superadmin-pages: - Created new exception.

---
+++ src/main/java/com/imcode/imcms/domain/exception/UserValidationException.java
@@ -0,0 +1,11 @@
+package com.imcode.imcms.domain.exception;
+
+/**
+ * When something wrong with user fields while create/edit user
+ *
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 27.06.18.
+ */
+public class UserValidationException extends RuntimeException {
+    private static final long serialVersionUID = 7799856979816003123L;
+}
