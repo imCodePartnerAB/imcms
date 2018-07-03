7b3dfd5a46f544dabbef4d26e4ca4401e9fc4b6e
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-03 12:20:08 +0300

    IMCMS-300 - New design to superadmin-pages: - Created Phone class.

---
+++ src/main/java/com/imcode/imcms/model/Phone.java
@@ -0,0 +1,10 @@
+package com.imcode.imcms.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Phone implements Serializable {
+    private static final long serialVersionUID = -6783249736220792503L;
+}
