e7e3e56c6ba91fa4f5fcdc3c20688d6adceddd65
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-04 16:27:45 +0300

    IMCMS-300 - New design to superadmin-pages: - Implemented deleting user's phones by user id with test coverage.

--- src/main/java/com/imcode/imcms/persistence/repository/PhoneRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/PhoneRepository.java
@@ -4,4 +4,5 @@ import com.imcode.imcms.persistence.entity.PhoneJPA;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhoneRepository extends JpaRepository<PhoneJPA, Integer> {
+    void deleteByUserId(int userId);
 }
--- src/test/java/com/imcode/imcms/persistence/repository/PhoneRepositoryTest.java
+++ src/test/java/com/imcode/imcms/persistence/repository/PhoneRepositoryTest.java
@@ -26,17 +26,18 @@ public class PhoneRepositoryTest extends TransactionalWebAppSpringTestConfig {
     @Autowired
     private UserDataInitializer userDataInitializer;
 
-    private User user;
-
     @Before
     public void setUp() throws Exception {
         phoneRepository.deleteAll();
+        phoneTypeRepository.deleteAll();
         assertTrue(phoneRepository.findAll().isEmpty());
-        user = userDataInitializer.createData("test-login");
+        assertTrue(phoneTypeRepository.findAll().isEmpty());
     }
 
     @Test
     public void testSave() {
+        final User user = userDataInitializer.createData("test-login");
+
         final PhoneTypeJPA phoneType1 = phoneTypeRepository.save(new PhoneTypeJPA(13, "test-type1"));
         final PhoneTypeJPA phoneType2 = phoneTypeRepository.save(new PhoneTypeJPA(14, "test-type2"));
 
@@ -47,5 +48,42 @@ public class PhoneRepositoryTest extends TransactionalWebAppSpringTestConfig {
 
         assertFalse(all.isEmpty());
         assertTrue(Arrays.asList(phone1, phone2).containsAll(all));
+    }
+
+    @Test
+    public void deleteByUserName_Expect_Deleted() {
+        final User user1 = userDataInitializer.createData("test-login-1");
+        final User user2 = userDataInitializer.createData("test-login-2");
+
+        final PhoneTypeJPA phoneType1 = phoneTypeRepository.save(new PhoneTypeJPA(13, "test-type-1"));
+        final PhoneTypeJPA phoneType2 = phoneTypeRepository.save(new PhoneTypeJPA(14, "test-type-2"));
+
+        final PhoneJPA phone11 = phoneRepository.save(new PhoneJPA("852412541", user1, phoneType1));
+        final PhoneJPA phone12 = phoneRepository.save(new PhoneJPA("852412542", user1, phoneType2));
+
+        final PhoneJPA phone21 = phoneRepository.save(new PhoneJPA("852412541", user2, phoneType1));
+        final PhoneJPA phone22 = phoneRepository.save(new PhoneJPA("852412542", user2, phoneType2));
+
+        final List<PhoneJPA> all1 = phoneRepository.findAll();
+
+        assertFalse(all1.isEmpty());
+        assertTrue(Arrays.asList(phone11, phone12, phone21, phone22).containsAll(all1));
+
+        // deleting one user's phones
+
+        phoneRepository.deleteByUserId(user1.getId());
+
+        final List<PhoneJPA> all2 = phoneRepository.findAll();
+
+        assertFalse(all2.isEmpty());
+        assertTrue(Arrays.asList(phone21, phone22).containsAll(all2));
+
+        // deleting another user's phones
+
+        phoneRepository.deleteByUserId(user2.getId());
+
+        final List<PhoneJPA> all3 = phoneRepository.findAll();
+
+        assertTrue(all3.isEmpty());
     }
 }
