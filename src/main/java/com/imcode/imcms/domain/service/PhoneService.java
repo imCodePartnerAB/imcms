fdee1931130eb4f8fd0dce7bdc509816e2eb4742
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-04 17:01:23 +0300

    IMCMS-300 - New design to superadmin-pages: - Implemented updating user phones in phone service.

--- src/main/java/com/imcode/imcms/domain/service/PhoneService.java
+++ src/main/java/com/imcode/imcms/domain/service/PhoneService.java
@@ -1,4 +1,9 @@
 package com.imcode.imcms.domain.service;
 
+import com.imcode.imcms.model.Phone;
+
+import java.util.List;
+
 public interface PhoneService {
+    void updateUserPhones(List<Phone> phones, int userId);
 }
