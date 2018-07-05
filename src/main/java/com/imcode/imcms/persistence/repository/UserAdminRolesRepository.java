d08532b91702a885d72824ae7f56dcd902a1006c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-05 11:53:45 +0300

    IMCMS-300 - New design to superadmin-pages: - Implemented methods in user admin roles repo with tests coverage.

--- src/main/java/com/imcode/imcms/persistence/repository/UserAdminRolesRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/UserAdminRolesRepository.java
@@ -4,5 +4,14 @@ import com.imcode.imcms.persistence.entity.UserAdminRole;
 import com.imcode.imcms.persistence.entity.UserRoleId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserAdminRolesRepository extends JpaRepository<UserAdminRole, UserRoleId> {
+
+    List<UserAdminRole> findUserAdminRoleByUserId(int userId);
+
+    List<UserAdminRole> findUserAdminRoleByRoleId(int roleId);
+
+    void deleteUserAdminRoleByUserId(int userId);
+
 }
