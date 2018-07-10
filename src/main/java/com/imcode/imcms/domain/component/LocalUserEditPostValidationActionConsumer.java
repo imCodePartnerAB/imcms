0205e6736fd6269d1a9dfa5986da070b6be9073c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-10 17:24:05 +0300

    IMCMS-300 - New design to superadmin-pages: - User creation/edit classes refactored.

--- src/main/java/com/imcode/imcms/domain/component/LocalUserEditPostValidationActionConsumer.java
+++ src/main/java/com/imcode/imcms/domain/component/LocalUserEditPostValidationActionConsumer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LocalUserEditPostValidationActionConsumer extends UserPostValidationActionConsumer {
 
-    LocalUserEditPostValidationActionConsumer(LocalUserCreationValidator userValidator) {
+    LocalUserEditPostValidationActionConsumer(LocalUserEditValidator userValidator) {
         super(userValidator);
     }
 }
