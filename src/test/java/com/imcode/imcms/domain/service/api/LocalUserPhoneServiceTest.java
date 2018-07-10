ff4090ad48629c947de0ca480c5960909bd68a99
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-10 19:17:07 +0300

    IMCMS-300 - New design to superadmin-pages: - Implemented receiving user phones in service layer.

--- src/test/java/com/imcode/imcms/domain/service/api/LocalUserPhoneServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/LocalUserPhoneServiceTest.java
@@ -1,6 +1,11 @@
 package com.imcode.imcms.domain.service.api;
 
+import com.imcode.imcms.model.Phone;
+import com.imcode.imcms.persistence.entity.PhoneJPA;
+import com.imcode.imcms.persistence.entity.PhoneTypeJPA;
+import com.imcode.imcms.persistence.entity.User;
 import com.imcode.imcms.persistence.repository.PhoneRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
@@ -8,12 +13,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
-import static org.mockito.BDDMockito.anyCollection;
-import static org.mockito.BDDMockito.inOrder;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.BDDMockito.times;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LocalUserPhoneServiceTest {
@@ -39,5 +43,19 @@ class LocalUserPhoneServiceTest {
 
         then(phoneRepository).should(inOrder, times(1)).deleteByUserId(userId);
         then(phoneRepository).should(inOrder, times(1)).save(anyCollection());
+    }
+
+    @Test
+    void getUserPhones() {
+        final int userId = 42;
+        final List<PhoneJPA> phoneJPAS = new ArrayList<>();
+        phoneJPAS.add(new PhoneJPA("123", mock(User.class), new PhoneTypeJPA()));
+        phoneJPAS.add(new PhoneJPA("456", mock(User.class), new PhoneTypeJPA()));
+
+        given(phoneRepository.findByUserId(userId)).willReturn(phoneJPAS);
+
+        final List<Phone> userPhones = phoneService.getUserPhones(userId);
+
+        Assertions.assertEquals(userPhones.size(), phoneJPAS.size());
     }
 }
