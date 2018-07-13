1ab0605f09b5552fb94d3afd9d7d3b02c4a1eed2
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-13 18:39:39 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Wired modules functionality.

--- src/main/webapp/js/imcms/components/imcms_authentication.js
+++ src/main/webapp/js/imcms/components/imcms_authentication.js
@@ -2,6 +2,11 @@
  * @author Serhii Maksymchuk from Ubrainians for imCode
  * 13.07.18
  */
-Imcms.define('imcms-authentication', [], function () {
-    return {};
+Imcms.define('imcms-authentication', ['imcms-auth-providers-rest-api'], function (authProvidersAPI) {
+
+    return {
+        getAuthProviders: function () {
+            return authProvidersAPI.read();
+        }
+    };
 });
