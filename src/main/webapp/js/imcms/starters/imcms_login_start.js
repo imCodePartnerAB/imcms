00d7393d1bc020ff73fe15a28de94c40c6083415
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-16 13:51:26 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Updated login provider buttons logic.

--- src/main/webapp/js/imcms/starters/imcms_login_start.js
+++ src/main/webapp/js/imcms/starters/imcms_login_start.js
@@ -2,10 +2,42 @@
  * @author Serhii Maksymchuk from Ubrainians for imCode
  * 13.07.18
  */
-Imcms.require(['imcms', 'jquery', 'imcms-authentication'], function (imcms, $, auth) {
+Imcms.require(['imcms', 'jquery', 'imcms-authentication', 'imcms-components-builder'], function (imcms, $, auth, components) {
+
+    function getOnLoginProviderButtonClick(providerId) {
+        return function () {
+            var $thisLoginProvider = $('#' + providerId);
+            if ($thisLoginProvider.hasClass('login-provider--active')) return;
+
+            $('.login-provider--active').removeClass('login-provider--active').slideUp();
+            $thisLoginProvider.addClass('login-provider--active').slideDown();
+        }
+    }
+
+    $('#default-login-button').click(getOnLoginProviderButtonClick('default-login-provider'));
+
+    var $loginPageButtonsContainer = $('#login-page-buttons');
+    var $loginProvidersContainer = $('#login-providers');
+
+    function buildLoginPageButton(authProvider) {
+        return components.buttons.neutralButton({
+            'class': 'imcms-info-body__button auth-provider-button',
+            text: authProvider.providerName,
+            click: getOnLoginProviderButtonClick(authProvider.providerId)
+        });
+    }
+
+    function buildLoginProviderArea(authProvider) {
+        return $('<div>', {
+            'class': 'auth-provider',
+            id: authProvider.providerId,
+            style: 'display: none;'
+        });
+    }
 
     function buildAuthProviderGUI(authProvider) {
-        console.log(authProvider);
+        $loginPageButtonsContainer.append(buildLoginPageButton(authProvider));
+        $loginProvidersContainer.append(buildLoginProviderArea(authProvider));
     }
 
     auth.getAuthProviders().done(function (authProviders) {
