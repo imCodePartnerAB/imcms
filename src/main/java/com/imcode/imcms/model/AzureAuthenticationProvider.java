df4dea16c2fbee784fca2483ed0ae038a05cc190
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-16 15:43:50 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Handled all required properties in AzureAuthenticationProvider.

--- src/main/java/com/imcode/imcms/model/AzureAuthenticationProvider.java
+++ src/main/java/com/imcode/imcms/model/AzureAuthenticationProvider.java
@@ -3,11 +3,20 @@ package com.imcode.imcms.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.Objects;
 import java.util.Properties;
 
+/**
+ * Authentication provider for Azure Active Directory
+ */
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class AzureAuthenticationProvider extends AuthenticationProvider {
+
+    private final String tenant;
+    private final String clientId;
+    private final String secretKey;
+    private final String authority = "https://login.microsoftonline.com/";
 
     {
         providerName = "Azure Active Directory";
@@ -16,6 +25,14 @@ public class AzureAuthenticationProvider extends AuthenticationProvider {
 
     public AzureAuthenticationProvider(Properties properties) {
         authenticationURL = "https://imcode.com/";
+
+        tenant = properties.getProperty("aad.tenant.name");
+        clientId = properties.getProperty("aad.client.id");
+        secretKey = properties.getProperty("aad.secret.key");
+
+        Objects.requireNonNull(tenant, "Azure Active Directory tenant is null!");
+        Objects.requireNonNull(clientId, "Azure Active Directory client id is null!");
+        Objects.requireNonNull(secretKey, "Azure Active Directory secret directory is null!");
     }
 
 }
