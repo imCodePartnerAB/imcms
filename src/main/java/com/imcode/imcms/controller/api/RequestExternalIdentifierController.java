d2b15a2724273d3e3b40937206bad74c1adeeb63
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-16 18:27:28 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Implemented entry point for login by external auth provider id.

---
+++ src/main/java/com/imcode/imcms/controller/api/RequestExternalIdentifierController.java
@@ -0,0 +1,45 @@
+package com.imcode.imcms.controller.api;
+
+import com.imcode.imcms.controller.exception.ExternalIdentifierNotEnabledException;
+import com.imcode.imcms.domain.service.AuthenticationProvidersService;
+import com.imcode.imcms.model.AuthenticationProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Handles requests from users for external identifiers.
+ * Builds required URL and redirects to specified identifier.
+ *
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 16.07.18.
+ */
+@Controller
+@RequestMapping("/external-identifiers/")
+class RequestExternalIdentifierController {
+
+    private final AuthenticationProvidersService authenticationProvidersService;
+
+    RequestExternalIdentifierController(AuthenticationProvidersService authenticationProvidersService) {
+        this.authenticationProvidersService = authenticationProvidersService;
+    }
+
+    @RequestMapping("{identifierId}")
+    public ModelAndView goToExternalIdentifierLoginPage(@PathVariable("identifierId") String identifierId,
+                                                        HttpServletRequest request,
+                                                        HttpSession session) {
+
+        final AuthenticationProvider provider = authenticationProvidersService.getAuthenticationProvider(identifierId)
+                .orElseThrow(ExternalIdentifierNotEnabledException::new); // or maybe redirect to login page?
+
+        // prepare session stuff here
+
+        return new ModelAndView(new RedirectView(provider.getAuthenticationURL()));
+    }
+
+}
