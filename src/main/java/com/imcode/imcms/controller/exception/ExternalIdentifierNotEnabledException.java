4409c0538b1be71f1a9a9a2af73d3e21b19c7cb4
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-16 18:19:27 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Created exception when external identifier not enabled.

---
+++ src/main/java/com/imcode/imcms/controller/exception/ExternalIdentifierNotEnabledException.java
@@ -0,0 +1,9 @@
+package com.imcode.imcms.controller.exception;
+
+/**
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 16.07.18.
+ */
+public class ExternalIdentifierNotEnabledException extends RuntimeException {
+    private static final long serialVersionUID = 6792272920243591023L;
+}
