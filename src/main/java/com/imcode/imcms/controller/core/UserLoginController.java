054cc13a658b7375b812cc297ed728992314ac48
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-16 14:30:31 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Controller fixed to accept all http method.

--- src/main/java/com/imcode/imcms/controller/core/UserLoginController.java
+++ src/main/java/com/imcode/imcms/controller/core/UserLoginController.java
@@ -1,7 +1,6 @@
 package com.imcode.imcms.controller.core;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping({"/login**", "/login/**"})
 class UserLoginController {
 
-    @GetMapping
+    @RequestMapping
     public ModelAndView goToLoginPage(ModelAndView modelAndView) {
         modelAndView.setViewName("Login");
         return modelAndView;
