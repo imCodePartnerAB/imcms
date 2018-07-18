e9ad51bfa8d2bff46492c1e3ebdee13115c432ae
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-18 17:39:46 +0300

    IMCMS-323 - Add support for LDAPS and Azure AD: - Simplified test.

--- src/test/java/com/imcode/imcms/domain/factory/AuthenticationProvidersFactoryTest.java
+++ src/test/java/com/imcode/imcms/domain/factory/AuthenticationProvidersFactoryTest.java
@@ -2,10 +2,9 @@ package com.imcode.imcms.domain.factory;
 
 import com.imcode.imcms.domain.component.AzureAuthenticationProvider;
 import com.imcode.imcms.model.AuthenticationProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -13,27 +12,37 @@ import java.util.Properties;
 
 import static imcode.server.DefaultImcmsServices.EXTERNAL_AUTHENTICATOR_AZURE_AD;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationProvidersFactoryTest {
 
-    @Mock
-    private Properties properties;
+    private Properties properties = new Properties();
+    private AuthenticationProvidersFactory authenticationProvidersFactory = new AuthenticationProvidersFactory(properties);
 
-    @InjectMocks
-    private AuthenticationProvidersFactory authenticationProvidersFactory;
+    @BeforeEach
+    void setUp() {
+        properties.clear();
+    }
 
     @Test
     void getProviders_When_PropertiesDoesNotContainsExternalAuthenticator_Expect_EmptyListReturned() {
-        given(properties.getProperty("ExternalAuthenticator", "")).willReturn("");
+        properties.setProperty("ExternalAuthenticator", "");
+        properties.setProperty("aad.tenant.name", "");
+        properties.setProperty("aad.client.id", "");
+        properties.setProperty("aad.secret.key", "");
+
         final List<AuthenticationProvider> providers = authenticationProvidersFactory.getProviders();
+
         assertTrue(providers.isEmpty());
     }
 
     @Test
     void getProviders_When_PropertiesContainsExternalAzureAuthenticator_Expect_ListWithAzureAuthenticatorReturned() {
-        given(properties.getProperty("ExternalAuthenticator", "")).willReturn(EXTERNAL_AUTHENTICATOR_AZURE_AD);
+        properties.setProperty("ExternalAuthenticator", EXTERNAL_AUTHENTICATOR_AZURE_AD);
+        properties.setProperty("aad.tenant.name", "");
+        properties.setProperty("aad.client.id", "");
+        properties.setProperty("aad.secret.key", "");
+
         final List<AuthenticationProvider> providers = authenticationProvidersFactory.getProviders();
 
         assertFalse(providers.isEmpty());
