896614073bb96572d34977a327cff881341929b8
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-24 15:13:12 +0300

    IMCMS-300 - New design to superadmin-pages: - Fixed test setup, moved to junit5.

--- src/test/java/com/imcode/imcms/domain/service/api/SearchDocumentServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/SearchDocumentServiceTest.java
@@ -4,27 +4,26 @@ import com.imcode.imcms.domain.dto.DocumentStatus;
 import com.imcode.imcms.domain.dto.DocumentStoredFieldsDTO;
 import com.imcode.imcms.domain.dto.SearchQueryDTO;
 import com.imcode.imcms.persistence.entity.Meta;
+import imcode.server.Imcms;
 import imcode.server.document.index.DocumentStoredFields;
 import imcode.server.document.index.IndexSearchResult;
 import imcode.server.document.index.ResolvingQueryIndex;
 import imcode.server.user.UserDomainObject;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
-public class SearchDocumentServiceTest {
+@ExtendWith(MockitoExtension.class)
+class SearchDocumentServiceTest {
 
     @Mock
     private ResolvingQueryIndex documentIndex;
@@ -35,11 +34,16 @@ public class SearchDocumentServiceTest {
     @InjectMocks
     private DefaultSearchDocumentService searchDocumentService;
 
+    @BeforeEach
+    void setUp() {
+        final UserDomainObject user = mock(UserDomainObject.class);
+        Imcms.setUser(user);
+    }
+
     @Test
-    public void getTextDocuments_When_DefaultSearchQueryIsUsing_Expect_AllReturned() {
+    void getTextDocuments_given_DefaultSearchQueryIsUsing_Expect_AllReturned() {
 
         final int documentNumber = 5;
-
         final List<DocumentStoredFieldsDTO> expected = new ArrayList<>();
 
         for (int i = 0; i < documentNumber; i++) {
@@ -54,8 +58,8 @@ public class SearchDocumentServiceTest {
 
         final SearchQueryDTO searchQueryDTO = new SearchQueryDTO();
 
-        when(documentIndex.search(eq(searchQueryDTO), Mockito.any(UserDomainObject.class)))
-                .thenReturn(indexSearchResult);
+        given(documentIndex.search(eq(searchQueryDTO), any(UserDomainObject.class)))
+                .willReturn(indexSearchResult);
 
         final List<DocumentStoredFields> documentStoredFieldsList = new ArrayList<>();
 
@@ -63,25 +67,22 @@ public class SearchDocumentServiceTest {
             final DocumentStoredFields mock = mock(DocumentStoredFields.class);
             documentStoredFieldsList.add(mock);
 
-            when(mock.id()).thenReturn(expected.get(i).getId());
-            when(mock.headline()).thenReturn(expected.get(i).getTitle());
-            when(mock.documentType()).thenReturn(expected.get(i).getType());
-            when(mock.documentStatus()).thenReturn(expected.get(i).getDocumentStatus());
-            when(mock.alias()).thenReturn(expected.get(i).getAlias());
+            given(mock.id()).willReturn(expected.get(i).getId());
+            given(mock.headline()).willReturn(expected.get(i).getTitle());
+            given(mock.documentType()).willReturn(expected.get(i).getType());
+            given(mock.documentStatus()).willReturn(expected.get(i).getDocumentStatus());
+            given(mock.alias()).willReturn(expected.get(i).getAlias());
         }
 
-        when(indexSearchResult.documentStoredFieldsList())
-                .thenReturn(documentStoredFieldsList);
+        given(indexSearchResult.documentStoredFieldsList()).willReturn(documentStoredFieldsList);
 
         final List<DocumentStoredFieldsDTO> actual = searchDocumentService.searchDocuments(searchQueryDTO);
 
-        assertThat(actual, hasSize(documentNumber));
-        assertThat(actual, containsInAnyOrder(expected.toArray()));
+        assertEquals(actual.size(), documentNumber);
+        assertTrue(actual.containsAll(expected));
+        assertTrue(expected.containsAll(actual));
 
-        verify(documentIndex, times(1))
-                .search(eq(searchQueryDTO), Mockito.any(UserDomainObject.class));
-
-        verify(indexSearchResult, times(1))
-                .documentStoredFieldsList();
+        then(documentIndex).should().search(eq(searchQueryDTO), any(UserDomainObject.class));
+        then(indexSearchResult).should().documentStoredFieldsList();
     }
 }
