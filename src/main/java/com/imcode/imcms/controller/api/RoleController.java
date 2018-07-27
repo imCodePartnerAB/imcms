e8907cc4c684ce313cb7c388b9f0b1e583a8bd97
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-27 18:34:30 +0300

    IMCMS-327 - New design to super admin page: roles tab: - Implemented role saving in controller.

--- src/main/java/com/imcode/imcms/controller/api/RoleController.java
+++ src/main/java/com/imcode/imcms/controller/api/RoleController.java
@@ -1,8 +1,11 @@
 package com.imcode.imcms.controller.api;
 
+import com.imcode.imcms.domain.dto.RoleDTO;
 import com.imcode.imcms.domain.service.RoleService;
 import com.imcode.imcms.model.Role;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +24,11 @@ public class RoleController {
     @GetMapping
     public List<Role> getRoles() {
         return roleService.getAll();
+    }
+
+    @PatchMapping
+    public Role saveRole(@RequestBody RoleDTO role) {
+        return roleService.save(role);
     }
 
 }
