00a3785a54721ebf6c85172573970916ad39f488
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-30 17:46:03 +0300

    IMCMS-327 - New design to super admin page: roles tab: - Implemented roles deletion in role rest client.

--- src/main/webapp/js/imcms/rest/imcms_roles_rest_api.js
+++ src/main/webapp/js/imcms/rest/imcms_roles_rest_api.js
@@ -1,3 +1,10 @@
 Imcms.define("imcms-roles-rest-api", ["imcms-rest-api"], function (rest) {
-    return new rest.API("/roles");
+    var url = "/roles";
+    var api = new rest.API(url);
+
+    api.remove = function (roleId) {
+        return rest.ajax.call({url: url + '/' + roleId, type: 'DELETE', json: false})
+    };
+
+    return api;
 });
