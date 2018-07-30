01b0bc6f9dcf7ccb75d1cd30c6d372071d1cb6db
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-30 18:51:11 +0300

    IMCMS-327 - New design to super admin page: roles tab: - Added new fields to azure AD group DTO.

--- src/main/java/com/imcode/imcms/domain/dto/AzureActiveDirectoryGroupDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/AzureActiveDirectoryGroupDTO.java
@@ -11,8 +11,15 @@ public class AzureActiveDirectoryGroupDTO {
     private String createdDateTime;
     private String description;
     private String displayName;
+    private String mail;
+    private String mailNickname;
+    private String onPremisesLastSyncDateTime;
+    private String onPremisesSecurityIdentifier;
+    private String visibility;
     private String[] groupTypes;
+    private String[] proxyAddresses;
     private boolean mailEnabled;
     private boolean securityEnabled;
+    private boolean onPremisesSyncEnabled;
 
 }
