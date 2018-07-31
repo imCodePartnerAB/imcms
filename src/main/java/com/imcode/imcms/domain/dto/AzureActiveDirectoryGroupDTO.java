6b33afa937c8f611a9666ff3d29c21142c8c0c91
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-07-31 12:12:34 +0300

    IMCMS-327 - New design to super admin page: roles tab: - Extended AzureActiveDirectoryGroupDTO class.

--- src/main/java/com/imcode/imcms/domain/dto/AzureActiveDirectoryGroupDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/AzureActiveDirectoryGroupDTO.java
@@ -16,8 +16,16 @@ public class AzureActiveDirectoryGroupDTO {
     private String onPremisesLastSyncDateTime;
     private String onPremisesSecurityIdentifier;
     private String visibility;
+    private String deletedDateTime;
+    private String classification;
+    private String preferredDataLocation;
+    private String renewedDateTime;
     private String[] groupTypes;
     private String[] proxyAddresses;
+    private String[] creationOptions;
+    private String[] onPremisesProvisioningErrors;
+    private String[] resourceBehaviorOptions;
+    private String[] resourceProvisioningOptions;
     private boolean mailEnabled;
     private boolean securityEnabled;
     private boolean onPremisesSyncEnabled;
