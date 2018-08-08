b01d52f7d98800bdbf7ad3940fb1de1f4c1e4eb3
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-08-08 14:32:07 +0300

    IMCMS-324 - Add role syncing from AD: - Introduced external user service.

---
+++ src/main/java/com/imcode/imcms/domain/service/ExternalUserService.java
@@ -0,0 +1,7 @@
+package com.imcode.imcms.domain.service;
+
+import com.imcode.imcms.model.ExternalUser;
+
+public interface ExternalUserService {
+    ExternalUser saveExternalUser(ExternalUser user);
+}
