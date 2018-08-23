94d414ebcf7e878479f4d017cecc5f6d2f0021a1
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-08-23 18:38:18 +0300

    IMCMS-350 - Image Editor improvements: - Added doc id and image index to Image Editor's title.

--- src/main/webapp/js/imcms/builders/windows/editors/imcms_image_editor_factory.js
+++ src/main/webapp/js/imcms/builders/windows/editors/imcms_image_editor_factory.js
@@ -1,23 +1,28 @@
 Imcms.define(
     "imcms-image-editor-factory",
     [
-        "imcms-image-editor-right-side-builder", "imcms-image-editor-left-side-builder",
-        "imcms-image-editor-body-head-builder", "imcms-i18n-texts", "imcms-bem-builder"
+        "imcms-image-editor-right-side-builder", "imcms-image-editor-left-side-builder", 'jquery', "imcms-i18n-texts",
+        "imcms-image-editor-body-head-builder", "imcms-bem-builder"
     ],
-    function (rightSideBuilder, leftSideBuilder, bodyHeadBuilder, texts, BEM) {
+    function (rightSideBuilder, leftSideBuilder, $, texts, bodyHeadBuilder, BEM) {
 
         texts = texts.editors.image;
+
+        var $imageInfo = $('<span>');
 
         return {
             buildEditor: function (opts) {
                 var $rightSidePanel = rightSideBuilder.build(opts);
                 var $leftSide = leftSideBuilder.build(opts);
                 var $bodyHead = bodyHeadBuilder.build(opts, $rightSidePanel);
+                var $head = opts.imageWindowBuilder.buildHead(texts.title);
+
+                $head.find('.imcms-title').append($imageInfo);
 
                 return new BEM({
                     block: "imcms-image_editor",
                     elements: {
-                        "head": opts.imageWindowBuilder.buildHead(texts.title),
+                        "head": $head,
                         "image-characteristics": $bodyHead,
                         "left-side": $leftSide,
                         "right-side": $rightSidePanel
@@ -25,6 +30,7 @@ Imcms.define(
                 }).buildBlockStructure("<div>", {"class": "imcms-editor-window"});
             },
             updateImageData: function ($tag, imageData) {
+                $imageInfo.text(': ' + $tag.attr('data-doc-id') + '-' + $tag.attr('data-index'));
                 rightSideBuilder.updateImageData($tag, imageData);
             }
         }
