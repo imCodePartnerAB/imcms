adc49ec6a2079b6b15513da08103f2e1f293ea8f
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-08-27 14:52:55 +0300

    IMCMS-350 - Image Editor improvements: - Bottom panel moved to top.

--- src/main/webapp/js/imcms/builders/windows/editors/imcms_image_editor_body_head_builder.js
+++ src/main/webapp/js/imcms/builders/windows/editors/imcms_image_editor_body_head_builder.js
@@ -1,7 +1,10 @@
 Imcms.define(
     "imcms-image-editor-body-head-builder",
-    ["imcms-i18n-texts", "imcms-bem-builder", "imcms-components-builder", "jquery"],
-    function (texts, BEM, components, $) {
+    [
+        "imcms-i18n-texts", "imcms-bem-builder", "imcms-components-builder", "jquery", 'imcms-image-edit-size-controls',
+        "imcms-image-rotate", "imcms-image-resize", "imcms-image-crop-angles", "imcms-image-cropping-elements"
+    ],
+    function (texts, BEM, components, $, imageEditSizeControls, imageRotate, imageResize, croppingAngles, cropElements) {
 
         texts = texts.editors.image;
 
@@ -58,12 +61,190 @@ Imcms.define(
             }).buildBlockStructure("<div>", {text: "Orig "});
         }
 
+        function resizeImage(opts) {
+            imageResize.resize(opts, opts.imageDataContainers, false);
+            opts.imageDataContainers.$heightControlInput.getInput().val(opts.image.height);
+            opts.imageDataContainers.$widthControlInput.getInput().val(opts.image.width);
+        }
+
+        function zoom(zoomCoefficient, imageDataContainers) {
+            var newHeight = ~~(cropElements.$image.height() * zoomCoefficient),
+                newWidth = ~~(cropElements.$image.width() * zoomCoefficient),
+                newCropAreaHeight = ~~(cropElements.$cropArea.height() * zoomCoefficient),
+                newCropAreaWeight = ~~(cropElements.$cropArea.width() * zoomCoefficient),
+                newCropAreaLeft = ~~(cropElements.$cropArea.position().left * zoomCoefficient),
+                newCropAreaTop = ~~(cropElements.$cropArea.position().top * zoomCoefficient)
+            ;
+
+            var borderSize = croppingAngles.getBorderSize();
+
+            if (newCropAreaLeft <= borderSize) {
+                newCropAreaLeft = borderSize;
+            }
+
+            if (newCropAreaTop <= borderSize) {
+                newCropAreaTop = borderSize;
+            }
+
+            resizeImage({
+                imageDataContainers: imageDataContainers,
+                isProportionsInverted: imageRotate.isProportionsInverted(),
+                image: {
+                    width: newWidth,
+                    height: newHeight
+                },
+                cropArea: {
+                    height: newCropAreaHeight,
+                    width: newCropAreaWeight,
+                    top: newCropAreaTop,
+                    left: newCropAreaLeft
+                }
+            });
+        }
+
+        function zoomPlus() {
+            zoom(1.1, this);
+        }
+
+        function zoomMinus() {
+            zoom(0.9, this);
+        }
+
+        function zoomContain(imageDataContainers, $editableImageArea) {
+            // fixme: save proportions! now image becomes just as editable area
+            // only one side should be as area's side and one as needed to save proportions
+            var proportionsInverted = imageRotate.isProportionsInverted();
+            var newHeight = $editableImageArea.height(),
+                newWidth = $editableImageArea.width(),
+                newCropAreaHeight = (proportionsInverted)
+                    ? $editableImageArea.width() : $editableImageArea.height(),
+                newCropAreaWeight = (proportionsInverted)
+                    ? $editableImageArea.height() : $editableImageArea.width(),
+                newCropAreaLeft = croppingAngles.getBorderSize(),
+                newCropAreaTop = croppingAngles.getBorderSize(),
+                twiceAngleBorderSize = croppingAngles.getDoubleBorderSize()
+            ;
+
+            resizeImage({
+                imageDataContainers: imageDataContainers,
+                isProportionsInverted: proportionsInverted,
+                image: {
+                    width: newWidth - twiceAngleBorderSize,
+                    height: newHeight - twiceAngleBorderSize
+                },
+                cropArea: {
+                    height: newCropAreaHeight - twiceAngleBorderSize,
+                    width: newCropAreaWeight - twiceAngleBorderSize,
+                    top: newCropAreaTop,
+                    left: newCropAreaLeft
+                }
+            });
+        }
+
+        function revertImageChanges(imageDataContainers) {
+            imageRotate.rotateImage("NORTH");
+
+            var newWidth = imageDataContainers.original.width;
+            var newHeight = imageDataContainers.original.height;
+
+            resizeImage({
+                imageDataContainers: imageDataContainers,
+                isProportionsInverted: false,
+                image: {
+                    width: newWidth,
+                    height: newHeight
+                },
+                cropArea: {
+                    height: newHeight,
+                    width: newWidth,
+                    top: croppingAngles.getBorderSize(),
+                    left: croppingAngles.getBorderSize()
+                }
+            });
+        }
+
+        function buildRevertButton(imageDataContainers) {
+            return components.buttons.negativeButton({
+                text: "Revert",
+                click: function () {
+                    revertImageChanges(imageDataContainers);
+                }
+            })
+        }
+
+        function buildScaleAndRotateControls(imageDataContainers, $editableImageArea) {
+            imageRotate.setDataContainers(imageDataContainers);
+
+            return new BEM({
+                block: "imcms-edit-image",
+                elements: {
+                    "button": [
+                        components.buttons.zoomPlusButton({
+                            title: texts.buttons.zoomIn,
+                            click: zoomPlus.bind(imageDataContainers)
+                        }),
+                        components.buttons.zoomMinusButton({
+                            title: texts.buttons.zoomOut,
+                            click: zoomMinus.bind(imageDataContainers)
+                        }),
+                        components.buttons.zoomContainButton({
+                            title: texts.buttons.zoomContain,
+                            click: function () {
+                                zoomContain(imageDataContainers, $editableImageArea);
+                            }
+                        }),
+                        components.buttons.rotateLeftButton({
+                            title: texts.buttons.rotateLeft,
+                            click: imageRotate.rotateLeft
+                        }),
+                        components.buttons.rotateRightButton({
+                            title: texts.buttons.rotateRight,
+                            click: imageRotate.rotateRight
+                        })
+                    ]
+                }
+            }).buildBlockStructure("<div>");
+        }
+
+        function buildSwitchViewControls(toggleImgArea, imageDataContainers) {
+            var $preview = components.texts.titleText("<div>", texts.preview, {
+                "data-tab": "prev",
+                click: toggleImgArea
+            });
+            var $origin = components.texts.titleText("<div>", texts.original, {
+                "data-tab": "origin",
+                click: toggleImgArea
+            });
+            $origin.modifiers = ["active"];
+
+            imageDataContainers.$tabOriginal = $origin;
+
+            return new BEM({
+                block: "imcms-editable-img-control-tabs",
+                elements: {
+                    "tab": [$preview, $origin]
+                }
+            }).buildBlockStructure("<div>");
+        }
+
+        function buildToolbar(toggleImgArea, imageDataContainers, $editableImageArea) {
+            return new BEM({
+                block: "imcms-editable-img-controls",
+                elements: {
+                    "control-size": imageEditSizeControls.buildEditSizeControls(imageDataContainers),
+                    "control-scale-n-rotate": buildScaleAndRotateControls(imageDataContainers, $editableImageArea),
+                    "image-revert": buildRevertButton(imageDataContainers),
+                    "control-view": buildSwitchViewControls(toggleImgArea, imageDataContainers)
+                }
+            }).buildBlockStructure("<div>");
+        }
+
         var $imgUrl;
 
         return {
             build: function (opts, $rightSidePanel) {
                 var bodyHeadBEM = new BEM({
-                    block: "imcms-image-characteristics",
+                    block: "imcms-image-toolbar",
                     elements: {
                         "button": "imcms-image-characteristic",
                         "img-title": "imcms-title imcms-image-characteristic",
@@ -88,9 +269,13 @@ Imcms.define(
 
                 var $heightWidthBlock = buildHeightWidthBlock(opts.imageDataContainers);
 
+                opts.imageDataContainers.$toolbar = buildToolbar(
+                    opts.toggleImgArea, opts.imageDataContainers, opts.imageDataContainers.$editableImageArea
+                );
+
                 return bodyHeadBEM.buildBlock("<div>", [
                     {
-                        "img-title": opts.imageDataContainers.$imageTitle
+                        "toolbar": opts.imageDataContainers.$toolbar
                     }, {
                         "button": $showHideRightPanelBtn,
                         modifiers: ["right-panel"]
--- src/main/webapp/js/imcms/builders/windows/editors/imcms_image_editor_left_side_builder.js
+++ src/main/webapp/js/imcms/builders/windows/editors/imcms_image_editor_left_side_builder.js
@@ -1,13 +1,8 @@
 Imcms.define(
     "imcms-image-editor-left-side-builder",
-    [
-        "imcms-bem-builder", "imcms-components-builder", "imcms-i18n-texts", "jquery",
-        "imcms-image-crop-angles", "imcms-image-cropping-elements", "imcms-image-rotate", "imcms-image-resize",
-        "imcms-image-edit-size-controls"
-    ],
+    ["imcms-bem-builder", "jquery", "imcms-image-crop-angles", "imcms-image-cropping-elements"],
 
-    function (BEM, components, texts, $, croppingAngles, cropElements, imageRotate, imageResize, imageEditSizeControls) {
-        texts = texts.editors.image;
+    function (BEM, $, croppingAngles, cropElements) {
 
         function buildPreviewImageArea(imageDataContainers) {
             var previewImageAreaBEM = new BEM({
@@ -51,197 +46,14 @@ Imcms.define(
             ]);
         }
 
-        function resizeImage(opts) {
-            imageResize.resize(opts, opts.imageDataContainers, false);
-            opts.imageDataContainers.$heightControlInput.getInput().val(opts.image.height);
-            opts.imageDataContainers.$widthControlInput.getInput().val(opts.image.width);
-        }
-
-        function zoom(zoomCoefficient, imageDataContainers) {
-            var newHeight = ~~(cropElements.$image.height() * zoomCoefficient),
-                newWidth = ~~(cropElements.$image.width() * zoomCoefficient),
-                newCropAreaHeight = ~~(cropElements.$cropArea.height() * zoomCoefficient),
-                newCropAreaWeight = ~~(cropElements.$cropArea.width() * zoomCoefficient),
-                newCropAreaLeft = ~~(cropElements.$cropArea.position().left * zoomCoefficient),
-                newCropAreaTop = ~~(cropElements.$cropArea.position().top * zoomCoefficient)
-            ;
-
-            var borderSize = croppingAngles.getBorderSize();
-
-            if (newCropAreaLeft <= borderSize) {
-                newCropAreaLeft = borderSize;
-            }
-
-            if (newCropAreaTop <= borderSize) {
-                newCropAreaTop = borderSize;
-            }
-
-            resizeImage({
-                imageDataContainers: imageDataContainers,
-                isProportionsInverted: imageRotate.isProportionsInverted(),
-                image: {
-                    width: newWidth,
-                    height: newHeight
-                },
-                cropArea: {
-                    height: newCropAreaHeight,
-                    width: newCropAreaWeight,
-                    top: newCropAreaTop,
-                    left: newCropAreaLeft
-                }
-            });
-        }
-
-        function zoomPlus() {
-            zoom(1.1, this);
-        }
-
-        function zoomMinus() {
-            zoom(0.9, this);
-        }
-
-        function zoomContain(imageDataContainers, $editableImageArea) {
-            // fixme: save proportions! now image becomes just as editable area
-            // only one side should be as area's side and one as needed to save proportions
-            var proportionsInverted = imageRotate.isProportionsInverted();
-            var newHeight = $editableImageArea.height(),
-                newWidth = $editableImageArea.width(),
-                newCropAreaHeight = (proportionsInverted)
-                    ? $editableImageArea.width() : $editableImageArea.height(),
-                newCropAreaWeight = (proportionsInverted)
-                    ? $editableImageArea.height() : $editableImageArea.width(),
-                newCropAreaLeft = croppingAngles.getBorderSize(),
-                newCropAreaTop = croppingAngles.getBorderSize(),
-                twiceAngleBorderSize = croppingAngles.getDoubleBorderSize()
-            ;
-
-            resizeImage({
-                imageDataContainers: imageDataContainers,
-                isProportionsInverted: proportionsInverted,
-                image: {
-                    width: newWidth - twiceAngleBorderSize,
-                    height: newHeight - twiceAngleBorderSize
-                },
-                cropArea: {
-                    height: newCropAreaHeight - twiceAngleBorderSize,
-                    width: newCropAreaWeight - twiceAngleBorderSize,
-                    top: newCropAreaTop,
-                    left: newCropAreaLeft
-                }
-            });
-        }
-
-        function revertImageChanges(imageDataContainers) {
-            imageRotate.rotateImage("NORTH");
-
-            var newWidth = imageDataContainers.original.width;
-            var newHeight = imageDataContainers.original.height;
-
-            resizeImage({
-                imageDataContainers: imageDataContainers,
-                isProportionsInverted: false,
-                image: {
-                    width: newWidth,
-                    height: newHeight
-                },
-                cropArea: {
-                    height: newHeight,
-                    width: newWidth,
-                    top: croppingAngles.getBorderSize(),
-                    left: croppingAngles.getBorderSize()
-                }
-            });
-        }
-
-        function buildRevertButton(imageDataContainers) {
-            return components.buttons.negativeButton({
-                text: "Revert",
-                click: function () {
-                    revertImageChanges(imageDataContainers);
-                }
-            })
-        }
-
-        function buildScaleAndRotateControls(imageDataContainers, $editableImageArea) {
-            imageRotate.setDataContainers(imageDataContainers);
-
-            return new BEM({
-                block: "imcms-edit-image",
-                elements: {
-                    "button": [
-                        components.buttons.zoomPlusButton({
-                            title: texts.buttons.zoomIn,
-                            click: zoomPlus.bind(imageDataContainers)
-                        }),
-                        components.buttons.zoomMinusButton({
-                            title: texts.buttons.zoomOut,
-                            click: zoomMinus.bind(imageDataContainers)
-                        }),
-                        components.buttons.zoomContainButton({
-                            title: texts.buttons.zoomContain,
-                            click: function () {
-                                zoomContain(imageDataContainers, $editableImageArea);
-                            }
-                        }),
-                        components.buttons.rotateLeftButton({
-                            title: texts.buttons.rotateLeft,
-                            click: imageRotate.rotateLeft
-                        }),
-                        components.buttons.rotateRightButton({
-                            title: texts.buttons.rotateRight,
-                            click: imageRotate.rotateRight
-                        })
-                    ]
-                }
-            }).buildBlockStructure("<div>");
-        }
-
-        function buildSwitchViewControls(toggleImgArea, imageDataContainers) {
-            var $preview = components.texts.titleText("<div>", texts.preview, {
-                "data-tab": "prev",
-                click: toggleImgArea
-            });
-            var $origin = components.texts.titleText("<div>", texts.original, {
-                "data-tab": "origin",
-                click: toggleImgArea
-            });
-            $origin.modifiers = ["active"];
-
-            imageDataContainers.$tabOriginal = $origin;
-
-            return new BEM({
-                block: "imcms-editable-img-control-tabs",
-                elements: {
-                    "tab": [$preview, $origin]
-                }
-            }).buildBlockStructure("<div>");
-        }
-
-        function buildBottomPanel(toggleImgArea, imageDataContainers, $editableImageArea) {
-            return new BEM({
-                block: "imcms-editable-img-controls",
-                elements: {
-                    "control-size": imageEditSizeControls.buildEditSizeControls(imageDataContainers),
-                    "control-scale-n-rotate": buildScaleAndRotateControls(imageDataContainers, $editableImageArea),
-                    "image-revert": buildRevertButton(imageDataContainers),
-                    "control-view": buildSwitchViewControls(toggleImgArea, imageDataContainers)
-                }
-            }).buildBlockStructure("<div>");
-        }
-
         return {
             build: function (opts) {
                 opts.imageDataContainers.$editableImageArea = buildEditableImageArea(opts.imageDataContainers);
                 var $previewImageArea = buildPreviewImageArea(opts.imageDataContainers);
 
-                opts.imageDataContainers.$toolbar = buildBottomPanel(
-                    opts.toggleImgArea, opts.imageDataContainers, opts.imageDataContainers.$editableImageArea
-                );
-
                 return $("<div>").append(
                     opts.imageDataContainers.$editableImageArea,
-                    $previewImageArea,
-                    opts.imageDataContainers.$toolbar
+                    $previewImageArea
                 );
             }
         };
