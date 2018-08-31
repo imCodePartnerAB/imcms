2a0994a21ec9d5ad3fccae640ae9c9453cb9f1c6
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-08-31 15:19:04 +0300

    IMCMS-365 - Page objects on other page: - Text fixes.

--- src/main/webapp/js/imcms/editor_initializer/imcms_editor_init_strategy.js
+++ src/main/webapp/js/imcms/editor_initializer/imcms_editor_init_strategy.js
@@ -11,7 +11,7 @@ define("imcms-editor-init-strategy", ["jquery", 'imcms-modal-window-builder'], f
                 const editorData = $editedTag.data();
 
                 if (editorData.external) {
-                    const confirmMessage = `This ${editorInitData.context} (nbr #${editorData.index})`
+                    const confirmMessage = `This ${editorInitData.context} (#${editorData.index})`
                         + ` is edited on page ${editorData.external}\nGo to the page?`;
 
                     modalWindowBuilder.buildConfirmWindow(confirmMessage, () => {
