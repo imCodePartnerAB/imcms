8734144843212f5459f657a47d5a6817eea6a925
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-09-04 18:46:29 +0300

    IMCMS-350 - Image Editor improvements: - Fixed starters.

--- src/main/webapp/js/imcms/starters/imcms_image_edit_start.js
+++ src/main/webapp/js/imcms/starters/imcms_image_edit_start.js
@@ -11,12 +11,12 @@ var $ = require("jquery");
 var events = require("imcms-events");
 var imcms = require("imcms");
 
-events.on("enable text editor blur", function () {
-    var returnUrl = $("#return-url").val();
-    window.location = (returnUrl) ? returnUrl : (imcms.contextPath + "/" + editorData.docId);
-});
-
 $(function () {
+    events.on("enable text editor blur", function () {
+        var returnUrl = $("#return-url").val();
+        window.location = (returnUrl) ? returnUrl : (imcms.contextPath + "/" + editorData.docId);
+    });
+
     var $editedTag = $(imageEditorInitData.EDIT_AREA_SELECTOR);
     var editorData = $editedTag.data();
     imageEditorInitData.editorBuilder.setTag($editedTag).build(editorData);
