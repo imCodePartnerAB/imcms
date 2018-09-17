b8d13bdfa3dc0b5dca5e060fd5c16b452a5b4e0c
3emluk <3emluk@gmail.com>
2018-09-17 14:11:39 +0300

    IMCMS-347 - Direct editing general improvements: - Add toolbar to text editor

--- src/main/webapp/js/imcms/starters/imcms_text_edit_start.js
+++ src/main/webapp/js/imcms/starters/imcms_text_edit_start.js
@@ -6,6 +6,8 @@ import '../../../css/imcms-imports_files.css';
  * @author Serhii Maksymchuk from Ubrainians for imCode
  * 16.02.18
  */
+const toolbarBuilder = require('imcms-standalone-editor-toolbar-builder');
+
 var textEditorInitializer = require("imcms-text-editor-initializer");
 var imageEditorInitializer = require("imcms-image-editor-initializer");
 var imcms = require("imcms");
@@ -16,4 +18,22 @@ imcms.textEditorFullScreenEnabled = true;
 $(function () {
     textEditorInitializer.initEditor({autoFocus: true});
     imageEditorInitializer.initEditor();
+
+    var $editedTag = $('.imcms-editor-content--text:first');
+    var editorData = $editedTag.data();
+
+    const toolbarContent = [
+        {
+            type: 'id',
+            text: editorData.docId,
+            title: '',
+        },
+        {
+            type: 'index',
+            text: editorData.index,
+            title: '',
+        }
+    ];
+
+    toolbarBuilder.buildPanel(toolbarContent);
 });
