17abf80aa4168685b80a6e685059fc545feec72a
3emluk <3emluk@gmail.com>
2018-09-24 14:01:45 +0300

    IMCMS-376 - Image list - erasing images: - Fix test: And clean data before run

--- src/test/java/com/imcode/imcms/controller/api/CategoryTypeControllerTest.java
+++ src/test/java/com/imcode/imcms/controller/api/CategoryTypeControllerTest.java
@@ -3,12 +3,18 @@ package com.imcode.imcms.controller.api;
 import com.imcode.imcms.components.datainitializer.CategoryDataInitializer;
 import com.imcode.imcms.controller.AbstractControllerTest;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CategoryTypeControllerTest extends AbstractControllerTest {
     @Autowired
     private CategoryDataInitializer categoryDataInitializer;
+
+    @Before
+    public void prepareData() {
+        categoryDataInitializer.cleanRepositories();
+    }
 
     @After
     public void cleanRepos() {
