fe3aa329ef0cc24cb81a487f9ad98c503d3a4e15
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-09-25 13:58:08 +0300

    IMCMS-360 - Introduce SCSS and minify for css files in imcms-projects: - Implemented webpack config for scss compilation.

---
+++ src/main/webapp/client.config.js
@@ -0,0 +1,21 @@
+/**
+ * Client configuration used by SCSS compiler. You have to declare relative
+ * compiled files destination path. And also you have to declare input entries,
+ * where key is output file name and value is relative path to the source file.
+ *
+ * @example
+ *  module.exports = {
+ *      entry: {
+ *          main: './css/test.scss'
+ *      },
+ *      destination: 'dist-css'
+ *  };
+ *
+ *  // Result files structure would be: './dist-css/main.css'
+ */
+module.exports = {
+    entry: {
+        main: './css/test.scss'
+    },
+    destination: 'dist-css'
+};
---
+++ src/main/webapp/scss.config.js
@@ -0,0 +1,54 @@
+/**
+ * @author Serhii Maksymchuk from Ubrainians for imCode
+ * 12.09.18
+ */
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const clientConfig = require('./client.config.js');
+
+const buildDir = path.resolve(__dirname, clientConfig.destination);
+
+const fs = require('fs');
+const WebpackOnBuildPlugin = require('on-build-webpack');
+
+module.exports = {
+    mode: 'production',
+    entry: clientConfig.entry,
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({
+                assetNameRegExp: /\.(s?)css$/,
+            })
+        ]
+    },
+    output: {
+        path: buildDir,
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(s?)css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([buildDir]),
+        new MiniCssExtractPlugin('[name].css'),
+        new WebpackOnBuildPlugin(() => {
+            fs.readdir(path.resolve(buildDir), (err, files) => {
+                const jsPattern = /.js$/;
+
+                files.forEach(file => {
+                    if (jsPattern.test(file)) {
+                        fs.unlink(path.resolve(buildDir, file));
+                    }
+                });
+            });
+        }),
+    ],
+};
