c3c40ba25c6f3cc576d51000a5a9eb7b86aaa903
3emluk <3emluk@gmail.com>
2018-09-25 15:05:12 +0300

    IMCMS-376 - Image list - erasing images: - Remove unused exception

--- src/main/java/com/imcode/imcms/domain/exception/ImageReferenceException.java
+++
@@ -1,7 +0,0 @@
-package com.imcode.imcms.domain.exception;
-
-public class ImageReferenceException extends RuntimeException {
-    public ImageReferenceException(String message) {
-        super(message);
-    }
-}
