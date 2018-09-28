b7532dff96019c6ea549cd77ffee518f974beccf
3emluk <3emluk@gmail.com>
2018-09-28 12:31:17 +0300

    IMCMS-376 - Image list - erasing images: - Add check method to js api

--- src/main/webapp/js/imcms/rest/imcms-image-folders-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-image-folders-rest-api.js
@@ -7,5 +7,9 @@ define("imcms-image-folders-rest-api", ["imcms-rest-api"], function (rest) {
     let api = new rest.API(url);
 
     api.canDelete = rest.ajax.bind({url: url + '/can-delete', type: "POST", json: true});
+
+    api.check = function (folderName) {
+        return rest.ajax.bind({url: url + '/check', type: "GET", json: false}, folderName);
+    };
     return api;
 });
