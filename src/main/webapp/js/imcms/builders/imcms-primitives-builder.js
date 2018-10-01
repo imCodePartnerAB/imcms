443630cd55e3c497fe0886d5096a39798ffb123c
Serhii Maksymchuk <serg.maximchuk@gmail.com>
2018-10-01 12:29:15 +0300

    IMCMS-389 - image handling crop: - Cleanup.

--- src/main/webapp/js/imcms/builders/imcms-primitives-builder.js
+++ src/main/webapp/js/imcms/builders/imcms-primitives-builder.js
@@ -2,45 +2,44 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 27.07.17.
  */
-define("imcms-primitives-builder", ["imcms-bem-builder"], function (BEM) {
-    var IMCMS_LABEL_CLASS = "imcms-label",
-        IMCMS_INPUT_CLASS = "imcms-input"
-    ;
+const BEM = require('imcms-bem-builder');
 
-    var primitivesBEM = new BEM({
-        // no block for now
-        elements: {
-            "label": IMCMS_LABEL_CLASS,
-            "input": IMCMS_INPUT_CLASS
-        }
-    });
+const IMCMS_LABEL_CLASS = "imcms-label";
+const IMCMS_INPUT_CLASS = "imcms-input";
 
-    return {
-        imcmsLabel: function (idFor, text, attributes) {
-            attributes = attributes || {};
-            attributes["for"] = idFor;
-            attributes.text = text;
-
-            return this.imcmsLabelFromObject(attributes);
-        },
-        imcmsLabelFromObject: function (attributes) {
-            return primitivesBEM.buildElement("label", "<label>", attributes);
-        },
-        imcmsInput: function (attributes) {
-            return primitivesBEM.buildElement("input", "<input>", attributes);
-        },
-        imcmsInputText: function (attributes, modifiersArr) {
-            attributes = attributes || {};
-            attributes.type = "text";
-            return primitivesBEM.buildElement("input", "<input>", attributes, modifiersArr);
-        },
-        imcmsInputTextArea: function (attributes, modifiersArr) {
-            return primitivesBEM.buildElement("input", "<textarea>", attributes, modifiersArr);
-        },
-        imcmsInputRadio: function (attributes, modifiersArr) {
-            attributes = attributes || {};
-            attributes.type = "radio";
-            return primitivesBEM.buildElement("input", "<input>", attributes, modifiersArr);
-        }
+const primitivesBEM = new BEM({
+    // no block for now
+    elements: {
+        "label": IMCMS_LABEL_CLASS,
+        "input": IMCMS_INPUT_CLASS
     }
 });
+
+module.exports = {
+    imcmsLabel: function (idFor, text, attributes) {
+        attributes = attributes || {};
+        attributes["for"] = idFor;
+        attributes.text = text;
+
+        return this.imcmsLabelFromObject(attributes);
+    },
+    imcmsLabelFromObject: function (attributes) {
+        return primitivesBEM.buildElement("label", "<label>", attributes);
+    },
+    imcmsInput: function (attributes) {
+        return primitivesBEM.buildElement("input", "<input>", attributes);
+    },
+    imcmsInputText: function (attributes, modifiersArr) {
+        attributes = attributes || {};
+        attributes.type = "text";
+        return primitivesBEM.buildElement("input", "<input>", attributes, modifiersArr);
+    },
+    imcmsInputTextArea: function (attributes, modifiersArr) {
+        return primitivesBEM.buildElement("input", "<textarea>", attributes, modifiersArr);
+    },
+    imcmsInputRadio: function (attributes, modifiersArr) {
+        attributes = attributes || {};
+        attributes.type = "radio";
+        return primitivesBEM.buildElement("input", "<input>", attributes, modifiersArr);
+    }
+};
--- src/main/webapp/js/imcms/components/imcms-uuid-generator.js
+++ src/main/webapp/js/imcms/components/imcms-uuid-generator.js
@@ -2,36 +2,34 @@
  * Created by Serhii Maksymchuk from Ubrainians for imCode
  * 01.08.17.
  */
-define("imcms-uuid-generator", [], function () {
-    var hexValues = [
-        '0', '1', '2', '3',
-        '4', '5', '6', '7',
-        '8', '9', 'a', 'b',
-        'c', 'd', 'e', 'f'
-    ];
+const hexValues = [
+    '0', '1', '2', '3',
+    '4', '5', '6', '7',
+    '8', '9', 'a', 'b',
+    'c', 'd', 'e', 'f'
+];
 
-    function getRandomHex() {
-        return hexValues[Math.round(Math.random() * 15)];
-    }
+function getRandomHex() {
+    return hexValues[Math.round(Math.random() * 15)];
+}
 
-    function getRandomHexes(howMany) {
-        var hexes = "";
-        for (var i = 0; i < howMany; i++) {
-            hexes += getRandomHex();
-        }
-        return hexes;
+function getRandomHexes(howMany) {
+    let hexes = "";
+    for (let i = 0; i < howMany; i++) {
+        hexes += getRandomHex();
     }
+    return hexes;
+}
 
-    return {
-        generateUUID: function () {
-            var first8 = getRandomHexes(8),
-                then4 = getRandomHexes(4),
-                more4 = getRandomHexes(4),
-                onceMore4 = getRandomHexes(4),
-                last12 = getRandomHexes(12)
-            ;
-            // result is something like 550e8400-e29b-41d4-a716-446655440f00
-            return first8 + "-" + then4 + "-" + more4 + "-" + onceMore4 + "-" + last12;
-        }
+module.exports = {
+    generateUUID: function () {
+        const first8 = getRandomHexes(8);
+        const then4 = getRandomHexes(4);
+        const more4 = getRandomHexes(4);
+        const onceMore4 = getRandomHexes(4);
+        const last12 = getRandomHexes(12);
+
+        // result is something like 550e8400-e29b-41d4-a716-446655440f00
+        return first8 + "-" + then4 + "-" + more4 + "-" + onceMore4 + "-" + last12;
     }
-});
+};
