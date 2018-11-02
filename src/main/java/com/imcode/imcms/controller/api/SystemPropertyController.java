86ca611de3e0465988716e80eb5ff7331f9c4172
Victor <vitekpavFurious>
2018-11-02 19:09:43 +0200

    IMCMS-337 New design to super admin page: system properties tab - Add additional endpoints to controller

--- src/main/java/com/imcode/imcms/controller/api/SystemPropertyController.java
+++ src/main/java/com/imcode/imcms/controller/api/SystemPropertyController.java
@@ -1,9 +1,13 @@
 package com.imcode.imcms.controller.api;
 
 import com.imcode.imcms.domain.service.SystemPropertyService;
-import org.springframework.web.bind.annotation.RestController;
+import com.imcode.imcms.mapping.jpa.SystemProperty;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/properties")
 public class SystemPropertyController {
 
     private final SystemPropertyService systemPropertyService;
@@ -11,4 +15,31 @@ public class SystemPropertyController {
     public SystemPropertyController(SystemPropertyService systemPropertyService) {
         this.systemPropertyService = systemPropertyService;
     }
+
+    @GetMapping
+    public List<SystemProperty> findAll() {
+        return systemPropertyService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public SystemProperty getById(@PathVariable Integer id) {
+        return systemPropertyService.findById(id);
+    }
+
+    @GetMapping("/{name}")
+    public SystemProperty findByName(@PathVariable String name) {
+        return systemPropertyService.findByName(name);
+    }
+
+    @PostMapping
+    public SystemProperty update(@RequestBody SystemProperty systemProperty) {
+        return systemPropertyService.update(systemProperty);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        systemPropertyService.deleteById(id);
+    }
+
+
 }
