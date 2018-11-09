d89b465e85d9a3a4fc9cdb61ec9fc74a4e612c60
Victor <vitekpavFurious>
2018-11-09 16:29:43 +0200

    IMCMS-337 New design to super admin page: system properties tab - Refactor code

--- src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-system-properties-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-system-properties-tab-builder.js
@@ -4,149 +4,191 @@
  */
 define(
     'imcms-system-properties-tab-builder',
-    ['imcms-super-admin-tab', 'imcms-i18n-texts', 'imcms-components-builder'],
-    function (SuperAdminTab, texts, components) {
+    ['imcms-super-admin-tab', 'imcms-bem-builder', 'imcms-i18n-texts', 'imcms-components-builder'],
+    function (SuperAdminTab, BEM, texts, components) {
 
         texts = texts.superAdmin.systemProperties;
 
-        let inputNumberPage;
-        let messageSystem;
-        let webMasterFieldName;
-        let webMasterFieldEmail;
-        let serverMasterName;
-        let serverMasterEmail;
+        function buildPageRow() {
+            let inputNumberPage;
 
-        function buildCreateFiledInputNumberPage() {
-            let $pageNumberBox = components.texts.textBox('<div>', texts.sections.startPage.input);
-            inputNumberPage = $pageNumberBox.$input;
+            function buildTitleTextStartPage() {
+                return components.texts.titleText('<div>', texts.sections.startPage.name, {})
+            }
 
-            return $pageNumberBox;
-        }
+            function buildCreateFiledInputNumberPage() {
+                let $pageNumberBox = components.texts.textBox('<div>', texts.sections.startPage.input);
+                inputNumberPage = $pageNumberBox.$input;
 
-        function buildPageNumberButton() {
-            let $button = components.buttons.positiveButton({
-                text: texts.changeButton,
-                click: function () {
+                return $pageNumberBox;
+            }
 
+            function buildPageNumberButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.changeButton,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+
+            return new BEM({
+                block: 'imcms-settings-row',
+                elements: {
+                    'settings-title': buildTitleTextStartPage(),
+                    'settings-input': buildCreateFiledInputNumberPage(),
+                    'settings-button': buildPageNumberButton(),
                 }
+            }).buildBlockStructure('<div>', {
+                'class': 'imcms-field'
             });
-
-            return components.buttons.buttonsContainer('<div>', [$button]);
         }
 
-        function buildTitleTextStartPage() {
-            return components.texts.titleText('<div>', texts.sections.startPage.name, {})
-        }
+        function buildSystemMessageRow() {
+            let messageSystem;
 
-
-        function buildCreateTitleSystemMessage() {
-            return components.texts.titleText('<div>', texts.sections.systemMessage.name, {})
-
-        }
-
-
-        function buildCreateSystemMessageInput() {
-            let $fieldForSystemMessage = components.texts.textBox('<div>', texts.sections.systemMessage.inputBox);
-            messageSystem = $fieldForSystemMessage.$input;
-
-            return $fieldForSystemMessage;
-        }
-
-
-        function buildCreateSystemMessageButton() {
-            function setStartDocument() {
+            function buildCreateTitleSystemMessage() {
+                return components.texts.titleText('<div>', texts.sections.systemMessage.name, {})
 
             }
 
-            let $button = components.buttons.positiveButton({
-                text: texts.changeButton,
-                click: setStartDocument()
-            });
+            function buildCreateSystemMessageInput() {
+                let $fieldForSystemMessage = components.texts.textBox('<div>', texts.sections.systemMessage.inputBox);
+                messageSystem = $fieldForSystemMessage.$input;
 
-            return components.buttons.buttonsContainer('<div>', [$button]);
-        }
+                return $fieldForSystemMessage;
+            }
 
 
-        function buildCreateTitleServerMaster() {
-            return components.texts.titleText('<div>', texts.sections.serverMaster.name, {})
-
-        }
-
-        function buildServerMasterNameInput() {
-            let $serverMasterName = components.texts.textBox('<div>', texts.sections.serverMaster.inputName);
-            serverMasterName = $serverMasterName.$input;
-
-            return $serverMasterName;
-        }
-
-        function buildServerMasterEmailInput() {
-            let $serverMasterEmailInput = components.texts.textBox('<div>', texts.sections.serverMaster.inputEmail);
-            serverMasterEmail = $serverMasterEmailInput.$input;
-
-            return serverMasterEmail;
-        }
-
-        function buildCreateServerMasterButton() {
-            console.log(texts.changeButton);
-
-            let $button = components.buttons.positiveButton({
-                text: texts.changeButton,
-                click: function setServerMasterbyNameAndEmail() {
+            function buildCreateSystemMessageButton() {
+                function setStartDocument() {
 
                 }
-            });
 
-            return components.buttons.buttonsContainer('<div>', [$button]);
-        }
+                let $button = components.buttons.positiveButton({
+                    text: texts.changeButton,
+                    click: setStartDocument()
+                });
 
-        function buildCreateTilteWebMaster() {
-            return components.texts.titleText('<div>', texts.sections.webMaster.name, {})
-
-        }
-
-        function buildCreateInputWebMasterName() {
-            let $nameWebMasterInput = components.texts.textBox('<div>', texts.sections.webMaster.inputName);
-            webMasterFieldName = $nameWebMasterInput.$input;
-
-            return $nameWebMasterInput;
-        }
-
-        function buildCreateInputWebMasterEmail() {
-            let $emailWebMasterInput = components.texts.textBox('<div>', texts.sections.webMaster.inputEmail);
-            webMasterFieldEmail = $emailWebMasterInput.$input;
-
-            return $emailWebMasterInput;
-        }
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
 
 
-        function buildCreateMasterWebButton() {
-            let $button = components.buttons.positiveButton({
-                text: texts.changeButton,
-                click: function () {
-
+            return new BEM({
+                block: 'imcms-settings-row',
+                elements: {
+                    'settings-title': buildCreateTitleSystemMessage(),
+                    'settings-input': buildCreateSystemMessageInput(),
+                    'settings-button': buildCreateSystemMessageButton(),
                 }
+            }).buildBlockStructure('<div>', {
+                'class': 'imcms-field'
             });
-
-            return components.buttons.buttonsContainer('<div>', [$button]);
         }
 
+        function buildServerMasterRow() {
+            let serverMasterName;
+            let serverMasterEmail;
+
+            function buildCreateTitleServerMaster() {
+                return components.texts.titleText('<div>', texts.sections.serverMaster.name, {})
+
+            }
+
+            function buildServerMasterNameInput() {
+                let $serverMasterName = components.texts.textBox('<div>', texts.sections.serverMaster.inputName);
+                serverMasterName = $serverMasterName.$input;
+
+                return $serverMasterName;
+            }
+
+            function buildServerMasterEmailInput() {
+                let $serverMasterEmailInput = components.texts.textBox('<div>', texts.sections.serverMaster.inputEmail);
+                serverMasterEmail = $serverMasterEmailInput.$input;
+
+                return serverMasterEmail;
+            }
+
+            function buildCreateServerMasterButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.changeButton,
+                    click: function setServerMasterbyNameAndEmail() {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+            return new BEM({
+                block: 'imcms-settings-row',
+                elements: {
+                    'settings-title': buildCreateTitleServerMaster(),
+                    'settings-input-name': buildServerMasterNameInput(),
+                    'settings-input-email': buildServerMasterEmailInput(),
+                    'settings-button': buildCreateServerMasterButton(),
+                }
+            }).buildBlockStructure('<div>', {
+                'class': 'imcms-field'
+            });
+        }
+
+        function buildWebMasterRow() {
+            let webMasterFieldName;
+            let webMasterFieldEmail;
+
+            function buildCreateTilteWebMaster() {
+                return components.texts.titleText('<div>', texts.sections.webMaster.name, {})
+
+            }
+
+            function buildCreateInputWebMasterName() {
+                let $nameWebMasterInput = components.texts.textBox('<div>', texts.sections.webMaster.inputName);
+                webMasterFieldName = $nameWebMasterInput.$input;
+
+                return $nameWebMasterInput;
+            }
+
+            function buildCreateInputWebMasterEmail() {
+                let $emailWebMasterInput = components.texts.textBox('<div>', texts.sections.webMaster.inputEmail);
+                webMasterFieldEmail = $emailWebMasterInput.$input;
+
+                return $emailWebMasterInput;
+            }
+
+
+            function buildCreateMasterWebButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.changeButton,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+            return new BEM({
+                block: 'imcms-settings-row',
+                elements: {
+                    'settings-title': buildCreateTilteWebMaster(),
+                    'settings-input-name': buildCreateInputWebMasterName(),
+                    'settings-input-email': buildCreateInputWebMasterEmail(),
+                    'settings-button': buildCreateMasterWebButton(),
+                }
+            }).buildBlockStructure('<div>', {
+                'class': 'imcms-field'
+            });
+        }
 
         return new SuperAdminTab(texts.name, [
-            buildTitleTextStartPage(),
-            buildCreateFiledInputNumberPage(),
-            buildPageNumberButton(),
-            buildCreateTitleSystemMessage(),
-            buildCreateSystemMessageInput(),
-            buildCreateSystemMessageButton(),
-            buildCreateTitleServerMaster(),
-            buildServerMasterNameInput(),
-            buildServerMasterEmailInput(),
-            buildCreateServerMasterButton(),
-            buildCreateTilteWebMaster(),
-            buildCreateInputWebMasterName(),
-            buildCreateInputWebMasterEmail(),
-            buildCreateMasterWebButton()
-
+            buildPageRow(),
+            buildSystemMessageRow(),
+            buildServerMasterRow(),
+            buildWebMasterRow(),
         ]);
     }
 );
