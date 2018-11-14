ad7cb921887222c168b4d6018801a76121224dce
Victor <vitekpavFurious>
2018-11-14 17:49:41 +0200

    IMCMS-336 New design to super admin page: profiles tab - Add new endpoints

--- src/main/java/com/imcode/imcms/controller/api/ProfilesController.java
+++ src/main/java/com/imcode/imcms/controller/api/ProfilesController.java
@@ -1,8 +1,15 @@
 package com.imcode.imcms.controller.api;
 
+import com.imcode.imcms.domain.dto.ProfileDTO;
 import com.imcode.imcms.domain.service.ProfileService;
 import com.imcode.imcms.model.Profile;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +30,23 @@ public class ProfilesController {
         return profileService.getAll();
     }
 
+    @GetMapping("/{id}")
+    public Profile getById(@PathVariable Integer id) {
+        return profileService.getById(id).orElseThrow(() -> new EmptyResultDataAccessException(id));
+    }
+
+    @PutMapping
+    public Profile create(@RequestBody ProfileDTO profile) {
+        return profileService.create(profile);
+    }
+
+    @PostMapping
+    public Profile update(@RequestBody ProfileDTO profile) {
+        return profileService.update(profile);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        profileService.deleteById(id);
+    }
 }
