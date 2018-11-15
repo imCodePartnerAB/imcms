7261056faa0c7bd2a505885624211ec58e6c812d
Victor <vitekpavFurious>
2018-11-15 18:58:59 +0200

    IMCMS-336 New design to super admin page: profiles tab - Clean up

--- src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-profiles-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-profiles-tab-builder.js
@@ -1,7 +1,4 @@
-/**
- * @author Serhii Maksymchuk from Ubrainians for imCode
- * 23.06.18
- */
+
 define(
     'imcms-profiles-tab-builder',
     ['imcms-super-admin-tab', 'imcms-i18n-texts', 'imcms-components-builder', 'imcms-bem-builder', 'imcms-field-wrapper',
