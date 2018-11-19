6341ddf3e4376556aa9f632d2da2effcabd938d3
Victor <vitekpavFurious>
2018-11-19 18:29:34 +0200

    IMCMS-336 New design to super admin page: profiles tab - Fixed profile table

--- src/main/webapp/js/imcms/builders/components/imcms-profile-to-row-transformer.js
+++ src/main/webapp/js/imcms/builders/components/imcms-profile-to-row-transformer.js
@@ -1,12 +1,15 @@
 define(
-    'imcms-profile-to-row-transformer', ['imcms-bem-builder', 'jquery'], function (BEM, $) {
+    'imcms-profile-to-row-transformer', ['imcms-bem-builder', 'jquery', 'imcms-components-builder'], function (BEM, $, components) {
 
         var profilesTableBEM = new BEM({
             block: 'profiles-table',
             elements: {
-                'profile-row': ''
+                'profile-row': '',
+                'profile-delete-button': ''
             }
         });
+
+        let $profileDeleteButtons;
 
         function getOnProfileClicked(profile, profileEditor) {
             return function () {
@@ -18,14 +21,60 @@ define(
             }
         }
 
+        function getOnDeleteClicked(profile, profileEditor) {
+            return function () {
+                var $this = $(this);
+
+                if ($this.hasClass('profile-table__profile-row--active')) return;
+
+                profileEditor.deleteButton($this, profile);
+            }
+        }
+
+        function buildProfileDeleteButtons() {
+            return $profileDeleteButtons = components.buttons.closeButton({
+                name: 'delete',
+                click: function () {
+
+                }
+            });
+        }
+
         return {
             transform: function (profile, profileEditor) {
-                return profilesTableBEM.makeBlockElement('profile-row', $('<div>', {
+                let $profileName = profilesTableBEM.makeBlockElement('profile-row', $("<div>", {
                     id: 'profile-id-' + profile.id,
-                    text: profile.name + ' ' + profile.documentName,
+                    text: profile.name,
                     click: getOnProfileClicked(profile, profileEditor)
-                }))
+                }));
+                $profileName.modifiers = ["profileName"];
+
+                let $profileDocName = profilesTableBEM.makeBlockElement('profile-row', $("<div>", {
+                    id: 'profile-id-' + profile.id,
+                    text: profile.documentName,
+                    click: getOnProfileClicked(profile, profileEditor)
+                }));
+                $profileDocName.modifiers = ["profileDocName"];
+
+                let $buttonDelete = components.buttons.closeButton({
+                    id: 'profile-id-' + profile.id,
+                    click: function () {
+                    }
+                });
+                $buttonDelete.modifiers = ["buttonDelete"];
+
+                return new BEM({
+                    block: "imcms-profiles-list",
+                    elements: {
+                        'profile-name-docName':
+                            [
+                                $profileName,
+                                $profileDocName,
+                                $buttonDelete
+                            ]
+                    }
+                }).buildBlockStructure("<div>");
             }
-        };
+        }
     }
 );
