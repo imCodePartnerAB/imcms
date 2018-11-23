8c3c57e57506128890e5fe870cf2ffefb7b12351
Victor <vitekpavFurious>
2018-11-23 17:28:21 +0200

    IMCMS-336 New design to super admin page: profiles tab - Update profile rest api

--- src/main/webapp/js/imcms/rest/imcms-profiles-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-profiles-rest-api.js
@@ -6,24 +6,12 @@ define("imcms-profiles-rest-api", ["imcms-rest-api"], function (rest) {
     let url = '/profiles';
     let api = new rest.API(url);
 
-    api.getAllProfiles = function () {
+    api.read = function () {
         return rest.ajax.call({url: url, type: 'GET', json: true});
     };
 
-    api.create = function (profile) {
-        return rest.ajax.call({url: url, type: 'PUT', json: true}, profile);
-    };
-
-    api.update = function (profile) {
-        return rest.ajax.call({url: url, type: 'POST', json: true}, profile);
-    };
-
-    api.deleteById = function (profile) {
+    api.remove = function (profile) {
         return rest.ajax.call({url: url + '/' + profile.id, type: 'DELETE', json: true});
-    };
-
-    api.getById = function (profile) {
-        return rest.ajax.call({url: url + '/' + profile.id, type: 'GET', json: true});
     };
 
     return api;
