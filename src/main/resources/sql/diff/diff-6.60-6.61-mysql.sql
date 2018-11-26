8d163fbf6ce62c21b38cbe6a19845edaf70ba6f7
3emluk <3emluk@gmail.com>
2018-11-26 15:54:11 +0200

    IMCMS-328 - New design to super admin page: IP access tab: - Update schema

---
+++ src/main/resources/sql/diff/diff-6.60-6.61-mysql.sql
@@ -0,0 +1,24 @@
+# update meta default_version_no field
+
+SET @schema_version__major_new = 6;
+SET @schema_version__minor_new = 61;
+
+DROP TABLE IF EXISTS ip_accesses;
+
+CREATE TABLE ip_access_rules (
+  rule_id    INT(11) AUTO_INCREMENT PRIMARY KEY,
+  enabled    BIT DEFAULT 0      NOT NULL,
+  restricted BIT DEFAULT 0      NOT NULL,
+  ip_range   VARCHAR(80) DEFAULT '' NOT NULL,
+  role_id    INT(11) DEFAULT NULL,
+  user_id    INT(11) DEFAULT NUll,
+  CONSTRAINT imcms_ip_access_rule_fk_role_id FOREIGN KEY (role_id) REFERENCES roles (role_id)
+    ON DELETE CASCADE,
+  CONSTRAINT imcms_ip_access_rule_fk_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
+    ON DELETE CASCADE
+);
+
+UPDATE database_version
+SET major = @schema_version__major_new,
+    minor = @schema_version__minor_new;
+
