7d5e30c584e9da94e275d967d09b994efce3b051
Victor <vitekpavFurious>
2018-11-26 18:49:59 +0200

    IMCMS-336 New design to super admin page: profiles tab - remove rest api read

--- src/main/webapp/js/imcms/rest/imcms-profiles-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-profiles-rest-api.js
@@ -6,10 +6,6 @@ define("imcms-profiles-rest-api", ["imcms-rest-api"], function (rest) {
     let url = '/profiles';
     let api = new rest.API(url);
 
-    api.read = function () {
-        return rest.ajax.call({url: url, type: 'GET', json: true});
-    };
-
     api.remove = function (profile) {
         return rest.ajax.call({url: url + '/' + profile.id, type: 'DELETE', json: true});
     };
