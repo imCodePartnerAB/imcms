b0dad0dbb2a48b5bc66f3ad54ebbde6d4b85a4e0
3emluk <3emluk@gmail.com>
2018-11-27 10:57:43 +0200

    IMCMS-328 - New design to super admin page: IP access tab: - Add basic service implementation

--- src/main/java/com/imcode/imcms/domain/service/IpAccessRuleService.java
+++ src/main/java/com/imcode/imcms/domain/service/IpAccessRuleService.java
@@ -1,4 +1,19 @@
 package com.imcode.imcms.domain.service;
 
+import com.imcode.imcms.model.IpAccessRule;
+
+import java.util.List;
+
 public interface IpAccessRuleService {
+
+    IpAccessRule getById(int id);
+
+    List<IpAccessRule> getAll();
+
+    IpAccessRule create(IpAccessRule rule);
+
+    IpAccessRule update(IpAccessRule rule);
+
+    void delete(int ruleId);
+
 }
--- src/main/java/com/imcode/imcms/domain/service/api/DefaultIpAccessRuleService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/DefaultIpAccessRuleService.java
@@ -1,9 +1,15 @@
 package com.imcode.imcms.domain.service.api;
 
+import com.imcode.imcms.domain.dto.IpAccessRuleDTO;
 import com.imcode.imcms.domain.service.IpAccessRuleService;
+import com.imcode.imcms.model.IpAccessRule;
+import com.imcode.imcms.persistence.entity.IpAccessRuleJPA;
 import com.imcode.imcms.persistence.repository.IpAccessRuleRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -13,5 +19,34 @@ public class DefaultIpAccessRuleService implements IpAccessRuleService {
 
     DefaultIpAccessRuleService(IpAccessRuleRepository ipAccessRuleRepository) {
         this.ipAccessRuleRepository = ipAccessRuleRepository;
+    }
+
+    @Override
+    public IpAccessRule getById(int id) {
+        final IpAccessRuleJPA rule = ipAccessRuleRepository.findOne(id);
+
+        return (null == rule) ? null : new IpAccessRuleDTO(rule);
+    }
+
+    @Override
+    public List<IpAccessRule> getAll() {
+        return ipAccessRuleRepository.findAll().stream()
+                .map(IpAccessRuleDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public IpAccessRule create(IpAccessRule rule) {
+        return new IpAccessRuleDTO(ipAccessRuleRepository.save(new IpAccessRuleJPA(rule)));
+    }
+
+    @Override
+    public IpAccessRule update(IpAccessRule rule) {
+        return new IpAccessRuleDTO(ipAccessRuleRepository.save(new IpAccessRuleJPA(rule)));
+    }
+
+    @Override
+    public void delete(int ruleId) {
+        ipAccessRuleRepository.delete(ruleId);
     }
 }
