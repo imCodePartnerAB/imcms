51592e89053066e096c10565bc235dd511a07efc
Victor <vitekpavFurious>
2018-11-27 17:34:40 +0200

    IMCMS-334 New design to super admin page: link validator tab - Update import

--- src/test/java/com/imcode/imcms/domain/service/api/W3cTextValidationServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/W3cTextValidationServiceTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @Transactional
 public class W3cTextValidationServiceTest extends WebAppSpringTestConfig {
