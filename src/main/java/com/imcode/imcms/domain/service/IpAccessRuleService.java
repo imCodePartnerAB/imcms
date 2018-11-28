6234095a9994581ebdde9d192471e00539fd19fe
3emluk <3emluk@gmail.com>
2018-11-28 11:04:39 +0200

    IMCMS-328 - New design to super admin page: IP access tab: - Add service interface method

--- src/main/java/com/imcode/imcms/domain/service/IpAccessRuleService.java
+++ src/main/java/com/imcode/imcms/domain/service/IpAccessRuleService.java
@@ -1,7 +1,9 @@
 package com.imcode.imcms.domain.service;
 
 import com.imcode.imcms.model.IpAccessRule;
+import imcode.server.user.UserDomainObject;
 
+import java.net.InetAddress;
 import java.util.List;
 
 public interface IpAccessRuleService {
@@ -15,5 +17,7 @@ public interface IpAccessRuleService {
     IpAccessRule update(IpAccessRule rule);
 
     void delete(int ruleId);
+
+    boolean isAllowedToAccess(InetAddress accessIp, UserDomainObject user);
 
 }
