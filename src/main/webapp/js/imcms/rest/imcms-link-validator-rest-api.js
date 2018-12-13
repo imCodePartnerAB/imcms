991efcf87be621b97bc1419695e02cbe0feca960
Victor <vitekpavFurious>
2018-12-13 10:27:23 +0200

    IMCMS-334 New design to super admin page: link validator tab - Rename

--- src/main/webapp/js/imcms/rest/imcms-link-validator-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-link-validator-rest-api.js
@@ -2,8 +2,8 @@ define('imcms-link-validator-rest-api', ["imcms-rest-api"], function (rest) {
     let url = '/links';
     let api = new rest.API(url);
 
-    api.search = function (queryObj) {
-        return rest.ajax.call({url: url, type: 'GET', json: true}, queryObj);
+    api.validate = function (filterObj) {
+        return rest.ajax.call({url: url, type: 'GET', json: true}, filterObj);
     };
 
     return api;
