978839e8b0a3eb0567951dbcdf24724266c31d5c
Victor <vitekpavFurious>
2018-12-13 16:41:36 +0200

    IMCMS-334 New design to super admin page: link validator tab - improve code and clean up

--- src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-link-validator-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-link-validator-tab-builder.js
@@ -6,65 +6,30 @@ define(
 
         texts = texts.superAdmin.linkValidator;
 
-        let fieldStartId;
-        let fieldEndId;
-        let $includeInactiveCheckbox;
+        let $inputStartId;
+        let $inputEndId;
+        let $filterOnlyBrokenList;
+        let $searchResultContainer;
+
+        function showOnlyBrokenLinks() {
+
+        }
 
         function buildTitleText() {
             return fieldWrapper.wrap(components.texts.titleText('<div>', texts.name))
         }
 
-        function buildContainerTitleAndFilter() {
-
-            function buildTitleOnlyBrokenLinks() {
-                return fieldWrapper.wrap(components.texts.titleText('<div>', texts.titleOnlyBrokenLinks))
-            }
-
-            function buildCheckBoxOnlyBrokenLinks() {
-                return $includeInactiveCheckbox = components.checkboxes.imcmsCheckbox('<div>', {
-                    click: function () {
-
-                    }
-                });
-            }
-
-            return new BEM({
-                block: 'title-filter-block',
-                elements: {
-                    'links-only-broken-title': buildTitleOnlyBrokenLinks(),
-                    'flag-broken-links': buildCheckBoxOnlyBrokenLinks(),
-                }
-            }).buildBlockStructure('<div>');
-
-        }
-
-        let titleRow;
-
-        function buildTitleTableRow() {
-            titleRow = new BEM({
-                block: 'table-title-row',
-                elements: {
-                    'page-alias': $('<div>', {text: texts.linkInfoRow.pageAlias}),
-                    'status': $('<div>', {text: texts.linkInfoRow.status}),
-                    'type': $('<div>', {text: texts.linkInfoRow.type}),
-                    'admin': $('<div>', {text: texts.linkInfoRow.admin}),
-                    'reference': $('<div>', {text: texts.linkInfoRow.reference}),
-                    'link': $('<div>', {text: texts.linkInfoRow.link}),
-                    'host-found': $('<div>', {text: texts.linkInfoRow.hostFound}),
-                    'host-reachable': $('<div>', {text: texts.linkInfoRow.hostReachable}),
-                    'page-found': $('<div>', {text: texts.linkInfoRow.pageFound})
-                }
-            }).buildBlockStructure('<div>', {
-                'class': 'table-title',
-
+        function buildFilterOnlyBrokenLinks() {
+            return $filterOnlyBrokenList = components.checkboxes.imcmsCheckbox('<div>', {
+                text: texts.titleOnlyBrokenLinks,
+                click: showOnlyBrokenLinks
             });
-            return titleRow;
         }
 
-        let $linkContainer;
+
 
         function buildLinksContainer() {
-            $linkContainer = $('<div>', {
+            let $linkContainer = $('<div>', {
                 'class': 'table-links',
                 style: 'display: none;'
             });
@@ -72,37 +37,26 @@ define(
             return fieldWrapper.wrap([$linkContainer]);
         }
 
-        function showLinksTable() {
-            return $linkContainer.slideDown('fast').append(buildTitleTableRow())
-        }
-
         function buildContainerInputIdsValidation() {
 
-            function buildTitleStartId() {
-                return fieldWrapper.wrap(components.texts.titleText('<div>', texts.titleStartId))
-            }
-
             function buildFieldStartId() {
-                var $documentStartIdBox = components.texts.textNumber('<div>', texts.fieldStartId);
-                fieldStartId = $documentStartIdBox.$input;
-                return $documentStartIdBox;
+                return $inputStartId = components.texts.textNumber('<div>', {
+                    placeholder: '1001',
+                    text: texts.titleStartId,
+                });
             }
-
-            function buildTitleEndId() {
-                return fieldWrapper.wrap(components.texts.titleText('<div>', texts.titleEndId))
-            }
-
 
             function buildFieldEndId() {
-                var $documentEndIdBox = components.texts.textNumber('<div>', texts.fieldEndId);
-                fieldEndId = $documentEndIdBox.$input;
-                return $documentEndIdBox;
+                return $inputEndId = components.texts.textNumber('<div>', {
+                    placeholder: '1001',
+                    text: texts.titleEndId,
+                });
             }
 
             function buildButtonValidation() {
                 let $button = components.buttons.positiveButton({
                     text: texts.buttonValidation,
-                    click: showLinksTable
+                    click: listValidationLinks
                 });
 
                 return components.buttons.buttonsContainer('<div>', [$button]);
@@ -111,9 +65,7 @@ define(
             return new BEM({
                 block: 'imcms-field-ids-block', // change this name if need!! =)
                 elements: {
-                    'title-text-start-id': buildTitleStartId(),
                     'links-field-start': buildFieldStartId(),
-                    'title-text-end-id': buildTitleEndId(),
                     'links-field-end': buildFieldEndId(),
                     'button-validation': buildButtonValidation()
                 }
@@ -122,9 +74,9 @@ define(
 
         return new SuperAdminTab(texts.name, [
             buildTitleText(),
-            buildContainerTitleAndFilter(),
+            buildFilterOnlyBrokenLinks(),
             buildContainerInputIdsValidation(),
-            buildLinksContainer()
+            $searchResultContainer = buildLinksContainer()
         ]);
     }
 );
