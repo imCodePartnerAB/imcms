c221536d566e168ed234b2b26c1960708009d570
3emluk <3emluk@gmail.com>
2018-12-17 17:32:37 +0200

    IMCMS-390 - Implement 2-factor authentication for ordinary login: - Not show error on verification code request

--- server/src/com/imcode/imcms/servlet/VerifyUser.java
+++ server/src/com/imcode/imcms/servlet/VerifyUser.java
@@ -24,7 +24,7 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 
-import static com.imcode.imcms.services.TwoFactorAuthService.PROPERTY_NAME_2FA;
+import static com.imcode.imcms.services.TwoFactorAuthService.*;
 
 public class VerifyUser extends HttpServlet {
 
@@ -81,7 +81,7 @@ public class VerifyUser extends HttpServlet {
     }
 
     private void goToLoginFailedPage(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-        if (null == req.getAttribute(REQUEST_ATTRIBUTE__ERROR)) {
+        if (!(null == req.getAttribute(REQUEST_ATTRIBUTE__ERROR) && null != req.getSession().getAttribute(REQUEST_PARAMETER_2FA))) {
             req.setAttribute(REQUEST_ATTRIBUTE__ERROR, ERROR__LOGIN_FAILED);
         }
         req.getRequestDispatcher("/imcms/" + Utility.getLoggedOnUser(req).getLanguageIso639_2()
