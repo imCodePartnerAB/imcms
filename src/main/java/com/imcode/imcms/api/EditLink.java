181585d9980019056b8bd1f36537d694e23f3c2d
Victor <vitekpavFurious>
2018-12-18 18:49:33 +0200

    IMCMS-334 New design to super admin page: link validator tab - Create class

---
+++ src/main/java/com/imcode/imcms/api/EditLink.java
@@ -0,0 +1,13 @@
+package com.imcode.imcms.api;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class EditLink {
+    private Integer metaId;
+    private String title;
+    private Integer index;
+
+}
