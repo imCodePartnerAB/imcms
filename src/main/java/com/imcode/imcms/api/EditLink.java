e29474d9e83ed88ec63cbe9394858e38386634d1
Victor <vitekpavFurious>
2018-12-22 12:09:42 +0200

    IMCMS-334 New design to super admin page: link validator tab - Remove annotation

--- src/main/java/com/imcode/imcms/api/EditLink.java
+++ src/main/java/com/imcode/imcms/api/EditLink.java
@@ -1,13 +1,10 @@
 package com.imcode.imcms.api;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class EditLink {
     private Integer metaId;
     private String title;
     private Integer index;
-
 }
