03d0712004e88887507977b9eb78e81d61482826
Victor <vitekpavFurious>
2019-01-10 12:11:54 +0200

    IMCMS-335 New design to super admin page: categories tab - Refactor and add initializer; - Add tests for delete different type document

--- src/test/java/com/imcode/imcms/domain/service/api/CategoryServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/service/api/CategoryServiceTest.java
@@ -1,15 +1,26 @@
 package com.imcode.imcms.domain.service.api;
 
 import com.imcode.imcms.WebAppSpringTestConfig;
+import com.imcode.imcms.components.datainitializer.CategoryDataInitializer;
+import com.imcode.imcms.domain.dto.CategoryDTO;
+import com.imcode.imcms.domain.dto.CategoryTypeDTO;
+import com.imcode.imcms.domain.dto.FileDocumentDTO;
+import com.imcode.imcms.domain.dto.TextDocumentDTO;
+import com.imcode.imcms.domain.dto.UrlDocumentDTO;
+import com.imcode.imcms.domain.factory.DocumentDtoFactory;
 import com.imcode.imcms.domain.service.CategoryService;
-import com.imcode.imcms.domain.service.CategoryTypeService;
+import com.imcode.imcms.domain.service.DocumentService;
 import com.imcode.imcms.model.Category;
-import com.imcode.imcms.model.CategoryType;
+import com.imcode.imcms.model.Roles;
 import com.imcode.imcms.persistence.entity.CategoryJPA;
 import com.imcode.imcms.persistence.entity.CategoryTypeJPA;
 import com.imcode.imcms.persistence.entity.Meta;
 import com.imcode.imcms.persistence.repository.CategoryRepository;
 import com.imcode.imcms.persistence.repository.MetaRepository;
+import imcode.server.Imcms;
+import imcode.server.user.UserDomainObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -18,7 +29,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,198 +43,206 @@ public class CategoryServiceTest extends WebAppSpringTestConfig {
     private CategoryService categoryService;
 
     @Autowired
-    private CategoryTypeService categoryTypeService;
+    private MetaRepository metaRepository;
 
     @Autowired
-    private MetaRepository metaRepository;
+    private DocumentService<TextDocumentDTO> textDocumentService;
+
+    @Autowired
+    private DocumentService<UrlDocumentDTO> urlDocumentService;
+
+    @Autowired
+    private DocumentService<FileDocumentDTO> fileDocumentService;
 
     @Autowired
     private CategoryRepository categoryRepository;
 
-    @Test
-    public void getAll_Expected_categoryServiceContainSavedCategory() {
-        final String testTypeName = "test_type_name";
-        final String testDescription = "Some description";
-        final String testImageUrl = "/test";
-        final CategoryTypeJPA categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
+    @Autowired
+    private CategoryDataInitializer categoryDataInitializer;
 
-        final CategoryTypeJPA savedCategoryType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+    @Autowired
+    private DocumentDtoFactory documentDtoFactory;
 
-        final CategoryJPA category = new CategoryJPA(
-                null, testTypeName, testDescription, testImageUrl, savedCategoryType
-        );
-        Category savedCategory = categoryService.save(category);
+    @BeforeEach
+    public void setUp() {
+        categoryDataInitializer.createData(4);
+    }
 
-        assertNotNull(savedCategory);
-        assertFalse(categoryService.getAll().isEmpty());
-        assertTrue(categoryService.getAll().contains(savedCategory));
+    @AfterEach
+    public void cleanUp() {
+        //categoryDataInitializer.cleanRepositories(); todo: really need this ?
     }
 
     @Test
-    public void getById_When_Exist_Expect_NotNull() {
-        final String testTypeName = "test_type_name";
-        final String testDescription = "Some description";
-        final String testImageUrl = "/test";
-        final CategoryTypeJPA categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
+    public void getAll_Expected_CorrectEntities() {
+        List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        assertEquals(categoriesDTO.size(), categoryService.getAll().size());
+    }
 
-        final CategoryTypeJPA savedCategoryType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+    @Test
+    public void getById_When_CategoryExist_Expect_CorrectEntity() {
+        List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
 
-        final CategoryJPA category = new CategoryJPA(
-                null, testTypeName, testDescription, testImageUrl, savedCategoryType
-        );
-        Category savedCategory = categoryService.save(category);
+        assertNotNull(firstCategoryDTO);
 
-        assertNotNull(savedCategory);
-
-        final Integer id = savedCategory.getId();
+        final Integer id = firstCategoryDTO.getId();
         assertTrue(categoryService.getById(id).isPresent());
     }
 
     @Test
-    public void save_When_NotExistBefore_Expect_Saved() {
-        final String testTypeName = "test_type_name" + System.currentTimeMillis();
-        final CategoryType categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
-        final CategoryTypeJPA savedType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
-
-        final String testCategoryName = "test_category_name" + System.currentTimeMillis();
-        final Category category = new CategoryJPA(testCategoryName, "dummy", "", savedType);
+    public void save_When_CategoryNotExistBefore_Expect_Saved() {
+        final List<CategoryTypeDTO> categoryTypesDTO = categoryDataInitializer.getCategoryTypesAsDTO();
+        final CategoryTypeJPA firstCategoryType = new CategoryTypeJPA(categoryTypesDTO.get(0));
+        final Category category = new CategoryJPA("name", "dummy", "", firstCategoryType);
         final Category savedCategory = categoryService.save(category);
 
-        final Optional<Category> oFound = categoryService.getById(savedCategory.getId());
-
-        assertTrue(oFound.isPresent());
-
-        final Category foundCategory = oFound.get();
-
-        assertNotNull(foundCategory);
-        assertEquals(savedCategory, foundCategory);
+        assertTrue(categoryService.getAll().contains(savedCategory));
     }
 
     @Test
-    public void update_When_CategoryExist_Expected_UpdatedCorrectEntity() {
-        final String testTypeName = "test_type_name" + System.currentTimeMillis();
-        final CategoryType categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
-        final CategoryTypeJPA savedType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+    public void save_When_CategoryNameExist_Expect_CorrectException() {
+        final List<CategoryTypeDTO> categoryTypesDTO = categoryDataInitializer.getCategoryTypesAsDTO();
+        final List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        final CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
+        final CategoryTypeJPA firstCategoryType = new CategoryTypeJPA(categoryTypesDTO.get(0));
+        final Category category = new CategoryJPA(firstCategoryDTO.getName(), "dummy", "", firstCategoryType);
 
-        final String testCategoryName = "test_category_name" + System.currentTimeMillis();
-        final Category category = new CategoryJPA(testCategoryName, "dummy", "", savedType);
-        final Category savedCategory = categoryService.save(category);
+        assertThrows(IllegalArgumentException.class, () -> categoryService.save(category));
+    }
 
-        final Optional<Category> oFound = categoryService.getById(savedCategory.getId());
-
-        assertTrue(oFound.isPresent());
-
+    @Test
+    public void update_When_GetExistCategory_Expected_UpdatedCorrectEntity() {
+        final List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        final CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
         final String otherName = "Other_category_name";
-        final Category getCategory = oFound.get();
-        getCategory.setName(otherName);
+        firstCategoryDTO.setName(otherName);
 
-        final Category savedUpdateCategory = categoryService.update(getCategory);
+        Category savedUpdateCategory = categoryService.update(firstCategoryDTO);
 
-        assertEquals(getCategory.getId(), savedCategory.getId());
+        assertTrue(categoryService.getAll().contains(savedUpdateCategory));
+
         assertNotNull(savedUpdateCategory);
-        assertNotEquals(oFound, savedUpdateCategory);
-
+        assertEquals(firstCategoryDTO.getId(), savedUpdateCategory.getId());
     }
 
     @Test
     public void update_When_CategoryNameExist_Expected_CorrectException() {
-        final String testTypeName = "test_type_name" + System.currentTimeMillis();
-        final CategoryType categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
-        final CategoryTypeJPA savedType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+        List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
 
-        final String testCategoryName1 = "test_category_name" + System.currentTimeMillis();
-        final String testCategoryName2 = "test_category_name2" + System.currentTimeMillis();
-        final Category category1 = new CategoryJPA(testCategoryName1, "dummy", "", savedType);
-        final Category category2 = new CategoryJPA(testCategoryName2, "ho-ho-ho", "../", savedType);
-        final Category savedCategory1 = categoryService.save(category1);
+        final String existName = categoriesDTO.get(1).getName();
+        firstCategoryDTO.setName(existName);
 
-        categoryService.save(category2);
-
-        final Optional<Category> oFound = categoryService.getById(savedCategory1.getId());
-
-        assertTrue(oFound.isPresent());
-
-
-        final Category getCategory = oFound.get();
-        getCategory.setName(testCategoryName2);
-
-        assertThrows(IllegalArgumentException.class, () -> categoryService.update(getCategory));
+        assertThrows(IllegalArgumentException.class, () -> categoryService.update(firstCategoryDTO));
     }
 
     @Test
     public void update_When_CategoryNameIsEmpty_Expected_CorrectException() {
-        final String testTypeName = "test_type_name" + System.currentTimeMillis();
-        final CategoryType categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
-        final CategoryTypeJPA savedType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+        List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
 
-        final String testCategoryName1 = "test_category_name" + System.currentTimeMillis();
-        final Category category1 = new CategoryJPA(testCategoryName1, "dummy", "", savedType);
-        final Category savedCategory1 = categoryService.save(category1);
+        firstCategoryDTO.setName("");
 
-        final Optional<Category> oFound = categoryService.getById(savedCategory1.getId());
-
-        assertTrue(oFound.isPresent());
-
-        final Category getCategory = oFound.get();
-        getCategory.setName("");
-
-        assertThrows(IllegalArgumentException.class, () -> categoryService.update(getCategory));
+        assertThrows(RuntimeException.class, () -> categoryService.update(firstCategoryDTO));
     }
 
     @Test
-    public void delete_When_CategoryHasNotDocument_Expect_Deleted() {
-        final String testTypeName = "test_type_name" + System.currentTimeMillis();
-        final CategoryType categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
-        final CategoryTypeJPA savedType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+    public void delete_When_DocumentHasNotCategory_Expect_Deleted() {
+        List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
+        final Integer firstCategoryId = firstCategoryDTO.getId();
 
-        final String testCategoryName = "test_category_name" + System.currentTimeMillis();
-        final Category category = new CategoryJPA(testCategoryName, "dummy", "", savedType);
-        final Category saved = categoryService.save(category);
-        final Integer savedCategoryId = saved.getId();
+        assertTrue(categoryService.getById(firstCategoryId).isPresent());
 
-        assertTrue(categoryService.getById(savedCategoryId).isPresent());
+        categoryService.delete(firstCategoryId);
 
-        categoryService.delete(savedCategoryId);
-
-        assertFalse(categoryService.getById(savedCategoryId).isPresent());
+        assertFalse(categoryService.getById(firstCategoryId).isPresent());
     }
 
     @Test
-    public void delete_When_CategoryHasDocument_Expect_CorrectException() {
-        final String testTypeName = "test_type_name" + System.currentTimeMillis();
-        final CategoryType categoryType = new CategoryTypeJPA(
-                null, testTypeName, 0, false, false
-        );
-        final CategoryTypeJPA savedType = new CategoryTypeJPA(categoryTypeService.create(categoryType));
+    public void delete_When_CategoryHasTextDocument_Expect_CorrectException() {
+        final UserDomainObject user = new UserDomainObject(1);
+        user.addRoleId(Roles.SUPER_ADMIN.getId());
+        Imcms.setUser(user);
 
-        final String testCategoryName = "test_category_name" + System.currentTimeMillis();
-        final Category category = new CategoryJPA(testCategoryName, "dummy", "", savedType);
-        final Category savedCategory = categoryService.save(category);
+        final List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        final CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
+        final TextDocumentDTO textDocument = documentDtoFactory.createEmptyTextDocument();
+        assertNotNull(textDocument);
+
+        textDocument.setCategories(new HashSet<>(Collections.singleton(firstCategoryDTO)));
+        final TextDocumentDTO savedTextDocument = textDocumentService.save(textDocument);
+
+        final Integer docId = savedTextDocument.getId();
+        final Integer categoryId = firstCategoryDTO.getId();
 
         final List<Meta> allMetas = metaRepository.findAll();
 
-        assertFalse(allMetas.isEmpty()); // at least one doc with id=1001 should exist
+        final List<Integer> metaIds = allMetas.stream().map(Meta::getId).collect(Collectors.toList());
+        assertTrue(metaIds.contains(savedTextDocument.getId()));
 
-        final Meta firstDoc = allMetas.get(0);
-        final Integer docId = firstDoc.getId();
-        final Integer categoryId = savedCategory.getId();
+        final List<Integer> categoryDocIds = categoryRepository.findCategoryDocIds(categoryId);
 
-        firstDoc.setCategories(new HashSet<>(Collections.singleton(savedCategory)));
-        metaRepository.save(firstDoc);
+        assertNotNull(categoryDocIds);
+        assertFalse(categoryDocIds.isEmpty());
+        assertTrue(categoryDocIds.contains(docId));
+
+        assertThrows(EmptyResultDataAccessException.class, () -> categoryService.delete(categoryId));
+    }
+
+    @Test
+    public void delete_When_CategoryHasUrlDocument_Expect_CorrectException() {
+        final UserDomainObject user = new UserDomainObject(1);
+        user.addRoleId(Roles.SUPER_ADMIN.getId());
+        Imcms.setUser(user);
+
+        final List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        final CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
+        final UrlDocumentDTO urlDocument = documentDtoFactory.createEmptyUrlDocument();
+        assertNotNull(urlDocument);
+
+        urlDocument.setCategories(new HashSet<>(Collections.singleton(firstCategoryDTO)));
+        final UrlDocumentDTO savedUrlDocument = urlDocumentService.save(urlDocument);
+
+        final Integer docId = savedUrlDocument.getId();
+        final Integer categoryId = firstCategoryDTO.getId();
+
+        final List<Meta> allMetas = metaRepository.findAll();
+
+        final List<Integer> metaIds = allMetas.stream().map(Meta::getId).collect(Collectors.toList());
+        assertTrue(metaIds.contains(savedUrlDocument.getId()));
+
+        final List<Integer> categoryDocIds = categoryRepository.findCategoryDocIds(categoryId);
+
+        assertNotNull(categoryDocIds);
+        assertFalse(categoryDocIds.isEmpty());
+        assertTrue(categoryDocIds.contains(docId));
+
+        assertThrows(EmptyResultDataAccessException.class, () -> categoryService.delete(categoryId));
+    }
+
+    @Test
+    public void delete_When_CategoryHasFileDocument_Expect_CorrectException() {
+        final UserDomainObject user = new UserDomainObject(1);
+        user.addRoleId(Roles.SUPER_ADMIN.getId());
+        Imcms.setUser(user);
+
+        final List<CategoryDTO> categoriesDTO = categoryDataInitializer.getCategoriesAsDTO();
+        final CategoryDTO firstCategoryDTO = categoriesDTO.get(0);
+        final FileDocumentDTO fileDocument = documentDtoFactory.createEmptyFileDocument();
+        assertNotNull(fileDocument);
+
+        fileDocument.setCategories(new HashSet<>(Collections.singleton(firstCategoryDTO)));
+        final FileDocumentDTO savedFileDocument = fileDocumentService.save(fileDocument);
+
+        final Integer docId = savedFileDocument.getId();
+        final Integer categoryId = firstCategoryDTO.getId();
+
+        final List<Meta> allMetas = metaRepository.findAll();
+
+        final List<Integer> metaIds = allMetas.stream().map(Meta::getId).collect(Collectors.toList());
+        assertTrue(metaIds.contains(savedFileDocument.getId()));
 
         final List<Integer> categoryDocIds = categoryRepository.findCategoryDocIds(categoryId);
 
