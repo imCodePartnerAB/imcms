e0d5fed48aa0fcbd0ade58db4ad966a3c797cc91
Victor <vitekpavFurious>
2019-01-11 17:50:02 +0200

    IMCMS-335 New design to super admin page: categories tab - Change unique categories columns

--- src/main/resources/sql/diff/diff-6.61-6.62-mysql.sql
+++ src/main/resources/sql/diff/diff-6.61-6.62-mysql.sql
@@ -2,7 +2,7 @@
 SET @schema_version__major_new = 6;
 SET @schema_version__minor_new = 62;
 
-ALTER TABLE categories ADD CONSTRAINT category_name_unq UNIQUE (name);
+ALTER TABLE categories ADD CONSTRAINT category_name_unq UNIQUE (name, category_type_id);
 ALTER TABLE category_types ADD CONSTRAINT category_type_name_unq UNIQUE (name);
 
 UPDATE database_version
