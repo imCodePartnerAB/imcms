064b5821708c926b960816e0fcd0204478f6a87a
Victor <vitekpavFurious>
2019-01-14 17:28:59 +0200

    IMCMS-335 New design to super admin page: categories tab - Add all methods;

--- src/main/java/com/imcode/imcms/controller/api/CategoryController.java
+++ src/main/java/com/imcode/imcms/controller/api/CategoryController.java
@@ -1,9 +1,16 @@
 package com.imcode.imcms.controller.api;
 
+import com.imcode.imcms.domain.dto.CategoryDTO;
 import com.imcode.imcms.domain.service.CategoryService;
 import com.imcode.imcms.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +32,23 @@ public class CategoryController {
         return categoryService.getAll();
     }
 
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable Integer id) {
+        return categoryService.getById(id).orElseThrow(() -> new EmptyResultDataAccessException(id));
+    }
+
+    @PostMapping
+    public Category create(@RequestBody CategoryDTO category) {
+        return categoryService.save(category);
+    }
+
+    @PutMapping
+    public Category update(@RequestBody CategoryDTO category) {
+        return categoryService.update(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        categoryService.delete(id);
+    }
 }
--- src/main/java/com/imcode/imcms/controller/api/CategoryTypeController.java
+++ src/main/java/com/imcode/imcms/controller/api/CategoryTypeController.java
@@ -1,9 +1,16 @@
 package com.imcode.imcms.controller.api;
 
+import com.imcode.imcms.domain.dto.CategoryTypeDTO;
 import com.imcode.imcms.domain.service.CategoryTypeService;
 import com.imcode.imcms.model.CategoryType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +32,23 @@ public class CategoryTypeController {
         return categoryTypeService.getAll();
     }
 
+    @GetMapping("/{id}")
+    public CategoryType getById(@PathVariable Integer id) {
+        return categoryTypeService.get(id).orElseThrow(() -> new EmptyResultDataAccessException(id));
+    }
+
+    @PostMapping
+    public CategoryType create(@RequestBody CategoryTypeDTO categoryType) {
+        return categoryTypeService.create(categoryType);
+    }
+
+    @PutMapping
+    public CategoryType update(@RequestBody CategoryTypeDTO categoryType) {
+        return categoryTypeService.update(categoryType);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Integer id) {
+        categoryTypeService.delete(id);
+    }
 }
