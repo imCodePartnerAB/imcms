7c0f98d4559ce60f1a3bbc060b21bee1d5f119c6
Victor <vitekpavFurious>
2019-01-15 14:22:25 +0200

    IMCMS-335 New design to super admin page: categories tab - Add for category type and category, buttons and titles

--- src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-categories-admin-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-categories-admin-tab-builder.js
@@ -4,11 +4,123 @@
  */
 define(
     'imcms-categories-admin-tab-builder',
-    ['imcms-super-admin-tab', 'imcms-i18n-texts'],
-    function (SuperAdminTab, texts) {
+    ['imcms-super-admin-tab', 'imcms-i18n-texts', 'imcms-components-builder', 'imcms-field-wrapper',
+        'imcms-bem-builder', 'jquery'],
+    function (SuperAdminTab, texts, components, fieldWrapper, BEM, $) {
 
         texts = texts.superAdmin.categories;
 
-        return new SuperAdminTab(texts.name, []);
+        function buildCategoryTypeButtonsContainer() {
+
+            function buildCategoryTypeCreateButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.createButtonName,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+            function buildCategoryTypeEditButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.editButtonName,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+
+            function buildCategoryTypeRemoveButton() {
+                let $button = components.buttons.positiveButton({
+                    text: 'BUtton',
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+            return new BEM({
+                block: 'type-buttons-block',
+                elements: {
+                    'title': $('<div>', {text: texts.titleCategoryType}),
+                    'create': buildCategoryTypeCreateButton(),
+                    'edit': buildCategoryTypeEditButton(),
+                    'remove': buildCategoryTypeRemoveButton()
+                }
+            }).buildBlockStructure('<div>');
+
+        }
+
+        function buildCategoryButtonsContainer() {
+
+            function buildCategoryCreateButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.createButtonName,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+            function buildCategoryEditButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.editButtonName,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+
+            function buildCategoryRemoveButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.removeButtonName,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+
+            function buildCategoryViewButton() {
+                let $button = components.buttons.positiveButton({
+                    text: texts.viewButtonName,
+                    click: function () {
+
+                    }
+                });
+
+                return components.buttons.buttonsContainer('<div>', [$button]);
+            }
+
+            return new BEM({
+                block: 'category-buttons-block',
+                elements: {
+                    'title': $('<div>', {text: texts.titleCategory}),
+                    'create': buildCategoryCreateButton(),
+                    'edit': buildCategoryEditButton(),
+                    'remove': buildCategoryRemoveButton(),
+                    'view': buildCategoryViewButton()
+                }
+            }).buildBlockStructure('<div>');
+        }
+
+        return new SuperAdminTab(texts.name, [
+            buildCategoryTypeButtonsContainer(),
+            buildCategoryButtonsContainer()
+        ]);
     }
 );
