3b75a77718e81e632c03e605c0d804107c34ea44
Victor <vitekpavFurious>
2019-01-22 11:57:39 +0200

    IMCMS-335 New design to super admin page: categories tab - Add annotation for update and create

--- src/main/java/com/imcode/imcms/controller/api/CategoryTypeController.java
+++ src/main/java/com/imcode/imcms/controller/api/CategoryTypeController.java
@@ -3,6 +3,7 @@ package com.imcode.imcms.controller.api;
 import com.imcode.imcms.domain.dto.CategoryTypeDTO;
 import com.imcode.imcms.domain.service.CategoryTypeService;
 import com.imcode.imcms.model.CategoryType;
+import com.imcode.imcms.security.CheckAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -37,12 +38,14 @@ public class CategoryTypeController {
         return categoryTypeService.get(id).orElseThrow(() -> new EmptyResultDataAccessException(id));
     }
 
+    @CheckAccess
     @PostMapping
     public CategoryType create(@RequestBody CategoryTypeDTO categoryType) {
         return categoryTypeService.create(categoryType);
     }
 
     @PutMapping
+    @CheckAccess
     public CategoryType update(@RequestBody CategoryTypeDTO categoryType) {
         return categoryTypeService.update(categoryType);
     }
