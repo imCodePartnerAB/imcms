8e4dfe2b9e9a8b34871aa24247ff6df7bd353b47
Victor <vitekpavFurious>
2019-01-22 11:56:51 +0200

    IMCMS-335 New design to super admin page: categories tab - Changed path GET end point

--- src/main/java/com/imcode/imcms/controller/api/CategoryController.java
+++ src/main/java/com/imcode/imcms/controller/api/CategoryController.java
@@ -33,8 +33,8 @@ public class CategoryController {
         return categoryService.getAll();
     }
 
-    @GetMapping("/categoryType/{id}")
-    public List<CategoryJPA> getCategoriesByCategoryTypeId(@PathVariable int id) {
+    @GetMapping("/category-type/{id}")
+    public List<CategoryJPA> getCategoriesByCategoryTypeId(@PathVariable Integer id) {
         return categoryService.getCategoriesByCategoryType(id);
     }
 
