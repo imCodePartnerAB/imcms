0d4fccf15bb828e725cdd7447b74e5b7db1ee20d
3emluk <3emluk@gmail.com>
2019-02-13 12:32:02 +0200

    IMCMS-396 - Check saves for transaction safety: - Code clean up

--- src/main/java/com/imcode/imcms/api/exception/DocumentLanguageException.java
+++ src/main/java/com/imcode/imcms/api/exception/DocumentLanguageException.java
@@ -3,22 +3,9 @@ package com.imcode.imcms.api.exception;
 public class DocumentLanguageException extends RuntimeException {
 
     public DocumentLanguageException() {
-        // TODO Auto-generated constructor stub
     }
 
     public DocumentLanguageException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
-
-    public DocumentLanguageException(Throwable cause) {
-        super(cause);
-        // TODO Auto-generated constructor stub
-    }
-
-    public DocumentLanguageException(String message, Throwable cause) {
-        super(message, cause);
-        // TODO Auto-generated constructor stub
-    }
-
 }
