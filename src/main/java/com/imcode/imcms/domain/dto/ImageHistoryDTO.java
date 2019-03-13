34cab3c8ddaa8adb4c5fb5ae79cf477a0367f93a
3emluk <3emluk@gmail.com>
2019-03-13 18:54:09 +0200

    IMCMS-407 - Text history, image history changes: - Improve class hierarchy

--- src/main/java/com/imcode/imcms/domain/dto/ImageHistoryDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/ImageHistoryDTO.java
@@ -1,12 +1,18 @@
 package com.imcode.imcms.domain.dto;
 
-import com.imcode.imcms.persistence.entity.ImageHistoryJPA;
+import com.imcode.imcms.persistence.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
 @AllArgsConstructor
-public class ImageHistoryDTO extends ImageHistoryJPA {
+public class ImageHistoryDTO extends ImageDTO {
+    private User modifiedBy;
+    private LocalDateTime modifiedAt;
 }
