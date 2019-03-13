95c26465ed2d6c4a2f56632af3659f55e492a088
3emluk <3emluk@gmail.com>
2019-03-13 18:57:46 +0200

    IMCMS-407 - Text history, image history changes: - Improve reverse mapping; - Add possibility to save plain DTO over JPA

--- src/main/java/com/imcode/imcms/domain/service/ImageHistoryService.java
+++ src/main/java/com/imcode/imcms/domain/service/ImageHistoryService.java
@@ -3,12 +3,16 @@ package com.imcode.imcms.domain.service;
 import com.imcode.imcms.domain.dto.ImageDTO;
 import com.imcode.imcms.domain.dto.ImageHistoryDTO;
 import com.imcode.imcms.persistence.entity.ImageJPA;
+import com.imcode.imcms.persistence.entity.LanguageJPA;
+import com.imcode.imcms.persistence.entity.Version;
 
 import java.util.List;
 
 public interface ImageHistoryService {
 
     void save(ImageJPA image);
+
+    void save(ImageDTO image, LanguageJPA language, Version version);
 
     List<ImageHistoryDTO> getAll(ImageDTO image);
 
--- src/main/java/com/imcode/imcms/domain/service/api/DefaultImageHistoryService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/DefaultImageHistoryService.java
@@ -5,13 +5,15 @@ import com.imcode.imcms.domain.dto.ImageHistoryDTO;
 import com.imcode.imcms.domain.service.ImageHistoryService;
 import com.imcode.imcms.domain.service.LanguageService;
 import com.imcode.imcms.domain.service.UserService;
+import com.imcode.imcms.model.Language;
 import com.imcode.imcms.persistence.entity.ImageHistoryJPA;
 import com.imcode.imcms.persistence.entity.ImageJPA;
 import com.imcode.imcms.persistence.entity.LanguageJPA;
 import com.imcode.imcms.persistence.entity.LoopEntryRefJPA;
+import com.imcode.imcms.persistence.entity.Version;
 import com.imcode.imcms.persistence.repository.ImageHistoryRepository;
+import com.imcode.imcms.util.function.TernaryFunction;
 import imcode.server.Imcms;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +21,7 @@ import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service("imageHistoryService")
@@ -28,19 +31,33 @@ public class DefaultImageHistoryService implements ImageHistoryService {
     private final ImageHistoryRepository imageHistoryRepository;
     private final LanguageService languageService;
     private final UserService userService;
-    private final ModelMapper modelMapper;
+    private final TernaryFunction<ImageDTO, Version, Language, ImageJPA> imageDTOToImageJPA;
+    private final Function<ImageHistoryJPA, ImageHistoryDTO> imageHistoryJPAToImageHistoryDTO;
 
-    public DefaultImageHistoryService(ImageHistoryRepository imageHistoryRepository, LanguageService languageService, UserService userService, ModelMapper modelMapper) {
+
+    public DefaultImageHistoryService(ImageHistoryRepository imageHistoryRepository, LanguageService languageService, UserService userService, TernaryFunction<ImageDTO, Version, Language, ImageJPA> imageDTOToImageJPA, Function<ImageHistoryJPA, ImageHistoryDTO> imageHistoryJPAToImageHistoryDTO) {
         this.imageHistoryRepository = imageHistoryRepository;
         this.languageService = languageService;
         this.userService = userService;
-        this.modelMapper = modelMapper;
+        this.imageDTOToImageJPA = imageDTOToImageJPA;
+        this.imageHistoryJPAToImageHistoryDTO = imageHistoryJPAToImageHistoryDTO;
     }
 
     @Override
     public void save(ImageJPA image) {
         final ImageHistoryJPA mappedHistory = new ImageHistoryJPA(
                 image,
+                userService.getUser(Imcms.getUser().getId()),
+                LocalDateTime.now()
+        );
+
+        imageHistoryRepository.save(mappedHistory);
+    }
+
+    @Override
+    public void save(ImageDTO image, LanguageJPA language, Version version) {
+        final ImageHistoryJPA mappedHistory = new ImageHistoryJPA(
+                imageDTOToImageJPA.apply(image, version, language),
                 userService.getUser(Imcms.getUser().getId()),
                 LocalDateTime.now()
         );
@@ -64,7 +81,7 @@ public class DefaultImageHistoryService implements ImageHistoryService {
 
         return imageHistory.stream()
                 .sorted(Comparator.comparing(ImageHistoryJPA::getModifiedAt).reversed())
-                .map(history -> modelMapper.map(history, ImageHistoryDTO.class))
+                .map(imageHistoryJPAToImageHistoryDTO)
                 .collect(Collectors.toList());
     }
 }
