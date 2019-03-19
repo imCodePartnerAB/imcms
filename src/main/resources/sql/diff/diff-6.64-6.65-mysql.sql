09aa678bd60c5cbb0476b2a176749a7426a2db2a
Victor <vitekpavFurious>
2019-03-19 11:36:16 +0200

    Revert "Issue IMCMS-335: New design to super admin page: categories tab - Revert"
    This reverts commit 2913233

---
+++ src/main/resources/sql/diff/diff-6.64-6.65-mysql.sql
@@ -0,0 +1,16 @@
+SET @schema_version__major_new = 6;
+SET @schema_version__minor_new = 65;
+
+ALTER TABLE category_types
+  DROP COLUMN is_image_archive;
+ALTER TABLE categories
+  DROP COLUMN image;
+
+ALTER TABLE category_types
+  MODIFY max_choices tinyint(1);
+ALTER TABLE category_types
+  CHANGE max_choices is_multi_select tinyint(1);
+
+UPDATE database_version
+SET major = @schema_version__major_new,
+    minor = @schema_version__minor_new;
