9d1c51866b345167345658706fd8c8ca3a56863a
Victor <vitekpavFurious>
2019-04-01 19:08:20 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Added method for allow root path; - Improve and refactor

--- src/main/java/com/imcode/imcms/domain/service/api/DefaultFileService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/DefaultFileService.java
@@ -1,10 +1,12 @@
 package com.imcode.imcms.domain.service.api;
 
 import com.imcode.imcms.domain.service.FileService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -13,6 +15,22 @@ import java.util.stream.Collectors;
 @Service
 public class DefaultFileService implements FileService {
 
+    @Value("#{'${FileAdminRootPaths}'.split(';')}")
+    private List<Path> rootPaths;
+
+
+    private boolean isAllowablePath(Path path) {
+        List<String> rootFilesName = rootPaths.stream()
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .collect(Collectors.toList());
+        for (String fileName : rootFilesName) {
+            if (path.toString().startsWith(fileName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     @Override
     public List<Path> getFiles(Path file) throws IOException {
@@ -32,26 +50,38 @@ public class DefaultFileService implements FileService {
 
     @Override
     public Path moveFile(Path src, Path target) throws IOException {
-        return Files.move(src, target);
+        if (isAllowablePath(src) && isAllowablePath(target)) {
+            return Files.move(src, target);
+        } else {
+            throw new NoSuchFileException("File not allowed move!");
+        }
     }
 
     @Override
     public Path copyFile(Path src, Path target) throws IOException {
-        return Files.copy(src, target);
+        if (isAllowablePath(src) && isAllowablePath(target)) {
+            return Files.copy(src, target);
+        } else {
+            throw new NoSuchFileException("File not allowed copy!");
+        }
     }
 
     @Override
-    public Path saveFile(Path file) {
+    public Path saveFile(Path file) { // fix
 
         return file.resolve(file.getFileName());
     }
 
     @Override
     public Path createFile(Path file) throws IOException {
-        if (Files.isDirectory(file)) {
-            return Files.createDirectory(file);
+        if (isAllowablePath(file)) {
+            if (Files.isDirectory(file)) {
+                return Files.createDirectory(file);
+            } else {
+                return Files.createFile(file);
+            }
         } else {
-            return Files.createFile(file);
+            throw new NoSuchFileException("Do not allow to create file!");
         }
     }
 }
