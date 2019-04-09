0892ee38ac5ded94f2d03cb54c6349997dfa1d6a
Victor <vitekpavFurious>
2019-04-09 11:36:45 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Fixing download file; - Refactor

--- src/main/java/com/imcode/imcms/controller/api/FileController.java
+++ src/main/java/com/imcode/imcms/controller/api/FileController.java
@@ -1,12 +1,14 @@
 package com.imcode.imcms.controller.api;
 
 import com.imcode.imcms.domain.service.api.DefaultFileService;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -36,21 +37,23 @@ public class FileController {
         this.defaultFileService = defaultFileService;
     }
 
-    private String getFileName(String pathName, String removeStart) {
-        Matcher matcher = FILE_NAME_PATTERN.matcher(pathName);
-        String path = null;
+    private String getFileName(String path, String endPointName) {
+        Matcher matcher = FILE_NAME_PATTERN.matcher(path);
+        String extractedPath = null;
         if (matcher.matches()) {
-            if (removeStart.isEmpty()) {
-                path = matcher.group("path");
+            if (endPointName.isEmpty()) {
+                extractedPath = matcher.group("path");
             } else {
-                path = matcher.group("path").substring(removeStart.length() - 1);
+                extractedPath = matcher.group("path").substring(endPointName.length() - 1);
             }
         }
-        return path;
+        return extractedPath;
     }
 
     @PostMapping("/upload/**")
-    public String uploadFile(@RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
+    public String uploadFile(HttpServletRequest request,
+                             @RequestParam MultipartFile file) throws IOException {
+
         final String destination = getFileName(request.getRequestURI(), "/upload/");
         final Path resolvePath = Paths.get(destination).resolve(file.getOriginalFilename());
         return defaultFileService.saveFile(resolvePath, file.getBytes(), CREATE_NEW).toString();
@@ -58,17 +61,15 @@ public class FileController {
 
     @GetMapping("/**")
     public List<Path> getFiles(HttpServletRequest request) throws IOException {
-        final String file = getFileName(request.getRequestURI(), "");
-        return defaultFileService.getFiles(Paths.get(file));
+        final String fileURI = getFileName(request.getRequestURI(), "");
+        return defaultFileService.getFiles(Paths.get(fileURI));
     }
 
     @PutMapping("/**")
-    public String saveFile(HttpServletRequest request) throws IOException {
-        final String file = getFileName(request.getRequestURI(), "");
-        final Path path = defaultFileService.getFile(Paths.get(file));
-        final byte[] content = Files.readAllBytes(path);
-
-        return defaultFileService.saveFile(path, content, null).toString();
+    public String saveFile(HttpServletRequest request, @RequestBody byte[] newContent) throws IOException {
+        final String fileURI = getFileName(request.getRequestURI(), "");
+        final Path path = defaultFileService.getFile(Paths.get(fileURI));
+        return defaultFileService.saveFile(path, newContent, null).toString();
     }
 
     @PostMapping("/**")
@@ -78,12 +79,12 @@ public class FileController {
     }
 
     @GetMapping("/file/**")
-    public ResponseEntity<byte[]> downloadFile(HttpServletRequest request) throws IOException {
-        final String file = getFileName(request.getRequestURI(), "/file/");
-        final Path path = defaultFileService.getFile(Paths.get(file));
-        final byte[] content = Files.readAllBytes(path);
-
-        return new ResponseEntity<>(content, HttpStatus.OK);
+    public ResponseEntity<Resource> downloadFile(HttpServletRequest request) throws IOException {
+        final String fileURI = getFileName(request.getRequestURI(), "/file/");
+        final Path path = defaultFileService.getFile(Paths.get(fileURI));
+        final Resource resource = new UrlResource(path.toUri());
+        return ResponseEntity.ok()
+                .body(resource);
     }
 
     @DeleteMapping("/**")
@@ -95,12 +96,21 @@ public class FileController {
     @PutMapping("/move/**")
     public String moveFile(HttpServletRequest request, @RequestParam Path target) throws IOException {
         final String file = getFileName(request.getRequestURI(), "/move/");
-        return defaultFileService.moveFile(Paths.get(file), target).toString();
+        final Path src = Paths.get(file);
+        return defaultFileService.moveFile(src, target).toString();
+    }
+
+    @PutMapping("/rename/**")
+    public String renameFile(HttpServletRequest request, @RequestParam String name) throws IOException {
+        final String file = getFileName(request.getRequestURI(), "/rename/");
+        final Path src = Paths.get(file);
+        return defaultFileService.moveFile(src, src.getParent().resolve(name)).toString();
     }
 
     @PostMapping("/copy/**")
     public String copyFile(HttpServletRequest request, @RequestParam Path target) throws IOException {
         final String file = getFileName(request.getRequestURI(), "/copy/");
-        return defaultFileService.copyFile(Paths.get(file), target).toString();
+        final Path src = Paths.get(file);
+        return defaultFileService.copyFile(src, target).toString();
     }
 }
