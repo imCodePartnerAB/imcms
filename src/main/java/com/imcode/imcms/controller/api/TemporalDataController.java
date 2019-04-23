64082e3de96726c63e1ad6e78362ee47af747ec3
3emluk <3emluk@gmail.com>
2019-04-23 18:55:14 +0300

    IMCMS-422 - Make imcms client projects work with context path: - Change endpoint method

--- src/main/java/com/imcode/imcms/controller/api/TemporalDataController.java
+++ src/main/java/com/imcode/imcms/controller/api/TemporalDataController.java
@@ -2,7 +2,6 @@ package com.imcode.imcms.controller.api;
 
 import com.imcode.imcms.domain.service.TemporalDataService;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +15,7 @@ public class TemporalDataController {
         this.temporalDataService = temporalDataService;
     }
 
-    @GetMapping("/document-index")
+    @DeleteMapping("/document-index")
     public void rebuildDocumentIndex() {
         temporalDataService.rebuildDocumentIndex();
     }
