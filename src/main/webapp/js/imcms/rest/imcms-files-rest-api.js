83f82b10cfa5f6caa658b9aedca9aeb56a4b24ec
Victor <vitekpavFurious>
2019-04-26 12:56:20 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Added rest api get files by file

--- src/main/webapp/js/imcms/rest/imcms-files-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-files-rest-api.js
@@ -5,6 +5,8 @@ define("imcms-files-rest-api", ["imcms-rest-api"], function (rest) {
 
     api.download = file => rest.ajax.call({url: `${url}/file/${file}`, type: 'GET', json: false});
 
+    api.get = file => rest.ajax.call({url: `${url}/${file}`, type: 'GET', json: false});
+
     api.upload = file => rest.ajax.call({url: `${url}/upload/`, type: 'POST', json: false}, file);
 
     api.create = isDirectory => rest.ajax.call({url: `${url}/`, type: 'POST', json: false}, isDirectory);
