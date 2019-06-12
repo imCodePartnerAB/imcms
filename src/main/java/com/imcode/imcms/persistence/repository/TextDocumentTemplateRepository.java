53b86536bb7f002906be3b4e2455a4214a7992e7
vladhuk <vladhookovskiy@gmail.com>
2019-06-12 18:54:19 +0300

    Issue IMCMS-331: New design to super admin page: templates tab - Add query;

--- src/main/java/com/imcode/imcms/persistence/repository/TextDocumentTemplateRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/TextDocumentTemplateRepository.java
@@ -2,11 +2,17 @@ package com.imcode.imcms.persistence.repository;
 
 import com.imcode.imcms.persistence.entity.TextDocumentTemplateJPA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TextDocumentTemplateRepository extends JpaRepository<TextDocumentTemplateJPA, Integer> {
 
     void deleteByDocId(Integer docId);
+
+    @Query(value = "SELECT meta_id FROM text_docs WHERE template_name = ?1", nativeQuery = true)
+    List<Integer> findDocIdByTemplateName(String templateName);
 
 }
