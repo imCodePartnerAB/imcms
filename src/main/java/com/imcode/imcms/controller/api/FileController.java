0c78d4639fafad700f06fbe142d88ddd6e616ded
vladhuk <vladhookovskiy@gmail.com>
2019-06-19 18:28:51 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Fix move file method;

--- src/main/java/com/imcode/imcms/controller/api/FileController.java
+++ src/main/java/com/imcode/imcms/controller/api/FileController.java
@@ -4,14 +4,7 @@ import com.imcode.imcms.api.SourceFile;
 import com.imcode.imcms.domain.service.api.DefaultFileService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.activation.MimetypesFileTypeMap;
@@ -134,10 +127,10 @@ public class FileController {
     }
 
     @PutMapping("/move/**")
-    public String moveFile(@RequestBody Properties pathParam) throws IOException {
+    public SourceFile moveFile(@RequestBody Properties pathParam) throws IOException {
         final Path src = Paths.get(pathParam.getProperty("src"));
         final Path target = Paths.get(pathParam.getProperty("target"));
-        return defaultFileService.moveFile(Collections.singletonList(src), target).toString();
+        return defaultFileService.moveFile(src, target);
     }
 
     @PutMapping("/rename/**")
