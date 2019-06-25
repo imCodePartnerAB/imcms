fecdac5491a4f7fbdcede0c1c18ff37c8c6e1571
Victor <vitekpavFurious>
2019-06-25 12:56:40 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Remove do not use const;

--- src/main/java/com/imcode/imcms/controller/api/FileController.java
+++ src/main/java/com/imcode/imcms/controller/api/FileController.java
@@ -4,7 +4,13 @@ import com.imcode.imcms.api.SourceFile;
 import com.imcode.imcms.domain.service.api.DefaultFileService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -30,8 +36,6 @@ import static java.util.regex.Pattern.compile;
 public class FileController {
 
     private static final Pattern FILE_NAME_PATTERN = compile("(.*?\\/files\\/)(?<path>.*)");
-
-    private static final String APPLICATION_OCTET_STREAM_VALUE = "application/octet-stream";
 
     private final DefaultFileService defaultFileService;
 
