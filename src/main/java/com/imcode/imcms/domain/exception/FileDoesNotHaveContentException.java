664d918ceb1f7c360a2f49b23edfade35b8fdea2
Victor <vitekpavFurious>
2019-07-18 14:23:40 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Create exception;

---
+++ src/main/java/com/imcode/imcms/domain/exception/FileDoesNotHaveContentException.java
@@ -0,0 +1,8 @@
+package com.imcode.imcms.domain.exception;
+
+public class FileDoesNotHaveContentException extends RuntimeException {
+
+    public FileDoesNotHaveContentException(String message) {
+        super(message);
+    }
+}
