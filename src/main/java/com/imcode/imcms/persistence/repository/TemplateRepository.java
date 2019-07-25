b51bb9200ebb6420249a56e569f0b099808ee5dd
Victor <vitekpavFurious>
2019-07-25 15:54:12 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Create new query;

--- src/main/java/com/imcode/imcms/persistence/repository/TemplateRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/TemplateRepository.java
@@ -2,8 +2,15 @@ package com.imcode.imcms.persistence.repository;
 
 import com.imcode.imcms.persistence.entity.TemplateJPA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TemplateRepository extends JpaRepository<TemplateJPA, String> {
+
+    @Modifying
+    @Query(value = "UPDATE template SET template_name = ?1 WHERE template_name = ?2", nativeQuery = true)
+    void updateTemplateName(String newTemplateName, String oldTemplateName);
+
 }
--- src/main/java/com/imcode/imcms/persistence/repository/TextDocumentTemplateRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/TextDocumentTemplateRepository.java
@@ -15,4 +15,7 @@ public interface TextDocumentTemplateRepository extends JpaRepository<TextDocume
     @Query(value = "SELECT meta_id FROM text_docs WHERE template_name = ?1", nativeQuery = true)
     List<Integer> findDocIdByTemplateName(String templateName);
 
+    @Query(value = "SELECT * FROM text_docs WHERE template_name = ?1", nativeQuery = true)
+    List<TextDocumentTemplateJPA> findTextDocumentTemplateByTemplateName(String templateName);
+
 }
