11325fcde2db6fbacd3c5c91d5a871fb509dbf9d
Victor <vitekpavFurious>
2019-07-26 12:02:41 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Add transaction annotation

--- src/main/java/com/imcode/imcms/persistence/repository/TemplateRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/TemplateRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TemplateRepository extends JpaRepository<TemplateJPA, String> {
 
+    @Transactional
     @Modifying
     @Query(value = "UPDATE template SET template_name = ?1 WHERE template_name = ?2", nativeQuery = true)
     void updateTemplateName(String newTemplateName, String oldTemplateName);
