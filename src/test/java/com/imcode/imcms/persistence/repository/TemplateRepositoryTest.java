8b6afc9a2ac02c27198255a6b39f87bc9f5bf83d
Victor <vitekpavFurious>
2019-07-26 12:01:35 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Add transaction; - Create test;

--- src/test/java/com/imcode/imcms/persistence/repository/TemplateRepositoryTest.java
+++ src/test/java/com/imcode/imcms/persistence/repository/TemplateRepositoryTest.java
@@ -5,13 +5,10 @@ import com.imcode.imcms.components.datainitializer.TemplateDataInitializer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-@Transactional
 public class TemplateRepositoryTest extends WebAppSpringTestConfig {
 
     @Autowired
@@ -26,7 +23,6 @@ public class TemplateRepositoryTest extends WebAppSpringTestConfig {
     }
 
     @Test
-    @Rollback(value = false)
     public void updateTemplateName_When_TemplateNameExist_Expected_UpdateName() {
         final String templateName = "test";
         templateDataInitializer.createData(templateName);
@@ -34,13 +30,19 @@ public class TemplateRepositoryTest extends WebAppSpringTestConfig {
 
         templateRepository.updateTemplateName(newName, templateName);
 
-//        assertNotEquals(templateName, template.getName());
         assertEquals(newName, templateRepository.findOne(newName).getName());
         assertNull(templateRepository.findOne(templateName));
     }
 
     @Test
     public void updateTemplateName_When_TemplateNameNotExist_Expected_Empty() {
+        final String templateName = "test";
+        final String newName = "newTest";
+        final String fakeName = "fakeTest";
+        templateDataInitializer.createData(templateName);
 
+        templateRepository.updateTemplateName(newName, fakeName);
+
+        assertNull(templateRepository.findOne(fakeName));
     }
 }
