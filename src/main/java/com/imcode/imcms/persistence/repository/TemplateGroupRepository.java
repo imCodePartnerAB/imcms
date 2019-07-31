68a652e24e009087d1818bbdf4cba96235a13e54
Victor <vitekpavFurious>
2019-07-31 12:18:17 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Add annotation

--- src/main/java/com/imcode/imcms/persistence/repository/TemplateGroupRepository.java
+++ src/main/java/com/imcode/imcms/persistence/repository/TemplateGroupRepository.java
@@ -2,7 +2,9 @@ package com.imcode.imcms.persistence.repository;
 
 import com.imcode.imcms.persistence.entity.TemplateGroupJPA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemplateGroupRepository extends JpaRepository<TemplateGroupJPA, Integer> {
 
     TemplateGroupJPA findByName(String name);
