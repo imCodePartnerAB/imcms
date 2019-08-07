1566d58d4aa3c8c52050a16d341ea2259d6675a3
Victor <vitekpavFurious>
2019-08-07 16:57:39 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Add sorted by type;

--- src/main/java/com/imcode/imcms/domain/service/api/DefaultFileService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/DefaultFileService.java
@@ -31,6 +31,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -131,6 +132,7 @@ public class DefaultFileService implements FileService {
         }
         return paths.stream()
                 .map(this::toSourceFile)
+                .sorted(Comparator.comparing(SourceFile::getFileType))
                 .collect(Collectors.toList());
     }
 
