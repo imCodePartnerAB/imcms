6ca61d31d9219c8e4171694f65b6692d5c8b13bc
Victor <vitekpavFurious>
2019-08-07 11:00:12 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Change query on delete

--- src/main/resources/sql/diff/diff-6.72-6.73-mysql.sql
+++ src/main/resources/sql/diff/diff-6.72-6.73-mysql.sql
@@ -1,23 +1,8 @@
 SET @schema_version__major_new = 6;
 SET @schema_version__minor_new = 73;
 
-ALTER TABLE imcms_template_group_crossref
-  DROP FOREIGN KEY imcms_template_group_crossref_group_id_fk_imcms_template_group;
-ALTER TABLE imcms_template_group_crossref
-  ADD CONSTRAINT imcms_template_group_crossref_group_id_fk_imcms_template_group
-FOREIGN KEY (group_id) REFERENCES imcms_template_group (group_id)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE;
-
-
-ALTER TABLE imcms_template_group_crossref
-  DROP FOREIGN KEY imcms_template_group_crossref_template_name_fk_template;
-ALTER TABLE imcms_template_group_crossref
-  ADD CONSTRAINT imcms_template_group_crossref_template_name_fk_template
-FOREIGN KEY (template_name) REFERENCES template (template_name)
-  ON DELETE CASCADE
-  ON UPDATE CASCADE;
-
+DROP table if exists imcms_template_group_crossref;
+DROP table if exists imcms_template_group;
 
 UPDATE database_version
 SET major = @schema_version__major_new,
