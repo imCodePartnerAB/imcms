1f37264259d3a94f2511d4930f7b45ae5758601b
Victor <vitekpavFurious>
2019-08-08 16:44:46 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Create new exception;

---
+++ src/main/java/com/imcode/imcms/api/exception/AloneTemplateInDbException.java
@@ -0,0 +1,8 @@
+package com.imcode.imcms.api.exception;
+
+public class AloneTemplateInDbException extends RuntimeException {
+
+    public AloneTemplateInDbException(String message) {
+        super(message);
+    }
+}
