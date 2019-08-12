4596211af608fb03616fc52fc57f16eb924db352
Victor <vitekpavFurious>
2019-08-12 10:10:34 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Rename;

---
+++ src/main/java/com/imcode/imcms/api/exception/AloneTemplateException.java
@@ -0,0 +1,8 @@
+package com.imcode.imcms.api.exception;
+
+public class AloneTemplateException extends RuntimeException {
+
+    public AloneTemplateException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/imcode/imcms/api/exception/AloneTemplateInDbException.java
+++
@@ -1,8 +0,0 @@
-package com.imcode.imcms.api.exception;
-
-public class AloneTemplateInDbException extends RuntimeException {
-
-    public AloneTemplateInDbException(String message) {
-        super(message);
-    }
-}
