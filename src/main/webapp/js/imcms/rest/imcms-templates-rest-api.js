f4cc6748ab0624960a676c4a2849468e520e7aec
vladhuk <vladhookovskiy@gmail.com>
2019-08-12 11:48:13 +0300

    Issue IMCMS-332: New design to super admin page: files tab - Add new requests;

--- src/main/webapp/js/imcms/rest/imcms-templates-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-templates-rest-api.js
@@ -1,3 +1,9 @@
 define("imcms-templates-rest-api", ["imcms-rest-api"], function (rest) {
-    return new rest.API("/templates");
+
+    const url = '/templates';
+    const api = new rest.API(url);
+
+    api.delete = id => rest.ajax.call({url: `${url}/${id}`, type: 'DELETE', json: false});
+
+    return api;
 });
