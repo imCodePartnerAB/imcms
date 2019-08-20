6751f3c4ae9b56d426312b123ba70fb76cd1dddb
Victor <vitekpavFurious>
2019-08-20 18:36:45 +0300

    Issue IMCMS-453: New tab in superadmin - Remove

--- src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-menu-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-menu-tab-builder.js
@@ -1,14 +1,1 @@
-/**
- * @author Pavlenko Victor from Ubrainians for imCode
- * 20.08.19
- */
-define(
-    'imcms-menu-tab-builder',
-    ['imcms-super-admin-tab', 'imcms-i18n-texts'],
-    function (SuperAdminTab, texts) {
 
-        texts = texts.superAdmin.menuTab;
-
-        return new SuperAdminTab(texts.name, []);
-    }
-);
