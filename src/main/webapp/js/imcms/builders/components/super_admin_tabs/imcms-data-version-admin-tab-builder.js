4c5cb11868d23c65f8856b03f3f56f197fc9e10b
Victor <vitekpavFurious>
2019-08-20 10:37:08 +0300

    Issue IMCMS-453: New tab in superadmin - Create build data version

--- src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-data-version-admin-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/super_admin_tabs/imcms-data-version-admin-tab-builder.js
@@ -1,14 +1,54 @@
 /**
- * @author Serhii Maksymchuk from Ubrainians for imCode
- * 23.06.18
+ * @author Pavlenko Victor from Ubrainians for imCode
+ * 19.08.19
  */
 define(
     'imcms-data-version-admin-tab-builder',
-    ['imcms-super-admin-tab', 'imcms-i18n-texts'],
-    function (SuperAdminTab, texts) {
+    ['imcms-super-admin-tab', 'imcms-i18n-texts', 'imcms-version-data-rest-api',
+        'imcms-modal-window-builder', 'imcms-bem-builder', 'jquery'],
+    function (SuperAdminTab, texts, versionDataApi, modal, BEM, $) {
 
         texts = texts.superAdmin.dataVersion;
 
-        return new SuperAdminTab(texts.name, []);
+
+        function getDataVersion() {
+            let $versionDataContainer = $('<div>', {
+                'class': 'versions-container'
+            });
+            versionDataApi.read().done(dataVersion => {
+                    $versionDataContainer.append(buildVersionDataContainer(dataVersion))
+                }
+            ).fail(() => modal.buildErrorWindow('Error!'));
+
+            return $versionDataContainer;
+        }
+
+        function buildVersionDataContainer(dataVersion) {
+            return new BEM({
+                block: 'data-version',
+                elements: {
+                    'imcms-version': $('<div>', {
+                        text: dataVersion.imcmsVersion
+                    }),
+                    'java-version': $('<div>', {
+                        text: dataVersion.javaVersion
+                    }),
+                    'server-info': $('<div>', {
+                        text: dataVersion.serverInfo
+                    }),
+                    'db-version': $('<div>', {
+                        text: dataVersion.dbVersion
+                    }),
+                    'db-name-version': $('<div>', {
+                        text: dataVersion.dbNameVersion
+                    }),
+                }
+            }).buildBlockStructure('<div>');
+        }
+
+
+        return new SuperAdminTab(texts.name, [
+            getDataVersion()
+        ]);
     }
 );
