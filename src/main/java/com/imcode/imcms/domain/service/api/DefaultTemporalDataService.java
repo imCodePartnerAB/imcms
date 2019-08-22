7925c7dc7d3831bfe7d477fa1fd907805c008089
Victor <vitekpavFurious>
2019-08-22 13:26:55 +0300

    Issue IMCMS-452: Status in Cache tab - Add return value and logger; - Add regexs and patterns; - Add method get date;

--- src/main/java/com/imcode/imcms/domain/service/api/DefaultTemporalDataService.java
+++ src/main/java/com/imcode/imcms/domain/service/api/DefaultTemporalDataService.java
@@ -3,18 +3,47 @@ package com.imcode.imcms.domain.service.api;
 import com.imcode.imcms.domain.component.PublicDocumentsCache;
 import com.imcode.imcms.domain.service.TemporalDataService;
 import imcode.server.document.index.ResolvingQueryIndex;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import static imcode.server.ImcmsConstants.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static imcode.server.ImcmsConstants.OTHER_CACHE_NAME;
+import static imcode.server.ImcmsConstants.STATIC_CACHE_NAME;
 import static net.sf.ehcache.CacheManager.getCacheManager;
 
 //TODO cover by tests if possible
 @Service
 public class DefaultTemporalDataService implements TemporalDataService {
 
-    private final PublicDocumentsCache publicDocumentsCache;
+    private static final String REINDEX_DATE_REGEX = "(Last-date-reindex:)\\s+(([\\d|-]*)\\s+([\\d|:]*))";
+    private static final String PUBLIC_DOC_CACHE_DATE_REGEX = "(Public-document-invalidate-cache-date:)\\s+(([\\d|-]*)\\s+([\\d|:]*))";
+    private static final String STATIC_CONTENT_CACHE_DATE_REGEX = "(Static-content-invalidate-cache-date:)\\s+(([\\d|-]*)\\s+([\\d|:]*))";
+    private static final String CONTENT_CACHE_DATE_REGEX = "(Content-invalidate-cache-date:)\\s+(([\\d|-]*)\\s+([\\d|:]*))";
 
+    private final static Logger logger = Logger.getLogger(DefaultTemporalDataService.class);
+
+    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private final PublicDocumentsCache publicDocumentsCache;
     private final ResolvingQueryIndex resolvingQueryIndex;
+
+    private final Pattern patternReindexDate = Pattern.compile(REINDEX_DATE_REGEX);
+    private final Pattern patternDocCacheDate = Pattern.compile(PUBLIC_DOC_CACHE_DATE_REGEX);
+    private final Pattern patternStaticContentCacheDate = Pattern.compile(STATIC_CONTENT_CACHE_DATE_REGEX);
+    private final Pattern patternContentCacheDate = Pattern.compile(CONTENT_CACHE_DATE_REGEX);
+
+    @Value("/WEB-INF/logs/all.log")
+    private Path path;
 
     public DefaultTemporalDataService(PublicDocumentsCache publicDocumentsCache, ResolvingQueryIndex resolvingQueryIndex) {
         this.publicDocumentsCache = publicDocumentsCache;
@@ -22,22 +51,43 @@ public class DefaultTemporalDataService implements TemporalDataService {
     }
 
     @Override
-    public void invalidatePublicDocumentCache() {
+    public String invalidatePublicDocumentCache() throws IOException {
         publicDocumentsCache.invalidateCache();
+        logger.info("Public-document-invalidate-cache-date: " + formatter.format(new Date()));
+        return getLastDateModification(patternDocCacheDate);
     }
 
     @Override
-    public void invalidateStaticContentCache() {
+    public String invalidateStaticContentCache() throws IOException {
         getCacheManager(null).getEhcache(STATIC_CACHE_NAME).removeAll();
+        logger.info("Static-content-invalidate-cache-date: " + formatter.format(new Date()));
+        return getLastDateModification(patternStaticContentCacheDate);
     }
 
     @Override
-    public void invalidateOtherContentCache() {
+    public String invalidateOtherContentCache() throws IOException {
         getCacheManager(null).getEhcache(OTHER_CACHE_NAME).removeAll();
+        logger.info("Content-invalidate-cache-date: " + formatter.format(new Date()));
+        return getLastDateModification(patternContentCacheDate);
     }
 
     @Override
-    public void rebuildDocumentIndex() {
+    public String rebuildDocumentIndex() throws IOException {
         resolvingQueryIndex.rebuild();
+        logger.info("Last-date-reindex: " + formatter.format(new Date()));
+        return getLastDateModification(patternReindexDate);
+    }
+
+    private String getLastDateModification(Pattern pattern) throws IOException {
+        final List<String> validLines = new ArrayList<>();
+        final List<String> logLines = Files.readAllLines(path);
+        for (String line : logLines) {
+            Matcher matcher = pattern.matcher(line);
+            if (matcher.find()) {
+                validLines.add(matcher.group(2));
+            }
+        }
+        final long amountValidLines = validLines.size();
+        return validLines.stream().skip(amountValidLines - 1).findFirst().get();
     }
 }
