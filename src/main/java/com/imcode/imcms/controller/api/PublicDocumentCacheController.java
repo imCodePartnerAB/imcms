b9542dc08a493a75c94115a74a41585f34215233
Victor <vitekpavFurious>
2019-08-28 10:51:27 +0300

    Issue IMCMS-459: Cache handling when not using Versions - Create controller and endpoint

---
+++ src/main/java/com/imcode/imcms/controller/api/PublicDocumentCacheController.java
@@ -0,0 +1,24 @@
+package com.imcode.imcms.controller.api;
+
+import com.imcode.imcms.domain.component.DocumentsCache;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/document-cache")
+public class PublicDocumentCacheController {
+
+    private final DocumentsCache documentsCache;
+
+    public PublicDocumentCacheController(DocumentsCache documentsCache) {
+        this.documentsCache = documentsCache;
+    }
+
+
+    @GetMapping
+    public void invalidateCache(@RequestParam Integer docId, @RequestParam(required = false) String alias) {
+        documentsCache.invalidateDoc(docId, alias);
+    }
+}
