dd6795b8bb5dc6ba2e24030cad0413b1706a4be6
Victor <vitekpavFurious>
2019-08-28 15:32:48 +0300

    Issue IMCMS-459: Cache handling when not using Versions - Add load animation; - Refactor

--- src/main/webapp/js/imcms/builders/components/page_info_tabs/imcms-cache-tab-builder.js
+++ src/main/webapp/js/imcms/builders/components/page_info_tabs/imcms-cache-tab-builder.js
@@ -1,28 +1,73 @@
 define('imcms-cache-tab-builder',
-    ["imcms-bem-builder", "imcms-components-builder", "imcms-i18n-texts", "jquery", "imcms-page-info-tab", 'imcms'],
-    function (BEM, components, texts, $, PageInfoTab, imcms) {
+    ["imcms-bem-builder", "imcms-components-builder", "imcms-i18n-texts", "jquery",
+        "imcms-page-info-tab", 'imcms', 'imcms-cache-document-rest-api', 'imcms-modal-window-builder'],
+    function (BEM, components, texts, $, PageInfoTab, imcms, documentCacheRest, modal) {
 
         texts = texts.pageInfo.cache;
 
+        function buildActions($title, $button, $loading, $success) {
+            return new BEM({
+                block: 'load-actions',
+                elements: {
+                    'title': $title,
+                    'button': $button,
+                    'loading': $loading,
+                    'success': $success
+                },
+            }).buildBlockStructure('<div>');
+        }
+
+        function buildLoadingAnimation() {
+            return $('<div>', {
+                class: 'loading-animation',
+                style: 'display: none;',
+            });
+        }
+
+        function buildSuccessAnimation() {
+            return $('<div>', {
+                class: 'success-animation',
+                style: 'display: none;',
+            });
+        }
+
+        function animateRequest(request, $loading, $success) {
+            $success.hide();
+            $loading.show();
+            request.done(() => {
+                $loading.hide();
+                $success.show();
+            }).fail(() => modal.buildErrorWindow('fed'));
+        }
+
         function buildDeleteDocsBlock() {
-            const docId = imcms.document.id;
-            const alias = imcms.document.alias;
+
+            function buildCacheActions() {
+                const $loading = buildLoadingAnimation();
+                const $success = buildSuccessAnimation();
+
+                let dataParam = {
+                    docId: imcms.document.id,
+                    alias: imcms.document.alias
+                };
+                const $cacheTitle = $('<div>', {
+                    text: texts.invalidateTitle
+                });
+
+                const $button = components.buttons.positiveButton({
+                    text: texts.invalidateButton,
+                    click: () => animateRequest(documentCacheRest.invalidate(dataParam), $loading, $success)
+                });
+
+                return buildActions($cacheTitle, $button, $loading, $success)
+            }
+
             return new BEM({
                 block: 'cache-remove-document-row',
                 elements: {
-                    'invalidate-title': $('<div>', {
-                        text: texts.invalidateTitle
-                    }),
-                    'invalidate': components.buttons.positiveButton({
-                        text: texts.invalidateButton,
-                        click: invalidateCache(docId, alias)
-                    })
+                    'build-data': buildCacheActions()
                 }
             }).buildBlockStructure('<div>')
-        }
-
-        function invalidateCache(currentDocId, alias) {
-
         }
 
         const CacheTab = function (name) {
