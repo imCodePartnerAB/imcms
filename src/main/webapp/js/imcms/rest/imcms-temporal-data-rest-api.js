53a9123b661d810382e462fd1c72c313ed38ae83
vladhuk <vladhookovskiy@gmail.com>
2019-08-29 18:52:04 +0300

    Issue IMCMS-452: Status in Cache tab - Add request;

--- src/main/webapp/js/imcms/rest/imcms-temporal-data-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-temporal-data-rest-api.js
@@ -7,6 +7,8 @@ define('imcms-temporal-data-rest-api', ['imcms-rest-api'], function (rest) {
     api.deleteStaticContentCache = () => rest.ajax.call({url: `${url}/static-content`, type: 'DELETE', json: true});
     api.deleteOtherContentCache = () => rest.ajax.call({url: `${url}/other-content`, type: 'DELETE', json: true});
 
+    api.getAmountOfIndexedDocuments = () => rest.ajax.call({url: `${url}/indexed-documents-amount`, type: 'GET', json: false});
+
     api.getDateDocumentIndex = () => rest.ajax.call({url: `${url}/date-reindex`, type: 'GET', json: false});
     api.getDateRemoveDocumentCache = () => rest.ajax.call({url: `${url}/date-public-document`, type: 'GET', json: false});
     api.getDateRemoveStaticContentCache = () => rest.ajax.call({url: `${url}/date-static-content`, type: 'GET', json: false});
