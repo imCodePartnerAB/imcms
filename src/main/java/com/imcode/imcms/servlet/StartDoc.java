7a9774ab694b7cf9445adf63beff1f77e972533c
vladhuk <vladhookovskiy@gmail.com>
2019-09-03 14:55:40 +0300

    Issue IMCMS-426: StartDoc changes - Change forward to redirect;

--- src/main/java/com/imcode/imcms/servlet/StartDoc.java
+++ src/main/java/com/imcode/imcms/servlet/StartDoc.java
@@ -1,19 +1,16 @@
 package com.imcode.imcms.servlet;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static imcode.server.ImcmsConstants.API_VIEW_DOC_PATH;
-
 public class StartDoc extends HttpServlet {
 
     private static final long serialVersionUID = -7352117429674871492L;
 
-    public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // just forward to new controller
-        request.getRequestDispatcher(API_VIEW_DOC_PATH).forward(request, response);
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // just redirect to new controller
+        response.sendRedirect("/");
     }
 }
