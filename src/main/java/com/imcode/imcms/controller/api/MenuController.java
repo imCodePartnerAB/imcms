a2ed92d4f961408cdf61d0755fd2f1ce4eb71ea6
Victor <vitekpavFurious>
2019-10-17 14:15:50 +0300

    Issue IMCMS-468: Menu tags only include the first level in the template - Add request param and set default value; - Inject in API

--- src/main/java/com/imcode/imcms/controller/api/MenuController.java
+++ src/main/java/com/imcode/imcms/controller/api/MenuController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,8 +29,9 @@ public class MenuController {
     }
 
     @GetMapping
-    public List<MenuItemDTO> getMenuItems(@ModelAttribute MenuDTO menu) {
-        return menuService.getMenuItems(menu.getDocId(), menu.getMenuIndex(), Imcms.getUser().getLanguage(), true);
+    public List<MenuItemDTO> getMenuItems(@ModelAttribute MenuDTO menu,
+                                          @RequestParam(value = "nested", defaultValue = "false") boolean disableNested) {
+        return menuService.getMenuItems(menu.getDocId(), menu.getMenuIndex(), Imcms.getUser().getLanguage(), !disableNested);
     }
 
     @PostMapping
