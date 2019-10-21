146d2ff83a0fe4be1b3998a6a0e19e45f128281c
Victor <vitekpavFurious>
2019-10-21 10:53:00 +0300

    Issue IMCMS-484: Improve the menu-administration - Change naming;

--- src/main/java/com/imcode/imcms/domain/service/MenuService.java
+++ src/main/java/com/imcode/imcms/domain/service/MenuService.java
@@ -9,18 +9,18 @@ import java.util.List;
 
 public interface MenuService extends VersionedContentService, DeleterByDocumentId {
 
-    List<MenuItemDTO> getMenuItems(int docId, int menuIndex, String language, boolean disableNested);
+    List<MenuItemDTO> getMenuItems(int docId, int menuIndex, String language, boolean nested);
 
     /**
-     * @param disableNested - false/true show nested in menu.
-     * disableNested (true) - Will return all the items in the menu, no matter what level. All sub levels are empty.
+     * @param nested - false/true show nested in menu.
+     * nested (false) - Will return all the items in the menu, no matter what level. All sub levels are empty.
      * ********************************************************************************
-     * disableNested (false) - will get just the menu items of the first level. Nested items are in sub levels.
+     * nested (true) - will get just the menu items of the first level. Nested items are in sub levels.
      *
      */
-    List<MenuItemDTO> getVisibleMenuItems(int docId, int menuIndex, String language, boolean disableNested);
+    List<MenuItemDTO> getVisibleMenuItems(int docId, int menuIndex, String language, boolean nested);
 
-    List<MenuItemDTO> getPublicMenuItems(int docId, int menuIndex, String language, boolean disableNested);
+    List<MenuItemDTO> getPublicMenuItems(int docId, int menuIndex, String language, boolean nested);
 
     List<Menu> getAll();
 
