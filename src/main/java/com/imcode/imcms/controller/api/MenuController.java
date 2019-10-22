330ea636884a73943ad5dddbedbd9edf6d8782ae
Victor <vitekpavFurious>
2019-10-22 09:45:26 +0300

    Issue IMCMS-484: Improve the menu-administration - Add get all types;

--- src/main/java/com/imcode/imcms/controller/api/MenuController.java
+++ src/main/java/com/imcode/imcms/controller/api/MenuController.java
@@ -5,6 +5,7 @@ import com.imcode.imcms.domain.dto.MenuItemDTO;
 import com.imcode.imcms.domain.service.MenuService;
 import com.imcode.imcms.security.AccessType;
 import com.imcode.imcms.security.CheckAccess;
+import com.imcode.imcms.sorted.TypeSort;
 import imcode.server.Imcms;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/menus")
@@ -32,6 +35,15 @@ public class MenuController {
     public List<MenuItemDTO> getMenuItems(@ModelAttribute MenuDTO menu,
                                           @RequestParam(defaultValue = "false") boolean nested) {
         return menuService.getMenuItems(menu.getDocId(), menu.getMenuIndex(), Imcms.getUser().getLanguage(), nested);
+    }
+
+    @GetMapping("/sort-types")
+    public List<TypeSort> getSortTypes(@RequestParam boolean nested) {
+        if (nested) {
+            return Arrays.asList(TypeSort.values());
+        } else {
+            return Arrays.stream(TypeSort.values()).skip(1).collect(Collectors.toList());
+        }
     }
 
     @PostMapping
