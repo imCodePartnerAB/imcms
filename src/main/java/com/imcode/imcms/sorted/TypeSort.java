a256d7fbc30adabf949e690616d7c09d129db61c
Victor <vitekpavFurious>
2019-10-22 09:44:06 +0300

    Issue IMCMS-484: Improve the menu-administration - Created types sort stuff;

---
+++ src/main/java/com/imcode/imcms/sorted/TypeSort.java
@@ -0,0 +1,12 @@
+package com.imcode.imcms.sorted;
+
+public enum TypeSort {
+    TREE_SORT,
+    MANUAL,
+    ALPHABETICAL,
+    ALPHABETICAL_REVERSE,
+    PUBLISHED_DATE,
+    PUBLISHED_DATE_REVERSE,
+    MODIFIED_DATE,
+    MODIFIED_DATE_REVERSE
+}
