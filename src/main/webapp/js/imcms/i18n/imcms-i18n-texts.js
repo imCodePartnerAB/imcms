d7e6f22798053b60e3a2c97cc2a4655f94263288
Victor <vitekpavFurious>
2019-10-26 00:00:33 +0300

    Issue IMCMS-484: Improve the menu-administration - Rename

--- src/main/webapp/js/imcms/i18n/imcms-i18n-texts.js
+++ src/main/webapp/js/imcms/i18n/imcms-i18n-texts.js
@@ -304,7 +304,7 @@ const texts = {
                 docTitle: "Rubrik",
                 status: "Status",
                 removeConfirmation: "Vill du ta bort menyalternativet \"",
-                typeSort: "Sortera på",
+                titleTypeSort: "Sortera på",
                 saveAndClose: "Spara och stäng",
                 error: {
                     createFailed: 'Det gick inte att spara menyn. Försök igen',
@@ -1103,7 +1103,7 @@ const texts = {
                 status: "Status",
                 removeConfirmation: "Do you want to remove the menu item \"",
                 saveAndClose: "Save and close",
-                typeSort: "Sort on",
+                titleTypeSort: "Sort on",
                 error: {
                     createFailed: 'Failed to save menu. Try again',
                     copyDocumentFailed: 'Failed to copy document. Try again',
