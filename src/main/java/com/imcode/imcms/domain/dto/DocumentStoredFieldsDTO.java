aeff629c5f0bc74144712133158e6ace57c1b3a6
Victor <vitekpavFurious>
2019-11-02 15:27:27 +0200

    Issue IMCMS-484: Improve the menu-administration - Remove fields

--- src/main/java/com/imcode/imcms/domain/dto/DocumentStoredFieldsDTO.java
+++ src/main/java/com/imcode/imcms/domain/dto/DocumentStoredFieldsDTO.java
@@ -5,8 +5,6 @@ import imcode.server.document.index.DocumentStoredFields;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 /**
  * Document's stored fields in solr indexes
  */
@@ -24,18 +22,12 @@ public class DocumentStoredFieldsDTO {
 
     private String alias;
 
-    private Date publishedDate;
-
-    private Date modifiedDate;
-
     public DocumentStoredFieldsDTO(DocumentStoredFields from) {
         id = from.id();
         title = from.headline();
         type = from.documentType();
         documentStatus = from.documentStatus();
         alias = from.alias();
-        publishedDate = from.publicationStart();
-        modifiedDate = from.modified();
     }
 
 }
