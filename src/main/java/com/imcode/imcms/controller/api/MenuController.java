c02a2b2981624c06ef6662c465b8e821bcb8f8a7
Victor <vitekpavFurious>
2019-11-06 11:34:24 +0200

    Issue IMCMS-484: Improve the menu-administration - Create endpoint for sorting items

--- src/main/java/com/imcode/imcms/controller/api/MenuController.java
+++ src/main/java/com/imcode/imcms/controller/api/MenuController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +42,11 @@ public class MenuController {
                 menu.isNested(),
                 menu.getTypeSort()
         );
+    }
+
+    @PutMapping("/sorting")
+    public List<MenuItemDTO> getSortedMenuItems(@RequestBody MenuDTO menuDTO) {
+        return menuService.getSortedMenuItems(menuDTO);
     }
 
     @GetMapping("/sort-types")
