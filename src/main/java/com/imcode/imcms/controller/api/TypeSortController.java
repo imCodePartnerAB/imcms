de1d94a69401714891ad4af2df0da9bba8d9ae18
Victor <vitekpavFurious>
2019-11-06 12:45:22 +0200

    Issue IMCMS-484: Improve the menu-administration - Create controller

---
+++ src/main/java/com/imcode/imcms/controller/api/TypeSortController.java
@@ -0,0 +1,29 @@
+package com.imcode.imcms.controller.api;
+
+import com.imcode.imcms.domain.service.TypeSorterMenuService;
+import com.imcode.imcms.sorted.TypeSort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/types-sort")
+public class TypeSortController {
+
+    private final TypeSorterMenuService typeSorterMenuService;
+
+    @Autowired
+    public TypeSortController(TypeSorterMenuService typeSorterMenuService) {
+        this.typeSorterMenuService = typeSorterMenuService;
+    }
+
+
+    @GetMapping
+    public List<TypeSort> getSortTypes(@RequestParam boolean nested) {
+        return typeSorterMenuService.typesSortByNested(nested);
+    }
+}
