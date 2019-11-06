ef6b2d589a346720b174da8d8c65a48280e348e9
Victor <vitekpavFurious>
2019-11-06 12:48:49 +0200

    Issue IMCMS-484: Improve the menu-administration - Remove api types sort

--- src/main/webapp/js/imcms/rest/imcms-menus-rest-api.js
+++ src/main/webapp/js/imcms/rest/imcms-menus-rest-api.js
@@ -6,7 +6,6 @@ define("imcms-menus-rest-api", ["imcms-rest-api"], function (rest) {
     let url = '/menus';
     let api = new rest.API(url);
 
-    api.getSortTypes = nested => rest.ajax.call({url: `${url}/sort-types`, type: 'GET', json: false}, nested);
     api.getSortedItems = menuData => rest.ajax.call({url: `${url}/sorting`, type: 'PUT', json: true}, menuData);
 
     return api;
