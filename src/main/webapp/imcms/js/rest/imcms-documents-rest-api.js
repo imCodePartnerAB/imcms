e313b35a54eb8627c1a7461a638f46d789e4ed9b
vladhuk <vladhookovskiy@gmail.com>
2019-11-28 19:46:53 +0200

    Issue IMCMS-492: GUI tweaks and fixes Item 4: - Add request;

--- src/main/webapp/imcms/js/rest/imcms-documents-rest-api.js
+++ src/main/webapp/imcms/js/rest/imcms-documents-rest-api.js
@@ -4,5 +4,7 @@ define('imcms-documents-rest-api', ['imcms-rest-api'], function (rest) {
 
     api.remove = docId => rest.ajax.call({url: `${url}/${docId}`, type: 'DELETE', json: false});
 
+    api.getUniqueAlias = alias => rest.ajax.call({url: `${url}/alias/unique/${alias}`, type: 'GET', json: false});
+
     return api;
 });
