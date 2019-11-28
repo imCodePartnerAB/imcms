86ce8a83cc4073c5db6b41efe63367fa09861d82
vladhuk <vladhookovskiy@gmail.com>
2019-11-28 19:51:23 +0200

    Issue IMCMS-492: GUI tweaks and fixes Item 4: - Add method to controller;

--- src/main/java/com/imcode/imcms/controller/api/DocumentController.java
+++ src/main/java/com/imcode/imcms/controller/api/DocumentController.java
@@ -6,13 +6,7 @@ import com.imcode.imcms.model.Document;
 import com.imcode.imcms.persistence.entity.Meta.DocumentType;
 import com.imcode.imcms.security.AccessType;
 import com.imcode.imcms.security.CheckAccess;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Serhii Maksymchuk from Ubrainians for imCode
@@ -36,6 +30,11 @@ class DocumentController {
         } else {
             return documentService.get(docId);
         }
+    }
+
+    @GetMapping("/alias/unique/{alias}")
+    public String getUniqueAlias(@PathVariable String alias) {
+        return documentService.getUniqueAlias(alias);
     }
 
     @PostMapping("/copy/{docId}")
