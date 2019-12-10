777754f97e12b038caee8a7f27753d103cb0cf9c
Victor <vitekpavFurious>
2019-12-10 13:45:44 +0200

    Revert "Issue IMCMS-500: Search in documents editor and menu editor - Move and refactor lower case"
    This reverts commit 08f6228

--- src/main/java/com/imcode/imcms/domain/component/DocumentSearchQueryConverter.java
+++ src/main/java/com/imcode/imcms/domain/component/DocumentSearchQueryConverter.java
@@ -40,7 +40,7 @@ public class DocumentSearchQueryConverter {
                         DocumentIndex.FIELD__ALIAS,
                         DocumentIndex.FIELD__URL})
                         .map(field -> String.format("%s:*%s*", field,
-                                searchQuery.getTerm().replaceAll("\\s+", "?")).toLowerCase())
+                                searchQuery.getTerm().replaceAll("\\s+", "?")))
                         .collect(Collectors.joining(" "))
                         : "*:*"
         );
