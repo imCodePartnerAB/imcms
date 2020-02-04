f4fd31c5f45ecbb5d739bff81fe856f4506b7f60
vladhuk <vladhookovskiy@gmail.com>
2020-02-04 13:01:55 +0200

    Issue IMCMS-504: Fixes in menu editor design - Fix tooltip position; - Refactor and clean up;

--- src/main/webapp/imcms/js/builders/components/imcms-overlays-builder.js
+++ src/main/webapp/imcms/js/builders/components/imcms-overlays-builder.js
@@ -9,7 +9,6 @@ define('imcms-overlays-builder', ['jquery', 'imcms-bem-builder'], function ($, B
          *     overlay: overlays.tooltip('Text on tooltip'),
          *     delay: {show: 300, hide: 0},
          *     placement: 'right',
-         *     trigger: 'hover',
          * });
          *
          * @param element {object} A target element for triggering an overlay
@@ -17,9 +16,8 @@ define('imcms-overlays-builder', ['jquery', 'imcms-bem-builder'], function ($, B
          * @param delay {number|object?} A millisecond delay amount to show and hide the Overlay once triggered. Default: 0
          *        Example: 1, {hide: 1}, {show: 1, hide: 2}
          * @param placement {string?} Values: 'top'|'right'|'bottom'|'left'. Default: 'right'
-         * @param trigger {string?} Values: 'click'|'hover'. Default: 'hover'
          */
-        const createOverlay = ({element, overlay, delay = 0, placement = 'top', trigger = 'hover'}) => {
+        const createOverlay = ({element, overlay, delay = 0, placement = 'top'}) => {
 
             const correctDelay = getCorrectDelay(delay);
 
@@ -73,34 +71,48 @@ define('imcms-overlays-builder', ['jquery', 'imcms-bem-builder'], function ($, B
         };
 
         const calculatePositionOfOverlay = (element, overlay, placement) => {
-            const elementOffset = element.offset();
-            const bodyOffsetTop = parseInt($('body').css('top'));
-            elementOffset.top -= bodyOffsetTop;
+            const elementOffset = getCorrectElementOffset(element);
+            const elementScale = getElementScale(element);
+            const overlayScale = getElementScale(overlay);
 
             switch (placement) {
                 case 'right':
                     return {
-                        top: elementOffset.top + element.outerHeight() / 2 - overlay.outerHeight() / 1.5,
-                        left: elementOffset.left + element.outerWidth(),
+                        top: elementOffset.top + elementScale.height / 2 - overlayScale.height / 2,
+                        left: elementOffset.left + elementScale.width,
                     };
                 case 'bottom':
                     return {
-                        top: elementOffset.top + overlay.outerHeight(),
+                        top: elementOffset.top + overlayScale.height,
                         left: elementOffset.left,
                     };
                 case 'left':
                     return {
-                        top: elementOffset.top + element.outerHeight() / 2 - overlay.outerHeight() / 1.5,
-                        left: elementOffset.left - overlay.outerWidth(),
+                        top: elementOffset.top + elementScale.height / 2 - overlayScale.height / 2,
+                        left: elementOffset.left - overlayScale.width,
                     };
                 case 'top':
                 default:
                     return {
-                        top: elementOffset.top - element.outerHeight(),
-                        left: elementOffset.left + element.outerWidth() / 2 - overlay.outerWidth() / 2,
+                        top: elementOffset.top - elementScale.height,
+                        left: elementOffset.left + elementScale.width / 2 - overlayScale.width / 2,
                     };
             }
         };
+
+        const getCorrectElementOffset = element => {
+            const bodyOffset = $('body').offset();
+            const elementOffset = element.offset();
+            return {
+                top: elementOffset.top - bodyOffset.top,
+                left: elementOffset.left - bodyOffset.left,
+            }
+        };
+
+        const getElementScale = element => ({
+            height: element.outerHeight(),
+            width: element.outerWidth(),
+        });
 
         /**
          * Universal tooltip with text and dark background
