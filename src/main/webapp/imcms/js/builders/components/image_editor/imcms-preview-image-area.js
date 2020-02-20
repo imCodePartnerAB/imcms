6905791af71ae0744390a308d9a9db6a9adabdf2
Victor <vitekpavFurious>
2020-02-20 18:18:48 +0200

    Issue IMCMS-508: Improve the image editor - Delete comment

--- src/main/webapp/imcms/js/builders/components/image_editor/imcms-preview-image-area.js
+++ src/main/webapp/imcms/js/builders/components/image_editor/imcms-preview-image-area.js
@@ -60,7 +60,7 @@ module.exports = {
     },
 
     setBackgroundPositionX(newPositionX) {
-        if ($previewImg[0].style.backgroundPositionX.trim().length === 0) { //probably bad idea check on empty. Maybe use length?
+        if ($previewImg[0].style.backgroundPositionX.trim().length === 0) {
             savePrePreviewPosition = true;
         }
         $previewImg[0].style.backgroundPositionX = `${newPositionX}px`;
