51c6c55fe9c1daa5319deb3649516757dca400f9
vladhuk <vladhookovskiy@gmail.com>
2020-02-21 16:00:49 +0200

    Issue IMCMS-488: Change the process for uploaded file and URL - Change order of tabs; - Add document creating step by step;

--- src/main/webapp/imcms/js/builders/components/page_info_tabs/imcms-page-info-tabs-builder.js
+++ src/main/webapp/imcms/js/builders/components/page_info_tabs/imcms-page-info-tabs-builder.js
@@ -2,11 +2,11 @@ const WindowTabsBuilder = require('imcms-window-tabs-builder');
 
 module.exports = new WindowTabsBuilder({
     tabBuilders: [
+        require('imcms-file-tab-builder'),
+        require('imcms-url-tab-builder'),
         require('imcms-appearance-tab-builder'),
         require('imcms-life-cycle-tab-builder'),
         require('imcms-templates-tab-builder'),
-        require('imcms-file-tab-builder'),
-        require('imcms-url-tab-builder'),
         require('imcms-keywords-tab-builder'),
         require('imcms-categories-tab-builder'),
         require('imcms-access-tab-builder'),
