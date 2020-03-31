c1af571721e255e9b9f5db058927bac439e35dfa
Victor <vitekpavFurious>
2020-03-31 12:06:53 +0300

    Issue IMCMS-518: Add support for Azure in RB-4 - Create factory to get provider

---
+++ server/src/com/imcode/imcms/domain/factory/AuthenticationProvidersFactory.java
@@ -0,0 +1,48 @@
+package com.imcode.imcms.domain.factory;
+
+import com.imcode.imcms.domain.component.AzureAuthenticationProvider;
+import com.imcode.imcms.domain.exception.ExternalIdentifierNotEnabledException;
+import com.imcode.imcms.model.AuthenticationProvider;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import static com.imcode.imcms.domain.component.AzureAuthenticationProvider.EXTERNAL_AUTHENTICATOR_AZURE_AD;
+
+public class AuthenticationProvidersFactory {
+
+    private final Properties properties;
+    private final Map<String, AuthenticationProvider> idToProvider = new HashMap<>();
+
+    public AuthenticationProvidersFactory(Properties imcmsProperties) {
+        properties = imcmsProperties;
+    }
+
+    public List<AuthenticationProvider> getProviders() {
+        final List<AuthenticationProvider> providers = new ArrayList<>();
+        final String externalAuthenticator = properties.getProperty("ExternalAuthenticator", "");
+        if (!externalAuthenticator.isEmpty()) {
+            providers.add(getProvider(externalAuthenticator));
+        }
+        return providers;
+    }
+
+    public AuthenticationProvider getProvider(String identifierId) {
+        identifierId = identifierId.toLowerCase();
+        final AuthenticationProvider authenticationProvider = idToProvider.get(identifierId);
+
+        if (authenticationProvider != null) return authenticationProvider;
+
+        switch (identifierId) {
+            case EXTERNAL_AUTHENTICATOR_AZURE_AD:
+                final AzureAuthenticationProvider provider = new AzureAuthenticationProvider(properties);
+                idToProvider.put(EXTERNAL_AUTHENTICATOR_AZURE_AD, provider);
+                return provider;
+        }
+
+        throw new ExternalIdentifierNotEnabledException(identifierId);
+    }
+}
