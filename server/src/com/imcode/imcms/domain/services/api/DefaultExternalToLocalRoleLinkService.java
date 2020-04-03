cc9a93cc27f38cdb1deb8045c592a7cab750f0c7
Victor <vitekpavFurious>
2020-04-03 23:13:45 +0300

    Issue IMCMS-518: Add support for Azure in RB-4 - Implement set and link external to local role; - Add save / delete role to db; - Refactor;

--- server/src/com/imcode/imcms/domain/services/api/DefaultExternalToLocalRoleLinkService.java
+++ server/src/com/imcode/imcms/domain/services/api/DefaultExternalToLocalRoleLinkService.java
@@ -1,6 +1,7 @@
 package com.imcode.imcms.domain.services.api;
 
 import com.imcode.imcms.domain.dto.ExternalRole;
+import com.imcode.imcms.domain.dto.ExternalToLocalRoleLink;
 import com.imcode.imcms.domain.services.ExternalToLocalRoleLinkService;
 import com.imcode.imcms.model.ExternalToLocalRoleLinkModel;
 import imcode.server.Imcms;
@@ -22,6 +23,37 @@ public class DefaultExternalToLocalRoleLinkService implements ExternalToLocalRol
     @Override
     public void setLinkedRoles(ExternalRole externalRole, Set<Integer> localRolesId) {
 
+        final Set<ExternalToLocalRoleLink> linksSent = localRolesId.stream()
+                .map(localRoleId -> new ExternalToLocalRoleLink(externalRole, localRoleId))
+                .collect(Collectors.toSet());
+
+        final Set<ExternalToLocalRoleLinkModel> alreadyLinkedRoles = getLinkedRoles(externalRole);
+
+        alreadyLinkedRoles.stream()
+                .filter(alreadyLinkedRole -> !linksSent.contains(removeId(alreadyLinkedRole)))
+                .forEach(this::delete);
+
+        final Set<ExternalToLocalRoleLink> alreadyLinkedRolesWithoutId = alreadyLinkedRoles.stream()
+                .map(this::removeId)
+                .collect(Collectors.toSet());
+
+        linksSent.stream()
+                .filter(linkSent -> !alreadyLinkedRolesWithoutId.contains(linkSent))
+                .map(from -> {
+                    final ExternalToLocalRoleLinkModel linkModel = new ExternalToLocalRoleLinkModel(from);
+                    linkModel.setRole(Imcms.getServices()
+                            .getImcmsAuthenticatorAndUserAndRoleMapper()
+                            .getRoleById(from.getLocalRoleId()));
+                    return linkModel;
+                })
+                .forEach(this::save);
+
+    }
+
+    private ExternalToLocalRoleLink removeId(ExternalToLocalRoleLinkModel roleLink) {
+        return new ExternalToLocalRoleLink(
+                roleLink.getProviderId(), roleLink.getExternalRoleId(), roleLink.getLocalRoleId()
+        );
     }
 
     @Override
@@ -41,7 +73,9 @@ public class DefaultExternalToLocalRoleLinkService implements ExternalToLocalRol
 
     @Override
     public Set<RoleDomainObject> toLinkedLocalRoles(Set<ExternalRole> externalRoles) {
-        return null;
+        return externalRoles.stream()
+                .flatMap(externalRole -> getLinkedLocalRoles(externalRole).stream())
+                .collect(Collectors.toSet());
     }
 
     private Set<ExternalToLocalRoleLinkModel> getLinkedRoles(ExternalRole externalRole) {
@@ -49,7 +83,7 @@ public class DefaultExternalToLocalRoleLinkService implements ExternalToLocalRol
     }
 
     private Set<ExternalToLocalRoleLinkModel> findByProviderIdAndExternalRoleId(ExternalRole externalRole) {
-        final String sql = "SELECT * from external_to_local_roles_links WHERE provider_id = ? AND external_role_id = ?";
+        final String sql = "SELECT * from external_to_local_roles_links WHERE provider_id =? AND external_role_id =?";
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
         Set<ExternalToLocalRoleLinkModel> externalRoleLinkModels = new HashSet<>();
@@ -76,5 +110,51 @@ public class DefaultExternalToLocalRoleLinkService implements ExternalToLocalRol
         }
 
         return externalRoleLinkModels;
+    }
+
+    private void delete(ExternalToLocalRoleLinkModel roleLinkModel) {
+
+        final String sql = "DELETE FROM external_to_local_roles_links WHERE id=?";
+
+        try {
+            PreparedStatement preparedStatement = Imcms.getApiDataSource().getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, roleLinkModel.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException s) {
+            log.error(String.format("%s delete not complete with id %d", s.getMessage(), roleLinkModel.getId()));
+        }
+    }
+
+    private void save(ExternalToLocalRoleLinkModel linkRole) {
+        String sql;
+        PreparedStatement preparedStatement;
+        try {
+            if (Imcms.getServices()
+                    .getImcmsAuthenticatorAndUserAndRoleMapper()
+                    .getRoleById(linkRole.getLocalRoleId()) != null) {
+
+                sql = "UPDATE external_to_local_roles_links " +
+                        "SET id =?, provider_id=?, external_role_id=?, linked_local_role_id=?";
+                preparedStatement = Imcms.getApiDataSource().getConnection().prepareStatement(sql);
+                preparedStatement.setInt(1, linkRole.getId());
+                preparedStatement.setString(2, linkRole.getProviderId());
+                preparedStatement.setString(3, linkRole.getExternalRoleId());
+                preparedStatement.setInt(4, linkRole.getLocalRoleId());
+
+                preparedStatement.executeUpdate();
+
+            } else {
+                sql = "INSERT INTO external_to_local_roles_links (provider_id, external_role_id, linked_local_role_id) values (?,?,?)";
+
+                preparedStatement = Imcms.getApiDataSource().getConnection().prepareStatement(sql);
+                preparedStatement.setString(1, linkRole.getProviderId());
+                preparedStatement.setString(2, linkRole.getExternalRoleId());
+                preparedStatement.setInt(3, linkRole.getLocalRoleId());
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            log.error(String.format("%s save not completed with roleId %d", e.getMessage(), linkRole.getId()));
+        }
     }
 }
