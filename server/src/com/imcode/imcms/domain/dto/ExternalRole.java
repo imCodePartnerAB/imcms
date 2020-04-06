8f91d198aec77f50b443b01807612d75566de3ae
Victor <vitekpavFurious>
2020-04-06 18:30:41 +0300

    Issue IMCMS-518: Add support for Azure in RB-4 - Add annotation lombok for create constructor

--- server/src/com/imcode/imcms/domain/dto/ExternalRole.java
+++ server/src/com/imcode/imcms/domain/dto/ExternalRole.java
@@ -1,9 +1,14 @@
 package com.imcode.imcms.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ExternalRole {
     protected String providerId;
     private String id;
+
+    public ExternalRole() {
+    }
 }
