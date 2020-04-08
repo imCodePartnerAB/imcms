f86c8bf0192cc0c17288680c752d420d3607526b
Victor <vitekpavFurious>
2020-04-08 13:02:40 +0300

    Issue IMCMS-518: Add support for Azure in RB-4 - Add throws exception;

--- server/src/com/imcode/imcms/domain/services/ExternalToLocalRoleLinkService.java
+++ server/src/com/imcode/imcms/domain/services/ExternalToLocalRoleLinkService.java
@@ -3,13 +3,14 @@ package com.imcode.imcms.domain.services;
 import com.imcode.imcms.domain.dto.ExternalRole;
 import imcode.server.user.RoleDomainObject;
 
+import java.sql.SQLException;
 import java.util.Set;
 
 public interface ExternalToLocalRoleLinkService {
 
-    void setLinkedRoles(ExternalRole externalRole, Set<Integer> localRolesId);
+    void setLinkedRoles(ExternalRole externalRole, Set<Integer> localRolesId) throws SQLException;
 
-    Set<RoleDomainObject> getLinkedLocalRoles(ExternalRole externalRole);
+    Set<RoleDomainObject> getLinkedLocalRoles(ExternalRole externalRole) throws SQLException;
 
-    Set<RoleDomainObject> toLinkedLocalRoles(Set<ExternalRole> externalRoles);
+    Set<RoleDomainObject> toLinkedLocalRoles(Set<ExternalRole> externalRoles) throws SQLException;
 }
