b716e6b5dcb52f81fac24814ed16ca7c63f2c69f
Victor <vitekpavFurious>
2020-04-23 12:50:08 +0300

    Issue IMCMS-493: User's languages are affected by the site's languages - Clean up;

--- src/main/java/com/imcode/imcms/domain/service/LanguageService.java
+++ src/main/java/com/imcode/imcms/domain/service/LanguageService.java
@@ -5,11 +5,7 @@ import com.imcode.imcms.model.Language;
 import java.util.List;
 
 public interface LanguageService {
-    /**
-     * Get language by it's two-letter ISO-639-1 code like "en" or "sv"
-     *
-     * @param code ISO-639-1 code
-     */
+
     Language findByCode(String code);
 
     List<Language> getAll();
