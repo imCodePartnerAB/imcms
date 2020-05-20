be91049c6af09cb6048d85c37fe9761476dec911
Victor <vitekpavFurious>
2020-05-20 16:02:27 +0300

    Issue IMCMS-501: Menu - Generated output asHtml() - Create class menu html pattern

---
+++ src/main/java/com/imcode/imcms/components/patterns/ImcmsMenuHtmlPatterns.java
@@ -0,0 +1,13 @@
+package com.imcode.imcms.components.patterns;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public final class ImcmsMenuHtmlPatterns {
+    private final String patternUlDataAttr = "";
+    private final String patternUlClassAttr = "";
+    private final String patternLiDataAttr = "";
+    private final String patternLiClassAttr = "";
+}
