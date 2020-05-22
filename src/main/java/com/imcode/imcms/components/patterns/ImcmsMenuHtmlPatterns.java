26b74f346bea2ea08bc4544fcf19d97baa2c5a14
Victor <vitekpavFurious>
2020-05-22 17:09:42 +0300

    Issue IMCMS-501: Menu - Generated output asHtml() - Add pattern ul; - Change naming field

--- src/main/java/com/imcode/imcms/components/patterns/ImcmsMenuHtmlPatterns.java
+++ src/main/java/com/imcode/imcms/components/patterns/ImcmsMenuHtmlPatterns.java
@@ -6,9 +6,10 @@ import lombok.ToString;
 @Getter
 @ToString
 public final class ImcmsMenuHtmlPatterns {
-    private final String patternUlDataAttr = "<ul data-menu-index=\"%d\" data-doc-id=\"%d\"";
+    private final String patternStartUlDataAttr = "<ul data-menu-index=\"%d\" data-doc-id=\"%d\"";
     private final String patternWithoutTagUlDataAttr = " data-menu-index=\"%d\" data-doc-id=\"%d\"";
     private final String patternStartUlClassAttr = "<ul class=\"%s %s %s %s--%d-%d\"";
+    private final String patternUlClassAttr = "<ul class=\"%s %s\">";
     private final String patternStartSimpleUlClassAttr = " class=\"%s %s %s %s--%d-%d\"";
     private final String patternLiDataAttr = "<li %s=\"%d\" %s=\"%d\" %s=\"%s\" %s=\"%d\" %s=\"%s\"";
     private final String patternSimpleLiDataAttr = " %s=\"%d\" %s=\"%d\" %s=\"%s\" %s=\"%d\" %s=\"%s\"";
