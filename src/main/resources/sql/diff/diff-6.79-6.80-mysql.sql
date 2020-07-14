11552211d269474de045888f7fa1e8129a998eab
Victor <vitekpavFurious>
2020-07-14 17:42:04 +0300

    Issue IMCMS-533: Create sorting possibility with numbers - Add default value and not null in column; - Update schema version

---
+++ src/main/resources/sql/diff/diff-6.79-6.80-mysql.sql
@@ -0,0 +1,16 @@
+SET @schema_version__major_new = 6;
+SET @schema_version__minor_new = 80;
+
+alter table imcms_menu
+    modify type_sort varchar(55) default ' ' not null;
+
+UPDATE imcms_menu
+set type_sort = 'MANUAL'
+where nested = 0;
+UPDATE imcms_menu
+set type_sort = 'TREE_SORT'
+where nested = 1;
+
+UPDATE database_version
+SET major = @schema_version__major_new,
+    minor = @schema_version__minor_new;
