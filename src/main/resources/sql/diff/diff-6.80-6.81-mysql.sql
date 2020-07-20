541440096050f2b965f2f812c5beeeec74646ad9
Victor <vitekpavFurious>
2020-07-20 14:03:21 +0300

    Issue IMCMS-533: Create sorting possibility with numbers - Modify schema and diffs and clean up;

--- src/main/resources/sql/diff/diff-6.80-6.81-mysql.sql
+++
@@ -1,9 +0,0 @@
-SET @schema_version__major_new = 6;
-SET @schema_version__minor_new = 81;
-
-alter table imcms_menu
-    modify type_sort varchar(55) default ' ';
-
-UPDATE database_version
-SET major = @schema_version__major_new,
-    minor = @schema_version__minor_new;
