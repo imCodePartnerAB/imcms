daf6c85c9f30c22c0c47858e7d69f77f0b2fba31
victorpavfurious <vitek.pavFurious@gmail.com>
2020-09-12 00:11:38 +0300

    Issue IMCMS-542: Add more languages to select from - Add stub for database test

--- src/main/resources/sql/diff/diff-6.84-6.85-mysql.sql
+++ src/main/resources/sql/diff/diff-6.84-6.85-mysql.sql
@@ -76,6 +76,8 @@ values (null, 'sma', 'Southern Sami', 'Åarjelsaemien', 1);
 insert into imcms_languages (id, code, name, native_name, enabled)
 values (null, 'smj', 'Lule Sami', 'Julevsámegielas', 1);
 
+DELETE from imcms_test.imcms_languages where id > 2;
+
 UPDATE database_version
 SET major = @schema_version__major_new,
     minor = @schema_version__minor_new;
