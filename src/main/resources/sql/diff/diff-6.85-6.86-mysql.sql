4ccf9d95cbe08379bf3900d1559c4640f49eed35
victorpavfurious <vitek.pavFurious@gmail.com>
2020-09-14 12:43:20 +0300

    Issue IMCMS-542: Add more languages to select from - Remove stub for test db;

--- src/main/resources/sql/diff/diff-6.85-6.86-mysql.sql
+++
@@ -1,9 +0,0 @@
-SET @schema_version__major_new = 6;
-SET @schema_version__minor_new = 86;
-
-DELETE from imcms_test.imcms_languages where id > 2;
-
-UPDATE database_version
-SET major = @schema_version__major_new,
-    minor = @schema_version__minor_new;
-
