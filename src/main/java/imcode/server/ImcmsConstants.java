dcd5beefca657e7e063c0f8249f18f09f90cf874
victorpavfurious <vitek.pavFurious@gmail.com>
2020-09-14 14:58:00 +0300

    Issue IMCMS-542: Add more languages to select from - Added constants final list langs;

--- src/main/java/imcode/server/ImcmsConstants.java
+++ src/main/java/imcode/server/ImcmsConstants.java
@@ -1,5 +1,8 @@
 package imcode.server;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class
 ImcmsConstants {
 
@@ -143,6 +146,7 @@ ImcmsConstants {
 
     public static final String ENG_CODE = "en";
     public static final String SWE_CODE = "sv";
+    public static final List<String> LANGUAGES = Arrays.asList("en", "sv", "be", "bs", "bg", "zh", "hr", "cs", "da", "nl", "et", "fi", "fr", "de", "el", "hu", "ga", "is", "it", "ja", "kk", "lb", "lt", "lv", "mn", "no", "pl", "pt", "ro", "ru", "sr", "gd", "sk", "sl", "es", "tr", "uk", "sma", "smj");
 
     public static final String ENG_CODE_ISO_639_2 = "eng";
     public static final String SWE_CODE_ISO_639_2 = "swe";
