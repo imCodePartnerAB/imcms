fab5a13dddde22a57d347798251faaae6b72a54e
Victor <vitekpavFurious>
2020-09-28 14:03:59 +0300

    Issue IMCMS-522: Add and update documentation by RB-6 - Add tests; - Clean up;

--- src/test/java/com/imcode/imcms/domain/factory/ImcmsServiceTest.java
+++ src/test/java/com/imcode/imcms/domain/factory/ImcmsServiceTest.java
@@ -1,21 +1,23 @@
 package com.imcode.imcms.domain.factory;
 
 import com.imcode.imcms.WebAppSpringTestConfig;
-import com.imcode.imcms.domain.dto.FileDocumentDTO;
-import com.imcode.imcms.domain.dto.TextDocumentDTO;
-import com.imcode.imcms.domain.dto.UrlDocumentDTO;
-import com.imcode.imcms.domain.exception.UnsupportedDocumentTypeException;
-import com.imcode.imcms.domain.service.DocumentService;
+import com.imcode.imcms.api.MailService;
+import com.imcode.imcms.domain.service.AccessService;
+import com.imcode.imcms.domain.service.AuthenticationProvidersService;
+import com.imcode.imcms.domain.service.CommonContentService;
+import com.imcode.imcms.domain.service.DocumentUrlService;
+import com.imcode.imcms.domain.service.ImageService;
+import com.imcode.imcms.domain.service.LanguageService;
+import com.imcode.imcms.domain.service.LoopService;
+import com.imcode.imcms.domain.service.TemplateService;
+import com.imcode.imcms.domain.service.TextDocumentTemplateService;
+import com.imcode.imcms.domain.service.UserService;
+import com.imcode.imcms.domain.service.VersionService;
 import imcode.server.ImcmsServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static com.imcode.imcms.persistence.entity.Meta.DocumentType.FILE;
-import static com.imcode.imcms.persistence.entity.Meta.DocumentType.HTML;
-import static com.imcode.imcms.persistence.entity.Meta.DocumentType.TEXT;
-import static com.imcode.imcms.persistence.entity.Meta.DocumentType.URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ImcmsServiceTest extends WebAppSpringTestConfig {
 
@@ -23,29 +25,100 @@ public class ImcmsServiceTest extends WebAppSpringTestConfig {
     private ImcmsServices imcmsServices;
 
     @Autowired
-    private DocumentService<TextDocumentDTO> textDocumentService;
+    private LanguageService languageService;
+
     @Autowired
-    private DocumentService<FileDocumentDTO> fileDocumentService;
+    private MailService mailService;
+
     @Autowired
-    private DocumentService<UrlDocumentDTO> urlDocumentService;
+    private AccessService accessService;
+
+    @Autowired
+    private TemplateService templateService;
+
+    @Autowired
+    private AuthenticationProvidersService authenticationProvidersService;
+
+    @Autowired
+    private CommonContentService commonContentService;
+
+    @Autowired
+    private DocumentUrlService documentUrlService;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    private LoopService loopService;
+
+    @Autowired
+    private TextDocumentTemplateService textDocumentTemplateService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private VersionService versionService;
+
 
     @Test
-    public void getTextDocumentServiceBean_WhenTypeTEXT_Expect_CorrectBeanClass() {
-        assertEquals(textDocumentService, imcmsServices.getDocumentServiceByType(TEXT));
+    public void languageService_Expect_CorrectBeanClass() {
+        assertEquals(languageService, imcmsServices.getLanguageService());
     }
 
     @Test
-    public void getFileDocumentServiceBean_WhenTypeFILE_Expect_CorrectBeanClass() {
-        assertEquals(fileDocumentService, imcmsServices.getDocumentServiceByType(FILE));
+    public void mailService_Expect_CorrectBeanClass() {
+        assertEquals(mailService, imcmsServices.getMailService());
     }
 
     @Test
-    public void getUrlDocumentServiceBean_WhenTypeUrl_Expect_CorrectBeanClass() {
-        assertEquals(urlDocumentService, imcmsServices.getDocumentServiceByType(URL));
+    public void accessService_Expect_CorrectBeanClass() {
+        assertEquals(accessService, imcmsServices.getAccessService());
     }
 
     @Test
-    public void getDocumentServiceBean_WhenTypeNoExists_Expect_CorrectException() {
-        assertThrows(UnsupportedDocumentTypeException.class, () -> imcmsServices.getDocumentServiceByType(HTML));
+    public void templateService_Expect_CorrectBeanClass() {
+        assertEquals(templateService, imcmsServices.getTemplateService());
     }
+
+    @Test
+    public void authenticationProvidersService_Expect_CorrectBeanClass() {
+        assertEquals(authenticationProvidersService, imcmsServices.getAuthenticationProvidersService());
+    }
+
+    @Test
+    public void commonContentService_Expect_CorrectBeanClass() {
+        assertEquals(commonContentService, imcmsServices.getCommonContentService());
+    }
+
+    @Test
+    public void documentUrlService_Expect_CorrectBeanClass() {
+        assertEquals(documentUrlService, imcmsServices.getDocumentUrlService());
+    }
+
+    @Test
+    public void imageService_Expect_CorrectBeanClass() {
+        assertEquals(imageService, imcmsServices.getImageService());
+    }
+
+    @Test
+    public void loopService_Expect_CorrectBeanClass() {
+        assertEquals(loopService, imcmsServices.getLoopService());
+    }
+
+    @Test
+    public void textDocumentTemplateService_Expect_CorrectBeanClass() {
+        assertEquals(textDocumentTemplateService, imcmsServices.getTextDocumentTemplateService());
+    }
+
+    @Test
+    public void userService_Expect_CorrectBeanClass() {
+        assertEquals(userService, imcmsServices.getUserService());
+    }
+
+    @Test
+    public void versionService_Expect_CorrectBeanClass() {
+        assertEquals(versionService, imcmsServices.getVersionService());
+    }
+
 }
