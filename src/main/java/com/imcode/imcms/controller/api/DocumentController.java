7966419fcc7cb5f006e664f489f1c95708cbd699
Victor <vitekpavFurious>
2020-11-13 15:18:23 +0200

    Issue IMCMS-510: Batch delete - Add endpoint for delete - Implement delete

--- src/main/java/com/imcode/imcms/controller/api/DocumentController.java
+++ src/main/java/com/imcode/imcms/controller/api/DocumentController.java
@@ -6,7 +6,18 @@ import com.imcode.imcms.model.Document;
 import com.imcode.imcms.persistence.entity.Meta.DocumentType;
 import com.imcode.imcms.security.AccessType;
 import com.imcode.imcms.security.CheckAccess;
-import org.springframework.web.bind.annotation.*;
+import imcode.server.Imcms;
+import imcode.server.user.UserDomainObject;
+import org.apache.cxf.interceptor.security.AccessDeniedException;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Serhii Maksymchuk from Ubrainians for imCode
@@ -58,5 +69,15 @@ class DocumentController {
     @DeleteMapping("/{docId}")
     public void delete(@PathVariable Integer docId) {
         documentService.deleteByDocId(docId);
+    }
+
+    @DeleteMapping("/deleteAll")
+    public void deleteAll(@RequestBody List<Integer> ids) {
+        final UserDomainObject currentUser = Imcms.getUser();
+        if (!currentUser.isSuperAdmin()) {
+            throw new AccessDeniedException("Current user doesn't has role SuperAdmin");
+        }
+
+        documentService.deleteByIds(ids);
     }
 }
