5e16dbd814288502f418d797be592cfecf049655
Victor <vitekpavFurious>
2020-11-26 12:02:40 +0200

    Issue IMCMS-560: wrong sort in menu - Remove file

--- src/main/webapp/imcms/js/rest/imcms-sort-types-rest-api.js
+++
@@ -1,8 +0,0 @@
-define("imcms-sort-types-rest-api", ["imcms-rest-api"], function (rest) {
-    let url = '/types-sort';
-    let api = new rest.API(url);
-
-    api.getSortTypes = nested => rest.ajax.call({url: `${url}`, type: 'GET', json: false}, nested);
-
-    return api;
-});
