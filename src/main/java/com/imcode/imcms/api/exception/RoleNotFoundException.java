7b3bae55ad13e76cdf95466355efd3daf6bf86bb
Victor <vitekpavFurious>
2020-12-17 16:08:05 +0200

    Issue IMCMS-564: Modify accès to the Document editor - Created exception

---
+++ src/main/java/com/imcode/imcms/api/exception/RoleNotFoundException.java
@@ -0,0 +1,8 @@
+package com.imcode.imcms.api.exception;
+
+public class RoleNotFoundException extends RuntimeException {
+
+    public RoleNotFoundException(String message) {
+        super(message);
+    }
+}
