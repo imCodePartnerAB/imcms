a90191334812411b86f1c984111c1e4b18e63e2a
Victor <vitekpavFurious>
2020-12-22 18:46:00 +0200

    Issue IMCMS-564: Modify accès to the Document editor - Improved condition to access content

--- src/main/java/com/imcode/imcms/servlet/SuperAdminFilter.java
+++ src/main/java/com/imcode/imcms/servlet/SuperAdminFilter.java
@@ -1,6 +1,8 @@
 package com.imcode.imcms.servlet;
 
 import com.imcode.imcms.api.ContentManagementSystem;
+import com.imcode.imcms.api.User;
+import imcode.server.Imcms;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -19,7 +21,13 @@ public class SuperAdminFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
         ContentManagementSystem cms = ContentManagementSystem.fromRequest(servletRequest);
-        if (!cms.getCurrentUser().isSuperAdmin()) {
+        final User currentUser = cms.getCurrentUser();
+
+        boolean isNotAccessToDocumentEditor = !Imcms.getServices()
+                .getAccessService()
+                .hasUserAccessToDocumentEditor(currentUser.getInternal());
+
+        if (!currentUser.isSuperAdmin() && isNotAccessToDocumentEditor) { // add new feature: if role has access to documents - accessible
             ((HttpServletResponse) servletResponse).setStatus(HttpServletResponse.SC_FORBIDDEN);
             servletResponse.getWriter().write("You need to be superadmin to access this resource.");
             return;
