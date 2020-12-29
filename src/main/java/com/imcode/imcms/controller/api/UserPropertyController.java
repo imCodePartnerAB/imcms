5cd22f1401c8ae9569685e10e179154e27f4bda6
Victor <vitekpavFurious>
2020-12-29 15:30:52 +0200

    Issue IMCMS-562: User properties - Remove exception throw

--- src/main/java/com/imcode/imcms/controller/api/UserPropertyController.java
+++ src/main/java/com/imcode/imcms/controller/api/UserPropertyController.java
@@ -3,8 +3,15 @@ package com.imcode.imcms.controller.api;
 import com.imcode.imcms.domain.dto.UserPropertyDTO;
 import com.imcode.imcms.domain.service.UserPropertyService;
 import com.imcode.imcms.model.UserProperty;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -30,7 +37,7 @@ public class UserPropertyController {
 
     @GetMapping("/name")
     public UserProperty getByUserIdAndKeyName(@RequestParam Integer id, @RequestParam String keyName) {
-        return userPropertyService.getByUserIdAndKeyName(id, keyName).orElseThrow(()-> new EmptyResultDataAccessException(keyName, id));
+        return userPropertyService.getByUserIdAndKeyName(id, keyName);
     }
 
     @GetMapping("/value")
