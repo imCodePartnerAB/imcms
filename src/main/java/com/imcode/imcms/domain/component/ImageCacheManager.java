e0ed541502f516889542a8dd5dfeb54658b0b325
Victor <vitekpavFurious>
2021-03-10 15:59:31 +0200

    Issue IMCMS-601: Explanation to cache - Implement remove images from caches

---
+++ src/main/java/com/imcode/imcms/domain/component/ImageCacheManager.java
@@ -0,0 +1,9 @@
+package com.imcode.imcms.domain.component;
+
+public interface ImageCacheManager {
+
+    void removePublicImagesFromCacheByKey(String key);
+
+    void removeAllImagesFromCacheByKey(String key);
+
+}
