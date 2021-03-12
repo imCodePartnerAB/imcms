63f8f2425bcea31209d02cdbcb3b14c07547a2b5
Victor <vitekpavFurious>
2021-03-12 12:24:11 +0200

    Issue IMCMS-601: Explanation to cache - Add cache handler for resources

--- src/main/java/com/imcode/imcms/config/WebConfig.java
+++ src/main/java/com/imcode/imcms/config/WebConfig.java
@@ -9,15 +9,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.CacheControl;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 
 import javax.servlet.ServletContext;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
@@ -26,7 +31,7 @@ import java.util.Properties;
         "com.imcode.imcms.controller",
         "com.imcode.imcms.aspects"
 })
-class WebConfig {
+class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public CommonsMultipartResolver multipartResolver(Properties imcmsProperties) {
@@ -43,6 +48,7 @@ class WebConfig {
         viewResolver.setSuffix(".jsp");
         viewResolver.setOrder(1);
         viewResolver.setExposedContextBeanNames("loopService", "imageService", "menuService", "textService");
+        viewResolver.setCache(true);
         return viewResolver;
     }
 
@@ -51,6 +57,7 @@ class WebConfig {
         final ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
         viewResolver.setBasename("views");
         viewResolver.setOrder(0);
+        viewResolver.setCache(true);
         return viewResolver;
     }
 
@@ -68,5 +75,14 @@ class WebConfig {
         ObjectMapper mapper = new ObjectMapper()
                 .registerModule(new JavaTimeModule()); // new module, NOT JSR310Module
         return mapper;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("\\w*\\/images\\/.*")
+                .addResourceLocations("classpath:/images/**")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic())
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
     }
 }
