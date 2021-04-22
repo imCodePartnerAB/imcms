70e9cbc3a7a02ea733575821f3a321f8e4fab514
victorpavfurious <vitek.pavFurious@gmail.com>
2021-04-22 19:05:20 +0300

    Issue IMCMS-629: P2-RB4-security: cross-site request forgery (CSRF) vulnerability in FileAdmin - Add default user after logged out

--- server/src/com/imcode/imcms/servlet/csrf/filter/CSRFProtectionFilter.java
+++ server/src/com/imcode/imcms/servlet/csrf/filter/CSRFProtectionFilter.java
@@ -2,6 +2,7 @@ package com.imcode.imcms.servlet.csrf.filter;
 
 import com.imcode.imcms.servlet.csrf.CsrfTokenManager;
 import com.imcode.imcms.servlet.csrf.component.CSRFTokenManagerImpl;
+import imcode.server.Imcms;
 import imcode.server.user.UserDomainObject;
 import imcode.util.Utility;
 import org.apache.commons.logging.Log;
@@ -9,6 +10,7 @@ import org.apache.commons.logging.LogFactory;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CSRFProtectionFilter implements Filter {
@@ -24,6 +26,7 @@ public class CSRFProtectionFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         final HttpServletRequest request = (HttpServletRequest) servletRequest;
+        final HttpServletResponse response = (HttpServletResponse) servletResponse;
         final UserDomainObject user = Utility.getLoggedOnUser(request);
         final CsrfTokenManager csrfTokenManager = new CSRFTokenManagerImpl();
 
@@ -32,6 +35,8 @@ public class CSRFProtectionFilter implements Filter {
                 if (!csrfTokenManager.isCorrectTokenForCurrentUser(user, request)) {
                     Utility.makeUserLoggedOut(request);
                     log.error("Potential CSRF detected on user login name: " + user.getLoginName());
+                    //add default user for system
+                    Utility.makeUserLoggedIn(request, Imcms.getServices().verifyUserByIpOrDefault(request.getRemoteAddr()));
                     throw new ServletException("Potential CSRF detected!! Inform a scary sysadmin ASAP!");
                 }
             }
