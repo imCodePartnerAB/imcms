b7cfe2fb515ea4427bafff81a4f6530d2d890613
romgor <romanhorkoon@gmail.com>
2022-01-19 18:37:16 +0200

    Issue IMCMS-698 Fix/improve roles and permissions: - Restrict changes to some data for users without EDIT permission

--- src/main/java/com/imcode/imcms/controller/api/DocumentController.java
+++ src/main/java/com/imcode/imcms/controller/api/DocumentController.java
@@ -3,18 +3,17 @@ package com.imcode.imcms.controller.api;
 import com.imcode.imcms.domain.dto.UberDocumentDTO;
 import com.imcode.imcms.domain.service.DelegatingByTypeDocumentService;
 import com.imcode.imcms.model.Document;
+import com.imcode.imcms.persistence.entity.Meta;
 import com.imcode.imcms.persistence.entity.Meta.DocumentType;
-import com.imcode.imcms.security.AccessType;
+import com.imcode.imcms.security.AccessContentType;
+import com.imcode.imcms.security.AccessRoleType;
 import com.imcode.imcms.security.CheckAccess;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import imcode.server.Imcms;
+import imcode.server.user.UserDomainObject;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Serhii Maksymchuk from Ubrainians for imCode
@@ -31,6 +30,7 @@ class DocumentController {
     }
 
     @GetMapping
+    @CheckAccess(role = AccessRoleType.DOCUMENT_EDITOR, docPermission = AccessContentType.DOC_INFO)
     public Document get(Integer docId, DocumentType type, Integer parentDocId) {
         if (docId == null) {
             return documentService.createNewDocument(type, parentDocId);
@@ -46,30 +46,66 @@ class DocumentController {
     }
 
     @PostMapping("/copy/{docId}")
+    @CheckAccess(role = AccessRoleType.DOCUMENT_EDITOR)
     public Document copy(@PathVariable final Integer docId) {
         return documentService.copy(docId);
     }
 
     /**
-     * Simply save document.
+     * Simply create document.
      *
-     * @param saveMe unified document, compatible with each {@link DocumentType} except HTML (yet?)
-     * @return saved document
+     * @param createMe unified document, compatible with each {@link DocumentType} except HTML (yet?)
+     * @return created document
      */
     @PostMapping
-    @CheckAccess(AccessType.DOC_INFO)
-    public Document save(@RequestBody UberDocumentDTO saveMe) {
-        return documentService.save(saveMe);
+    @CheckAccess(role = AccessRoleType.DOCUMENT_EDITOR)
+    public Document create(@RequestBody UberDocumentDTO createMe) {
+        return documentService.save(createMe);
     }
 
-    @CheckAccess
+    /**
+     * Update document and checks the user's access to change some data
+     *
+     * @param updateMe unified document, compatible with each {@link DocumentType} except HTML (yet?)
+     * @return updated document
+     */
+    @PutMapping
+    @CheckAccess(docPermission = AccessContentType.DOC_INFO)
+    public Document update(@RequestBody UberDocumentDTO updateMe) {
+        final UserDomainObject user = Imcms.getUser();
+        final Document document = documentService.get(updateMe.getId());
+
+        if(!Imcms.getUser().isSuperAdmin() && !hasEditPermission(user, document)){
+            updateMe.setRestrictedPermissions(document.getRestrictedPermissions());
+            updateMe.setProperties(document.getProperties());
+        }
+
+        return documentService.save(updateMe);
+    }
+
     @DeleteMapping("/{docId}")
+    @CheckAccess(role = {AccessRoleType.ADMIN_PAGES, AccessRoleType.DOCUMENT_EDITOR})
     public void delete(@PathVariable Integer docId) {
         documentService.deleteByDocId(docId);
     }
 
     @DeleteMapping("/deleteAll")
+    @CheckAccess(role = AccessRoleType.DOCUMENT_EDITOR)
     public void deleteAll(@RequestBody List<Integer> ids) {
         documentService.deleteByIds(ids);
+    }
+
+    private boolean hasEditPermission(UserDomainObject user, Document document){
+        boolean hasEdIt = false;
+
+        Map<Integer, Meta.Permission> roleIdToPermission = document.getRoleIdToPermission();
+        for (Integer roleId : user.getRoleIds()) {
+            if(Meta.Permission.EDIT.equals(roleIdToPermission.get(roleId))) {
+                hasEdIt = true;
+                break;
+            }
+        }
+
+        return hasEdIt;
     }
 }
