e9c766480329290780b14543b012b57282edf26d
eugyak <zheniayakymchuck@gmail.com>
2022-04-23 19:12:29 +0300

    Issue GOT-91 Replace Log4j in imcms4: - improve ContextListener

--- server/src/com/imcode/imcms/servlet/ContextListener.java
+++ server/src/com/imcode/imcms/servlet/ContextListener.java
@@ -3,16 +3,21 @@ package com.imcode.imcms.servlet;
 import com.imcode.imcms.domain.services.core.ServerSettingsChecker;
 import imcode.server.Imcms;
 import imcode.util.Prefs;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.regex.Matcher;
 
 public class ContextListener implements ServletContextListener {
 
@@ -20,15 +25,16 @@ public class ContextListener implements ServletContextListener {
      * Became accessible only after {@link #configureLogging} method calling
      */
     private static Logger log;
+	private static final String WEB_APP_PATH_REGEX = "(?i)\\$\\{\\s*WEB_APP_PATH\\s*}";
 
-    public void contextInitialized(ServletContextEvent servletContextEvent) {
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
         ServletContext servletContext = servletContextEvent.getServletContext();
         final File realPathToWebApp = new File(servletContext.getRealPath("/"));
 
         File configPath = new File(realPathToWebApp, "WEB-INF/conf");
         Prefs.setConfigPath(configPath);
 
-        configureLogging(realPathToWebApp.toString(), configPath);
+        configureLogging(realPathToWebApp, configPath);
         ServerSettingsChecker.check();
         logPlatformInfo(servletContext.getServerInfo());
 
@@ -63,14 +69,27 @@ public class ContextListener implements ServletContextListener {
         }
     }
 
-    private void configureLogging(String root, File configPath) {
-        System.setProperty("com.imcode.imcms.path", root);
+    private void configureLogging(File root, File configPath) {
         File configFile = new File(configPath, "log4j.xml");
-        DOMConfigurator.configure(configFile.toString());
+	    updateLog4jConfigFile(root, configFile);
+		Configurator.initialize(null, ConfigurationSource.fromUri(configFile.toURI()));
 
-        log = Logger.getLogger(ContextListener.class);
+        log = LogManager.getLogger(ContextListener.class);
         log.info("Logging started");
     }
+
+	private void updateLog4jConfigFile(File webappRealPath, File log4jConfig) {
+		try {
+			final String webappRootPath = webappRealPath.getCanonicalPath().replaceAll("\\\\", "/");
+			final String log4jConfigData = FileUtils
+					.readFileToString(log4jConfig, StandardCharsets.UTF_8)
+					.replaceAll(WEB_APP_PATH_REGEX, Matcher.quoteReplacement(webappRootPath));
+
+			FileUtils.write(log4jConfig, log4jConfigData);
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot update log4j configuration file", e);
+		}
+	}
 
     private void logPlatformInfo(String serverInfo) {
         log.info("Servlet Engine: " + serverInfo);
