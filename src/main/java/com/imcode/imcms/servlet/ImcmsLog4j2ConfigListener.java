d30436a3ed80f06e5e8bfcf397f99ef986de0265
eugyak <zheniayakymchuck@gmail.com>
2022-05-31 11:41:04 +0300

    Issue IMCMS-705 Replace Log4j: - rename/update log4j2 initialization file

--- src/main/java/com/imcode/imcms/servlet/ImcmsLog4j2ConfigListener.java
+++ src/main/java/com/imcode/imcms/servlet/ImcmsLog4j2ConfigListener.java
@@ -1,20 +1,19 @@
 package com.imcode.imcms.servlet;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+import java.nio.charset.Charset;
 import java.util.regex.Matcher;
 
-public class ImcmsLog4jConfigListener implements ServletContextListener {
+public class ImcmsLog4j2ConfigListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent event) {
@@ -32,27 +31,26 @@ public class ImcmsLog4jConfigListener implements ServletContextListener {
 
     Logger initLog4j(File webappRoot) {
         // all occurrences of ${com.imcode.imcms.path} must be replaced with real WEB_APP root path.
-        String WEBAPP_ROOT_RE = "(?i)\\$\\{\\s*com\\.imcode\\.imcms\\.path\\s*\\}";
-        File log4jConfFile = new File(webappRoot, "WEB-INF/conf/log4j.xml");
+	    final String WEBAPP_ROOT_RE = "(?i)\\$\\{\\s*com\\.imcode\\.imcms\\.path\\s*\\}";
+	    final File log4jConfFile = new File(webappRoot, "WEB-INF/conf/log4j2.xml");
 
         if (!log4jConfFile.exists()) {
             throw new RuntimeException(String.format("Log4j configuration file %s does not exists.", log4jConfFile));
         }
 
         try {
-            String webappRootPath = webappRoot.getCanonicalPath().replaceAll("\\\\", "/");
-            String log4jConf = FileUtils
-                    .readFileToString(log4jConfFile, "utf-8")
-                    .replaceAll(WEBAPP_ROOT_RE, Matcher.quoteReplacement(webappRootPath));
+	        final String webappRootPath = webappRoot.getCanonicalPath().replaceAll("\\\\", "/");
+	        final String log4jConfigData = FileUtils
+			        .readFileToString(log4jConfFile, "utf-8")
+			        .replaceAll(WEBAPP_ROOT_RE, Matcher.quoteReplacement(webappRootPath));
+	        FileUtils.write(log4jConfFile, log4jConfigData, Charset.defaultCharset());
 
-            Reader log4jConfReader = new StringReader(log4jConf);
-
-            new DOMConfigurator().doConfigure(log4jConfReader, LogManager.getLoggerRepository());
+	        Configurator.reconfigure(log4jConfFile.toURI());
         } catch (IOException e) {
-            throw new RuntimeException("Error occurred while reading log4.xml file", e);
+            throw new RuntimeException("Error occurred while reading log4j2.xml file", e);
         }
 
-        return Logger.getLogger(getClass());
+        return LogManager.getLogger(getClass());
     }
 
 
