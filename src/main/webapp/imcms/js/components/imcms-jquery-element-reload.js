dc9e0ff9e2cb5a36e4d2dbdffd04b9c5ff853ad5
romgor <romanhorkoon@gmail.com>
2022-07-05 16:31:11 +0300

    Issue IMCMS-658 Java version and dependencies: - Minor fix

--- src/main/webapp/imcms/js/components/imcms-jquery-element-reload.js
+++ src/main/webapp/imcms/js/components/imcms-jquery-element-reload.js
@@ -14,10 +14,10 @@ define("imcms-jquery-element-reload", ["imcms-jquery-string-selector"], function
             reloadStringCommand = location.href + " " + elementAsStr + ">*";
 
         if ((callback) && (typeof callback === "function")) {
-            $reloadMe.on("load",reloadStringCommand, callback);
+            $reloadMe.load(reloadStringCommand, callback);
 
         } else {
-            $reloadMe.on("load",reloadStringCommand);
+            $reloadMe.load(reloadStringCommand);
         }
     }
 });
