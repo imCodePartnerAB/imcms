f33a623603c92a4ba4406a0449f4522db4edc068
eugyak <zheniayakymchuck@gmail.com>
2022-08-03 08:25:11 +0300

    Issue IMCMS-713 RB6 - login: - set default content type to APPLICATION_JSON

--- src/main/java/com/imcode/imcms/config/WebConfig.java
+++ src/main/java/com/imcode/imcms/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.imcode.imcms.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.imcode.imcms.domain.service.ImageService;
 import imcode.server.Imcms;
 import imcode.server.ImcmsServices;
 import org.springframework.context.annotation.Bean;
@@ -10,8 +9,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.CacheControl;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -74,7 +75,12 @@ class WebConfig implements WebMvcConfigurer {
         return mapper;
     }
 
-    @Override
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.defaultContentType(MediaType.APPLICATION_JSON);
+	}
+
+	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("/webapp/images/", "/webapp/images/generated", "")
