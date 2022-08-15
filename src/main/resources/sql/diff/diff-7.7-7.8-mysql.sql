030b087ba9d1ed2bf2da89e64d881ebd76a70edc
romgor <romanhorkoon@gmail.com>
2022-08-15 12:15:04 +0300

    Issue IMCMS-727 Improve public document cache: - Fix the problem

--- src/main/resources/sql/diff/diff-7.7-7.8-mysql.sql
+++ src/main/resources/sql/diff/diff-7.7-7.8-mysql.sql
@@ -2,8 +2,8 @@ SET @schema_version__major_new = 7;
 SET @schema_version__minor_new = 8;
 
 ALTER TABLE meta
-    ADD COLUMN cache_for_unauthorized boolean NOT NULL default true,
-    ADD COLUMN cache_for_authorized boolean NOT NULL default false;
+    ADD COLUMN cache_for_unauthorized int NOT NULL default true,
+    ADD COLUMN cache_for_authorized int NOT NULL default false;
 
 UPDATE meta SET cache_for_unauthorized = true, cache_for_authorized = false WHERE meta_id IN (SELECT meta_id FROM text_docs);
 
