a0ebf715e0409b1fe6888c85b2b78418b782527b
eugyak <zheniayakymchuck@gmail.com>
2022-08-15 11:41:49 +0300

    Issue IMCMS-723 Metda Description: - add DocumentMetadata model, entity, service, repo etc

---
+++ src/main/java/com/imcode/imcms/model/DocumentMetadata.java
@@ -0,0 +1,33 @@
+package com.imcode.imcms.model;
+
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * <p> Describes HTML meta tag content <p/>
+ *
+ * @see MetaTag
+ */
+@NoArgsConstructor
+public abstract class DocumentMetadata implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 2264975307106484000L;
+
+	public DocumentMetadata(DocumentMetadata from) {
+		setMetaTag(from.getMetaTag());
+		setContent(from.getContent());
+	}
+
+	public abstract MetaTag getMetaTag();
+
+	public abstract void setMetaTag(MetaTag metaTag);
+
+	/**
+	 * @return meta tag content
+	 */
+	public abstract String getContent();
+
+	public abstract void setContent(String content);
+}
