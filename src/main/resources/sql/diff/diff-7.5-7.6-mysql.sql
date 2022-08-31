44076ec942094c4ba60e3c6349159f4cccb7234b
eugyak <zheniayakymchuck@gmail.com>
2022-08-31 20:45:32 +0300

    Issue IMCMS-704 Create multiple aliases depending on the language: - update migration script

--- src/main/resources/sql/diff/diff-7.5-7.6-mysql.sql
+++ src/main/resources/sql/diff/diff-7.5-7.6-mysql.sql
@@ -1,15 +1,22 @@
 SET @schema_version__major_new = 7;
 SET @schema_version__minor_new = 6;
 
-DELETE
-FROM document_properties
-WHERE key_name = 'imcms.document.alias';
-
 ALTER TABLE meta
 	ADD COLUMN default_language_alias_enabled boolean default false after disabled_language_show_rule;
 
 ALTER TABLE imcms_doc_i18n_meta
 	ADD COLUMN alias varchar(255) after headline;
+
+UPDATE meta, imcms_doc_i18n_meta commonContent, document_properties properties
+SET commonContent.alias = properties.value,
+	meta.default_language_alias_enabled = true WHERE commonContent.doc_id = properties.meta_id
+  AND meta.meta_id = properties.meta_id
+  AND properties.key_name = 'imcms.document.alias'
+  AND commonContent.language_id = 2;
+
+DELETE
+FROM document_properties
+WHERE key_name = 'imcms.document.alias';
 
 UPDATE database_version
 SET major = @schema_version__major_new,
