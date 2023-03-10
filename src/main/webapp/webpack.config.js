3a6dda920a6b876400433124e8ad33195e1e3ec6
romgor <romanhorkoon@gmail.com>
2023-03-10 18:50:04 +0200

    Issue IMCMS-747 Make generic export from RB4 and import to RB6: - Increase imcms version

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -7,9 +7,9 @@ module.exports = merge(baseConfig, {
     mode: 'development',
     watch: true,
     output: {
-        path: path.resolve(__dirname, './../../../build/libs/exploded/imcms-6.0.0-beta18-SNAPSHOT.war/dist')
+        path: path.resolve(__dirname, './../../../build/libs/exploded/imcms-6.0.0-beta19-SNAPSHOT.war/dist')
     },
     plugins: [
-        new CleanWebpackPlugin(['./../../../build/libs/exploded/imcms-6.0.0-beta18-SNAPSHOT.war/dist'])
+        new CleanWebpackPlugin(['./../../../build/libs/exploded/imcms-6.0.0-beta19-SNAPSHOT.war/dist'])
     ],
 });
