75f712372f491647314264962bfe5b1eb6918fb2
romgor <romanhorkoon@gmail.com>
2023-03-24 11:50:02 +0200

    Issue IMCMS-759 Release beta18: - Release imcms version

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -7,9 +7,9 @@ module.exports = merge(baseConfig, {
     mode: 'development',
     watch: true,
     output: {
-        path: path.resolve(__dirname, './../../../build/libs/exploded/imcms-6.0.0-beta18-SNAPSHOT.war/dist')
+        path: path.resolve(__dirname, './../../../build/libs/exploded/imcms-6.0.0-beta18.war/dist')
     },
     plugins: [
-        new CleanWebpackPlugin(['./../../../build/libs/exploded/imcms-6.0.0-beta18-SNAPSHOT.war/dist'])
+        new CleanWebpackPlugin(['./../../../build/libs/exploded/imcms-6.0.0-beta18.war/dist'])
     ],
 });
