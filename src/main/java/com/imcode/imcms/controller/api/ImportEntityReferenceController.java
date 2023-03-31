400d02ccdf8ce5ae99383eab20a6bc7051eed6d2
eugyak <zheniayakymchuck@gmail.com>
2023-03-31 17:02:52 +0300

    Issue IMCMS-747 Make generic export from RB4 and import to RB6: - restrict access to several endpoints

--- src/main/java/com/imcode/imcms/controller/api/ImportEntityReferenceController.java
+++ src/main/java/com/imcode/imcms/controller/api/ImportEntityReferenceController.java
@@ -3,6 +3,8 @@ package com.imcode.imcms.controller.api;
 import com.imcode.imcms.domain.dto.ImportEntityReferenceDTO;
 import com.imcode.imcms.domain.service.ImportEntityReferenceManagerService;
 import com.imcode.imcms.model.ImportEntityReferenceType;
+import com.imcode.imcms.security.AccessRoleType;
+import com.imcode.imcms.security.CheckAccess;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +18,14 @@ public class ImportEntityReferenceController {
 	private final ImportEntityReferenceManagerService importEntityReferenceManagerService;
 
 	@GetMapping
+	@CheckAccess(role = AccessRoleType.ADMIN_PAGES)
 	public List<ImportEntityReferenceDTO> getAllReferencesByType(@RequestParam String type) {
 		return importEntityReferenceManagerService.getAllReferencesByType(ImportEntityReferenceType.valueOf(type));
 	}
 
 	@PutMapping
 	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@CheckAccess(role = AccessRoleType.ADMIN_PAGES)
 	public void updateReference(@RequestBody ImportEntityReferenceDTO importEntityReference) {
 		importEntityReferenceManagerService.updateReference(importEntityReference);
 	}
