0e4f75a26d43ea4549f28e8c84512e2ced625205
eugyak <zheniayakymchuck@gmail.com>
2023-04-14 13:21:21 +0300

    Issue IMCMS-747 Make generic export from RB4 and import to RB6: - fix migration

--- src/main/resources/sql/diff/diff-7.11-7.12-mysql.sql
+++ src/main/resources/sql/diff/diff-7.11-7.12-mysql.sql
@@ -2,13 +2,13 @@ SET @schema_version__major_new = 7;
 SET @schema_version__minor_new = 12;
 
 ALTER TABLE import_template_references
-	DROP CONSTRAINT import_template_references_FK_linked_entity_id_templates;
+	DROP FOREIGN KEY import_template_references_FK_linked_entity_id_templates;
 ALTER TABLE import_category_references
-	DROP CONSTRAINT import_category_references_FK_linked_entity_id_categories;
+	DROP FOREIGN KEY import_category_references_FK_linked_entity_id_categories;
 ALTER TABLE import_category_type_references
-	DROP CONSTRAINT import_category_type_FK_linked_entity_id_category_types;
+	DROP FOREIGN KEY import_category_type_FK_linked_entity_id_category_types;
 ALTER TABLE import_role_references
-	DROP CONSTRAINT import_role_references_FK_linked_entity_id_roles;
+	DROP FOREIGN KEY import_role_references_FK_linked_entity_id_roles;
 
 
 UPDATE database_version
