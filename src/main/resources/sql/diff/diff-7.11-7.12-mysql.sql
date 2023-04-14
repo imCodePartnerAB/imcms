0220dfb74aec6a0d4a8ad4239b10c9de76f61164
romgor <romanhorkoon@gmail.com>
2023-04-14 15:32:37 +0300

    Issue IMCMS-758 RB 6: Make versioning reusable: - Adapt query for mysql (7.11-7.12) - Delete metadata automatically after deleting commonData

--- src/main/resources/sql/diff/diff-7.11-7.12-mysql.sql
+++ src/main/resources/sql/diff/diff-7.11-7.12-mysql.sql
@@ -12,13 +12,13 @@ CREATE TABLE document_waste_basket
 
 
 ALTER TABLE import_template_references
-	DROP CONSTRAINT import_template_references_FK_linked_entity_id_templates;
+	DROP FOREIGN KEY import_template_references_FK_linked_entity_id_templates;
 ALTER TABLE import_category_references
-	DROP CONSTRAINT import_category_references_FK_linked_entity_id_categories;
+	DROP FOREIGN KEY import_category_references_FK_linked_entity_id_categories;
 ALTER TABLE import_category_type_references
-	DROP CONSTRAINT import_category_type_FK_linked_entity_id_category_types;
+	DROP FOREIGN KEY import_category_type_FK_linked_entity_id_category_types;
 ALTER TABLE import_role_references
-	DROP CONSTRAINT import_role_references_FK_linked_entity_id_roles;
+	DROP FOREIGN KEY import_role_references_FK_linked_entity_id_roles;
 
 UPDATE database_version
 SET major = @schema_version__major_new,
---
+++ src/main/resources/sql/diff/diff-7.12-7.13-mysql.sql
@@ -0,0 +1,14 @@
+SET @schema_version__major_new = 7;
+SET @schema_version__minor_new = 13;
+
+ALTER TABLE imcms_doc_metadata
+    DROP FOREIGN KEY imcms_doc_metadata_FK_imcms_doc_i18n_meta;
+
+ALTER TABLE imcms_doc_metadata
+    ADD CONSTRAINT imcms_doc_metadata_FK_imcms_doc_i18n_meta
+    FOREIGN KEY (imcms_doc_i18n_meta_id) REFERENCES imcms_doc_i18n_meta (id)
+    ON DELETE CASCADE;
+
+UPDATE database_version
+SET major = @schema_version__major_new,
+    minor = @schema_version__minor_new;
