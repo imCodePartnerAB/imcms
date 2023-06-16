e1762fefd008180f87b843d142daa9b8c7778a60
eugyak <zheniayakymchuck@gmail.com>
2023-06-16 09:30:35 +0300

    Issue IMCMS-747 Make generic export from RB4 and import to RB6: - fix ImportToLocalTextDocumentTemplateResolver

--- src/main/java/com/imcode/imcms/domain/component/ImportToLocalTextDocumentTemplateResolver.java
+++ src/main/java/com/imcode/imcms/domain/component/ImportToLocalTextDocumentTemplateResolver.java
@@ -3,26 +3,27 @@ package com.imcode.imcms.domain.component;
 import com.imcode.imcms.domain.dto.ImportEntityReferenceDTO;
 import com.imcode.imcms.domain.dto.TextDocumentTemplateDTO;
 import com.imcode.imcms.domain.service.ImportEntityReferenceManagerService;
-import com.imcode.imcms.domain.service.TextDocumentTemplateService;
+import com.imcode.imcms.domain.service.TemplateService;
 import com.imcode.imcms.model.ImportEntityReferenceType;
+import com.imcode.imcms.model.Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ImportToLocalTextDocumentTemplateResolver {
-	private final TextDocumentTemplateService textDocumentTemplateService;
+	private final TemplateService templateService;
 	private final ImportEntityReferenceManagerService importEntityReferenceManagerService;
 
 	public TextDocumentTemplateDTO resolve(String templateName) {
 		final ImportEntityReferenceDTO templateReference = importEntityReferenceManagerService.getReference(templateName, ImportEntityReferenceType.TEMPLATE);
 		final Integer templateReferenceLinkedEntityId = templateReference.getLinkedEntityId();
 
-		TextDocumentTemplateDTO textDocumentTemplate = TextDocumentTemplateDTO.createDefault();
+		final TextDocumentTemplateDTO textDocumentTemplate = TextDocumentTemplateDTO.createDefault();
 		if (templateReferenceLinkedEntityId != null) {
-			textDocumentTemplate = textDocumentTemplateService.get(templateReferenceLinkedEntityId)
-					.map(TextDocumentTemplateDTO::new)
-					.orElseThrow();
+			final Template template = templateService.getById(templateReferenceLinkedEntityId);
+			textDocumentTemplate.setTemplateName(template.getName());
+			textDocumentTemplate.setChildrenTemplateName(template.getName());
 		}
 
 		return textDocumentTemplate;
