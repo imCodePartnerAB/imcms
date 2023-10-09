2087827f1b3b9d7d1385c9b48007578948856e44
eugyak <zheniayakymchuck@gmail.com>
2023-10-09 15:34:13 +0300

    Issue IMCMS-777 File admin should save the overwritten file on upload: - add file conflict resolver to admin manager file tab - add localization texts - improve ui

--- src/main/webapp/imcms/js/rest/imcms-files-rest-api.js
+++ src/main/webapp/imcms/js/rest/imcms-files-rest-api.js
@@ -18,15 +18,40 @@ define("imcms-files-rest-api", ["imcms-rest-api"], function (rest) {
 
     api.copy = pathParam => rest.ajax.call({url: `${url}/copy/`, type: 'POST', json: true}, pathParam);
 
-    api.change = content => rest.ajax.call({url: `${url}/`, type: 'PUT', json: true}, content);
+    api.copyWithRename = formData => rest.ajax.call({
+        url: `${url}/copy/rename/`,
+        type: 'POST',
+        contentType: false,
+        processData: false
+    }, formData);
+
+    api.change = formData => rest.ajax.call({
+        url: `${url}/`,
+        type: 'PUT',
+        contentType: false,
+        processData: false
+    }, formData);
 
     api.move = pathParam => rest.ajax.call({url: `${url}/move/`, type: 'PUT', json: true}, pathParam);
 
+    api.moveWithRename = formData => rest.ajax.call({
+        url: `${url}/move/rename/`,
+        type: 'PUT',
+        contentType: false,
+        processData: false
+    }, formData);
+
     api.rename = pathParam => rest.ajax.call({url: `${url}/rename/`, type: 'PUT', json: true}, pathParam);
+
+    api.defaultRename = pathParam => rest.ajax.call({url: `${url}/rename/default/`, type: 'PUT', json: true}, pathParam);
 
     api.deleteFile = sourceFile => rest.ajax.call({url: `${url}/`, type: 'DELETE', json: true}, sourceFile);
 
     api.getDocuments = template => rest.ajax.call({url: `${url}/docs`, type: 'GET', json: false}, template);
+
+    api.exists = pathParam => rest.ajax.call({url: `${url}/exists/`, type: 'GET', json: false}, pathParam);
+
+    api.existsAll = pathsParam => rest.ajax.call({url: `${url}/exists/all/`, type: 'GET', json: false}, pathsParam);
 
     return api;
 });
