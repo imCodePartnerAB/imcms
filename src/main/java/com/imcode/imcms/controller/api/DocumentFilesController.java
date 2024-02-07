3948f482f4eaad7882e0eecc557685186fdbeb06
eugyak <zheniayakymchuck@gmail.com>
2024-02-07 11:09:17 +0200

    Issue IMCMS-796 Problems with image listings on samer.se: - retrieve multipart files by original filenames

--- src/main/java/com/imcode/imcms/controller/api/DocumentFilesController.java
+++ src/main/java/com/imcode/imcms/controller/api/DocumentFilesController.java
@@ -5,16 +5,14 @@ import com.imcode.imcms.mapping.DocumentMapper;
 import com.imcode.imcms.model.DocumentFile;
 import com.imcode.imcms.security.AccessContentType;
 import com.imcode.imcms.security.CheckAccess;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -42,7 +40,7 @@ public class DocumentFilesController {
 
         final Map<String, Queue<MultipartFile>> fileNameToFiles = files.stream()
                 .collect(Collectors.toMap(
-                        MultipartFile::getOriginalFilename,
+                        multipartFile -> FilenameUtils.getName(multipartFile.getOriginalFilename()),
                         (MultipartFile file) -> {
                             final Queue<MultipartFile> fileList = new ArrayDeque<>();
                             fileList.add(file);
@@ -51,14 +49,13 @@ public class DocumentFilesController {
                         (files1, files2) -> {
                             files1.addAll(files2);
                             return files1;
-                        }
+                        }, LinkedHashMap::new
                 ));
 
         final List<DocumentFile> saveUs = documentFileService.getByDocId(docId)
                 .stream()
                 .peek(documentFileDTO -> {
-                    final String filename = documentFileDTO.getFilename();
-                    final Queue<MultipartFile> multipartFiles = fileNameToFiles.get(filename);
+                    final Queue<MultipartFile> multipartFiles = fileNameToFiles.get(documentFileDTO.getOriginalFilename());
 
                     if (multipartFiles != null) {
                         documentFileDTO.setMultipartFile(multipartFiles.poll());
